d8be7a56b7b79ff08d983d18d8f6131d4bb13c8d
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-02-22 15:52:07 +0000

    Added prolog file for simple testing
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@13365 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ challenge_cocktail_party/prolog/init.pl
@@ -0,0 +1,8 @@
+% 2013 by Sjoerd van den Dries
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dependencies
+
+:- register_ros_package(wire_jpl).
+
+:- use_module(library('tester')).
---
+++ challenge_cocktail_party/prolog/tester.pl
@@ -0,0 +1,22 @@
+:- module(tester, [open_door/0,
+                   close_door/0,
+                   reset_wire/0,
+                   add_person/0,
+                   say/1]).
+
+open_door :-
+    query((retractall(object_state(entrance_door, _)),
+           assert(object_state(entrance_door, open)))).
+
+close_door :-
+    query((retractall(object_state(entrance_door, _)),
+           assert(object_state(entrance_door, close)))).
+
+add_person :-
+	wire:add_object_evidence(john, person, 1, 1, 1).
+
+say(X) :-
+	query(assert(heard_words(X))).
+
+reset_wire :-
+	write('Currently not implemented.'), nl.
