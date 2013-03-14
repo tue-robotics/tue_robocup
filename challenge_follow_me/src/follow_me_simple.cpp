1feaf1377c7ab67e5ae8d3708ebfe9f5e32566c1
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-03-14 16:13:39 +0000

    Can find person and output (updated) position
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@13789 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_follow_me/src/follow_me_simple.cpp
+++ challenge_follow_me/src/follow_me_simple.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+string TRACKING_FRAME = "/map";
+
+double FIND_RATE = 1;
+double FOLLOW_RATE = 1;
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "follow_me_simple");
     ros::NodeHandle nh;
@@ -15,10 +20,11 @@ int main(int argc, char **argv) {
 
     // wait until person is found
     ROS_INFO("Waiting until person is found ...");
-    ros::Rate r(1);
+
+    ros::Rate find_rate(FIND_RATE);
     while(ros::ok() && person_id == "") {
-        vector<wire::PropertySet> object_states = client.queryMAPObjects();
-        for(vector<wire::PropertySet>::iterator it_obj = object_states.begin(); it_obj != object_states.end() && person_id == ""; ++it_obj) {
+        vector<wire::PropertySet> objects = client.queryMAPObjects(TRACKING_FRAME);
+        for(vector<wire::PropertySet>::iterator it_obj = objects.begin(); it_obj != objects.end() && person_id == ""; ++it_obj) {
             wire::PropertySet& obj = *it_obj;
             const wire::Property& prop_class = obj.getProperty("class_label");
             if (prop_class.isValid()) {
@@ -33,10 +39,38 @@ int main(int argc, char **argv) {
                 }
             }
         }
-        r.sleep();
+        find_rate.sleep();
     }
 
-    ROS_INFO("Found person with ID %s and position %s", person_id.c_str(), person_pos.toString().c_str());
+    ROS_INFO("Found person with ID %s and position %s (frame: %s)", person_id.c_str(), person_pos.toString().c_str(), TRACKING_FRAME.c_str());
+
+    // follow person
+    ROS_INFO("Following person with ID %s", person_id.c_str());
+
+    ros::Rate follow_rate(FOLLOW_RATE);
+    while(ros::ok()) {
+        vector<wire::PropertySet> objects = client.queryMAPObjects(TRACKING_FRAME);
+        for(vector<wire::PropertySet>::iterator it_obj = objects.begin(); it_obj != objects.end(); ++it_obj) {
+            wire::PropertySet& obj = *it_obj;
+            if (obj.getID() == person_id) {
+                const wire::Property& prop_pos = obj.getProperty("position");
+                if (prop_pos.isValid()) {
+                    person_pos = prop_pos.getValue();
+                }
+            }
+        }
+
+        // get expected position
+        if (person_pos.getExpectedValue().isValid()) {
+            pbl::Vector pos_exp = person_pos.getExpectedValue().getVector();
+
+            cout << pos_exp << endl;
+        } else {
+            ROS_WARN("Could not determine most likely position from PDF: %s", person_pos.toString().c_str());
+        }
+
+        follow_rate.sleep();
+    }
 
     return 0;
 }
