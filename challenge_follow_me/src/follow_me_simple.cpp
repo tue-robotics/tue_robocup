3dec2f97c85b51f60f244a0e1ae6300905ee55b7
Jos Elfring <j.elfring@tue.nl>
2013-04-03 14:26:45 +0000

    In progress, a simple executive for the follow me challenge
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@14219 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_follow_me/src/follow_me_simple.cpp
+++ challenge_follow_me/src/follow_me_simple.cpp
@@ -8,106 +8,162 @@
 
 using namespace std;
 
-string TRACKING_FRAME = "/base_link";
+//! World model frame
+const string TRACKING_FRAME = "/base_link";
 
+//! Rate at which the move base goal is updated
+const double FOLLOW_RATE = 1;
+
+//! Rate check for operator at start of the challenge
 double FIND_RATE = 1;
-double FOLLOW_RATE = 1;
+
+//! Move base action client
+actionlib::SimpleActionClient<tue_move_base_msgs::MoveBaseAction>* move_base_;
+
+
+void findOperator() {
+
+}
+
+void amigoSpeak(string sentence) {
+    ROS_INFO("AMIGO: \'%s\'", sentence.c_str());
+}
+
+
+//! Get position and ID of the operator from the estimated world state
+bool getPositionOperator(vector<wire::PropertySet>& objects, string& ID, pbl::PDF& pos) {
+
+    for(vector<wire::PropertySet>::iterator it_obj = objects.begin(); it_obj != objects.end() && ID == ""; ++it_obj) {
+        wire::PropertySet& obj = *it_obj;
+        const wire::Property& prop_name = obj.getProperty("name");
+        if (prop_name.isValid()) {
+            if (prop_name.getValue().getExpectedValue().toString() == "operator") {
+                const wire::Property& prop_pos = obj.getProperty("position");
+                if (prop_pos.isValid()) {
+                    ID = obj.getID();
+                    pos = prop_pos.getValue();
+                    return true;
+                } else {
+                    ROS_WARN("Found a operator witour position");
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+
+void memorizeOperator() {
+
+    // TODO: no need to move arms? maybe instruction suffices
+
+}
+
+
+void moveTowardsPosition(pbl::PDF& pos) {
+
+    pbl::Vector pos_exp = pos.getExpectedValue().getVector();
+
+    tue_move_base_msgs::MoveBaseGoal move_base_goal;
+
+    geometry_msgs::PoseStamped start;
+    start.header.frame_id = TRACKING_FRAME;
+    start.pose.position.x = 0;
+    start.pose.position.y = 0;
+    start.pose.position.z = 0;
+
+    // set orientation
+    // TODO: set correctly
+    start.pose.orientation.x = 0;
+    start.pose.orientation.y = 0;
+    start.pose.orientation.z = 0;
+    start.pose.orientation.w = 1;
+
+    // First turn towards operator, then drive?
+    move_base_goal.path.push_back(start);
+
+    geometry_msgs::PoseStamped end_goal;
+    end_goal.header.frame_id = TRACKING_FRAME;
+    end_goal.pose.position.x = 0.75 * pos_exp(0);
+    end_goal.pose.position.y = 0.75 * pos_exp(1);
+    end_goal.pose.position.z = 0.75 * pos_exp(2);
+
+    // set orientation
+    // TODO: set correctly
+    end_goal.pose.orientation.x = 0;
+    end_goal.pose.orientation.y = 0;
+    end_goal.pose.orientation.z = 0;
+    end_goal.pose.orientation.w = 1;
+
+    move_base_goal.path.push_back(end_goal);
+
+    //! Send goal to move base client
+    move_base_->sendGoal(move_base_goal);
+
+}
+
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "follow_me_simple");
     ros::NodeHandle nh;
 
+    ROS_INFO("Started \'Follow me\'");
+
+    //! Query WIRE for objects
     wire::Client client;
 
-    actionlib::SimpleActionClient<tue_move_base_msgs::MoveBaseAction> move_base("move_base", true);
-    move_base.waitForServer();
+    //! Action client for sending move base goals
+    move_base_ = new actionlib::SimpleActionClient<tue_move_base_msgs::MoveBaseAction>("move_base",true);
+    move_base_->waitForServer();
 
+    ROS_INFO("Move base client connected to the move base server");
+
+    //! Create a model for the operator
+    memorizeOperator();
+
+    //! Administration
     string person_id;
     pbl::PDF person_pos;
 
-    // wait until person is found
-    ROS_INFO("Waiting until person is found ...");
-
+    //! Operator must be in the world model, remember position and ID
     ros::Rate find_rate(FIND_RATE);
     while(ros::ok() && person_id == "") {
+
+        //! Get objects in estimated world state
         vector<wire::PropertySet> objects = client.queryMAPObjects(TRACKING_FRAME);
-        for(vector<wire::PropertySet>::iterator it_obj = objects.begin(); it_obj != objects.end() && person_id == ""; ++it_obj) {
-            wire::PropertySet& obj = *it_obj;
-            const wire::Property& prop_class = obj.getProperty("class_label");
-            if (prop_class.isValid()) {
-                if (prop_class.getValue().getExpectedValue().toString() == "person") {
-                    const wire::Property& prop_pos = obj.getProperty("position");
-                    if (prop_pos.isValid()) {
-                        person_id = obj.getID();
-                        person_pos = prop_pos.getValue();
-                    } else {
-                        ROS_WARN("Found a person WITHOUT position (ID: %s)", obj.getID().c_str());
-                    }
-                }
-            }
+
+        //! Start challenge once the operator is found
+        if (getPositionOperator(objects, person_id, person_pos)) {
+            break;
         }
+
         find_rate.sleep();
     }
 
-    ROS_INFO("Found person with ID %s and position %s (frame: %s)", person_id.c_str(), person_pos.toString().c_str(), TRACKING_FRAME.c_str());
+    ROS_INFO("Found operator with ID %s and position %s (frame: %s)", person_id.c_str(), person_pos.toString().c_str(), TRACKING_FRAME.c_str());
+    amigoSpeak("I am ready to start following you");
 
-    // follow person
-    ROS_INFO("Following person with ID %s", person_id.c_str());
-
+    //! Follow operator
     ros::Rate follow_rate(FOLLOW_RATE);
     while(ros::ok()) {
+
+        //! Get objects from the world state
         vector<wire::PropertySet> objects = client.queryMAPObjects(TRACKING_FRAME);
-        for(vector<wire::PropertySet>::iterator it_obj = objects.begin(); it_obj != objects.end(); ++it_obj) {
-            wire::PropertySet& obj = *it_obj;
-            if (obj.getID() == person_id) {
-                const wire::Property& prop_pos = obj.getProperty("position");
-                if (prop_pos.isValid()) {
-                    person_pos = prop_pos.getValue();
-                }
-            }
-        }
 
-        // get expected position
-        if (person_pos.getExpectedValue().isValid()) {
-            pbl::Vector pos_exp = person_pos.getExpectedValue().getVector();
+        // TODO: check for ITP 2 (= elevator): sound input
 
-            tue_move_base_msgs::MoveBaseGoal move_base_goal;
+        //! Check for the (updated) operator position
+        if (getPositionOperator(objects, person_id, person_pos)) {
 
-            geometry_msgs::PoseStamped start;
-            start.header.frame_id = TRACKING_FRAME;
-            start.pose.position.x = 0;
-            start.pose.position.y = 0;
-            start.pose.position.z = 0;
+            //! Move towards operator
+            moveTowardsPosition(person_pos);
 
-            // set orientation
-            // TODO: set correctly
-            start.pose.orientation.x = 0;
-            start.pose.orientation.y = 0;
-            start.pose.orientation.z = 0;
-            start.pose.orientation.w = 1;
-
-            move_base_goal.path.push_back(start);
-
-            geometry_msgs::PoseStamped end_goal;
-            end_goal.header.frame_id = TRACKING_FRAME;
-            end_goal.pose.position.x = 0.75 * pos_exp(0);
-            end_goal.pose.position.y = 0.75 * pos_exp(1);
-            end_goal.pose.position.z = 0.75 * pos_exp(2);
-
-            // set orientation
-            // TODO: set correctly
-            end_goal.pose.orientation.x = 0;
-            end_goal.pose.orientation.y = 0;
-            end_goal.pose.orientation.z = 0;
-            end_goal.pose.orientation.w = 1;
-
-            move_base_goal.path.push_back(end_goal);
-
-            // send to move_base
-            move_base.sendGoal(move_base_goal);
 
         } else {
-            ROS_WARN("Could not determine most likely position from PDF: %s", person_pos.toString().c_str());
+
+            //! Lost operator
+            findOperator();
         }
 
         follow_rate.sleep();
