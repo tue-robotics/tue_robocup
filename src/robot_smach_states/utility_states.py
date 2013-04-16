c9de111140ddabc70483025555b826660b25a886
Erik Geerts <e.g.geerts@live.nl>
2013-04-16 19:15:07 +0000

    set lights on blue at initialization
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_smach_states@14766 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/robot_smach_states/utility_states.py
+++ src/robot_smach_states/utility_states.py
@@ -22,6 +22,8 @@ class Initialize(smach.State):
         self.robot = robot
 
     def execute(self, userdata):
+        self.robot.lights.set_color(0,0,1)  #be sure lights are blue
+        
         self.robot.head.reset_position()
         self.robot.leftArm.reset_arm()
         #self.robot.leftArm.send_gripper_goal_close()
