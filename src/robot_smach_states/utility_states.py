304fa7948c655193a5c0715665266a089eb69311
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2013-04-26 15:03:58 +0000

    Added reset costmap to set_initial_pose
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_smach_states@15313 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/robot_smach_states/utility_states.py
+++ src/robot_smach_states/utility_states.py
@@ -304,6 +304,11 @@ class Set_initial_pose(smach.State):
             return "error"
 
         self.robot.base.set_initial_pose(x,y,phi)
+        
+        # Reset costmap: costmap is obviously entirely off if the localization was wrong before giving the initial pose
+        self.robot.base.reset_costmap()
+        # Wait 0.5 s just to be sure
+        rospy.sleep(rospy.Duration(0.5))
 
         return "done"
 
