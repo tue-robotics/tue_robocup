d73e82b7a447c4c7b0b5c94ecf9aee748762952f
Loy van Beek <loy.vanbeek@gmail.com>
2013-04-28 08:54:21 +0000

    Added a @deprecated_replace_with-decorator
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_smach_states@15439 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/robot_smach_states/util/__init__.py
+++ src/robot_smach_states/util/__init__.py
@@ -23,3 +23,23 @@ def deprecated(func_or_cls):
                 print func_or_cls.__name__, 'is deprecated'
         return func_or_cls(*args, **kwargs)
     return wrapper
+
+def deprecated_replace_with(replacement):
+    #import ipdb; ipdb.set_trace()
+    def deprecated(func_or_cls):
+        """Print a deprecation warning once on first use of the function.
+
+        >>> @deprecated                      # doctest: +SKIP
+        ... def f():
+        ...     pass
+        >>> f()                              # doctest: +SKIP
+        f is deprecated
+        """
+        count = [0]
+        def wrapper(*args, **kwargs):
+            RED = '\033[91m'
+            END = '\033[0m'
+            rospy.logerr(RED+"Function or class {0} is deprecated. You should use {1} instead".format(func_or_cls.__name__, replacement)+END)
+            return func_or_cls(*args, **kwargs)
+        return wrapper
+    return deprecated
