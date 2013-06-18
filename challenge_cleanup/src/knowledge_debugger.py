49433545e6c246351938c4cb75b4dab92902b365
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-06-18 19:40:42 +0000

    Updated queries
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@16827 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_cleanup/src/knowledge_debugger.py
+++ challenge_cleanup/src/knowledge_debugger.py
@@ -7,6 +7,8 @@ import smach
 from robot_skills.amigo import Amigo
 import robot_smach_states as states
 
+import pprint
+
 from robot_skills.reasoner  import Conjunction, Compound, Reasoner
 
 query_meeting_point = Compound("waypoint", 
@@ -15,12 +17,12 @@ query_meeting_point = Compound("waypoint",
 query_exploration_target_in_room = Conjunction( Compound("goal", Compound("clean_up", "Room")),
                                                 Compound("exploration_target", "Room", "Target"),
                                                 Compound("not", Compound("explored", "Target")),
-                                                Compound("base_pose", "Target", Compound("pose_2d", "X", "Y", "Phi"))
+                                                Compound("waypoint", "Target", Compound("pose_2d", "X", "Y", "Phi"))
                                                )
 query_room = Conjunction(   Compound("goal", Compound("clean_up", "Room")), 
                             Compound("waypoint", "Room", Compound("pose_2d", "X", "Y", "Phi"))) 
 query_exploration_target = Conjunction( Compound("current_exploration_target", "Target"),
-                                        Compound("base_pose", "Target", Compound("pose_2d", "X", "Y", "Phi")))
+                                        Compound("waypoint", "Target", Compound("pose_2d", "X", "Y", "Phi")))
 query_lookat = Conjunction( Compound("current_exploration_target", "Target"),
                             Compound("point_of_interest", "Target", Compound("point_3d", "X", "Y", "Z")))
 #Make sure the object we're dealing with isn't already disposed (i.e. handled for cleanup)
@@ -37,16 +39,16 @@ query_current_object_class = Conjunction(
 query_dropoff_loc = Conjunction(
                         Compound("current_object",      "Obj_to_Dispose"), #Of the current object
                         Compound("instance_of",         "Obj_to_Dispose",   Compound("exact", "ObjectType")), #Gets its type
-                        Compound("storage_class",       "ObjectType",       "Disposal_type"), #Find AT what sort of thing it should be disposed, e.g. a trashbin
+                        Compound("storage_class",       "ObjectType",       "Disposal_type"), #Find AT what sort of thing it should be disposed, e.g. a trashbi
                         Compound("instance_of",         "Dispose_to_object","Disposal_type"), #Find objects of that are of type trashbin
                         Compound("point_of_interest",   "Dispose_to_object", Compound("point_3d", "X", "Y", "Z"))) #Get locations of those things
 query_dropoff_loc_backup = Conjunction( Compound("instance_of", "Dispose_to_object",  "trashbin"), #Find objects of that are of type trashbin
-                                            Compound("point_of_interest",  "Dispose_to_object",  Compound("point_3d", "X", "Y", "Z"))) #Get locations of those things
+                                            Compound("point_of_interest",  "Dispose_to_object",  Compound("point_3d", "X", "Y", "Z"))) #Get locations of those 
 meeting_point = Conjunction(    Compound("waypoint", Compound("meeting_point", "Waypoint"), Compound("pose_2d", "X", "Y", "Phi")),
                                 Compound("not", Compound("unreachable", Compound("meeting_point", "Waypoint"))))
 
 if __name__ == "__main__":
-    rospy.init_node('knowledge_debugger', log_level=rospy.DEBUG)
+    rospy.init_node('knowledge_debugger')
 
     r = Reasoner()
 
@@ -70,7 +72,7 @@ if __name__ == "__main__":
                 new_mask[name] = answer
                 rospy.logdebug("querying {0}:{1} = {2}".format(name, term, answer))
                 if new_mask[name] != old_mask[name]:
-                    rospy.loginfo("query {0} changed. Was {1}, now is {2}".format(name, old_mask[name], new_mask[name]))
+                    rospy.loginfo("query {0} changed. Was {1}, now is {2}".format(name, pprint.pformat(old_mask[name]), pprint.pformat(new_mask[name])))
             old_mask = new_mask
 
             rospy.sleep(1)
