d0a3c3ad8ac9f15be880930535a2d06e6d27fe79
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-06-20 09:13:47 +0000

    Added cleanup scenario for fast_simulator
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@16903 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_cleanup/test/testenv_robotics_testlab_A.py
+++ challenge_cleanup/test/testenv_robotics_testlab_A.py
@@ -5,8 +5,14 @@ import rospy
 from fast_simulator import client
 
 if __name__ == "__main__":
-    rospy.init_node('open_challenge_rgo2013_tester')
+    rospy.init_node('fast_simulator_challenge_tester')
 
     W = client.SimWorld()
 
-    table = W.add_object("coke-1", "coke", 4.965, -1.49, 0.5)    
+    coke1 = W.add_object("coke-1", "coke", 3.738, -2.134, 0.8)    
+    coke2 = W.add_object("coke-2", "coke", 4.705, -1.605, 0.76)    
+    coke3 = W.add_object("coke-3", "coke", 4.964, 2.021, 0.87)    
+    coke4 = W.add_object("coke-4", "coke", 0.994, 0.985, 0.53)    
+    coke5 = W.add_object("coke-5", "coke", 0.724, 2.118, 0.63)    
+    coke6 = W.add_object("coke-6", "coke", 0.293, -1.117, 0.84)    
+    coke7 = W.add_object("coke-7", "coke", 2.049, -1.064, 0.84)    
