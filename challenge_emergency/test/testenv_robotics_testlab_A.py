3c6a8504ab3f4b56c8cf40adaff8dfafda84cd6b
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-06-23 14:22:07 +0000

    Added test environment
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@16993 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ challenge_emergency/test/testenv_robotics_testlab_A.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('fast_simulator')
+import rospy
+
+from fast_simulator import client
+
+if __name__ == "__main__":
+    rospy.init_node('challenge_tester')
+
+    W = client.SimWorld()
+
+    person1 = W.add_object("person-1", "person", 3.821, -1.379, 0)    
+    person2 = W.add_object("person-2", "person", 5.872, 1.256, 0)    
+    person3 = W.add_object("person-3", "person", 1.611, 1.908, 0)    
+    person4 = W.add_object("person-4", "person", 0.455, 1.501, 0)    
+    person5 = W.add_object("person-5", "person", 0.820, -1.741, 0)
