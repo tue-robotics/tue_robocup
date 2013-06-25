70849acbff7e52f7d06eafea7e6b0db417d47493
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-06-25 18:57:06 +0000

    Updated clean up fast simulator scenario
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@17167 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_cleanup/test/testenv_rwc2013.py
+++ challenge_cleanup/test/testenv_rwc2013.py
@@ -10,28 +10,28 @@ if __name__ == "__main__":
     W = client.SimWorld()
 
     # pantry
-    obj1 = W.add_object("cif-1", "cif", 4.529, -4.078, 0.85)
+    obj1 = W.add_object("drops-1", "drops", 4.529, -4.078, 0.85)
 
     # couch table
-    coke1 = W.add_object("coke-1", "coke", 8.664, -1.166, 0.5)    
+    coke1 = W.add_object("marmalade-2", "marmalade", 8.664, -1.166, 0.5)    
 
     # large table
-    coke2 = W.add_object("cif-2", "cif", 5.703, -0.732, 0.8)    
-    coke3 = W.add_object("tea_pack-3", "tea_pack", 5.605, -2.709, 0.85)  
+    coke2 = W.add_object("tomato-soup-3", "tomato_soup", 5.703, -0.732, 0.8)    
+    coke3 = W.add_object("cleaner-4", "cleaner", 5.605, -2.709, 0.85)  
 
     # bed_cabinet_1
-    coke4 = W.add_object("coke-4", "coke", 10.229, -5.404, 0.5)    
+    coke4 = W.add_object("tomato-soup-5", "tomato_soup", 10.229, -5.404, 0.5)    
 
     # bed_cabinet_2
-    coke5 = W.add_object("rubiks-5", "rubiks", 10.219, -7.630, 0.5)    
+    coke5 = W.add_object("cleaner-6", "cleaner", 10.219, -7.630, 0.5)    
 
     # kitchen table
-    coke6 = W.add_object("coke-6", "coke", 3.914, -6.331, 0.8)    
-    coke7 = W.add_object("coke-7", "coke", 2.293, -6.324, 0.8)    
+    coke6 = W.add_object("marmalade-7", "marmalade", 3.914, -6.331, 0.8)    
+    coke7 = W.add_object("coke-8", "coke", 2.293, -6.324, 0.8)    
 
     # bedroom cabinet
-    coke4 = W.add_object("coke-8", "coke", 6.155, -6.470, 0.5)    
-    coke5 = W.add_object("coke-9", "coke", 6.039, -7.215, 0.5)    
+    coke4 = W.add_object("drops-9", "drops", 6.155, -6.470, 0.5)    
+    coke5 = W.add_object("marmalade-10", "marmalade", 6.039, -7.215, 0.5)    
 
     W.wait_for_amigo_speech(["What can I do for you?"])
     W.speak("cleanupthekitchen")
