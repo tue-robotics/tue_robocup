bc188d6ad231aa0c30465241f736de5da41e654c
Erik Geerts <e.g.geerts@live.nl>
2013-06-27 11:12:11 +0000

    added test file for fast simulator
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@17301 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ challenge_egpsr/test/testenv_rwc2013_egpsr.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('fast_simulator')
+import rospy
+
+import sys
+
+from fast_simulator import client
+
+if __name__ == "__main__":
+    rospy.init_node('fast_simulator_challenge_tester')
+
+    W = client.SimWorld()
+
+    # pantry
+    obj1 = W.add_object("drops-1", "drops", 4.529, -4.078, 0.85)
+
+    # couch table
+    coke1 = W.add_object("marmalade-2", "marmalade", 8.664, -1.166, 0.5)    
+
+    # large table
+    coke2 = W.add_object("tomato-sauce-3", "tomato_sauce", 5.703, -0.732, 0.8)    
+    coke3 = W.add_object("cleaner-4", "cleaner", 5.605, -2.709, 0.85)  
+
+    # bed_cabinet_1
+    coke4 = W.add_object("tomato-sauce-5", "tomato_sauce", 10.229, -5.404, 0.5)    
+
+    # bed_cabinet_2
+    coke5 = W.add_object("cleaner-6", "cleaner", 10.219, -7.630, 0.5)    
+
+    # kitchen table
+    #coke6 = W.add_object("marmalade-7", "marmalade", 3.914, -6.331, 0.8)    
+    coke7 = W.add_object("coke-8", "coke", 2.293, -6.324, 0.8)    
+
+    # bedroom cabinet
+    coke8 = W.add_object("drops-9", "drops", 6.155, -6.470, 0.5)    
+    coke9 = W.add_object("marmalade-10", "marmalade", 6.039, -7.215, 0.5)    
+
+    coke10 = W.add_object("tomato_sauce-11", "tomato_sauce", 9.024, -3.979, 0.5)    
+    coke11 = W.add_object("cleaner-12", "cleaner", 8.309, -4.172, 0.5)    
+
+
+
+    # W.wait_for_amigo_speech(["What can I do for you?"])
+
+    # try:
+    #     room = sys.argv[1]
+    # except IndexError:
+    #     room = "livingroom"
+
+    # W.speak("cleanupthe{0}".format(room))
+    # #W.wait_for_amigo_speech(["Is that corect?","Is that OK?", "Is that okay?", "Am I right?", "Is that alright?"])
+    # W.wait_for_amigo_speech([lambda txt: "?" in txt])
+    # rospy.sleep(3.0)
+    # W.speak("yes")
+
+    # W.wait_for_amigo_speech([lambda txt: "done" in txt]) 
+    # ["I cleaned up everything I could find, so my work here is done. Have a nice day!", "I'm done, everything I could find is cleaned up."]
+
+    # rospy.loginfo("Done")
