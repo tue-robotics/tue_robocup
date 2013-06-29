949a9c3b8bc652bf2086e9087f4b2b786194636a
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-06-29 16:35:20 +0000

    Added table test
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@17411 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ challenge_final/test/table.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('fast_simulator')
+import rospy
+
+import sys
+
+from fast_simulator import client
+
+if __name__ == "__main__":
+    rospy.init_node('fast_simulator_challenge_tester')
+
+   
+
+    W = client.SimWorld()
+
+    freq = 20
+
+    table_x = 5.7805
+    table_y = -1.4685
+    table_vy = 0.03
+    table_angle = 0
+    table_angle_v = 0.03   
+
+    table = W.add_object("dinner_table", "dinner_table", table_x, table_y, 0)        
+
+    while not rospy.is_shutdown():
+            
+        while table_angle < 0.5:
+            table.set_position(table_x, table_y, 0, rz = table_angle)
+            rospy.sleep(1 / freq)
+            table_angle += float(table_angle_v) / freq
+            table_y += float(table_vy) / freq
+
+        while table_angle > -0.2:
+            table.set_position(table_x, table_y, 0, rz = table_angle)
+            rospy.sleep(1 / freq)
+            table_angle -= float(table_angle_v) / freq
+            table_y -= float(table_vy) / freq
+
