4d43a5ebe3d5b28ce99d00021cb96853d4627a6c
Bas Coenen <s.a.m.coenen@student.tue.nl>
2013-08-08 13:57:27 +0000

    running simple nav in rwc2013 world, so updated coordinates
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_smach_states@17921 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- examples/simple_navigate.py
+++ examples/simple_navigate.py
@@ -14,17 +14,14 @@ if __name__ == "__main__":
 
     rospy.sleep(2) # wait for tf cache to be filled
 
-    nav_state = navigation.NavigateGeneric(robot, goal_pose_2d=(1, 7, 0), look_at_path_distance=2.7)
+    nav_state = navigation.NavigateGeneric(robot, goal_pose_2d=(6, -1, 0), look_at_path_distance=2.7)
     nav_state.execute()
 
-    nav_state1 = navigation.NavigateGeneric(robot, goal_pose_2d=(7, 7, 0), look_at_path_distance=2.7)
+    nav_state1 = navigation.NavigateGeneric(robot, goal_pose_2d=(7, -7, 0), look_at_path_distance=2.7)
     nav_state1.execute()
     
-    nav_state2 = navigation.NavigateGeneric(robot, goal_pose_2d=(7, 1, -1.57), look_at_path_distance=2.7)
+    nav_state2 = navigation.NavigateGeneric(robot, goal_pose_2d=(1.5, -7.5, -1.57), look_at_path_distance=2.7)
     nav_state2.execute()
     
-    nav_state3 = navigation.NavigateGeneric(robot, goal_pose_2d=(1, 1, 3.14), look_at_path_distance=2.7)
+    nav_state3 = navigation.NavigateGeneric(robot, goal_pose_2d=(2, 0, 3.14), look_at_path_distance=2.7)
     nav_state3.execute()
-    
-    # nav_state = navigation.NavigateGeneric(robot, lookat_point_3d=(3,0.5,0), look_at_path_distance=2.7)
-    # nav_state.execute()
