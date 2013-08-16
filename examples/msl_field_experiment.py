40b43e5162f52381a8181fe9247a0be8d3dd8563
Bas Coenen <s.a.m.coenen@student.tue.nl>
2013-08-16 09:05:56 +0000

    locations for test run
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_smach_states@18113 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ examples/msl_field_experiment.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('robot_smach_states')
+import rospy
+
+from robot_skills.amigo import Amigo
+
+import robot_skills
+from robot_smach_states import navigation
+
+if __name__ == "__main__":
+    rospy.init_node('simple_navigate')
+
+    robot = Amigo()
+
+    rospy.sleep(2) # wait for tf cache to be filled
+    
+    nav_state_begin_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(4, -4.4, 1.57), look_at_path_distance=2.7)
+    nav_state_begin_corridor.execute()
+    
+    nav_state_door = navigation.NavigateGeneric(robot, goal_pose_2d=(3, 5, -3.14), look_at_path_distance=2.7)
+    nav_state_door.execute()    
+    
+    nav_state_end_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(-3, 5, -1.57), look_at_path_distance=2.7)
+    nav_state__end_corridor.execute()
+    
+    nav_state_table = navigation.NavigateGeneric(robot, goal_pose_2d=(2.2, 1.7, 1.57), look_at_path_distance=2.7)
+    nav_state_table.execute() 
+        
+    nav_state_begin_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(4, -4.4, 1.57), look_at_path_distance=2.7)
+    nav_state_begin_corridor.execute()
