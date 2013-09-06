9f58879894a49265c623846b7d78c0c1d27a2b59
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-09-06 09:36:11 +0000

    Test now also checks if head position is correct
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_skills@18436 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- test/test_full.py
+++ test/test_full.py
@@ -43,59 +43,59 @@ if __name__ == "__main__":
     max_err = 0.02
 
     p = geometry_msgs.msg.PointStamped()
-    p.header.frame_id = '/amigo/base_link'
+    p.header.frame_id = '/amigo/torso'
 
     # straight
-    p.point.x = 10
+    p.point.x = 0.4
     p.point.y = 0
-    p.point.z = 1.5
+    p.point.z = 10
     amigo.head.send_goal(p, timeout=10.0, keep_tracking=False, min_pan=0, max_pan=0, min_tilt=0, max_tilt=0)
-   
+    # print str(joint_positions['neck_pan_joint']) + " " + str(joint_positions['neck_tilt_joint'])   
     show_test("head straight",      in_bounds(joint_positions['neck_pan_joint'], 0, max_err)
                                 and in_bounds(joint_positions['neck_tilt_joint'], 0, max_err))
 
     # up
-    p.point.x = 5
+    p.point.x = 0.5
     p.point.y = 0
-    p.point.z = 3
+    p.point.z = 1
     amigo.head.send_goal(p, timeout=10.0, keep_tracking=False, min_pan=0, max_pan=0, min_tilt=0, max_tilt=0)
-
+    # print str(joint_positions['neck_pan_joint']) + " " + str(joint_positions['neck_tilt_joint'])
     show_test("head up",      in_bounds(joint_positions['neck_pan_joint'], 0, max_err)
-                          and in_bounds(joint_positions['neck_tilt_joint'], -0.303218606856, max_err))
+                          and in_bounds(joint_positions['neck_tilt_joint'], -0.26, max_err))
 
     # down
-    p.point.x = 1
+    p.point.x = -0.4
     p.point.y = 0
-    p.point.z = 0
+    p.point.z = 1
     amigo.head.send_goal(p, timeout=10.0, keep_tracking=False, min_pan=0, max_pan=0, min_tilt=0, max_tilt=0)
-
+    # print str(joint_positions['neck_pan_joint']) + " " + str(joint_positions['neck_tilt_joint'])
     show_test("head down",      in_bounds(joint_positions['neck_pan_joint'], 0, max_err)
-                            and in_bounds(joint_positions['neck_tilt_joint'], 0.928546372105, max_err))
+                            and in_bounds(joint_positions['neck_tilt_joint'], 0.54, max_err))
 
     # right
-    p.point.x = 1
-    p.point.y = -10
-    p.point.z = 1.5
+    p.point.x = 0.4
+    p.point.y = 100
+    p.point.z = 1
     amigo.head.send_goal(p, timeout=10.0, keep_tracking=False, min_pan=0, max_pan=0, min_tilt=0, max_tilt=0)
-
-    show_test("head right",      in_bounds(joint_positions['neck_pan_joint'], -1.456987231, max_err)
+    # print str(joint_positions['neck_pan_joint']) + " " + str(joint_positions['neck_tilt_joint'])
+    show_test("head right",      in_bounds(joint_positions['neck_pan_joint'], -1.547, max_err)
                             and in_bounds(joint_positions['neck_tilt_joint'], 0, max_err))
 
     # left
-    p.point.x = 1
-    p.point.y = 10
-    p.point.z = 1.5
+    p.point.x = 0.4
+    p.point.y = -100
+    p.point.z = 1
     amigo.head.send_goal(p, timeout=10.0, keep_tracking=False, min_pan=0, max_pan=0, min_tilt=0, max_tilt=0)
-
-    show_test("head left",      in_bounds(joint_positions['neck_pan_joint'], 1.463121363, max_err)
+    # print str(joint_positions['neck_pan_joint']) + " " + str(joint_positions['neck_tilt_joint'])
+    show_test("head left",      in_bounds(joint_positions['neck_pan_joint'], 1.547, max_err)
                              and in_bounds(joint_positions['neck_tilt_joint'], 0, max_err))
 
     # straight
-    p.point.x = 10
+    p.point.x = 0.4
     p.point.y = 0
-    p.point.z = 1.5
+    p.point.z = 10
     amigo.head.send_goal(p, timeout=10.0, keep_tracking=False, min_pan=0, max_pan=0, min_tilt=0, max_tilt=0)
-
+    # print str(joint_positions['neck_pan_joint']) + " " + str(joint_positions['neck_tilt_joint'])
     show_test("head straight",      in_bounds(joint_positions['neck_pan_joint'], 0, max_err)
                                 and in_bounds(joint_positions['neck_tilt_joint'], 0, max_err))
 
