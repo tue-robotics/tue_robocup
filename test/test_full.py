b3514cbe0c971c3497a29a08407098037cf63909
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-09-07 22:37:21 +0000

    Added gripper test
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_skills@18451 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- test/test_full.py
+++ test/test_full.py
@@ -7,13 +7,23 @@ import robot_skills.amigo
 
 import geometry_msgs
 import sensor_msgs
+import amigo_msgs
 
 global joint_positions
+global left_gripper_meas, right_gripper_meas
 
 def joint_state_callback(msg):
     global joint_positions
     for i in range(len(msg.name)):
         joint_positions[msg.name[i]] = msg.position[i]
+
+def left_gripper_callback(msg):
+    global left_gripper_meas
+    left_gripper_meas = msg
+
+def right_gripper_callback(msg):
+    global right_gripper_meas
+    right_gripper_meas = msg
 
 def in_bounds(v, test, bound):
     return abs(v - test) < bound
@@ -24,21 +34,15 @@ def show_test(message, test):
     else:
         print '{0:30s} {1:3s}'.format("\033[1m" + message + "\033[0m:", "\033[0;31mERROR\033[0m")
 
-if __name__ == "__main__":
-    rospy.init_node('amigo_skills_test_full')
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+#                                           HEAD
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+def test_head(amigo):
 
     global joint_positions
-    joint_positions = {}
-
-    rospy.Subscriber("/amigo/neck/measurements", sensor_msgs.msg.JointState, joint_state_callback)
-
-    amigo = robot_skills.amigo.Amigo(wait_services=True)    
-
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #
-    #                                           HEAD
-    #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
     max_err = 0.02
 
@@ -99,4 +103,40 @@ if __name__ == "__main__":
     show_test("head straight",      in_bounds(joint_positions['neck_pan_joint'], 0, max_err)
                                 and in_bounds(joint_positions['neck_tilt_joint'], 0, max_err))
 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+#                                         GRIPPERS
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+def test_grippers(amigo):
+    global left_gripper_meas, right_gripper_meas
+
+    amigo.leftArm.send_gripper_goal_open(time_out=10)
+    show_test("left gripper open", left_gripper_meas.direction == amigo_msgs.msg.AmigoGripperMeasurement.OPEN)
+
+    amigo.leftArm.send_gripper_goal_close(time_out=10)
+    show_test("left gripper close", left_gripper_meas.direction == amigo_msgs.msg.AmigoGripperMeasurement.CLOSE)
+
+    amigo.rightArm.send_gripper_goal_open(time_out=10)
+    show_test("right gripper open", right_gripper_meas.direction == amigo_msgs.msg.AmigoGripperMeasurement.OPEN)
+
+    amigo.rightArm.send_gripper_goal_close(time_out=10)
+    show_test("right gripper close", right_gripper_meas.direction == amigo_msgs.msg.AmigoGripperMeasurement.CLOSE)
+
+if __name__ == "__main__":
+    rospy.init_node('amigo_skills_test_full')
+
+    global joint_positions
+    joint_positions = {}
+
+    rospy.Subscriber("/amigo/neck/measurements", sensor_msgs.msg.JointState, joint_state_callback)
+    rospy.Subscriber("/amigo/left_gripper/measurements", amigo_msgs.msg.AmigoGripperMeasurement, left_gripper_callback)
+    rospy.Subscriber("/amigo/right_gripper/measurements", amigo_msgs.msg.AmigoGripperMeasurement, right_gripper_callback)
+
+    amigo = robot_skills.amigo.Amigo(wait_services=True)    
+
+    test_grippers(amigo)
+    test_head(amigo)
 
