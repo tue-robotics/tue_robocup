3892f614a38daf76b4f99a924a54785bb7564889
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2013-10-01 20:27:50 +0000

    Field experiment for??
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_smach_states@18796 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- examples/msl_field_experiment.py
+++ examples/msl_field_experiment.py
@@ -14,17 +14,17 @@ if __name__ == "__main__":
 
     rospy.sleep(2) # wait for tf cache to be filled
     
-    nav_state_begin_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(4, -4.4, 1.57), look_at_path_distance=2.7)
+    nav_state_begin_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(4.2, 4.2, -1.57), look_at_path_distance=2.7)
     nav_state_begin_corridor.execute()
     
-    nav_state_door = navigation.NavigateGeneric(robot, goal_pose_2d=(3, 5, -3.14), look_at_path_distance=2.7)
+    nav_state_door = navigation.NavigateGeneric(robot, goal_pose_2d=(1.8, 0.8, 3.14), look_at_path_distance=2.7)
     nav_state_door.execute()    
     
-    nav_state_end_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(-3, 5, -1.57), look_at_path_distance=2.7)
-    nav_state_end_corridor.execute()
+    #nav_state_end_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(-3, 5, -1.57), look_at_path_distance=2.7)
+    #nav_state_end_corridor.execute()
     
-    nav_state_table = navigation.NavigateGeneric(robot, goal_pose_2d=(2.2, 1.7, 1.57), look_at_path_distance=2.7)
-    nav_state_table.execute() 
+    #nav_state_table = navigation.NavigateGeneric(robot, goal_pose_2d=(2.2, 1.7, 1.57), look_at_path_distance=2.7)
+    #nav_state_table.execute() 
         
-    nav_state_begin_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(4, -4.4, 1.57), look_at_path_distance=2.7)
-    nav_state_begin_corridor.execute()
+    #nav_state_begin_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(4, -4.4, 1.57), look_at_path_distance=2.7)
+    #nav_state_begin_corridor.execute()
