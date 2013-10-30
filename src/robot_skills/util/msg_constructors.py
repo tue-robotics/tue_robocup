42b2476989275ecb1c41c9748dee5cc7bca1f776
Loy van Beek <loy.vanbeek@gmail.com>
2013-10-30 23:08:21 +0000

    Added tsts and ways to get orientations (Quaternion, Pose, PoseStamped) from roll pitch and yaw
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_executioner/robot_skills@19422 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/robot_skills/util/msg_constructors.py
+++ src/robot_skills/util/msg_constructors.py
@@ -7,7 +7,9 @@ import rospy
 import std_msgs.msg as std
 import geometry_msgs.msg as gm
 
-import transformations
+import tf
+
+number = (int, long, float)
 
 def Point(x=0, y=0, z=0):
     """Make a Point
@@ -38,18 +40,55 @@ def PointStamped(x=0, y=0, z=0, frame_id="/map", stamp=None):
 
     return gm.PointStamped(header=Header(frame_id, stamp), point=Point(x,y,z))
 
-def Quaternion(x=0, y=0, z=0, w=0):
-    return gm.Quaternion(x, y, z, w)
+def Quaternion(x=0, y=0, z=0, w=0, roll=0, pitch=0, yaw=0):
+    """Supply either at least one of x, y, z, w 
+    or at least one of roll, pitch, yaw."""
+    if x or y or z or w:
+        return gm.Quaternion(x, y, z, w)
+    elif roll or pitch or yaw:
+        quat_parts = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+        quat = Quaternion(*quat_parts)
+        return quat
 
-def Pose(x=0, y=0, z=0, phi=0):
-    quat = transformations.euler_z_to_quaternion(phi)
+def Pose(x=0, y=0, z=0, phi=0, roll=0, pitch=0, yaw=0):
+    """
+    >>> pose = Pose(yaw=0.5)
+    >>> pose
+    position: 
+      x: 0
+      y: 0
+      z: 0
+    orientation: 
+      x: 0.0
+      y: 0.0
+      z: 0.247403959255
+      w: 0.968912421711
+    """
+    z_rotation = phi or yaw #Get the one that is not 0
+
+    quat = Quaternion(roll=roll, pitch=pitch, yaw=z_rotation)
+    
     pos = Point(x ,y, z)
 
     return gm.Pose(pos, quat)
 
-def PoseStamped2D(x=0, y=0, z=0, phi=0, frame_id="/map", stamp=None, pointstamped=None):
-    """Build a posestamped from any number of arguments"""
-    number = (int, long, float)
+def PoseStamped(x=0, y=0, z=0, phi=0, 
+    roll=0, pitch=0, yaw=0,
+    frame_id="/map", stamp=None, pointstamped=None):
+    """Build a geometry_msgs.msgs.PoseStamped from any number of arguments.
+    Each value defaults to 0
+    >>> ps = PoseStamped(yaw=0.5)
+    >>> ps.pose
+    position: 
+      x: 0
+      y: 0
+      z: 0
+    orientation: 
+      x: 0.0
+      y: 0.0
+      z: 0.247403959255
+      w: 0.968912421711
+    """
     
     if not stamp:
         stamp = rospy.get_rostime()
@@ -59,7 +98,9 @@ def PoseStamped2D(x=0, y=0, z=0, phi=0, frame_id="/map", stamp=None, pointstampe
                               pose=gm.Pose(pointstamped.point, Quaternion()))
     elif isinstance(x, number) and isinstance(y, number) and isinstance(z, number):
         return gm.PoseStamped(header=Header(frame_id, stamp), 
-                              pose=Pose(x, y, z, phi))
+                              pose=Pose(x, y, z, phi, roll, pitch,yaw))
+    else:
+        raise ValueError("Either supply a number for x, y and z or a PointStamped to pointstamped")
 
 if __name__ == "__main__":
     rospy.init_node("msg_constructors_tester")
