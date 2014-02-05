027277f865b122f0288b6885eb5597500b8bad34
Erik Geerts <e.g.geerts@live.nl>
2014-02-05 21:08:58 +0000

    added last part of challenge
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@21333 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ challenge_atomic_actions/src/what_did_you_say.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('challenge_atomic_actions')
+import rospy
+#import robot_parts.speech
+from std_msgs.msg import String
+import geometry_msgs
+import smach
+
+from robot_skills.amigo import Amigo
+import robot_smach_states as states
+import robot_skills.util.msg_constructors as msgs
+
+from robot_smach_states.util.startup import startup
+
+from speech_interpreter.srv import AskUser # for speech_to_text only
+
+from psi import *
+
+###########################
+# Created by: Erik Geerts #
+###########################
+
+
+class Ask_questions(smach.State):
+    def __init__(self, robot):
+        smach.State.__init__(self, outcomes=["succeeded", "failed"])
+
+        self.robot = robot
+        self.ask_user_service_questions = rospy.ServiceProxy('interpreter/ask_user', AskUser)
+
+    def execute(self, userdata=None):
+
+        # Here you can define how many times you want to try to listen and want the maximum duration is to listen to operator.
+        self.response = self.ask_user_service_questions("questions", 10 , rospy.Duration(60))
+
+        if self.response.keys[0] == "answer":
+            return "succeeded"
+        else:
+            return "failed"
+
+########################
+##### STATEMACHINE #####
+########################
+
+
+def setup_statemachine(robot):
+
+    #retract old facts
+    robot.reasoner.query(Compound("retractall", Compound("challenge", "X")))
+    robot.reasoner.query(Compound("retractall", Compound("fetch_carry_object", "X")))
+
+    #Load database
+    robot.reasoner.query(Compound("load_database","tue_knowledge",'prolog/locations.pl'))
+    robot.reasoner.query(Compound("load_database","tue_knowledge",'prolog/egpsr.pl'))
+
+    #Assert the current challenge.
+    robot.reasoner.query(Compound("assertz",Compound("challenge", "atomic_actions")))
+
+    # Define arm used.    
+    robot = Amigo()
+
+    sm = smach.StateMachine(outcomes=['Done','Aborted'])
+
+    with sm:
+        # DURING A CHALLENGE, AMIGO STARTS AT A DESIGNATED POSITION, NOT IN FRONT OF A DOOR
+
+        ######################################################
+        ##################### INITIALIZE #####################             
+        ######################################################
+
+        smach.StateMachine.add('INITIALIZE',
+                                states.Initialize(robot),
+                                transitions={   'initialized':'ASK_FIRST_QUESTION',    ###### IN CASE NEXT STATE IS NOT "GO_TO_DOOR" SOMETHING IS SKIPPED
+                                                'abort':'Done'})
+
+
+        ######################################################
+        ################### ASK FETCH CARRY ##################            
+        ######################################################
+
+        smach.StateMachine.add("ASK_FIRST_QUESTION",
+                                Ask_questions(robot),
+                                transitions={'succeeded':'ASK_SECOND_QUESTION',
+                                             'failed':'ASK_FIRST_QUESTION'})
+
+        smach.StateMachine.add("ASK_SECOND_QUESTION",
+                                Ask_questions(robot),
+                                transitions={'succeeded':'ASK_THIRD_QUESTION',
+                                             'failed':'ASK_SECOND_QUESTION'})
+
+        smach.StateMachine.add("ASK_THIRD_QUESTION",
+                                Ask_questions(robot),
+                                transitions={'succeeded':'Done',
+                                             'failed':'ASK_THIRD_QUESTION'})
+
+    return sm
+
+if __name__ == "__main__":
+    rospy.init_node('what_did_you_say')
+    rospy.loginfo("-------------------- WHAT DIT YOU SAY ---------------------")
+    rospy.loginfo("-----------------------------------------------------------")
+    rospy.loginfo("------------ MAKE SURE YOU ADDED THE SENTENCES ------------")
+    rospy.loginfo("-------------- speech_interpreter.pl in map ---------------")
+    rospy.loginfo("----------- tue_reasoner/tue_knowledge/prolog -------------")
+    rospy.loginfo("-------- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -----------")
+    rospy.loginfo("-----------------------------------------------------------")
+         
+    startup(setup_statemachine)
