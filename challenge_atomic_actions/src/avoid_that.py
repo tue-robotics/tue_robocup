908009a6964870f29377f3cd129f8fc7aafa7e31
Rein Appeldoorn <reinzor@gmail.com>
2014-02-05 18:45:00 +0000

    Avoid that challenge
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@21315 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_atomic_actions/src/avoid_that.py
+++ challenge_atomic_actions/src/avoid_that.py
@@ -8,15 +8,43 @@ from robot_skills.amigo import Amigo
 import robot_smach_states as states
 
 from robot_skills.reasoner  import Conjunction, Compound
-from robot_smach_states.util.startup import startup
 
 class AvoidThat(smach.StateMachine):
 
     def __init__(self, robot):
         smach.StateMachine.__init__(self, outcomes=['Done','Aborted'])
-        print "lol"
 
+        robot.reasoner.query(Compound("load_database", "tue_knowledge", 'prolog/locations.pl'))
+
+        robot.reasoner.assertz(Compound("challenge", "avoid_that"))
+
+        query_waypoint =  Compound("waypoint", "goal", Compound("pose_2d", "X", "Y", "Phi"))
+        
+        with self:
+
+            smach.StateMachine.add('NAVIGATE_TO_WAYPOINT',
+                                    states.NavigateGeneric(robot, goal_query=query_waypoint),
+                                    transitions={   "arrived":"SAY_GOAL_REACHED",
+                                                    "unreachable":'SAY_GOAL_UNREACHABLE',
+                                                    "preempted":'Aborted',
+                                                    "goal_not_defined":'SAY_GOAL_NOT_DEFINED'})
+
+            smach.StateMachine.add("FAILED_TO_START_CHALLENGE", 
+                                    states.Say(robot, [ "Sorry, I can't start the challenge for some reason. Aborting now."]),
+                                    transitions={   'spoken':'Aborted'})
+
+            smach.StateMachine.add("SAY_GOAL_UNREACHABLE", 
+                                    states.Say(robot, [ "Sorry, the goal is unreachable. Aborting now."]),
+                                    transitions={   'spoken':'Aborted'})
+
+            smach.StateMachine.add("SAY_GOAL_NOT_DEFINED", 
+                                    states.Say(robot, [ "Sorry, I don't know where to go. Please specify my waypoint. Aborting now."]),
+                                    transitions={   'spoken':'Aborted'})
+
+            smach.StateMachine.add("SAY_GOAL_REACHED", 
+                                    states.Say(robot, [ "Yeah, I reached my goal. Exiting challenge."]),
+                                    transitions={   'spoken':'Aborted'})
 
 if __name__ == "__main__":
     rospy.init_node('avoid_that_exec')
-    startup(AvoidThat)
+    states.util.startup(AvoidThat)
