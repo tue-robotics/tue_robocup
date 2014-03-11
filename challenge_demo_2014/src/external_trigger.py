954cdb709b844f97e6057dab35729ab7bbd4203f
Ramon Wijnands <rayman747@hotmail.com>
2014-03-11 16:48:06 +0000

    fix doorbell trigger
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@22273 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_demo_2014/src/external_trigger.py
+++ challenge_demo_2014/src/external_trigger.py
@@ -8,7 +8,7 @@ from robot_skills.amigo import Amigo
 import robot_smach_states as states
 
 from robot_skills.reasoner  import Compound
-
+from robot_smach_states.util.startup import startup
 import std_msgs.msg
 
 
@@ -16,35 +16,37 @@ class WaitForTrigger(smach.State):
 
     trigger_received = False
 
-    def __init__(self):
+    def __init__(self, robot, triggers):
         smach.State.__init__(self, 
-                             outcomes=['trigger_received', 'Aborted'])
+                             outcomes=triggers+['preempted'])
+        self.robot = robot
+        self.triggers = triggers
 
         # Get the ~private namespace parameters from command line or launch file.
         self.rate = float(rospy.get_param('~rate', '1.0'))
-        rospy.loginfo('listening for trigger with a rate of %d Hz', self.rate)
-
-        topic = rospy.get_param('~topic', 'trigger')
-        rospy.loginfo('topic = %s', topic)
+        topic     = rospy.get_param('~topic', 'trigger')
+        
         rospy.Subscriber(topic, std_msgs.msg.String, self.callback)
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state WaitForTrigger')
+        rospy.loginfo('topic: /%s', topic)
+        rospy.loginfo('rate:  %d Hz', self.rate)
 
+    def execute(self, userdata):
         while not rospy.is_shutdown() and not self.trigger_received:
             rospy.sleep(1/self.rate)
 
         if self.trigger_received:
-            return 'trigger_received'
+            return self.trigger_received
         else:
-            return 'Aborted'
+            return 'preempted'
 
     def callback(self, data):
         # Simply print out values in our custom message.
         rospy.loginfo('trigger_received: %s', data.data)
-        self.trigger_received = True
+        if data.data in self.triggers:
+            self.trigger_received = data.data
 
-class AvoidThat(smach.StateMachine):
+class ChallengeDemo2014(smach.StateMachine):
 
     def __init__(self, robot):
         smach.StateMachine.__init__(self, outcomes=['Done','Aborted'])
@@ -53,21 +55,32 @@ class AvoidThat(smach.StateMachine):
 
         robot.reasoner.assertz(Compound("challenge", "challenge_demo_2014"))
 
-        query_waypoint =  Compound("waypoint", "goal", Compound("pose_2d", "X", "Y", "Phi"))
+        query_start = Compound("waypoint", "start",         Compound("pose_2d", "X", "Y", "Phi"))
+        query_door  = Compound("waypoint", "front_of_door", Compound("pose_2d", "X", "Y", "Phi"))
         
         with self:
 
-            smach.StateMachine.add('NAVIGATE_TO_WAYPOINT',
-                                    states.NavigateGeneric(robot, goal_query=query_waypoint),
-                                    transitions={   "arrived":"SAY_GOAL_REACHED",
+            smach.StateMachine.add('NAVIGATE_TO_START',
+                                    states.NavigateGeneric(robot, goal_query=query_start),
+                                    transitions={   "arrived":"SAY_START_REACHED",
                                                     "unreachable":'SAY_GOAL_UNREACHABLE',
                                                     "preempted":'Aborted',
                                                     "goal_not_defined":'SAY_GOAL_NOT_DEFINED'})
+            
+            smach.StateMachine.add("SAY_START_REACHED", 
+                                    states.Say(robot, [ "I'm going to wait for further instructions"]),
+                                    transitions={   'spoken':'WAIT_FOR_TRIGGER'})
 
-            smach.StateMachine.add("START_CHALLENGE", 
-                                    states.Say(robot, [ "I received a trigger, I'm starting."]),
+            smach.StateMachine.add("WAIT_FOR_TRIGGER", 
+                                    WaitForTrigger(robot, ['doorbell']),
+                                    transitions={   'doorbell':'SAY_TRIGGER_RECEIVED',
+                                                    'preempted': 'Aborted'})
+            
+            smach.StateMachine.add("SAY_TRIGGER_RECEIVED", 
+                                    states.Say(robot, [ "I received a signal, I'm going to the door."]),
                                     transitions={   'spoken':'Aborted'})
 
+            # navigation states
             smach.StateMachine.add("SAY_GOAL_UNREACHABLE", 
                                     states.Say(robot, [ "Sorry, the goal is unreachable. Aborting now."]),
                                     transitions={   'spoken':'Aborted'})
@@ -76,23 +89,19 @@ class AvoidThat(smach.StateMachine):
                                     states.Say(robot, [ "Sorry, I don't know where to go. Please specify my waypoint. Aborting now."]),
                                     transitions={   'spoken':'Aborted'})
 
-            smach.StateMachine.add("SAY_GOAL_REACHED", 
-                                    states.Say(robot, [ "Yeah, I reached my goal. Exiting challenge."]),
-                                    transitions={   'spoken':'Aborted'})
-
-def AvoidThatTester():
+def WaitForTriggerTester():
 
     sm = smach.StateMachine(outcomes=['Done'])
 
     with sm:
-        # Add states to the container
-        smach.StateMachine.add('WAITFORTRIGGER', WaitForTrigger(), 
-                               transitions={'trigger_received': 'Done',
-                                            'Aborted':          'Done'})
-
+        smach.StateMachine.add("WAIT_FOR_TRIGGER", 
+                                WaitForTrigger(None, ['doorbell']),
+                                transitions={   'doorbell': 'Done',
+                                                'Aborted':  'Done'})
     sm.execute()
 
 
 if __name__ == "__main__":
     rospy.init_node('challenge_demo_2014_exec')
-    AvoidThatTester()
+    #WaitForTriggerTester()
+    startup(ChallengeDemo2014)
