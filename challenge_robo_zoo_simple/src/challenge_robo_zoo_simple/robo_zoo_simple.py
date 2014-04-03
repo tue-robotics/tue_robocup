a6e7ca32b35af113d091bcb4b698b5a64ddc8fb5
Loy van Beek <loy.vanbeek@gmail.com>
2014-04-03 11:17:54 +0000

    Added some explanation as to how to add something new
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@23008 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_robo_zoo_simple/src/challenge_robo_zoo_simple/robo_zoo_simple.py
+++ challenge_robo_zoo_simple/src/challenge_robo_zoo_simple/robo_zoo_simple.py
@@ -8,12 +8,7 @@ import robot_smach_states as states
 from robot_smach_states.util.startup import startup
 import random
 
-from psi import Compound, Conjunction, Sequence
-from robot_skills.arms import State as ArmState
-from robot_skills.util import msg_constructors as msg
-from robot_skills.util import transformations
-
-#from part1 import Part1
+from part1 import TurnAround
 
 class RandomOutcome(smach.State):
     """Of the state's registered outcomes, just select a random one"""
@@ -28,23 +23,37 @@ class RandomOutcome(smach.State):
 
 
 class RoboZooSimple(smach.StateMachine):
-    """Calls each part in turn and makes a random switch between parts, i.e smach State(Machine)s"""
+    """Calls each part in turn and makes a random switch between parts, i.e smach State(Machine)s.
+
+    ADDING A NEW PART:
+    - Create a file with a new thing to do for the challenge, like waving or making a joke.
+        This file must contain one main state(machine).
+    - Import the file and the stae(machine) from it.
+    - Add a new state to the machine below. All of its transitions should make it go back to SELECT_RANDOM
+    - Edit the SELECT_RANDOM state to have a new transition to the newly added state.
+        The list after 'robot' in its constructor is a list of the possible outcomes and should match with the possible outcomes in its transitions.
+        RandomOutcome takes a random item from this list as an outcome."""
     
     def __init__(self, robot):
         smach.StateMachine.__init__(self, outcomes=["Done", "Aborted", "Failed"])
         with self:
             smach.StateMachine.add( "SELECT_RANDOM",
-                                    RandomOutcome(robot, ["1","2"]),
+                                    RandomOutcome(robot, ["1","2","3"]),
                                     transitions={"1":"SAY_HI",
-                                                 "2":"MAKE_JOKES"})
+                                                 "2":"MAKE_JOKES",
+                                                 "3":"TURN_AROUND"})
 
             smach.StateMachine.add( "SAY_HI",
                                     states.Say(robot, ["Howdy", "Hi there"]),
                                     transitions={"spoken":"SELECT_RANDOM"})
 
             smach.StateMachine.add( "MAKE_JOKES",
-                                    states.Say(robot, ["Should I tell a joke?"]),
+                                    states.Say(robot, ["Should I tell a joke?", "Want to hear a robot joke?", "Two robots walk into a bar. Hahahahaha, robots can't walk that well"]),
                                     transitions={"spoken":"SELECT_RANDOM"})
+
+            smach.StateMachine.add( "TURN_AROUND",
+                                    TurnAround(robot),
+                                    transitions={"Done":"SELECT_RANDOM", "Aborted":"SELECT_RANDOM", "Failed":"SELECT_RANDOM"})
 
 if __name__ == "__main__":
     rospy.init_node("challenge_robo_zoo")
