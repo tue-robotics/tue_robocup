019da915e9f26838ae70f6683361448b0fc6dab3
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2014-04-08 20:34:14 +0000

    Added sounds
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@23312 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ challenge_robo_zoo_simple/src/challenge_robo_zoo_simple/sounds.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import roslib; roslib.load_manifest('challenge_robo_zoo_simple')
+
+import smach
+
+import os, sys
+
+class R2D2(smach.State):
+    def __init__(self, robot):
+        smach.State.__init__(self, outcomes=["Done"])
+
+    def execute(self, userdata=None):
+        
+        try:
+            os.system("mpg123 ~/Music/R2D2.mp3")
+        except:
+            rospy.logerror("Something went terribly wrong when playing r2d2")        
+        
+        return "Done"
+        
+class Toeter(smach.State):
+    def __init__(self, robot):
+        smach.State.__init__(self, outcomes=["Done"])
+
+    def execute(self, userdata=None):
+        
+        try:
+            os.system("mpg123 ~/Music/Toeter1.mp3")
+        except:
+            rospy.logerror("Something went terribly wrong when playing Toeter1 ")        
+        
+        return "Done"
