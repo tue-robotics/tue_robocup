b1172a79600f5701b73dadf1129027f16ee5d17d
Loy van Beek <loy.vanbeek@gmail.com>
2014-04-22 20:03:43 +0000

    Added a walk like an egyptian (from the Bangles song) as a motion sequence for Amigo
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@23559 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ challenge_robo_zoo_simple/src/challenge_robo_zoo_simple/walk_like_an_egyptian.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+import roslib; roslib.load_manifest('challenge_robo_zoo_simple')
+
+import rospy
+import smach
+import robot_skills.util.msg_constructors as msgs
+import robot_smach_states as states
+
+egyptian_pose_start     = [-1.570, 0.000, -1.570, 1.963, 0.000, -1.177, 0.000]
+egyptian_pose_center    = [-1.570, 0.000, -1.570, 1.570, 0.000, -0.785, 0.000]
+egyptian_pose_end       = [-1.570, 0.000, -1.570, 1.177, 0.000, -0.393, 0.000]
+
+egyptian_motion_left = [egyptian_pose_center, egyptian_pose_start, egyptian_pose_center, egyptian_pose_end, egyptian_pose_center]
+egyptian_motion_right= [egyptian_pose_center, egyptian_pose_end, egyptian_pose_center, egyptian_pose_start, egyptian_pose_center]
+
+def walk_like_an_egyptian(robot):
+    for i in range(2):
+        robot.head.send_goal(msgs.PointStamped(0,0,0, frame_id="/amigo/grippoint_left"), pan_vel=5.0, tilt_vel=5.0)
+        robot.head.send_goal(msgs.PointStamped(0,0,0, frame_id="/amigo/grippoint_left"), keep_tracking=True, pan_vel=5.0, tilt_vel=5.0)
+        robot.leftArm.send_joint_trajectory(egyptian_motion_left, timeout=10)
+
+        robot.head.send_goal(msgs.PointStamped(0,0,0, frame_id="/amigo/grippoint_right"), pan_vel=5.0, tilt_vel=5.0)
+        robot.head.send_goal(msgs.PointStamped(0,0,0, frame_id="/amigo/grippoint_right"), keep_tracking=True, pan_vel=5.0, tilt_vel=5.0)
+        robot.rightArm.send_joint_trajectory(egyptian_motion_right, timeout=10)
+
+    robot.rightArm.reset_arm()
+    robot.leftArm.reset_arm()
+    robot.head.reset_position()
+
+if __name__ == "__main__":
+    rospy.init_node("walk_like_an_egyptian")
+    import robot_skills.amigo
+    amigo = robot_skills.amigo.Amigo(wait_services=True)
+
+    walk_like_an_egyptian(amigo)
