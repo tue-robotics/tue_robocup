03775dba7dec88611377f32ec67b2c644a279d02
Loy van Beek <loy.vanbeek@gmail.com>
2014-05-02 15:02:28 +0000

    Some tweaks to the macarena move. Because of the joint limit, it doesn't look as good as in simulation
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@23935 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_robo_zoo_simple/src/challenge_robo_zoo_simple/macarana.py
+++ challenge_robo_zoo_simple/src/challenge_robo_zoo_simple/macarana.py
@@ -9,25 +9,26 @@ import signal
 
 #0. Arms are in reset pose
 #1. Right arm straight forward and hand up
-arm_straight_1 =                [0.000, 1.570, 0.000, 0.000, 0.000, 0.000, 0.000]
+arm_straight_1 =                [0.000, 1.500, 0.000, 0.000, 0.000, 0.000, 0.000]
 #2. Move up and down (spindle)
 #3. Turn the right hand upside down,  move the left arm up as wel
-arm_straight_2 =                [0.000, 1.570, 1.570, 0.000, 1.570, 0.000, 0.000]
+arm_straight_2 =                [0.000, 1.500, 1.500, 0.000, 1.500, 0.000, 0.000]
 #4. Right hand to the left shoulder
-right_hand_to_left_shoulder =   [0.000, 1.750, 1.370, 1.700, 1.570, 0.000, 0.000]
+right_hand_to_left_shoulder =   [0.000, 1.500, 1.200, 1.500, 1.500, 0.000, 0.000]
 #5. Left hand to right shoulder
-left_hand_to_right_shoulder =   [0.000, 1.290, 1.570, 1.700, 1.570, 0.000, 0.000]
+left_hand_to_right_shoulder =   [0.000, 1.500, 1.000, 1.000, 0.000, 0.000, 0.000]
 #6. Right arm to head
-right_arm_to_head =             [0.000, 1.750, 0.000, 1.900, 1.570, 0.500, 0.000]
+right_arm_to_head_1 =           [0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]
+right_arm_to_head_2 =           [0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]
 #6a. Left arm to head
-left_arm_to_head_1 =            [0.000, 1.290, 1.570, 1.300, 1.570, 0.000, 0.000]
-left_arm_to_head_2 =            [0.000, 1.750, 1.570, 1.300, 1.570, 0.000, 0.000]
-left_arm_to_head_3 =            [0.000, 1.750, 0.000, 1.300, 1.570, 0.000, 0.000]
-left_arm_to_head_4 =            [0.000, 1.750, 0.000, 1.900, 1.570, 0.500, 0.000]
+left_arm_to_head_1 =            [0.000, 1.290, 1.500, 1.300, 1.500, 0.000, 0.000]
+left_arm_to_head_2 =            [0.000, 1.500, 1.500, 1.300, 1.500, 0.000, 0.000]
+left_arm_to_head_3 =            [0.000, 1.500, 0.000, 1.300, 1.500, 0.000, 0.000]
+left_arm_to_head_4 =            [0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]
 #7. Right arm down to the hips
 arm_to_hips_1 =                 [-0.200, -0.200, 0.200, 0.800, 0.000, 0.000, 0.000] 
-arm_to_hips_2 =                 [-0.400, 0.000, 1.570, 0.600, 0.000, 0.000, 0.000]
-arm_to_hips_3 =                 [-0.400, 0.000, 1.570, 0.000, 0.000, 0.000, 0.000]
+arm_to_hips_2 =                 [-0.400, 0.000, 1.500, 0.600, 0.000, 0.000, 0.000]
+arm_to_hips_3 =                 [-0.400, 0.000, 1.500, 0.000, 0.000, 0.000, 0.000]
 arm_to_hips_4 =                 [-0.400, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000]
 
 zero =                          [-0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000]
@@ -59,54 +60,57 @@ def macarena(robot):
     stopEvent = threading.Event()
 
     up_and_down = threading.Thread(target=spindle_up_down, args=(robot, 0.3, 0.4, stopEvent))
-    up_and_down.start()
+#    up_and_down.start()
     #robot.spindle.send_goal(0.3)
     def _left(*args, **kwargs): #The underscore  makes the outlining below easier to read
         if not robot.leftArm.send_joint_goal(*args, **kwargs):
-            robot.speech.speak("Guys, could you help me, i'm stuck in the macarena")
             raise Exception("Arms dit not reach goal,  need help")
     
     def right(*args, **kwargs): 
         if not robot.rightArm.send_joint_goal(*args, **kwargs):
-            robot.speech.speak("Guys, could you help me, i'm stuck in the macarena")
             raise Exception("Arms dit not reach goal,  need help")
+    #Defined shortcuts above
 
-    for i in range(1):
-        right(*arm_straight_1, timeout=5)
-        _left(*arm_straight_1, timeout=5)
+    try:
+        for i in range(2):
+            right(*arm_straight_1, timeout=10)
+            _left(*arm_straight_1, timeout=10)
 
-        right(*arm_straight_2, timeout=5)
-        _left(*arm_straight_2, timeout=5)
+            right(*arm_straight_2, timeout=5)
+            _left(*arm_straight_2, timeout=5)
 
-        right(*right_hand_to_left_shoulder, timeout=5)
-        _left(*left_hand_to_right_shoulder, timeout=5)
+            right(*right_hand_to_left_shoulder, timeout=10)
+            _left(*left_hand_to_right_shoulder, timeout=10)
 
-        right(*right_arm_to_head, timeout=5)
+            right(*right_arm_to_head_1, timeout=10) 
+            #right(*right_arm_to_head_2, timeout=10) 
 
-        _left(*left_arm_to_head_1, timeout=5)
-        _left(*left_arm_to_head_2, timeout=5)
-        _left(*left_arm_to_head_3, timeout=5)
-        _left(*left_arm_to_head_4, timeout=5)
+            #_left(*left_arm_to_head_1, timeout=5) #?
+            #_left(*left_arm_to_head_2, timeout=5) #?
+            _left(*left_arm_to_head_3, timeout=5)
+            _left(*left_arm_to_head_4, timeout=5)
 
-        right(*arm_to_hips_1, timeout=5)
-        _left(*arm_to_hips_1, timeout=5)
-        right(*arm_to_hips_2, timeout=5)
-        _left(*arm_to_hips_2, timeout=5)
-        right(*arm_to_hips_3, timeout=5)
-        _left(*arm_to_hips_3, timeout=5)
-        right(*arm_to_hips_4, timeout=5)
-        _left(*arm_to_hips_4, timeout=5)
-        right(*arm_to_hips_1, timeout=5)
-        _left(*arm_to_hips_1, timeout=5)
-        
-        # right(*zero, timeout=10)
-        # _left(*zero, timeout=10)
+            right(*arm_to_hips_1, timeout=10)
+            _left(*arm_to_hips_1, timeout=10)
+            right(*arm_to_hips_2, timeout=5)
+            _left(*arm_to_hips_2, timeout=5)
+            right(*arm_to_hips_3, timeout=5)
+            _left(*arm_to_hips_3, timeout=5)
+            right(*arm_to_hips_4, timeout=5)
+            _left(*arm_to_hips_4, timeout=5)
+            right(*arm_to_hips_1, timeout=10)
+            _left(*arm_to_hips_1, timeout=10)
 
-    stopEvent.set()
-    up_and_down.join()
-    robot.rightArm.reset_arm()
-    robot.leftArm.reset_arm()
-    robot.head.reset_position()
+        stopEvent.set()
+        up_and_down.join()
+        robot.rightArm.reset_arm()
+        robot.leftArm.reset_arm()
+        robot.head.reset_position()
+    except Exception, e:
+        robot.speech.speak("Guys, could you help me, i'm stuck in the maca-rena")
+        rospy.logerr(e)
+            # right(*zero, timeout=10)
+            # _left(*zero, timeout=10)
 
 def start_music():
     import subprocess
