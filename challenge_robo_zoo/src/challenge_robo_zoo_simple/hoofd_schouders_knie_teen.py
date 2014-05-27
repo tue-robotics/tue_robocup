c1decc20610c9502fe746336714711083bd7d0c3
Loy van Beek <loy.vanbeek@gmail.com>
2014-05-27 19:18:21 +0000

    Made speech in step disabled by default because its too annoying and slow. Tuned some more poses
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@24581 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_robo_zoo/src/challenge_robo_zoo_simple/hoofd_schouders_knie_teen.py
+++ challenge_robo_zoo/src/challenge_robo_zoo_simple/hoofd_schouders_knie_teen.py
@@ -13,9 +13,9 @@ TURNSPEED = 1.0
 
 #2. Move up and down (spindle)
 #4. Right hand to the left shoulder
-right_hand_to_left_shoulder =   [0.000, 1.500, 1.200, 1.500, 1.500, 0.000, 0.000]
+right_hand_to_left_shoulder =   [0.000, 0.400, 0.000, 2.100, 1.500, 0.500, 0.000]
 #5. Left hand to right shoulder
-left_hand_to_right_shoulder =   [0.000, 1.500, 1.000, 1.000, 0.000, 0.000, 0.000]
+left_hand_to_right_shoulder =   [0.000, 0.400, 0.000, 2.100, 1.500, 0.500, 0.000]
 #6. Right arm to head
 right_arm_to_head_1 =           [0.000, 1.400, 0.000, 2.100, 1.500, 0.500, 0.000]
 #6a. Left arm to head
@@ -25,7 +25,8 @@ right_arm_to_eyes_1 =           [0.000, 1.400, 0.350, 1.800, 0.0, 0.0, 0.500]
 left_arm_to_eyes_1 =            [0.000, 1.400, 0.350, 1.800, 0.0, 0.0, 0.500]
 #6. Right arm to nose
 right_arm_to_nose_1 =           [0.000, 1.100, 0.350, 2.000, 0.000, 0.300, 0.500]
-arm_to_hips_1 =                 [-0.200, -0.200, 0.200, 0.800, 0.000, 0.000, 0.000] 
+arm_to_knees_1 =                [-0.200, -0.200, 0.200, 0.800, 0.000, 0.000, 0.000] 
+arm_to_toes_1 =                [-0.200, -0.200, 0.200, 0.800, 0.000, 0.000, 0.000] 
 
 zero =                          [-0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000]
 #7a. Left arm down to the hips
@@ -49,6 +50,7 @@ def hoofdschoudersknieteen(robot):
     #TODO: Look at hand at given times
 
     def _left(*args, **kwargs): #The underscore  makes the outlining below easier to read
+        return
         if not robot.leftArm.send_joint_goal(*args, **kwargs):
             raise Exception("Arms did not reach goal,  need help")
     
@@ -57,46 +59,46 @@ def hoofdschoudersknieteen(robot):
             raise Exception("Arms did not reach goal,  need help")
     #Defined shortcuts above
 
-    def hoofd(speak=True):
-        if speak: robot.speech.speak("Hoofd", language='nl', block=False)
+    def hoofd(speak=False):
         robot.spindle.send_goal(0.4, timeout=4.0)
+        if speak: robot.speech.speak("Hoofd", language='nl', block=False)
         right(*right_arm_to_head_1, timeout=0) #Dont wait, both arms should move in sync
         _left(*left_arm_to_head_1,  timeout=10)
 
-    def schouders(speak=True):
-        if speak: robot.speech.speak("Schouders", language='nl', block=False)
+    def schouders(speak=False):
         robot.spindle.send_goal(0.4, timeout=4.0)
+        if speak: robot.speech.speak("Schouders", language='nl', block=False)
         right(*right_hand_to_left_shoulder, timeout=10) #Wait, otherwise arms collide
         _left(*left_hand_to_right_shoulder, timeout=10)
 
-    def knie(speak=True): #TODO: tune poses
+    def knie(speak=False): #TODO: tune poses
+        robot.spindle.send_goal(0.2, timeout=4.0)
         if speak: robot.speech.speak("Knie", language='nl', block=False)
-        robot.spindle.send_goal(0.3, timeout=4.0)
-        right(*arm_to_hips_1, timeout=0)#Dont wait, both arms should move in sync
-        _left(*arm_to_hips_1, timeout=10)
+        right(*arm_to_knees_1, timeout=0)#Dont wait, both arms should move in sync
+        _left(*arm_to_knees_1, timeout=10)
     
-    def teen(speak=True): #TODO: tune poses
-        if speak: robot.speech.speak("En teen", language='nl', block=False)
+    def teen(speak=False): #TODO: tune poses
         robot.spindle.send_goal(0.1, timeout=4.0)
-        right(*arm_to_hips_1, timeout=0)#Dont wait, both arms should move in sync
-        _left(*arm_to_hips_1, timeout=10)
+        if speak: robot.speech.speak("En teen", language='nl', block=False)
+        right(*arm_to_knees_1, timeout=0)#Dont wait, both arms should move in sync
+        _left(*arm_to_knees_1, timeout=10)
 
-    def oren(speak=True):
-        if speak: robot.speech.speak("Oren", language='nl', block=False)
+    def oren(speak=False):
         robot.spindle.send_goal(0.4, timeout=4.0)
+        if speak: robot.speech.speak("Oren", language='nl', block=False)
         right(*right_arm_to_head_1, timeout=0) #Dont wait, both arms should move in sync
         _left(*left_arm_to_head_1,  timeout=10)
 
-    def ogen(speak=True):
-        if speak: robot.speech.speak("Ogen", language='nl', block=False)
+    def ogen(speak=False):
         robot.spindle.send_goal(0.4, timeout=4.0)
+        if speak: robot.speech.speak("Ogen", language='nl', block=False)
         right(*right_arm_to_eyes_1, timeout=0) #Dont wait, both arms should move in sync
         _left(*left_arm_to_eyes_1,  timeout=10)
 
-    def puntje_van_je_neus(turn=False, speak=True):
-        if speak: robot.speech.speak("Puntje van je neus!", language='nl', block=False)
+    def puntje_van_je_neus(turn=False, speak=False):
         if turn:
             robot.base.force_drive(0, 0, TURNSPEED, (2*math.pi)/TURNSPEED) #Turn a full circle at TURNSPEED rad/sec
+        if speak: robot.speech.speak("Puntje van je neus!", language='nl', block=False)
         robot.spindle.send_goal(0.4, timeout=4.0)
         right(*right_arm_to_head_1, timeout=0) #Dont wait, both arms should move in sync
         robot.leftArm.reset_arm() #Move left arm down, only use right arm to point at 'nose'
