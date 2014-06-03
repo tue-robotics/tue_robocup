0256054098a4648270dcbda7153435dfa873723f
Ramon Wijnands <rayman747@hotmail.com>
2014-06-03 16:15:37 +0000

    improve buttons
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@24764 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_demo/gui/js/object-detector.js
+++ challenge_demo/gui/js/object-detector.js
@@ -16,13 +16,21 @@ $( document ).ready(function() {
     trigger.subscribe(object_callback);
 
     function object_callback(msg) {
-        var objs = msg.data.split('|');
+        var objs = msg.data;
+        if (!objs) {
+            objs = [];
+        } else {
+            objs = objs.split('|');
+        }
+
+        console.log(objs);
+
         var data = objs.map(function (o) {
             var c;
             if (color_map[o]) {
                 c = color_map[o];
             } else {
-                c = color_map[o] =  random_colour();
+                c = color_map[o] =  random_color();
             }
             return {
                 color: c, name: o
@@ -39,5 +47,23 @@ $( document ).ready(function() {
 
     function random_colour(){
         return '#'+Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    var golden_ratio_conjugate = 0.618033988749895;
+    var h = Math.random(); // use random start value
+
+    function random_color(){
+        h += golden_ratio_conjugate;
+        h %= 1;
+        var c = get_color(h, 0.7, 0.8);
+        console.log(c);
+        return c;
+    }
+
+    function get_color(h, s, l) {
+        h = Math.floor(h*360);
+        s = Math.floor(s*100);
+        l = Math.floor(l*100);
+        return 'hsl(' + h + ',' + s + '%,' + l + '%)';
     }
 });
