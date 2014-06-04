2d3caf814c100da7e2de2d9f11e108a1fb5c6ebe
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2014-06-04 18:01:36 +0000

    Fixed current person vis
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@24862 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_demo/src/visualize_current_person.py
+++ challenge_demo/src/visualize_current_person.py
@@ -26,30 +26,33 @@ if __name__ == '__main__':
     marker.pose.orientation.z = 0.0
     marker.pose.orientation.w = 1.0
     marker.lifetime = rospy.Duration(1.0)
-    marker.pose.position.z = marker.scale.z/2
+    
     marker.scale.x = 0.5
     marker.scale.y = 0.5
     marker.scale.z = 1.8
+    
+    marker.pose.position.z = marker.scale.z/2
 
     marker.color.r = 1.0;
     marker.color.g = 1.0;
     marker.color.b = 0.0;
-    marker.color.a = 0.5;
+    marker.color.a = 0.25;
 
     while not rospy.is_shutdown():
 
         ''' Query reasoner '''
         answers = client.query(term)
         if answers:
-        	answer = answers[0]
-        	marker.position.x = answer["X"]
-        	marker.position.z = answer["Y"]
+            answer = answers[0]
+            marker.pose.position.x = answer["X"]
+            marker.pose.position.y = answer["Y"]
+            #rospy.loginfo("ID = {0}".format(answer["ObjectID"]))
 
-        	''' Publish marker '''
-        	marker.header.stamp = rospy.Time.now()
-        	marker_pub.publish(marker)
-        	rospy.loginfo("Person found, publishing: {0}".format(marker))
-        else:
-        	rospy.loginfo("No person found yet")
-
+            ''' Publish marker '''
+            marker.header.stamp = rospy.Time.now()
+            marker_pub.publish(marker)
+            #rospy.loginfo("Person found, publishing: {0}".format(marker))
+        #else:
+            #rospy.loginfo("No person found yet")
+            
         rospy.sleep(rospy.Duration(0.1))
