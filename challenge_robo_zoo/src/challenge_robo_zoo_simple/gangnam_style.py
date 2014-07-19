77d709b9e16583f96b749a40a9240fb8cad870c0
Loy van Beek <loy.vanbeek@gmail.com>
2014-07-19 19:57:34 +0000

    More tuning for joint angles and sequence. Not yet OK, but want to test in sim
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@25562 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_robo_zoo/src/challenge_robo_zoo_simple/gangnam_style.py
+++ challenge_robo_zoo/src/challenge_robo_zoo_simple/gangnam_style.py
@@ -78,7 +78,7 @@ def motionB(robot):
         robot.head.send_goal(msgs.PointStamped(0,0,0, frame_id="/amigo/grippoint_right"), pan_vel=1.0, tilt_vel=1.0)
         robot.head.send_goal(msgs.PointStamped(0,0,0, frame_id="/amigo/grippoint_right"), keep_tracking=True, pan_vel=1.0, tilt_vel=1.0)
 
-        robot.rightArm.send_joint_goal(*gangnam_poseB_right_pre_start, timeout=1)
+        #robot.rightArm.send_joint_goal(*gangnam_poseB_right_pre_start, timeout=1)
         robot.rightArm.send_joint_goal(*gangnam_poseB_right_start, timeout=1)
         
         robot.leftArm.send_joint_goal(-0.050, 1.500, 1.5300, 1.300, -0.15, 0.000, 0.000)
