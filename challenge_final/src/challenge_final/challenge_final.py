c6b73e9b7d9b82135d8ba47a2cfa7ea5c9f5c1f3
Loy van Beek <loy.vanbeek@gmail.com>
2014-07-19 18:16:34 +0000

    Added nav to waypoints in final challenge
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_robocup@25551 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- challenge_final/src/challenge_final/challenge_final.py
+++ challenge_final/src/challenge_final/challenge_final.py
@@ -12,21 +12,50 @@ import robot_smach_states as states
 class FinalChallenge2014(smach.StateMachine):
 
     def __init__(self, robot):
-        smach.StateMachine.__init__(self, outcomes=['Done','Failed'])
+        smach.StateMachine.__init__(self, outcomes=['Done','Failed', "Aborted"])
         self.robot = robot
 
         with self:
-			smach.StateMachine.add('INITIALIZE',
+            smach.StateMachine.add('INITIALIZE',
                                 states.Initialize(robot),
                                 transitions={   'initialized':'DRIVE_TO_WAYPOINT_1',    ###### IN CASE NEXT STATE IS NOT "GO_TO_DOOR" SOMETHING IS SKIPPED
                                                 'abort':'Aborted'})
 
-			smach.StateMachine.add('DRIVE_TO_WAYPOINT_1', 
+            smach.StateMachine.add('DRIVE_TO_WAYPOINT_1', 
                                     states.NavigateGeneric(robot, goal_name="final_waypoint_1"),
-                                    transitions={   'arrived':'Done', 
+                                    transitions={   'arrived':'WAIT_1', 
                                                     'preempted':'Failed', 
                                                     'unreachable':'Failed', 
                                                     'goal_not_defined':'Failed'})
+
+            smach.StateMachine.add('WAIT_1', 
+                                    states.Wait_time(robot, 3),
+                                    transitions={   'waited':'DRIVE_TO_WAYPOINT_2', 
+                                                    'preempted':'Aborted'})
+
+            smach.StateMachine.add('DRIVE_TO_WAYPOINT_2',
+                                    states.NavigateGeneric(robot, goal_name="final_waypoint_2"),
+                                    transitions={   'arrived':'WAIT_2', 
+                                                    'preempted':'Failed', 
+                                                    'unreachable':'Failed', 
+                                                    'goal_not_defined':'Failed'})
+
+            smach.StateMachine.add('WAIT_2', 
+                                    states.Wait_time(robot, 3),
+                                    transitions={   'waited':'DRIVE_TO_WAYPOINT_3', 
+                                                    'preempted':'Aborted'})
+
+            smach.StateMachine.add('DRIVE_TO_WAYPOINT_3', 
+                                    states.NavigateGeneric(robot, goal_name="final_waypoint_3"),
+                                    transitions={   'arrived':'WAIT_3', 
+                                                    'preempted':'Failed', 
+                                                    'unreachable':'Failed', 
+                                                    'goal_not_defined':'Failed'})
+
+            smach.StateMachine.add('WAIT_3', 
+                                    states.Wait_time(robot, 3),
+                                    transitions={   'waited':'Done', 
+                                                    'preempted':'Aborted'})
 
 
 if __name__ == "__main__":
