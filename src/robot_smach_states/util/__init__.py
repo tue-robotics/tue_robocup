5dcb5b493476b507339e3c37bb4431dd7fa7b0c5
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2014-11-18 22:27:16 +0100

    Removed a whole bunch of load_manifests

--- src/robot_smach_states/util/__init__.py
+++ src/robot_smach_states/util/__init__.py
@@ -1,5 +1,5 @@
 """Several ROS, Python, Smach, TF etc related utility functions"""
-import roslib; roslib.load_manifest('robot_smach_states')
+import roslib; 
 import rospy
 
 from startup import startup, startup_final
