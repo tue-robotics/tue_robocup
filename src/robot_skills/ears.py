a7f9f6c528d1bdd87174d53ffa2c6ce1a67e2100
Rein Appeldoorn <reinzor@gmail.com>
2014-12-03 13:10:08 +0100

    Broke api and implemented new speech recognition, works now :)

--- src/robot_skills/ears.py
+++ src/robot_skills/ears.py
@@ -2,7 +2,8 @@
 import roslib; roslib.load_manifest('robot_skills')
 import rospy
 
-from dragonfly_speech_recognition.srv import *
+from dragonfly_speech_recognition.srv import GetSpeech, GetSpeechRequest
+from dragonfly_speech_recognition.msg import Choice
 
 
 ## TODO: some words are appended like 'applejuice', this should be handled gracefully in some way
@@ -16,29 +17,31 @@ class Ears:
     """
 
     def __init__(self,entries=30):
-        
-        self.speech_recognition_service = rospy.ServiceProxy('dragonfly_speech_recognition/recognize_speech', GetSpeech)
+        rospy.wait_for_service("get_speech_client/get_speech")
+        self.get_speech_client_service = rospy.ServiceProxy('get_speech_client/get_speech', GetSpeech)
 
     #Function listens explained on wiki: http://servicerobot.cstwiki.wtb.tue.nl/index.php?title=Using_the_dragonfly_speech_recognition
-    def listen(self, spec, choices = [], timeout = rospy.Duration(10)):
+    def recognize(self, spec, choices={}, time_out = rospy.Duration(10)):
         req = GetSpeechRequest()
         req.spec = spec
-        req.choices = choices
-        req.time_out = timeout
+        req.choices = [ Choice(id=k, values=v) for k, v in choices.iteritems() ]
+        req.time_out = time_out
 
         answer = None
 
         try:
-            resp = self.speech_recognition_service(req)
-            if resp:
-                answer.result = resp.result
-                for i in range(len(resp.choices)):
-                    answer.choices[resp.choices[i].id] = resp.choices[i].values[0]
+            answer = self.get_speech_client_service(req)
+            print answer
+            if answer:
+                answer.choices = dict((x.id, x.values[0]) for x in answer.choices)
         except rospy.ServiceException as e:
-            rospy.logerr("Could not connect to dragonfly_speech_recognition: " + str(e))
+            rospy.logerr("Service exeption: %s"%e)
+        except:
+            rospy.logerr("Something else went wrong, please notify Rein")
 
         return answer
 
 if __name__ == "__main__":
-    rospy.init_node('amigo_ears_executioner', anonymous=True)
+    rospy.init_node('robot_ears', anonymous=True)
     ears = Ears()
+
