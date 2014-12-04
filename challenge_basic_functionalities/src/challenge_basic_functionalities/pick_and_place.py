ca6916a98f31e98d03f018915794699ac648d0e0
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-12-04 20:26:33 +0100

    New pick and place

--- challenge_basic_functionalities/src/challenge_basic_functionalities/pick_and_place.py
+++ challenge_basic_functionalities/src/challenge_basic_functionalities/pick_and_place.py
@@ -5,6 +5,31 @@ import smach
 
 from robot_smach_states.utility_states import Initialize
 from robot_smach_states.human_interaction import Say
+from robot_smach_states.manip import Grab
+
+# ----------------------------------------------------------------------------------------------------
+
+class SimpleDesignator:
+
+    __init__(self):
+        self.entity_id = None
+
+    def resolve():
+        return self.entity_id
+
+# ----------------------------------------------------------------------------------------------------
+
+class LookForObjects(smach.State):
+   def __init__(self, robot, designator):
+       smach.State.__init__(self,outcomes=['done', 'failed'])
+       self.robot = robot
+       self.designator = designator
+
+   def execute(self, userdata):
+        self.designator.entity_id = "dinner_table"   # TODO
+        return 'done'
+
+# ----------------------------------------------------------------------------------------------------
 
 class PickAndPlace(smach.StateMachine):
 
@@ -12,17 +37,24 @@ class PickAndPlace(smach.StateMachine):
         # ToDo: get rid of hardcode poi lookat
         smach.StateMachine.__init__(self, outcomes=["Done", "Aborted", "Failed"])
         self.robot = robot
+        designator = SimpleDesignator()
 
         with self:
 
             smach.StateMachine.add( 'INIT',
                                     Initialize(robot),
-                                    transitions={"initialized": "PICKUP_OBJECT",
+                                    transitions={"initialized": "LOOK_FOR_OBJECTS",
                                                  "abort":       "Aborted"})
 
+            smach.StateMachine.add( 'LOOK_FOR_OBJECTS',
+                                    LookForObjects(robot, self.designator),
+                                    transitions={"done":   "PICKUP_OBJECT",
+                                                 "failed": "LOOK_FOR_OBJECTS"})
+
             smach.StateMachine.add( 'PICKUP_OBJECT',
-                                    Say(robot, ["Sorry, this task is not yet implemented. Ramon, fix this!"]),
-                                     transitions={   'spoken':'Failed'})
+                                    Grab(robot, self.designator, side="left"),
+                                    transitions={"done":   "Done",
+                                                 "failed": "PICKUP_OBJECT"})
 
 if __name__ == "__main__":
     rospy.init_node('pick_and_place_exec')
