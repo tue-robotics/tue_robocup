7dd8364916b79e7f9ba1b971e8c6e93fb8e05c3b
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-12-04 19:37:37 +0100

    Updated what_did_you_say

--- challenge_basic_functionalities/src/challenge_basic_functionalities/what_did_you_say.py
+++ challenge_basic_functionalities/src/challenge_basic_functionalities/what_did_you_say.py
@@ -6,6 +6,40 @@ import smach
 from robot_smach_states.utility_states import Initialize
 from robot_smach_states.human_interaction import Say
 
+# dictionary of questions and answers
+QA_MAP = { "Sjoerd" : "Is awesome",
+           "Rein" : "Is quite ok",
+           "Janno" : "Is very alright" }
+
+class AnswerQuestion(smach.State):
+
+    def __init__(self, robot, question_nr):
+        smach.State.__init__(self, outcomes = ['done', 'failed'])
+        self.robot = robot
+        self.question_nr = question_nr
+
+    def execute(self, userdate=None):
+
+        self.robot.speak("Please ask question " + str(self.question_nr))
+
+        req = "(" + "|".join(QA_MAP.keys()) + ")"
+
+        res = self.robot.ears.recognize(spec=req)
+
+        if not res:
+            self.robot.speak("I could not hear your question.")
+            return "failed"
+
+        try:
+            q_answer = QA_MAP[res.result]
+        except KeyError:
+            print "[what_did_you_say] Received question is not in map. THIS SHOULD NEVER HAPPEN!"
+            return "failed"
+
+        self.robot.speak("Your question was: " + res.result + ". The answer is: " + q_answer)
+
+        return "done"
+
 class WhatDidYouSay(smach.StateMachine):
 
     def __init__(self, robot, grasp_arm="left"):
@@ -17,12 +51,23 @@ class WhatDidYouSay(smach.StateMachine):
 
             smach.StateMachine.add( 'INIT',
                                     Initialize(robot),
-                                    transitions={"initialized": "ASK_QUESTION",
-                                                 "abort":       "Aborted"})
+                                    transitions={ "initialized": "ASK_QUESTION_1",
+                                                  "abort":       "Aborted"})
 
-            smach.StateMachine.add( 'ASK_QUESTION',
-                                    Say(robot, ["Sorry, this task is not yet implemented. Rein and Erik, fix this!"]),
-                                     transitions={   'spoken':'Failed'})
+            smach.StateMachine.add( 'ASK_QUESTION_1',
+                                    AnswerQuestion(robot, 1),
+                                    transitions={ 'done' : 'ASK_QUESTION_2'},
+                                                  'failed' : 'ASK_QUESTION_1'))
+
+            smach.StateMachine.add( 'ASK_QUESTION_2',
+                                    AnswerQuestion(robot, 1),
+                                    transitions={ 'done' : 'ASK_QUESTION_3'},
+                                                  'failed' : 'ASK_QUESTION_2'))
+
+            smach.StateMachine.add( 'ASK_QUESTION_3',
+                                    AnswerQuestion(robot, 1),
+                                    transitions={ 'done' : 'Done'},
+                                                  'failed' : 'ASK_QUESTION_3'))
 
 if __name__ == "__main__":
     rospy.init_node('what_did_you_say_exec')
