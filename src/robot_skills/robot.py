0d0a944a0da1b019ca1604016fba9c65cbe3e29f
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-12-04 20:57:26 +0100

    Fixed ears include

--- src/robot_skills/robot.py
+++ src/robot_skills/robot.py
@@ -53,28 +53,28 @@ class Robot(object):
 
         # Human Robot Interaction
         self.speech = speech.Speech(wait_service=wait_services)
-        self.ears = ears.Ears()
+        self.ears = ears.Ears(self.robot_name)
         self.ebutton = ebutton.EButton()
         self.lights = lights.Lights()
 
         # Perception: can we get rid of this???
         self.perception = perception_ed.PerceptionED(wait_service=wait_services)
-        
+
         # Reasoning/world modeling
         self.ed = world_model_ed.ED(wait_service=wait_services)
         self.reasoner = reasoner.Reasoner()
-        
+
         # Miscellaneous
         self.pub_target = rospy.Publisher("/target_location", geometry_msgs.msg.Pose2D)
         self.base_link_frame = "/"+self.robot_name+"/base_link"
-    
+
     def publish_target(self, x, y):
         self.pub_target.publish(geometry_msgs.msg.Pose2D(x, y, 0))
-        
+
     def tf_transform_pose(self, ps,frame):
         output_pose = geometry_msgs.msg.PointStamped
         self.tf_listener.waitForTransform(frame, ps.header.frame_id, rospy.Time(), rospy.Duration(2.0))
-        output_pose = self.tf_listener.transformPose(frame, ps) 
+        output_pose = self.tf_listener.transformPose(frame, ps)
         return output_pose
 
     def store_position_knowledge(self, label, dx=0.78, z=0.75, drop_dz=0.13, filename='/tmp/locations.pl'):
@@ -87,7 +87,7 @@ class Robot(object):
             return
 
         env_name = ans_env[0]["Env"]
-        
+
         # Determine base position as (x, y, phi)
         pose = self.base.location
         (pos, quat) = pose.pose.position, pose.pose.orientation
@@ -114,7 +114,7 @@ class Robot(object):
         print poi
 
         dropoff = Compound("dropoff_point", env_name, "Challenge", label, Compound("point_3d", round(ps_MAP.point.x, 3), round(ps_MAP.point.y, 3), round(ps_MAP.point.z + drop_dz, 3)))
-        print dropoff        
+        print dropoff
 
         with open(filename, "a") as myfile:
             myfile.write(str(base_pose) + ".\n")
@@ -189,7 +189,7 @@ class Robot(object):
     def get_base_goal_poses(self, target_point_stamped, x_offset, y_offset, cost_threshold_norm=0.2):
 
         request = amigo_inverse_reachability.srv.GetBaseGoalPosesRequest()
-        
+
         request.robot_pose = self.base.location.pose
 
         request.target_pose.position = target_point_stamped.point
@@ -199,17 +199,17 @@ class Robot(object):
         request.target_pose.orientation.w = 1
 
         request.cost_threshold_norm = cost_threshold_norm
-        
+
         request.x_offset = x_offset
         request.y_offset = y_offset
         rospy.logdebug("Inverse reachability request = {0}".format(request).replace("\n", " ").replace("\t", " "))
         response = self._get_base_goal_poses(request)
         rospy.logdebug("Inverse reachability response = {0}".format(response).replace("\n", " ").replace("\t", " "))
-        
+
         ## Only get poses that are in the same room as the grasp point.
-        
+
         rooms_dimensions = self.reasoner.query(Compound("room_dimensions", "Room", Compound("size", "Xmin", "Ymin", "Zmin", "Xmax", "Ymax", "Zmax")))
-        
+
         if rooms_dimensions:
             for x in range(0,len(rooms_dimensions)):
                 room_dimensions = rooms_dimensions[x]
@@ -238,7 +238,7 @@ class Robot(object):
                     base_goal_poses.append(geometry_msgs.msg.PoseStamped())
                     base_goal_poses[-1].header.frame_id = "/map"
                     base_goal_poses[-1].header.stamp = rospy.Time()
-                    base_goal_poses[-1].pose = base_goal_pose            
+                    base_goal_poses[-1].pose = base_goal_pose
                 # else:
                     # print "point deleted\n"
             # print "AFTER base_goal_poses length = ", len(base_goal_poses), "\n"
