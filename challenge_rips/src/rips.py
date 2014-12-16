af07c3209f52e0788fd2070869e9fe024de5dbbf
Rein Appeldoorn <reinzor@gmail.com>
2014-12-16 22:29:39 +0100

    cleanup

--- challenge_rips/src/rips.py
+++ challenge_rips/src/rips.py
@@ -2,93 +2,16 @@
 import roslib;
 import rospy
 import smach
-
-from robot_skills.amigo import Amigo
-from robot_skills.sergio import Sergio
-import robot_smach_states as states
-from robot_smach_states.util.startup import startup
-from geometry_msgs.msg import Point
-
-from robot_smach_states.navigation import NavigateToWaypoint
-from robot_smach_states.navigation import NavigateToObserve
-
-from speech_interpreter.srv import AskUser
-
-from psi import *
-import robot_skills.util.msg_constructors as msgs
-
 import sys
 
-from robot_smach_states.designators.designator import Designator, VariableDesignator
-
-
-###########################
-# Created by: Erik Geerts #
-###########################
-
-##########################################
-############## What to run: ##############
-##########################################
-# - see README file
-
-#############################################################
-## Locations that must be defined in database on forehand: ##
-##################### updated 16-4-2013 #####################
-#############################################################
-# - initial
-# - registration_table
-# - exit
-
-##########################################
-############### TODO list: ###############
-##########################################
-# -  Instead of having a goal pose, a goal area should be enough, so that it will drive as close as possible to the goal position.
-#    In this challenge this would be handy, since amigo will then introduce itself near the table instead of outside the door.
-# -  In case an obstacle is detected, altough it is not there and amigo is not looking in that direction, a path cannot be found
-#    if this 'obstacle' is blocking the way. A solution might be that Amigo is forced to look in the direction of the goal pose
-#    if no solution is found at the first try.
-
-
-#class AmigoIntroductionRIPS(smach.State):
-#    def __init__(self, robot=None):
-#        smach.State.__init__(self, outcomes=['finished'])
-#
-#        self.robot = robot
-#
-#    def execute(self, userdata):
-#        rospy.loginfo("Introducing AMIGO")
-#
-#        self.robot.head.reset_position()
-#
-#        self.robot.speech.speak("Hello, my name is amigo")
-#        rospy.sleep(1.0)
-#        self.robot.speech.speak("I am participating in robocup 2014 on behalf of Tech United Eindhoven")
-#        self.robot.speech.speak("If you want me to stop, you can press my emergency button on my back")
-#        self.robot.speech.speak("Thank you for your attention, I will now leave the arena")
-#
-#        return 'finished'
+from robot_smach_states.designators.designator import Designator
+import robot_smach_states as states
 
 def setup_statemachine(robot):
-
-    #retract old facts
-    #TODO: maybe retract more facts like other challenges?
-    robot.reasoner.query(Compound("retractall", Compound("challenge", "X")))
-
-    #Load database
-    robot.reasoner.query(Compound("load_database","tue_knowledge",'prolog/locations.pl'))
-
-    #Assert the current challenge.
-    robot.reasoner.query(Compound("assertz",Compound("challenge", "registration")))
 
     sm = smach.StateMachine(outcomes=['Done','Aborted'])
 
     with sm:
-
-        #smach.StateMachine.add('INITIALIZE_FIRST',
-        #                        states.Initialize(robot),
-        #                        transitions={   'initialized':'START_CHALLENGE_ROBUST',
-        #                                        'abort':'Aborted'})
-
 
         # Start challenge via StartChallengeRobust
         smach.StateMachine.add( "START_CHALLENGE_ROBUST",
@@ -97,10 +20,6 @@ def setup_statemachine(robot):
                                                     "Aborted":"GO_TO_INTERMEDIATE_WAYPOINT",
                                                     "Failed":"GO_TO_INTERMEDIATE_WAYPOINT"})   # There is no transition to Failed in StartChallengeRobust (28 May)
 
-        #smach.StateMachine.add("SAY_START_CHALLENGE",
-        #                            states.Say(robot, "Hello, I am Amigo, human cyborg relations", block=False),
-        #                            transitions={   "spoken":"GO_TO_INTERMEDIATE_WAYPOINT"})
-
         smach.StateMachine.add('GO_TO_INTERMEDIATE_WAYPOINT',
                                     states.NavigateToObserve(robot, Designator("dinner_table"), radius=0.7),
                                     transitions={   'arrived':'ASK_CONTINUE',
@@ -108,7 +27,7 @@ def setup_statemachine(robot):
                                                     'goal_not_defined':'ASK_CONTINUE'})
 
         smach.StateMachine.add("ASK_CONTINUE",
-                        states.human_interaction.AskContinue(robot),
+                        states.AskContinue(robot),
                         transitions={   'continue':'SAY_CONTINUEING',
                                         'no_response':'SAY_CONTINUEING'})
 
@@ -140,4 +59,4 @@ if __name__ == '__main__':
         print "[CHALLENGE RIPS] Please provide robot name as argument."
         exit(1)
 
-    startup(setup_statemachine, robot_name=robot_name)
+    states.util.startup(setup_statemachine, robot_name=robot_name)
