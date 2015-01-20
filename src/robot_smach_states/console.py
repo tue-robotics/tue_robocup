5340dbf4acfef77f355a8e905ce2e4e500e70eb6
Ramon Wijnands <rayman747@hotmail.com>
2015-01-20 14:59:02 +0100

    rewrite console.py to use docopt and dynamic loading

--- src/robot_smach_states/console.py
+++ src/robot_smach_states/console.py
@@ -1,43 +1,63 @@
 #!/usr/bin/ipython -i
 
-# ROS
-import roslib; roslib.load_manifest('robot_console')
-import rospy
+"""Robot console
+
+Usage:
+  robot-console <robot>...
+  robot-console <robot>... [--part=<parts>]
+
+Examples:
+  robot-console amigo
+  robot-console sergio --part=arms,head
+  robot-console amigo sergio
+"""
+
 import sys
+import importlib
+import rospy
+from docopt import docopt, DocoptExit
 
-# Robot interfaces
-from robot_skills import robot, amigo, sergio
-
-# State machines
+# load state machines
 import robot_smach_states as state_machine
 
-def loadRobot(robot_name):
-    if robot_name == "amigo":
-        globals()[robot_name] = amigo.Amigo(wait_services=False)
-    elif robot_name == "sergio":
-        globals()[robot_name] = sergio.Sergio(wait_services=False)
-    else:
-        return None
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
 
-    print """\033[92m
-    Succesfully loaded robot '%s'\033[1;m"""%robot_name
+def load_robot(robot_name, parts=None):
+    try:
+        module = importlib.import_module('robot_skills.' + robot_name)
+        robot = module.__getattribute__(robot_name.title())(wait_services=False)
+    except (ImportError, AttributeError) as e:
+        msg = '\nrobot "%s" could not be found!!!\n' % robot_name
+        msg += '\nException:\n' + str(e) + '\n'
+        raise DocoptExit(msg)
 
-if __name__ == "__main__":
-    rospy.init_node("robot_console", anonymous=True)
+    # register as global
+    globals()[robot_name] = robot
 
-    print """
-    \033[1;33m-------------------------------------------------------------------
-    | TU/e Robot Console - version 0.1                                |
-    |                                                                 |
-    | use loadRobot([robot_name]) to load robot ; available robots:   |
-    |    - amigo                                                      |
-    |    - sergio                                                     |
-    |                                                                 |
-    | Robots can be automatically loaded with arguments.              |
-    -------------------------------------------------------------------\033[1;m"""
+    print bcolors.OKGREEN+'\tSuccesfully loaded robot "%s"' % robot_name +bcolors.ENDC
 
-    for arg in sys.argv:
-        loadRobot(arg)
+if __name__ == '__main__':
+    try:
+        arguments = docopt(__doc__)
+        rospy.init_node("robot_console", anonymous=True)
 
-    print """\033[92m
-    Succesfully loaded statemachines  as 'state_machine'\033[1;m"""
+        print """
+        \033[1;33m-------------------------------------------------------------------
+        | TU/e Robot Console - version 0.1                                |
+        -------------------------------------------------------------------\033[1;m"""
+
+        print bcolors.OKGREEN+'\tSuccesfully loaded statemachines as "state_machine"'+bcolors.ENDC
+
+        parts = arguments['--part']
+        for robot in arguments['<robot>']:
+            load_robot(robot, parts=parts)
+
+    except DocoptExit as e:
+        print bcolors.FAIL + str(e) + bcolors.ENDC
+        exit() # quit ipython
