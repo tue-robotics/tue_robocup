470580de78955e7713912f0ecd4d98c5b198346e
Erik Geerts <e.g.geerts@live.nl>
2015-01-20 21:06:20 +0100

    updated questions

--- challenge_basic_functionalities/src/challenge_basic_functionalities/what_did_you_say.py
+++ challenge_basic_functionalities/src/challenge_basic_functionalities/what_did_you_say.py
@@ -8,9 +8,31 @@ from robot_smach_states.human_interaction import Say
 from dragonfly_speech_recognition.msg import Choice
 
 # dictionary of questions and answers
-QA_MAP = { "Sjoerd" : "Is awesome",
-           "Rein" : "Is quite ok",
-           "Janno" : "Is very alright" }
+QA_MAP = { "what time is it" : "time to buy a watch",
+           "what is the capital of germany" : "berlin",
+           "what is the heaviest animal in the world" : "Is quite ok",
+           "who is the president of america" : "barack obama",
+           "what is your name" : "amigo",
+           "who is your example" : "erik geerts",
+           "what is your motto" : "yolo",
+           "which football club is the best" : "feyenoord",
+           "who is the best looking person around here" : "erik geerts of course",
+           "which town has been bombed" : "schijndel",
+           "which person is not able to say yes" : "dirk holtz",
+           "what is the capital of brazil" : "brasilia",
+           "what is the oldest drug used on earth" : "alcohol",
+           "in which year was robocup founded" : "nineteen ninety seven",
+           "how many rings has the olympic flag" : "five",
+           "what is the worlds most popular green vegetable" : "lettuce",
+           "which insect has the best eyesight" : "dragonfly",
+           "who lives in a pineapple under the sea" : "spongebob squarepants",
+           "what is your teams name" : "tech united eindhoven",
+           "what is the answer to the ultimate question about life the universe and everything" : "forty two",
+           "what is the capital of poland" : "warsaw",
+           "which country grows the most potatoes" : "russia",
+           "which country grew the first orange" : "china",
+           "how many countries are in europe" : "fifty",
+           "which fish can hold objects in its tail" : "sea horse"}
 
 class AnswerQuestion(smach.State):
 
@@ -20,6 +42,10 @@ class AnswerQuestion(smach.State):
         self.question_nr = question_nr
 
     def execute(self, userdate=None):
+
+        print "------- POSSIBLE QUESTIONS -------"
+        for x in range(0, len(QA_MAP)):
+            print "Question", 1+x,":", QA_MAP.keys()[x],"?"
 
         self.robot.speech.speak("Please ask question " + str(self.question_nr))
 
