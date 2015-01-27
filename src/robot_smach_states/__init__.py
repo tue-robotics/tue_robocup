35991aafb994db7badfba62cdbdc90f1965fbe86
Ramon Wijnands <rayman747@hotmail.com>
2015-01-27 20:53:17 +0100

    also import manip.* flat

--- src/robot_smach_states/__init__.py
+++ src/robot_smach_states/__init__.py
@@ -2,11 +2,10 @@ from drive_to_person import *
 from highlevel import *
 from human_interaction import *
 from introduction import *
+from manip import *
 from manipulation import *
 from motion_sequences import *
 from navigation import *
 from perception import *
 from reasoning import *
 from utility_states import *
-
-#__all__ = ["human_interaction", "manipulation", "navigation", "utility_states", "perception", "follow_me_states", "motion_sequences"]
