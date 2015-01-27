f77ead796231275ae843b7aa62d2eac9b166a065
Rokus Ottervanger <rokusottervanger@gmail.com>
2015-01-27 22:20:10 +0100

    Added second waypoint to challenge navigation and added test file

--- challenge_navigation/src/challenge_navigation.py
+++ challenge_navigation/src/challenge_navigation.py
@@ -54,7 +54,7 @@ def setup_statemachine(robot):
                                 transitions={   'spoken'            :   'GOTO_WAYPOINT_B'})
 
         smach.StateMachine.add('GOTO_WAYPOINT_B',
-                                states.NavigateToObserve(robot, Designator("dinner_table"), radius=0.7),
+                                states.NavigateToObserve(robot, Designator("operator_table1"), radius=0.7),
                                 transitions={   'arrived'           :   'SAY_WAYPOINT_B_REACHED',
                                                 'unreachable'       :   'SAY_WAYPOINT_B_FAILED',
                                                 'goal_not_defined'  :   'SAY_WAYPOINT_B_FAILED'})
--- challenge_navigation/test/test_challenge_navigation.py
+++ challenge_navigation/test/test_challenge_navigation.py
@@ -7,10 +7,10 @@ from fast_simulator import client
 if __name__ == "__main__":
     rospy.init_node('fast_simulator_object_spawner')
 
+
     W = client.SimWorld()
-
-    W.add_object("coke-1", "coke",  0.856, 3.671, 0.8)
-    W.add_object("coke-2", "coke",  0.476, 3.659, 0.8)
-    W.add_object("coke-3", "coke", -0.003, 3.663, 0.8)
-    W.add_object("coke-4", "coke",  0.676, 1.906, 0.9)
-
+    # import ipdb; ipdb.set_trace()
+    W.add_object("table-1", "table",  1.0, 1.0, 0.0)
+    #W.add_object("coke-2", "coke",  0.476, 3.659, 0.8)
+    #W.add_object("coke-3", "coke", -0.003, 3.663, 0.8)
+    #W.add_object("coke-4", "coke",  0.676, 1.906, 0.9)
