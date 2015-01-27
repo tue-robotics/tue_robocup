5f9ca4c240df4cc8cb04195e5a2ce7f6d350735e
Ramon Wijnands <rayman747@hotmail.com>
2015-01-27 21:16:53 +0100

    manip overwrites manipulation

--- src/robot_smach_states/__init__.py
+++ src/robot_smach_states/__init__.py
@@ -2,8 +2,8 @@ from drive_to_person import *
 from highlevel import *
 from human_interaction import *
 from introduction import *
-from manip import *
 from manipulation import *
+from manip import *
 from motion_sequences import *
 from navigation import *
 from perception import *
