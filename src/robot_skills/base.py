d42290fe406f665d9fe15cf82caf1501d39727bf
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2015-02-05 14:23:54 +0100

    Slight changes in nav analyzer

--- src/robot_skills/base.py
+++ src/robot_skills/base.py
@@ -38,7 +38,8 @@ class LocalPlanner():
         goal.plan = plan
         goal.orientation_constraint = orientation_constraint
         self._orientation_constraint = orientation_constraint
-        self._action_client.send_goal(goal, done_cb = self.__doneCallback, feedback_cb = self.__feedbackCallback) 
+        self.analyzer.count_plan(plan[0], plan[-1], 0.0, computePathLength(plan))
+        self._action_client.send_goal(goal, done_cb = self.__doneCallback, feedback_cb = self.__feedbackCallback)
         self._goal_handle = self._action_client.gh
         rospy.loginfo("Goal handle = {0}".format(self._goal_handle))
         self.__setState("controlling", None, None, plan)
@@ -115,11 +116,11 @@ class GlobalPlanner():
 
         end_time = rospy.Time.now()
         plan_time = (end_time-start_time).to_sec()
-        
+
         path_length = self.computePathLength(resp.plan)
-        
-        if path_length > 0:
-            self.analyzer.count_plan(resp.plan[0], resp.plan[-1], plan_time, path_length)
+
+        #if path_length > 0:
+        #    self.analyzer.count_plan(resp.plan[0], resp.plan[-1], plan_time, path_length)
 
         return resp.plan
 
@@ -136,6 +137,7 @@ class GlobalPlanner():
         return self.position_constraint
 
     def computePathLength(self, path):
+        #rospy.logwarn("Please use the other computepathlength")
         distance = 0.0
         for index, pose in enumerate(path):
             if not index == 0:
@@ -152,7 +154,7 @@ class Base(object):
         self._robot_name = robot_name
         self._cmd_vel = rospy.Publisher('/' + self._robot_name + '/base/references', Twist, queue_size=10)
         self._initial_pose_publisher = rospy.Publisher('/' + self._robot_name + '/initialpose', PoseWithCovarianceStamped, queue_size=10)
-        
+
         self.analyzer = nav_analyzer.NavAnalyzer(self._robot_name)
 
         # The plannners
@@ -172,13 +174,13 @@ class Base(object):
         return plan
 
     def force_drive(self, vx, vy, vth, timeout):
-        
+
         # Cancel the local planner goal
         self.local_planner.cancelCurrentPlan()
-        
+
         v = Twist()        # Initialize velocity
         t_start = rospy.Time.now()
-        
+
         # Drive
         v.linear.x = vx
         v.linear.y = vy
@@ -186,35 +188,35 @@ class Base(object):
         while (rospy.Time.now() - t_start) < rospy.Duration(timeout):
             self._cmd_vel.publish(v)
             rospy.sleep(0.1)
-        
+
         # Stop driving
         v.linear.x = 0.0
         v.linear.y = 0.0
         v.angular.z= 0.0
         self._cmd_vel.publish(v)
-        
+
         return True
 
     def get_location(self):
         return get_location(self._robot_name, self._tf_listener)
-        
+
     def set_initial_pose(self, x, y, phi):
 
         initial_pose = geometry_msgs.msg.PoseWithCovarianceStamped()
-        
+
         initial_pose.header.frame_id = "/map"
-        
+
         initial_pose.pose.pose.position.x = x
         initial_pose.pose.pose.position.y = y
         initial_pose.pose.pose.position.z = 0.0
-        initial_pose.pose.pose.orientation = transformations.euler_z_to_quaternion(phi) 
+        initial_pose.pose.pose.orientation = transformations.euler_z_to_quaternion(phi)
         initial_pose.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
         #rospy.loginfo("initalpose = {0}".format(initial_pose))
 
         self._initial_pose_publisher.publish(initial_pose)
 
-        return True 
+        return True
 
     ########################################################
     ###### Are the following functions deprecated ??? ######
@@ -243,10 +245,10 @@ def get_location(robot_name, tf_listener):
         time = rospy.Time.now()
         tf_listener.waitForTransform("/map", "/" + robot_name + "/base_link", time, rospy.Duration(20.0))
         (ro_trans, ro_rot) = tf_listener.lookupTransform("/map", "/" + robot_name + "/base_link", time)
-        
+
         position = geometry_msgs.msg.Point()
         orientation = geometry_msgs.msg.Quaternion()
-        
+
         position.x = ro_trans[0]
         position.y = ro_trans[1]
         orientation.x = ro_rot[0]
@@ -258,11 +260,18 @@ def get_location(robot_name, tf_listener):
         target_pose.header.frame_id = "/map"
         target_pose.header.stamp = time
         return target_pose
-    
+
     except (tf.LookupException, tf.ConnectivityException):
-        rospy.logerr("tf request failed!!!")        
+        rospy.logerr("tf request failed!!!")
         target_pose =  geometry_msgs.msg.PoseStamped(pose=geometry_msgs.msg.Pose(position=position, orientation=orientation))
         target_pose.header.frame_id = "/map"
         return target_pose
 
-
+def computePathLength(path):
+    distance = 0.0
+    for index, pose in enumerate(path):
+        if not index == 0:
+            dx = path[index].pose.position.x - path[index-1].pose.position.x
+            dy = path[index].pose.position.y - path[index-1].pose.position.y
+            distance += math.sqrt( dx*dx + dy*dy)
+    return distance
