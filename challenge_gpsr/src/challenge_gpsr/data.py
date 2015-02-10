6dee7ef9d8f8d54fb0d263cef1f8e7c030ed69b4
Rein Appeldoorn <reinzor@gmail.com>
2015-02-10 21:55:22 +0100

    Added separated data file

---
+++ challenge_gpsr/src/challenge_gpsr/data.py
@@ -0,0 +1,12 @@
+objects_aeuoi = ["orange juice"]
+objects_rest = ["beer", "milk", "sevenup"]
+objects = objects_aeuoi + objects_rest
+object_types_plural = ["drinks","snacks","cleaningstuff", "food"]
+object_types_singular = ["drink","snack"]
+object_types = object_types_plural + object_types_singular
+
+locations_aeuoi = ["appliance"]
+locations_rest = ["bed", "shelf", "table", "trashbin", "bin"]
+locations = locations_aeuoi + locations_rest
+rooms = ["kitchen", "hallway", "livingroom", "bedroom", "corridor"]
+persons = ["me","a person", "anna", "beth", "carmen", "jennifer", "jessica","kimberly", "kristina", "laura", "mary", "sarah", "alfred", "charles", "daniel", "james", "john", "luis", "paul", "richard", "robert", "steve"]
