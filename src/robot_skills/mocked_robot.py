460abb27fa66dee9604b16b4c53831c5733dfa0f
Loy van Beek <loy.vanbeek@gmail.com>
2015-02-14 16:33:06 +0100

    TODO: Place makes use of robot.spindle which Amigo aliases to .torso. Should that be used directly?

--- src/robot_skills/mocked_robot.py
+++ src/robot_skills/mocked_robot.py
@@ -190,6 +190,7 @@ class Mockbot(object):
         # Body parts
         self.base = Base()
         self.torso = Torso()
+        self.spindle = self.torso
         self.leftArm = Arm(self.robot_name, self.tf_listener, "left")
         self.rightArm = Arm(self.robot_name, self.tf_listener, "right")
         self.arms = {"left":self.leftArm, "right":self.rightArm}
