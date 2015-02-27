9127a8484859797188a62b6463cf44e78d50501f
Loy van Beek <loy.vanbeek@gmail.com>
2015-02-27 19:04:21 +0100

    Fixed usage of send arm goal

--- challenge_robo_zoo/src/challenge_robo_zoo_simple/macarana.py
+++ challenge_robo_zoo/src/challenge_robo_zoo_simple/macarana.py
@@ -10,29 +10,29 @@ from musicmanager import music
 
 #0. Arms are in reset pose
 #1. Right arm straight forward and hand up
-arm_straight_1 =                [0.000, 1.500, 0.000, 0.000, 0.000, 0.000, 0.000]
+arm_straight_1 =                [[0.000, 1.500, 0.000, 0.000, 0.000, 0.000, 0.000]]
 #2. Move up and down (spindle)
 #3. Turn the right hand upside down,  move the left arm up as wel
-arm_straight_2 =                [0.000, 1.500, 1.500, 0.000, 1.500, 0.000, 0.000]
+arm_straight_2 =                [[0.000, 1.500, 1.500, 0.000, 1.500, 0.000, 0.000]]
 #4. Right hand to the left shoulder
-right_hand_to_left_shoulder =   [0.000, 1.500, 1.200, 1.500, 1.500, 0.000, 0.000]
+right_hand_to_left_shoulder =   [[0.000, 1.500, 1.200, 1.500, 1.500, 0.000, 0.000]]
 #5. Left hand to right shoulder
-left_hand_to_right_shoulder =   [0.000, 1.500, 1.000, 1.000, 0.000, 0.000, 0.000]
+left_hand_to_right_shoulder =   [[0.000, 1.500, 1.000, 1.000, 0.000, 0.000, 0.000]]
 #6. Right arm to head
-right_arm_to_head_1 =           [0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]
-right_arm_to_head_2 =           [0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]
+right_arm_to_head_1 =           [[0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]]
+right_arm_to_head_2 =           [[0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]]
 #6a. Left arm to head
-left_arm_to_head_1 =            [0.000, 1.290, 1.500, 1.300, 1.500, 0.000, 0.000]
-left_arm_to_head_2 =            [0.000, 1.500, 1.500, 1.300, 1.500, 0.000, 0.000]
-left_arm_to_head_3 =            [0.000, 1.500, 0.000, 1.300, 1.500, 0.000, 0.000]
-left_arm_to_head_4 =            [0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]
+left_arm_to_head_1 =            [[0.000, 1.290, 1.500, 1.300, 1.500, 0.000, 0.000]]
+left_arm_to_head_2 =            [[0.000, 1.500, 1.500, 1.300, 1.500, 0.000, 0.000]]
+left_arm_to_head_3 =            [[0.000, 1.500, 0.000, 1.300, 1.500, 0.000, 0.000]]
+left_arm_to_head_4 =            [[0.000, 1.500, 0.000, 1.500, 1.500, 0.500, 0.000]]
 #7. Right arm down to the hips
-arm_to_hips_1 =                 [-0.200, -0.200, 0.200, 0.800, 0.000, 0.000, 0.000] 
-arm_to_hips_2 =                 [-0.400, 0.000, 1.500, 0.600, 0.000, 0.000, 0.000]
-arm_to_hips_3 =                 [-0.400, 0.000, 1.500, 0.000, 0.000, 0.000, 0.000]
-arm_to_hips_4 =                 [-0.400, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000]
+arm_to_hips_1 =                 [[-0.200, -0.200, 0.200, 0.800, 0.000, 0.000, 0.000] ]
+arm_to_hips_2 =                 [[-0.400, 0.000, 1.500, 0.600, 0.000, 0.000, 0.000]]
+arm_to_hips_3 =                 [[-0.400, 0.000, 1.500, 0.000, 0.000, 0.000, 0.000]]
+arm_to_hips_4 =                 [[-0.400, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000]]
 
-zero =                          [-0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000]
+zero =                          [[-0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000]]
 #7a. Left arm down to the hips
 
 import threading
@@ -63,44 +63,44 @@ def macarena(robot):
     #up_and_down_head.start()
     robot.head.send_goal(msgs.PointStamped(0.2, 0, 1.3, frame_id="/amigo/base"), keep_tracking=True)
     
-    def _left(*args, **kwargs): #The underscore  makes the outlining below easier to read
-        if not robot.leftArm.send_joint_goal(*args, **kwargs):
-            raise Exception("Arms dit not reach goal,  need help")
+    def _left(trajectory, timeout=10.0): #The underscore  makes the outlining below easier to read
+        if not robot.leftArm._send_joint_trajectory(trajectory):
+            raise Exception("Arms did not reach goal,  need help")
     
-    def right(*args, **kwargs): 
-        if not robot.rightArm.send_joint_goal(*args, **kwargs):
-            raise Exception("Arms dit not reach goal,  need help")
+    def right(trajectory, timeout=10.0): 
+        if not robot.rightArm._send_joint_trajectory(trajectory):
+            raise Exception("Arms did not reach goal,  need help")
     #Defined shortcuts above
 
     try:
         for i in range(1):
-            right([[arm_straight_1]], timeout=rospy.Duration(10))
-            _left([[arm_straight_1]], timeout=rospy.Duration(10))
+            right(arm_straight_1, timeout=rospy.Duration(10))
+            _left(arm_straight_1, timeout=rospy.Duration(10))
 
-            right([[arm_straight_2]], timeout=rospy.Duration(5))
-            _left([[arm_straight_2]], timeout=rospy.Duration(5))
+            right(arm_straight_2, timeout=rospy.Duration(5))
+            _left(arm_straight_2, timeout=rospy.Duration(5))
 
-            right([[right_hand_to_left_shoulder]], timeout=rospy.Duration(10))
-            _left([[left_hand_to_right_shoulder]], timeout=rospy.Duration(10))
+            right(right_hand_to_left_shoulder, timeout=rospy.Duration(10))
+            _left(left_hand_to_right_shoulder, timeout=rospy.Duration(10))
 
-            right([[right_arm_to_head_1]], timeout=rospy.Duration(10)) 
-            #right(*right_arm_to_head_2, timeout=rospy.Duration(10)) 
+            right(right_arm_to_head_1, timeout=rospy.Duration(10))
+            #right(*right_arm_to_head_2,] timeout=rospy.Duration(10))
 
-            #_left(*left_arm_to_head_1, timeout=rospy.Duration(5)) #?
-            #_left(*left_arm_to_head_2, timeout=rospy.Duration(5)) #?
-            #_left(*left_arm_to_head_3, timeout=rospy.Duration(5))
-            _left([[left_arm_to_head_4]], timeout=rospy.Duration(5))
+            #_left(*left_arm_to_head_1) ], timeout=rospy.Duration(5))
+            #_left(*left_arm_to_head_2) ], timeout=rospy.Duration(5))
+            #_left(*left_arm_to_head_,] timeout=rospy.Duration(5))
+            _left(left_arm_to_head_4, timeout=rospy.Duration(5))
 
-            right([[arm_to_hips_1]], timeout=rospy.Duration(10))
-            _left([[arm_to_hips_1]], timeout=rospy.Duration(10))
-            right([[arm_to_hips_2]], timeout=rospy.Duration(5))
-            _left([[arm_to_hips_2]], timeout=rospy.Duration(5))
-            right([[arm_to_hips_3]], timeout=rospy.Duration(5))
-            _left([[arm_to_hips_3]], timeout=rospy.Duration(5))
-            right([[arm_to_hips_4]], timeout=rospy.Duration(5))
-            _left([[arm_to_hips_4]], timeout=rospy.Duration(5))
-            right([[arm_to_hips_1]], timeout=rospy.Duration(15))
-            _left([[arm_to_hips_1]], timeout=rospy.Duration(15))
+            right(arm_to_hips_1, timeout=rospy.Duration(10))
+            _left(arm_to_hips_1, timeout=rospy.Duration(10))
+            right(arm_to_hips_2, timeout=rospy.Duration(5))
+            _left(arm_to_hips_2, timeout=rospy.Duration(5))
+            right(arm_to_hips_3, timeout=rospy.Duration(5))
+            _left(arm_to_hips_3, timeout=rospy.Duration(5))
+            right(arm_to_hips_4, timeout=rospy.Duration(5))
+            _left(arm_to_hips_4, timeout=rospy.Duration(5))
+            right(arm_to_hips_1, timeout=rospy.Duration(15))
+            _left(arm_to_hips_1, timeout=rospy.Duration(15))
 
         stopEvent.set()
         up_and_down_spindle.join()
