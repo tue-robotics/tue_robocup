1e2ea8c2f21a901d89c921cd8e16134573671fcc
Rein Appeldoorn <reinzor@gmail.com>
2015-02-27 16:28:37 +0100

    fixed statemachine symbolic nav

--- src/robot_smach_states/navigation/navigate_to_symbolic.py
+++ src/robot_smach_states/navigation/navigate_to_symbolic.py
@@ -11,20 +11,20 @@ import rospy
 # ----------------------------------------------------------------------------------------------------
 
 class NavigateToSymbolic(NavigateTo):
-    def __init__(self, robot, entity_designator, area_name):
+    def __init__(self, robot, entity_designator_area_name_map, entity_lookat_designator):
         super(NavigateToSymbolic, self).__init__(robot)
 
         self.robot                 = robot
-        self.entity_designator     = entity_designator
-        self.area_name             = area_name
+        self.entity_designator_area_name_map     = entity_designator_area_name_map
+        self.entity_lookat_designator = entity_lookat_designator
 
     def generateConstraint(self):
-        entity_id = self.entity_designator.resolve().id
-
         ''' PositionConstraint '''
-        pc = self.robot.ed.navigation.get_position_constraint(entity_id, self.area_name)
+        entity_id_area_name_map = { k.resolve().id: v for k,v self.entity_designator_area_name_map.iteritems() }
+        pc = self.robot.ed.navigation.get_position_constraint(entity_id_area_name_map)
 
         ''' Orientation constraint is the entity itself...'''
-        oc = OrientationConstraint(frame=entity_id)
+        entity_lookat_id = self.entity_lookat_designator.resolve().id
+        oc = OrientationConstraint(frame=entity_lookat_id)
 
         return pc, oc
