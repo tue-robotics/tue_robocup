4071819cf76d00ffaffc80573e8440185e61b57b
Erik Geerts <e.g.geerts@live.nl>
2015-02-27 20:58:28 +0100

    data.py works now on amigo again

--- challenge_gpsr/src/challenge_gpsr/data.py
+++ challenge_gpsr/src/challenge_gpsr/data.py
@@ -15,28 +15,21 @@ persons_women = ["alfred", "charles", "daniel", "james", "john", "luis", "paul",
 persons_general = ["me","a person"] 
 persons = persons_general + persons_men + persons_women
 
-# spec = """ ((<1_action> to (((a|the) <1_locations_rest>)|((an|the) <1_locations_aeuoi>)|(the <1_locations_rooms>))) 
-#         ((((<2_action_person> <2_person>)|(<2_action> ((the <2_objects_plural>)|(an <2_objects_aeuoi>)|(a <2_objects_rest_singular>))))
-#             (and (<3_action_person>|(<3_action> (them|it|) to (the <3_location>|<3_person>)))))
-# 		|((<2_action_count> the <2_objects_types_plural>) and (<3_action_report> to <3_person>))))"""
-
-
 spec123_1 = "(<1_action> to the <1_locations_rooms> <2_action_person> ((<2_person_men> and (<3_action_follow_men>|<3_action_interact>))|(<2_person_women> and (<3_action_follow_women>|<3_action_interact>))|(<2_person_me> and (<3_action_follow_me>|<3_action_interact>))|(<2_person_general> and <3_action_interact>)))"
 
 spec1 = "(<1_action> to (((a|the) <1_locations_rest>)|((an|the) <1_locations_aeuoi>)|(the <1_locations_rooms>)) )"
-spec23_1 = "((<2_action> ((the <2_objects_plural>)|(an <2_objects_aeuoi>)|(a <2_objects_rest_singular>))) and <3_action> (them|it|) to (the <3_location>|<3_person>))"
+spec23_1 = "((<2_action> ((the <2_objects_plural>)|(an <2_objects_aeuoi>)|(a <2_objects_rest_singular>))) (and <3_action> (them|it) to ((the <3_location>)|<3_person_me>)))"
 spec23_2 = "(<2_action_count> the <2_objects_types_plural> and <3_action_report> to me)"
 
 spec = "("+spec123_1+"|("+spec1+"("+spec23_1+"|"+spec23_2+")))"
 
-
-choices = {"1_action":["go","navigate","move","advance"],
+choices = {"1_action":["go","navigate","move","advance"],	
  "1_locations_aeuoi":locations_aeuoi,
  "1_locations_rest":locations_rest,
  "1_locations_rooms":rooms,
  "2_action_person": ["find"],
  "2_person_men":persons_men,
- "2_person_women":persons_men,
+ "2_person_women":persons_women,
  "2_person_me":["me"],
  "2_person_general":["a person"],
  "2_action":["grasp","get","take","find"],
@@ -52,23 +45,4 @@ choices = {"1_action":["go","navigate","move","advance"],
  "3_action":["take","move","bring"],
  "3_location":locations + rooms,
  "3_action_report":["report",],
- "3_person":persons}
-
- # choices = {"1_action":["go","navigate","move","advance"],
- # "1_locations_aeuoi":locations_aeuoi,
- # "1_locations_rest":locations_rest,
- # "1_locations_rooms":rooms,
- # "2_action_person": ["find"],
- # "2_person":persons,
- # "2_action":["grasp","get","take","find"],
- # "2_action_count":["count"],
- # "2_objects_plural":objects + object_types_plural,
- # "2_objects_types_plural":object_types_plural,
- # "2_objects_aeuoi":objects_aeuoi,
- # "2_objects_rest_singular":objects_rest + object_types_singular,
- # "3_action_person":["tell the time", "follow her","follow him"],
- # "3_action":["take","move","bring"],
- # "3_location":locations + rooms,
- # "3_action_report":["report"],
- # "3_person":persons}
-
+ "3_person_me":["me"]}
