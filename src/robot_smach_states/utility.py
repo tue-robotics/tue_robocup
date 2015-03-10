ef0300e85f4a9af7f4554a3674f2ba22471132c7
Loy van Beek <loy.vanbeek@gmail.com>
2015-03-10 22:57:25 +0100

    Added state to publish marker for entity

--- src/robot_smach_states/utility.py
+++ src/robot_smach_states/utility.py
@@ -4,6 +4,7 @@ import smach
 
 ''' For siren demo challenge '''
 import thread
+from visualization_msgs.msg import Marker
 
 import robot_skills.util.msg_constructors as msgs
 import std_msgs.msg
@@ -363,6 +364,54 @@ class UnlockDesignator(smach.State):
         self.locking_designator.unlock()
         return 'unlocked'
 
+class MarkEntityInRviz(smach.State):
+    def __init__(self, entity_designator, namespace="designator"):
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'])
+        self.entity_designator = entity_designator
+        self.namespace = namespace
+        self.publisher = rospy.Publisher("executive_markers", Marker)
+
+    def execute(self, userdata=None):
+        try:
+            entity = self.entity_designator.resolve()
+
+            marker = Marker()
+            #Set the frame ID and timestamp.  See the TF tutorials for information on these.
+            marker.header.frame_id = "/map"
+            marker.header.stamp = rospy.Time.now()
+
+            #Set the namespace and id for this marker.  This serves to create a unique ID
+            #Any marker sent with the same namespace and id will overwrite the old one
+            marker.ns = self.namespace
+            marker.id = 0
+
+            #Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+            marker.type = Marker.ARROW
+
+            #Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+            marker.action = Marker.ADD
+
+            #Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+            marker.pose = entity.pose
+
+            #Set the scale of the marker -- 1x1x1 here means 1m on a side
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+
+            #Set the color -- be sure to set alpha to something non-zero!
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.color.a = 1.0
+
+            marker.lifetime = rospy.Duration()
+
+            self.publisher.publish(marker)
+            return 'succeeded'
+        except DesignatorResolvementError, e:
+            rospy.logerr(e)
+            return 'failed'
 
 if __name__ == "__main__":
     import doctest
