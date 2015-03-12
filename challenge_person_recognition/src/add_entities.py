23e70db35fb165f5c79aca6b1acb88f37d94e882
Luis Ferreira <luisfferreira@outlook.com>
2015-03-12 17:32:50 +0100

    updated people locations

--- challenge_person_recognition/src/add_entities.py
+++ challenge_person_recognition/src/add_entities.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     W.add_object("loy-1", "loy", 1.114, 4.233, 0)
 
     # crowd in the main room
-    W.add_object("loy-2", "loy", 0.713, 0.219, 0.000)
-    W.add_object("loy-3", "loy", 0.397, -0.090, 0.000)
-    W.add_object("loy-4", "loy", 0.613, 0.517, 0.000)
+    W.add_object("loy-2", "loy", 0.234, 0.912, 0.000)
+    W.add_object("loy-3", "loy", 0.385, 0.252, 0.000)
+    W.add_object("loy-4", "loy", 0.566, -0.513, 0.000)
