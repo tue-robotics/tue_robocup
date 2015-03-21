317dd9315273b34d2430c5a6c77f00c0e2e533af
Loy van Beek <loy.vanbeek@gmail.com>
2015-03-21 16:39:59 +0100

    Added basic challenge robonurse

---
+++ challenge_robonurse/src/challenge_robonurse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+"""This challenge is defined in https://raw.githubusercontent.com/RoboCupAtHome/RuleBook/master/RoboNurse.tex
+"""
+
+import rospy
+import smach
+import sys
+import random
+
+from robot_smach_states.util.designators import *
+import robot_smach_states as states
+from robot_smach_states.util.startup import startup
+from robot_smach_states import Grab
+from robot_smach_states import Place
+from robot_skills.util import msg_constructors as geom
+from robot_skills.util import transformations
+
+class RoboNurse(smach.StateMachine):
+    def __init__(self, robot):
+        smach.StateMachine.__init__(self, outcomes=['Done','Aborted'])
+
+        with self:
+            smach.StateMachine.add( states.Say(robot, ["I will be your RoboNurse"]), 
+                                    transitions={"spoken":"Done"})
+
+
+############################## initializing program ######################
+if __name__ == '__main__':
+    rospy.init_node('manipulation_exec')
+
+    if len(sys.argv) > 1:
+        robot_name = sys.argv[1]
+    else:
+        print "[CHALLENGE MANIPULATION] Please provide robot name as argument."
+        exit(1)
+
+    startup(RoboNurse, robot_name=robot_name)
