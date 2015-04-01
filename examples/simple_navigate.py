8d5440ea24b5f61d768aead214b80754dd244b80
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2015-04-01 14:33:36 +0200

    Updated simple nav

--- examples/simple_navigate.py
+++ examples/simple_navigate.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python
 import roslib; roslib.load_manifest('robot_smach_states')
 import rospy
-
-from robot_skills.amigo import Amigo
+import sys
 
 import robot_skills
 from robot_smach_states import navigation
 
+from robot_smach_states.util.designators import Designator, EdEntityDesignator, check_resolve_type
+from robot_smach_states.navigation import NavigateToPose, NavigateToObserve
+
 if __name__ == "__main__":
     rospy.init_node('simple_navigate')
 
-    robot = Amigo()
+    robot_name = sys.argv[1]
+    if robot_name == 'amigo':
+        from robot_skills.amigo import Amigo as Robot
+    elif robot_name == 'sergio':
+        from robot_skills.sergio import Sergio as Robot
+    else:
+        print "unknown robot"
+        sys.exit()
+
+    robot = Robot()
 
     rospy.sleep(2) # wait for tf cache to be filled
     
-    nav_state_end_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(0, 0.25, 0), look_at_path_distance=2.4)
-    nav_state_end_corridor.execute()
-    
-    #nav_state_door = navigation.NavigateGeneric(robot, goal_pose_2d=(1, 0, 0), look_at_path_distance=2.7)
-    #nav_state_door.execute()    
-    
-    #nav_state_living_room = navigation.NavigateGeneric(robot, goal_pose_2d=(7.8, 0.7, 0), look_at_path_distance=2.7)
-    #nav_state_living_room.execute()
-    
-    #nav_state_bedroom = navigation.NavigateGeneric(robot, goal_pose_2d=(7, -6.5, 1.57), look_at_path_distance=2.7)
-    #nav_state_bedroom.execute() 
-        
-    nav_state_begin_corridor = navigation.NavigateGeneric(robot, goal_pose_2d=(4.0, 0, 1.57), look_at_path_distance=2.4)
-    nav_state_begin_corridor.execute()
+    #nav_state = NavigateToPose(robot, 1, 0, 0)
+    #nav_state.execute()
+
+    nav_state = NavigateToObserve(robot, EdEntityDesignator(robot=robot, id="operator_table2"))
+    nav_state.execute()
+
