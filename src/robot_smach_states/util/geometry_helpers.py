42584ca0f7c8ed9e2beadcf81a2969d93036688e
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2015-04-02 14:56:12 +0200

    Added on top off function

--- src/robot_smach_states/util/geometry_helpers.py
+++ src/robot_smach_states/util/geometry_helpers.py
@@ -1,3 +1,5 @@
+import math
+
 import geometry_msgs.msg as gm
 
 from ed.msg import EntityInfo
@@ -35,4 +37,19 @@ def isPointInsideHull(p, chull):
 
 	return True
 
+def onTopOff(e1, e2, ht=0.1):
+	""" Checks whether entity e1 is on top of entity e2 
+		e1: entity e1
+		e2: entity e2
+		ht: height threshold: the bottom of e1 and the top of e2 need to be within ht m
+	"""
+
+	''' First: check if center point of e1 is within convex hull of e2 '''
+	if not isPointInsideHull(e1.center_point, e2.convex_hull):
+		return False
+
+	if math.fabs(e1.z_min-e2.z_max) > ht:
+		return False
+
+	return True
 
