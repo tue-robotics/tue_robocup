e50a2fcb05c8df4da19214c1318d10c4aa04770c
Loy van Beek <loy.vanbeek@gmail.com>
2015-04-02 17:03:28 +0200

    Removed moved files

--- open_door.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-#Find a topic thats a laser scan and publish a fake laser scan from laser_door_open.yaml on it
-rostopic pub $(rostopic find sensor_msgs/LaserScan) sensor_msgs/LaserScan -f laser_door_open.yaml
