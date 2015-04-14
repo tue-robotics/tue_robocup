5f1340165ae6c16a359b48e8156c83744974146f
Ramon Wijnands <rayman747@hotmail.com>
2015-04-14 22:48:36 +0200

    implement LookAtEntity

--- src/robot_smach_states/perception.py
+++ src/robot_smach_states/perception.py
@@ -3,25 +3,39 @@ import sys
 import rospy
 import smach
 
+from std_msgs.msg import Header
+from geometry_msgs.msg import Point, PointStamped
+from ed.msg import EntityInfo
+
 from robot_smach_states.state import State
 from robot_smach_states.util.designators import check_type, EdEntityDesignator
-from ed.msg import EntityInfo
 
 
 class LookAtEntity(State):
-    def __init__(self, robot, entity):
+    def __init__(self, robot, entity, keep_following=False):
         check_type(entity, EntityInfo)
 
         State.__init__(self, locals(), outcomes=['succeeded'])
 
-    def run(self, robot, entity):
+    def run(self, robot, entity, keep_following=False):
+        if keep_following:
+            center_point = Point()
+            frame_id = '/'+entity
+        else:
+            center_point = entity.center_point
+            frame_id = "/map"
+
+        rospy.loginfo('Look at %s in frame %s' % (repr(center_point).replace('\n', ' '), frame_id))
+        point_stamped = PointStamped(point=center_point,
+                                     header=Header(frame_id=frame_id))
+        robot.head.look_at_point(point_stamped)
         return "succeeded"
 
 
 # Testing
 
 def setup_statemachine(robot):
-    entity = EdEntityDesignator(robot, id='coke-1')
+    entity = EdEntityDesignator(robot, id='hallway_couch')
 
     sm = smach.StateMachine(outcomes=['Done', 'Aborted'])
     with sm:
