53bad31ca4c578a7b3bb6e716c50fe6268e42cb1
Max Baeten <m.j.j.baeten@gmail.com>
2015-04-21 23:33:28 +0200

    start on restaurant challenge

--- challenge_restaurant/src/challenge_restaurant.py
+++ challenge_restaurant/src/challenge_restaurant.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 
 """This challenge is defined in https://raw.githubusercontent.com/RoboCupAtHome/RuleBook/master/Restaurant.tex
+In short:
+
+1) The robot follows a guide inside a unknown space while learning the environment. The guide will tell the robot where the tables are.
+2) After the robot has been guided back to the kitchen, the robot asks which table he should go to ask an order.
+   Robot drives to that location and asks for an order which can be either a drink (coke) or a combo (burger with fries)
+   Robot then drives to the kitchen and graps the drink or asks for a tray with the combo.
+3) At any time during step 2) when somebody at an other table waves at the robot it should go there and ask for an order
+4) Once two orders are specified, the robot should deliver the combo and the drink.
 """
 
 import rospy
@@ -21,6 +29,22 @@ import geometry_msgs.msg as gm
 from robot_skills.util import transformations 
 from cb_planner_msgs_srvs.msg import PositionConstraint
 
+class GUIDING_PHASE(smach.State):
+    def __init__(self, robot, time_out=rospy.Duration(10)):
+        smach.State.__init__(self, outcomes=["answered"])
+        self.robot = robot
+        self.time_out = time_out
+
+    def execute(self, userdata):
+        # add while guiding
+        res = amigo.ears.recognize("<choice>",{"choice":["TABLE A","TABLE B","TABLE C"]})
+        
+        if res:
+            if "question" in res.choices:
+                rospy.loginfo("Is this talble: '%s'?"%res.result)
+                self.robot.speech.speak("Is this talble %s"%res.choice['question'])
+        return "answered"
+
 def setup_statemachine(robot):
 
     sm = smach.StateMachine(outcomes=['Done', 'Aborted'])
@@ -30,6 +54,10 @@ def setup_statemachine(robot):
                                 states.Initialize(robot),
                                 transitions={   'initialized':'Done',
                                                 'abort':'Aborted'})
+        smach.StateMachine.add('SAY_INTRO', states.Say(robot, "Hi Guide, Show me your restaurant please"), transitions={ 'spoken' :'GUIDING_PHASE'})
+        
+    
+    
     return sm
 
 
