610738ca3d6fd3d65c5479e09a31ca058f02492f
Rein Appeldoorn <reinzor@gmail.com>
2015-04-23 11:05:35 +0200

    added rgo2015 speech sentences

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_speech_recognition.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_speech_recognition.py
@@ -1,32 +1,29 @@
-from datetime import datetime
 choice_answer_mapping = {
-    "what time is it":"It is %s"%datetime.now().strftime("%I %M %p"),
-    "what is the capital of germany":"berlin",
-    "what is the heaviest animal in the world":"The blue whale",
-    "who is the president of america":"barack obama",
-    "what is your name":"It's me Amigo",
-    "who is the ugliest person in the world":"Rein Appeldoorn",
-    "what is your motto":"I am a banana!",
-    "which football club is the best":"Ajax",
-    "who is the best looking person around here":"Simple question, Erik Geerts",
-    "which team member is your favorite operator":"that i cannot choose, but if I have to, it would be my dear Erik",
-    "which town has been bombed":"Rotterdam and Schijndel",
-    "which person is not able to say yes":"Dirk Holtz",
-    "what is the capital of brazil":"brasilia",
-    "what is the oldest drug used on earth":"alcohol",
-    "in which year was robocup founded":"nineteen ninety seven",
-    "how many rings has the olympic flag":"five",
-    "what is the worlds most popular green vegetable":"lettuce",
-    "which insect has the best eyesight":"dragonfly",
-    "who lives in a pineapple under the sea":"spongebob squarepants",
-    "what is your teams name":"Tech united eindhoven",
-    "what is the answer to the ultimate question about life the universe and everything":"forty two",
-    "what is the capital of poland":"warsaw",
-    "which country grows the most potatoes":"russia",
-    "which country grew the first orange":"china",
-    "how many countries are in europe":"fifty",
-    "Who is the banana king?":"Sjoerd van den Dries",
-    "which fish can hold objects in its tail":"sea horse"
+        "Which American state is nearest to the former Soviet Union ":"ALASKA"
+        "How many tentacles does a squid have ":"TEN"
+        "What is converted into alcohol during brewing ":"SUGAR"
+        "Which river forms the eastern section of the border between England and Scotland ":"TWEED"
+        "In what year was Prince Andrew born ":"1960  19th February "
+        "Name the two families in Romeo and Juliet ":"MONTAGUE & CAPULET"
+        "If cats are feline  what are sheep ":"OVINE"
+        "For which fruit is the US state of Georgia famous ":"PEACH"
+        "Which is the financial centre and main city of Switzerland ":"ZURICH"
+        "Which TV programme s theme tune was called Hit and Miss ":"JUKE BOX JURY"
+        "Which guitarist is known as Slowhand ":"ERIC CLAPTON"
+        "What is an infant whale commonly called ":"CALF"
+        "What do the British call the vegetables that Americans call zucchini ":"COURGETTES"
+        "What is an otter s home called ":"HOLT"
+        "How have vegetables been cut which are served Julienne ":"THIN STRIPS  or shreds or sliced lengthways "
+        "In Roman mythology  Neptune is the equivalent to which Greek god ":"POSEIDON"
+        "Which TV character said   Live long and prosper  ":"MR SPOCK  Star Trek "
+        "In which State would you find the city of Birmingham ":"ALABAMA"
+        "Which hills divide England from Scotland ":"CHEVIOTS"
+        "What continent has the fewest flowering plants ":"ANTARTICA"
+        "What is Canada s national animal ":"BEAVER"
+        "What is the alternative common name for a Black Leopard ":"PANTHER"
+        "What in Cornwall is the most southerly point of mainland Britain ":"LIZARD POINT"
+        "What explorer introduced pigs to North America ":"CRISTOPHER COLUMBUS"
+        "What is the world biggest island ":"GREENLAND"
 }
 
 spec = '<question>'
---
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_speech_recognition.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+choice_answer_mapping = {
+    "what time is it":"It is %s"%datetime.now().strftime("%I %M %p"),
+    "what is the capital of germany":"berlin",
+    "what is the heaviest animal in the world":"The blue whale",
+    "who is the president of america":"barack obama",
+    "what is your name":"It's me Amigo",
+    "who is the ugliest person in the world":"Rein Appeldoorn",
+    "what is your motto":"I am a banana!",
+    "which football club is the best":"Ajax",
+    "who is the best looking person around here":"Simple question, Erik Geerts",
+    "which team member is your favorite operator":"that i cannot choose, but if I have to, it would be my dear Erik",
+    "which town has been bombed":"Rotterdam and Schijndel",
+    "which person is not able to say yes":"Dirk Holtz",
+    "what is the capital of brazil":"brasilia",
+    "what is the oldest drug used on earth":"alcohol",
+    "in which year was robocup founded":"nineteen ninety seven",
+    "how many rings has the olympic flag":"five",
+    "what is the worlds most popular green vegetable":"lettuce",
+    "which insect has the best eyesight":"dragonfly",
+    "who lives in a pineapple under the sea":"spongebob squarepants",
+    "what is your teams name":"Tech united eindhoven",
+    "what is the answer to the ultimate question about life the universe and everything":"forty two",
+    "what is the capital of poland":"warsaw",
+    "which country grows the most potatoes":"russia",
+    "which country grew the first orange":"china",
+    "how many countries are in europe":"fifty",
+    "Who is the banana king?":"Sjoerd van den Dries",
+    "which fish can hold objects in its tail":"sea horse"
+}
+
+spec = '<question>'
+choices = {'question': [k for k,v in choice_answer_mapping.iteritems()]}
+
