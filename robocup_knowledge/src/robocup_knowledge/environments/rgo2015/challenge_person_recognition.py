3c30d08c513c126330ffcf17181315654176a293
Max Baeten <m.j.j.baeten@student.tue.nl>
2015-04-23 14:18:52 +0200

    added person recognition challenge specific knowledge file

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_person_recognition.py
@@ -0,0 +1,5 @@
+# PERSON RECOGNITION RGO2015
+
+names = ["Anna","Beth","Carmen","Jennifer","Jessica","Kimberly","Kristina","Laura","Mary","Sarah","Alfred","Charles","Daniel","James","John","Luis","Paul","Richard","Robert","Steve"]
+
+locations = [ 'kitchen', 'livingroom', 'hall', 'bedroom', 'office']
