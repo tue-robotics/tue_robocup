5645e61a2c166a42c929ed0a2a696f74b6e51740
Rein Appeldoorn <reinzor@gmail.com>
2015-04-24 15:45:52 +0200

    restaurant knowledge

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_restaurant.py
@@ -0,0 +1,5 @@
+guiding_spec = "amigo <number> <side>"
+guiding_choices = {"number" : ["one", "two", "three"], "side" : ["left", "right", "front"]}
+
+operator_id = "operator"
+navigation_position_constraint = "x^2 + y^2 < 1^2"
