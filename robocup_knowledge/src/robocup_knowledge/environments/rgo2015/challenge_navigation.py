c917650f9b9abbd18e519ad0c8c5a956ce77b6d6
Max Baeten <m.j.j.baeten@student.tue.nl>
2015-04-24 15:30:01 +0200

    Revert "changed for new goal"
    This reverts commit 6d7ca272d89c05b3b5b434b403a604c2886e35ff.

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_navigation.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_navigation.py
@@ -1,11 +1,11 @@
 target1 = {
-    'near' : 'small_table',
-    'in' : 'hall',
-    'lookat' : 'small_table'
-}
-
-target2 = {
     'near' : 'counter',
     'in' : 'office',
     'lookat' : 'counter'
+}
+
+target2 = {
+    'near' : 'couchtable',
+    'in' : 'livingroom',
+    'lookat' : 'couchtable'
 }
