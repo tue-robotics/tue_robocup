b20268133e20106b38489fd4b6b6d383ccba9a5e
Rein Appeldoorn <reinzor@gmail.com>
2015-04-24 17:49:35 +0200

    updated knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_restaurant.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_restaurant.py
@@ -2,7 +2,7 @@
 guiding_spec = "amigo <location> <side>"
 
 # Do not alter keys or side values
-guiding_choices = {"location" : ["restaurant_table_1", "restaurant_table_2", "restaurant_table_3"], "side" : ["left", "right", "front"]}
+guiding_choices = {"location" : ["restaurant location one", "restaurant location two", "restaurant location three"], "side" : ["left", "right", "front"]}
 
 operator_id = "operator"
 navigation_position_constraint = "x^2 + y^2 < 1^2"
