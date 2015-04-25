20d58c648a33f1431150e9f254d79efee1321db9
Rein Appeldoorn <reinzor@gmail.com>
2015-04-25 15:41:13 +0200

    Added specs orders

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_restaurant.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_restaurant.py
@@ -1,3 +1,6 @@
+from robocup_knowledge import knowledge_loader
+common = knowledge_loader.load_knowledge("common")
+
 # Do not alter the keys please :)
 guiding_spec = "(<location> <side>|Please follow me)"
 
@@ -7,3 +10,11 @@ guiding_choices = {"location" : ["one", "two", "three"], "side" : ["left", "righ
 navigation_position_constraint_operator = "x^2 + y^2 < 0.2^2"
 
 kitchen_radius = 2
+
+# Do not alter the keys please :)
+order_spec = "(<beverage>|<food1> and <food2>)"
+
+# Do not alter keys or side values
+drinks = [ o["name"] for o in common.objects if o["category"] == "drink" ] 
+foods = [ o["name"] for o in common.objects if o["category"] == "food" ] 
+order_choices = {"beverage": drinks, "food1": foods, "food2": foods }
