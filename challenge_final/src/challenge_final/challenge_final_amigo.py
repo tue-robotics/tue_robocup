ae9c1dcf898e9e8d02b364bf74404723b770c572
Erik Geerts <e.g.geerts@live.nl>
2015-04-25 18:37:21 +0200

    final

---
+++ challenge_final/src/challenge_final/challenge_final_amigo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import rospy
+import smach
+import sys
+import random
+import math
+
+from robot_smach_states.util.designators import *
+import robot_smach_states as states
+from robot_smach_states.util.startup import startup
+
+from robocup_knowledge import load_knowledge
+challenge_knowledge = load_knowledge('challenge_final')
+INITIAL_POSE = challenge_knowledge.initial_pose_amigo
+
+############################## main statemachine ######################
+def setup_statemachine(robot):
+
+    sm = smach.StateMachine(outcomes=['Done', 'Aborted'])
+
+    with sm:
+        smach.StateMachine.add("INITIALIZE",
+                                states.StartChallengeRobust(robot, INITIAL_POSE, use_entry_points = True),
+                                transitions={   "Done"              :   "Done",
+                                                "Aborted"           :   "Done",
+                                                "Failed"            :   "Done"})
+
+    return sm
+
+############################## initializing program ######################
+if __name__ == '__main__':
+    rospy.init_node('final_exec_amigo')
+
+    startup(setup_statemachine, robot_name='amigo')
--- robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_final.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_final.py
@@ -1,1 +1,9 @@
-initial_pose_sergio = "initial_pose_door_B"
+initial_pose_sergio = "initial_pose_door_A"
+
+initial_pose_amigo = "initial_pose_door_B"
+
+explore_location_1 = "final_cupboard"
+explore_location_2 = "final_dinnertable_1"
+explore_location_3 = "final_dinnertable_2"
+explore_location_4 = "final_bookcase"
+explore_location_5 = "final_counter"
