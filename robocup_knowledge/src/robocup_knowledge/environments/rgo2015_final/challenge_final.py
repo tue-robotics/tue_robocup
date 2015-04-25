4b750c9e4315879f34752302189301aec24b1d1b
Erik Geerts <e.g.geerts@live.nl>
2015-04-25 20:12:59 +0200

    added kb file

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015_final/challenge_final.py
@@ -0,0 +1,26 @@
+initial_pose_sergio = "initial_pose_door_B"
+
+initial_pose_amigo = "initial_pose_door_B"
+
+# first map 3 locations + objects on it
+explore_location_1 = "final_cupboard"
+explore_location_2 = "final_dinnertable"
+explore_location_3 = "final_desk"
+explore_locations_part_1 = [explore_location_1, explore_location_2, explore_location_3]
+
+# then the location where sergio should receive the command of the operator (at the jury)
+task_location_sergio = "final_task_location_sergio"
+
+# then continue mapping
+explore_location_4 = "final_tv"
+explore_location_5 = "final_kitchencounter"
+explore_location_6 = "final_cabinet"
+explore_location_7 = "final_bartable"
+explore_location_8 = "final_small_table_1"
+explore_location_9 = "final_small_table_2"
+explore_location_10 = "final_coathanger"
+explore_location_11 = "final_left_bedside_table"
+explore_location_12 = "final_right_bedside_table"
+explore_locations_part_2 = [explore_location_4, explore_location_5, explore_location_6, explore_location_7, explore_location_8, explore_location_9, explore_location_10, explore_location_11, explore_location_12]
+
+end_location_sergio = "final_last_position_sergio"
