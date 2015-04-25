f590b5efc5785416e8097fb98c9c56f24dfafb2e
Erik Geerts <e.g.geerts@live.nl>
2015-04-25 08:34:50 +0200

    updated robonurse

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_robonurse.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_robonurse.py
@@ -1,3 +1,6 @@
 room = "livingroom"
-grannies_table="couchtable"
-bottle_shelf="cabinet"
+grannies_table = "couchtable"
+bottle_shelf = "cupboard"
+
+spec = "((the <color> pills)|(<color> one)|(<color>))"
+choices = {'color':['red','white','blue']}
