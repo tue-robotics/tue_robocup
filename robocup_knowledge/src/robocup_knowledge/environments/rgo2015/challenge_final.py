3b5afd6173381630ed2a2682af54224791f05046
Erik Geerts <e.g.geerts@live.nl>
2015-04-25 19:16:46 +0200

    updated knowledge final

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_final.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2015/challenge_final.py
@@ -5,5 +5,10 @@ initial_pose_amigo = "initial_pose_door_B"
 explore_location_1 = "final_cupboard"
 explore_location_2 = "final_dinnertable_1"
 explore_location_3 = "final_dinnertable_2"
+
+explore_locations_part_1 = [explore_location_1, explore_location_2, explore_location_3]
+
 explore_location_4 = "final_bookcase"
 explore_location_5 = "final_counter"
+
+explore_locations_part_2 = [explore_location_4, explore_location_5]
