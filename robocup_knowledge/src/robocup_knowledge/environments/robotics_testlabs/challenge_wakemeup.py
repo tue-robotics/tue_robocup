ee07e952d48c86239c94adf1316000c6b51d55de
Rokus Ottervanger <rokusottervanger@gmail.com>
2015-05-26 22:37:00 +0200

    Added knowledge for wakemeup challenge

---
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_wakemeup.py
@@ -0,0 +1,27 @@
+# Tunable params
+find_person = {
+    'within_range' : 2.0,
+    'under_z' : 0.3,
+    'min_chull_area' : 0.06,
+    'min_exist_prob' : 0.6
+}
+
+give_newspaper_timeout = 3
+
+
+# Knowledge
+bed = 'bed'
+
+bed_nav_goal = {
+    'near' : bed,
+    'in' : 'bedroom',
+    'lookat' : 'bed'
+}
+
+breakfast_order_options = {
+    "fruit_snack"  : ["apple"],
+    "cereal" : ["cereal", "choco-flakes"], 
+    "milk": ["whole-milk"]
+}
+
+
