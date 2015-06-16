1690d1aa690697ba8f293a91c454dc35720134da
Max Baeten <m.j.j.baeten@student.tue.nl>
2015-06-16 18:53:25 +0200

    fixed rips waypoints

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_rips.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_rips.py
@@ -1,4 +1,4 @@
-starting_point = "initial_pose_door_B"
-exit_1 = "exit_1_A"
-exit_2 = "exit_2_A"
-exit_3 = "exit_3_A"
+starting_point = "initial_pose"
+exit_1 = "exit_1_rips"
+exit_2 = "exit_2_rips"
+exit_3 = "exit_3_rips"
