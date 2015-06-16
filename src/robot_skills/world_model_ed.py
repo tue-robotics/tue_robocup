d2a89b358b936e1c41effd098c54ce04fecd48a4
Erik Geerts <e.g.geerts@live.nl>
2015-06-16 21:06:34 +0200

    added function for getting possible human entities. Filtering on room can be added

--- src/robot_skills/world_model_ed.py
+++ src/robot_skills/world_model_ed.py
@@ -181,3 +181,35 @@ class ED:
         all_entities = self.get_entities(parse=False)
         matches = filter(lambda fill_id: fill_id.startswith(short_id), [entity.id for entity in all_entities])
         return matches
+
+    def get_closest_possible_person_entity(self, type="", center_point=Point(), radius=0, room = ""):
+        if isinstance(center_point, PointStamped):
+            center_point = self._transform_center_point_to_map(center_point)
+
+        entities = self.get_entities(type="", center_point=center_point, radius=radius)
+
+        # HACK
+        entities = [ e for e in entities if len(e.convex_hull) > 0 and e.type == "" and e.flags == ['possible_human']]
+
+        # if only the persons in a certain room should be found:
+        if not (room == "" and len(entities) == 0):
+            print "room in ed = ", room
+            room_entity = self.get_entity(id=str(room))
+            x_min_room = room_entity.data['areas'][0]['shape'][0]['box']['min']['x']
+            x_max_room = room_entity.data['areas'][0]['shape'][0]['box']['max']['x']
+            y_min_room = room_entity.data['areas'][0]['shape'][0]['box']['min']['y']
+            y_max_room = room_entity.data['areas'][0]['shape'][0]['box']['max']['y']
+
+            entities = [e for e in entities if e.pose.position.x > x_min_room and e.pose.position.x < x_max_room and e.pose.position.y > y_min_room and e.pose.position.y < y_max_room]
+
+        if len(entities) == 0:
+            return None
+
+        # Sort by distance
+        try:
+            entities = sorted(entities, key=lambda entity: hypot(center_point.x - entity.pose.position.x, center_point.y - entity.pose.position.y))
+        except:
+            print "Failed to sort entities"
+            return None
+
+        return entities[0]
