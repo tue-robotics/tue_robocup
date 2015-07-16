bda2f8bcb26b27e5ac6145b9ec52cb8d5cc313e4
Rein Appeldoorn <reinzor@gmail.com>
2015-07-16 04:23:59 +0200

    Copied rgo2015 folder to rwc2015 :)

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_navigation.py
@@ -0,0 +1,11 @@
+target1 = {
+    'near' : 'small_table',
+    'in' : 'hall',
+    'lookat' : 'small_table'
+}
+
+target2 = {
+    'near' : 'counter',
+    'in' : 'office',
+    'lookat' : 'counter'
+}
