4fa20accd18b9a23606533d243a1724e8620405d
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-07-17 21:35:46 +0800

    Updated knowledge for manipulation

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_manipulation.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_manipulation.py
@@ -1,11 +1,17 @@
+''' Cabinet used for the challenge '''
+cabinet = "bookcase"
+
 ''' Shelves where objects might be '''
-object_shelves=["right_bookcase/shelf3","right_bookcase/shelf4","right_bookcase/shelf5"]
+object_shelves =[cabinet + "/shelf2", cabinet + "/shelf3"]
 
 ''' Shelf where we will actually try to grasp '''
-grasp_shelf="right_bookcase/shelf3"
+grasp_shelf = cabinet + "/shelf2"
 
 ''' Shelf where we will actually place stuff '''
-place_shelf="right_bookcase/shelf2"
+place_shelf = cabinet + "/shelf1"
 
 ''' Room where everything will take place '''
-room="living_room"
+room = "hallway"
+
+'''Object types that can be recognized'''
+object_types = ['coke']
