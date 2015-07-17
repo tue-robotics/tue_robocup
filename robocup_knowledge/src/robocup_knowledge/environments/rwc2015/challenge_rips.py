8ce88881f40cb0eb845c63ae278cbf2e859cc32f
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2015-07-17 16:15:26 +0800

    Updated rips knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_rips.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_rips.py
@@ -1,4 +1,4 @@
-starting_point = "initial_pose_door_B"
-exit_1 = "exit_1_A"
-exit_2 = "exit_2_A"
-exit_3 = "exit_3_A"
+starting_point = "initial_pose_door_A"
+exit_1 = "exit_door_B1"
+exit_2 = "exit_door_B2"
+exit_3 = "exit_door_B3"
