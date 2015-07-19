d7a081a815ebeb1273e183be1d3e95bbd4af18e4
Luis Ferreira <luisfferreira@outlook.com>
2015-07-19 12:02:08 +0800

    updated location of entities on the script

--- challenge_person_recognition/src/add_entities_rwc2015.py
+++ challenge_person_recognition/src/add_entities_rwc2015.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     W = client.SimWorld()
 
     # people in the room
-    W.add_object("person-1", "loy", 9.005, -1.867, 0.000)
-    W.add_object("person-2", "erik", 8.525, -1.894, 0.000)
-    W.add_object("person-3", "tim", 7.970, -1.909, 0.000)
-    W.add_object("person-4", "rob", 7.604, -1.873, 0.000)
-    W.add_object("person-5", "sjoerd", 7.151, -2.278, 0.000)
-    W.add_object("person-6", "rob", 6.618, -1.725, 0.000)
+    W.add_object("person-1", "loy", 7.222, -0.907, 0.000)
+    W.add_object("person-2", "erik", 7.701, -1.069, 0.000)
+    W.add_object("person-3", "tim", 8.353, -1.008, 0.000)
+    W.add_object("person-4", "sjoerd", 9.204, -0.906, 0.000)
+    # W.add_object("person-5", "sjoerd", 7.151, -2.278, 0.000)
+    # W.add_object("person-6", "rob", 6.618, -1.725, 0.000)
 
     # operator
-    W.add_object("person-7", "sjoerd", 10.842, -5.057, 0.000)
+    W.add_object("person-7", "sjoerd", 7.125, -5.639, 0.000)
     
     #loy, erik, tim, sjoerd, rob
