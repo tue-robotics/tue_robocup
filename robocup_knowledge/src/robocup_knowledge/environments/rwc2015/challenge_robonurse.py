188b7a61344b55a7256097c63ba63ffc7a5b36ba
Erik Geerts <e.g.geerts@live.nl>
2015-07-20 18:02:41 +0800

    updated knowledge robonurse

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_robonurse.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_robonurse.py
@@ -1,7 +1,7 @@
-room = "livingroom"
-grannies_table = "couchtable"
-bottle_shelf = "dinnertable"
-bottle_shelf_waypoint = "dinnertable_waypoint"
+room = "hallway"
+grannies_table = "hallwaytable"
+bottle_shelf = "bookcase"
+bottle_shelf_waypoint = "bookcase_waypoint"
 
 spec = "((the <color> pills)|(<color> one)|(<color>))"
 choices = {'color':['red','white','blue']}
