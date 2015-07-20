b9122265b4efb1d01fc8fae8202af4d637d669a3
Rokus Ottervanger <rokusottervanger@gmail.com>
2015-07-20 12:32:06 +0800

    Using correct sim model names

--- challenge_wakemeup/test/test_challenge_wakemeup.py
+++ challenge_wakemeup/test/test_challenge_wakemeup.py
@@ -87,24 +87,24 @@ if __name__ == "__main__":
 
     dx1 = 0.02
     # Kitchen counter (food)
-    W.add_object("milk",  "sim_apple", 2.25+(dx1*1), 2.0, 0.85, 0.0, 0.0 )
-    W.add_object("milk2", "sim_lemon", 2.35+(dx1*2), 2.0, 0.85, 0.0, 0.0 )
-    W.add_object("milk3", "sim_pear", 2.45+(dx1*3), 2.0, 0.85, 0.0, 0.0 )
-    W.add_object("milk4", "sim_coconut_cereals", 2.55+(dx1*4), 2.0, 0.85, 0.0, 0.0 )
-    W.add_object("milk5", "sim_coco_balls", 2.65+(dx1*5), 2.0, 0.85, 0.0, 0.0 )
-    W.add_object("milk5", "sim_coco_balls", 2.75+(dx1*6), 2.0, 0.85, 0.0, 0.0 )
+    W.add_object("apple",  "sim-apple", 2.25+(dx1*1), 2.0, 0.85, 0.0, 0.0 )
+    W.add_object("lemon", "sim-lemon", 2.35+(dx1*2), 2.0, 0.85, 0.0, 0.0 )
+    W.add_object("pear", "sim-pear", 2.45+(dx1*3), 2.0, 0.85, 0.0, 0.0 )
+    W.add_object("coconut_cereals", "sim-coconut_cereals", 2.55+(dx1*4), 2.0, 0.85, 0.0, 0.0 )
+    W.add_object("coco_balls", "sim-coco_balls", 2.65+(dx1*5), 2.0, 0.85, 0.0, 0.0 )
+    W.add_object("egg_stars", "sim-egg_stars", 2.75+(dx1*6), 2.0, 0.85, 0.0, 0.0 )
 
     # Kitchen table (drinks)
     dx = 0.03
-    W.add_object("fruit1", "sim_pure_milk", 2.80+dx, -1.25, 0.85, 0.0, 0.0 )
+    W.add_object("pure_milk", "sim-pure_milk", 2.80+dx, -1.25, 0.85, 0.0, 0.0 )
     #W.add_object("fruit2", "coke", 2.85+dx*2, -1.05, 0.85, 0.0, 0.0 )
-    W.add_object("fruit3", "sim_papaya_milk", 2.90+dx*3, -1.25, 0.85, 0.0, 0.0 )
+    W.add_object("papaya_milk", "sim-papaya_milk", 2.90+dx*3, -1.25, 0.85, 0.0, 0.0 )
     #W.add_object("fruit4", "coke", 2.95+dx*4, -1.05, 0.85, 0.0, 0.0 )
-    W.add_object("fruit5", "sim_pure_milk", 3.00+dx*5, -1.25, 0.85, 0.0, 0.0 )
+    W.add_object("pure_milk", "sim-pure_milk", 3.00+dx*5, -1.25, 0.85, 0.0, 0.0 )
     #W.add_object("fruit6", "coke", 3.05+dx*6, -1.05, 0.85, 0.0, 0.0 )
-    W.add_object("fruit7", "sim_papaya_milk", 3.10+dx*7, -1.25, 0.85, 0.0, 0.0 )
+    W.add_object("papaya_milk", "sim-papaya_milk", 3.10+dx*7, -1.25, 0.85, 0.0, 0.0 )
     #W.add_object("fruit8", "coke", 3.15+dx*8, -1.05, 0.85, 0.0, 0.0 )
-    W.add_object("fruit9", "sim_papaya_milk", 3.20+dx*9, -1.25, 0.85, 0.0, 0.0 )
+    W.add_object("papaya_milk", "sim-papaya_milk", 3.20+dx*9, -1.25, 0.85, 0.0, 0.0 )
     
 
     print "Dynamic wake me up simulator"
