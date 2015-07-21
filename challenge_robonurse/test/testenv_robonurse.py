310370a36cd58b34cc425a33e82fdc25a01a392c
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-07-21 15:27:31 +0800

    Better test env for robonurse

--- challenge_robonurse/test/testenv_robonurse.py
+++ challenge_robonurse/test/testenv_robonurse.py
@@ -25,6 +25,10 @@ if __name__ == "__main__":
        # Put stuff in the cabinet
 
         W.add_object("coke_d1", "sim-coke",         1.150, -5.418, 0.71)
-        W.add_object("coke_d2", "sim-papaya_milk",  1.134, -5.218, 0.71)
+        W.add_object("coke_d2", "sim-papaya_milk",  1.134, -5.218, -0.71)
+        W.add_object("coke_d4", "sim-lemon",        1.134, -5.218, 0.71)
         W.add_object("coke_d3", "sim-pure_milk",    1.110, -5.018, 0.71)
+        W.add_object("coke_d5", "sim-pure_milk",    1.110, -4.818, 0.71)
+
+        W.add_object("granny",   "tim",     2.712, -5.597, 0)
 
