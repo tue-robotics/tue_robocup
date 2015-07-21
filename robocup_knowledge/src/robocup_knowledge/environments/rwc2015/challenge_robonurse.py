7913edd72ea7b9e037be8dfc53ee30cdb2ea53bc
Erik Geerts <e.g.geerts@live.nl>
2015-07-21 10:13:24 +0800

    sorry

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_robonurse.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2015/challenge_robonurse.py
@@ -1,6 +1,7 @@
 room = "hallway"
 grannies_table = "hallwaytable"
-bottle_shelf = "bookcase"
+bottle_shelves = "bookcase" # not used yet
+bottle_shelf = "bookcase/shelf6"
 bottle_shelf_waypoint = "bookcase_waypoint"
 
 spec = "((the <color> pills)|(<color> one)|(<color>))"
