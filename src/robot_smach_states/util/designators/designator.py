fcd82a9208e7cfaf487a2dd9130642218d371654
Loy van Beek <loy.vanbeek@gmail.com>
2015-08-03 22:06:57 +0200

    Move the only test in designator.py to a separate test file

--- src/robot_smach_states/util/designators/designator.py
+++ src/robot_smach_states/util/designators/designator.py
@@ -1,68 +1,8 @@
 #!/usr/bin/python
-"""
-Designators are intended to encapsulate the process of resolving values needed
-at runtime based on write-time definitions.  This sound very vague, but
-imagine a navigation task that needs to navigate to some object.
 
-The objects you want to navigate to is not yet known beforehand, because its not yet present.
-Instead, you can define some function that finds an object to navigate to.
-
-A designator is an object that encapsulates such a function.
-The only interface method is that they have a resolve function that gives some value.
-
-How .resolve() does its work is not important here and
-    may be a custom implementation for all instances.
-
-The library here defines a couple of standard designators:
-- Designator:           simply returns a predefined value that defaults to None
-- VariableDesignator:   any user of this designator can set the designators value and
-                        can be used to pass around data.
-- AttrDesignator:       Some designator types wrap other designators, like AttrDesignator.
-                        It returns some attribute of of whatever the wrapped designator resolved to
-- FuncDesignator:       Apply a function to the resolution of another, wrapped, designator
-"""
 
 from robot_smach_states.util.geometry_helpers import *
 from robot_smach_states.util.designators.core import Designator, VariableDesignator
-
-
-def test_visited_and_unreachable():
-    """In our RoboCup executives, we keep track of which items are 'processed' or visited,
-    and which cannot be processed because they are unreachable.
-
-    One way to do this, is to mark objectIDs (targets) as visited and unreachable in the reasoner,
-    e.g. a global blackboard that can be manipulated an queried through Prolog.
-
-    This gets messy with complex queries. With designators, it should be a lot simpler"""
-    success = lambda goal: goal % 2  # Even targets will fail
-
-    target_list = [i for i in range(10)] + [i for i in range(10)]  # all are in twice!
-    targets = Designator(target_list)
-
-    unreachable_set = set()
-    unreachable = VariableDesignator(unreachable_set)
-
-    visited_set = set()
-    visited = VariableDesignator(visited_set)
-
-    for target in target_list:
-        if target not in unreachable.resolve() and target not in visited.resolve():
-            print "Processing target {0}".format(target)
-            if success(target):
-                visited.current.add(target)  # This is a set so no += [...]
-                print "Target {0} was successfull, its visited".format(target)
-            else:
-                unreachable.current.add(target)  # This is a set so no += [...]
-                print "Target {0} failed, its unreachable".format(target)
-        else:
-            print "NOT processing target {0}: its unreachable or already visited".format(target)
-
-    print "##### Done #####"
-    print "These are unreachable: {0}".format(unreachable.resolve())
-    print "These are visited: {0}".format(visited.resolve())
-
-    assert len(unreachable.resolve())   == 5
-    assert len(visited.resolve())       == 5
 
 if __name__ == "__main__":
     import doctest
