5ce76328369a13f274d08486a9d3273dcf6f4169
Loy van Beek <loy.vanbeek@gmail.com>
2015-08-04 22:36:57 +0200

    Adding a very ugly hack to introspect the designator/state-machine graph.

--- challenge_person_recognition/src/person_recognition.py
+++ challenge_person_recognition/src/person_recognition.py
@@ -570,6 +570,33 @@ class ChallengePersonRecognition(smach.StateMachine):
                                    states.Say(robot,"My work here is done, goodbye!"),
                                    transitions={'spoken':'Done'})
 
+            analyse_designators()
+
+def analyse_designators():
+    from robot_smach_states.util.designators import Designator
+    import gc
+
+    designators = [obj for obj in gc.get_objects() if isinstance(obj, Designator)]
+    resolve_types = {desig:desig.resolve_type for desig in designators}
+
+    states = [obj for obj in gc.get_objects() if isinstance(obj, smach.State)]
+    label2state = smach.StateMachine.get_children(smach.StateMachine._currently_opened_container())
+    #Do smach.StateMachine._currently_opened_container().get_children()["FIND_CROWD_CONTAINER"].get_children() recursively to get all labels but5 namespace them to their parent.
+    state2label = {state:label for label,state in label2state.iteritems()}
+    state_designator_relations = [] #List of (state, designator)-tuples
+    
+    # import ipdb; ipdb.set_trace()
+    for state in states:
+        for slot_key, slot_value in state.__dict__.iteritems():
+            if slot_value in designators: #Dunno which is faster/simpler: I can also lookup which __slots__ are instance fo designator again
+                name = state2label.get(state, state) #Get the abel of state, if not possible, just default to ugly __repr__
+                state_designator_relations += [(name, slot_value, slot_key, slot_value.resolve_type)]
+
+    import pprint
+    pprint.pprint(resolve_types)
+    print "-" * 10
+    pprint.pprint(state_designator_relations)
+    import ipdb; ipdb.set_trace()
 
 
 # ----------------------------------------------------------------------------------------------------
