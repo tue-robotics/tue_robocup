98221332493e16e8523116b12e431516e2b2af5b
Loy van Beek <loy.vanbeek@gmail.com>
2015-08-04 22:44:19 +0200

    Move designator introspection stuff to designators-module in robot_smach_states

--- challenge_person_recognition/src/person_recognition.py
+++ challenge_person_recognition/src/person_recognition.py
@@ -15,7 +15,7 @@ from robot_skills.sergio import Sergio
 from robot_skills.mockbot import Mockbot
 from robocup_knowledge import load_knowledge
 
-from robot_smach_states.util.designators import EdEntityDesignator, VariableDesignator, DeferToRuntime
+from robot_smach_states.util.designators import EdEntityDesignator, VariableDesignator, DeferToRuntime, analyse_designators
 
 # ----------------------------------------------------------------------------------------------------
 
@@ -571,33 +571,6 @@ class ChallengePersonRecognition(smach.StateMachine):
                                    transitions={'spoken':'Done'})
 
             analyse_designators()
-
-def analyse_designators():
-    from robot_smach_states.util.designators import Designator
-    import gc
-
-    designators = [obj for obj in gc.get_objects() if isinstance(obj, Designator)]
-    resolve_types = {desig:desig.resolve_type for desig in designators}
-
-    states = [obj for obj in gc.get_objects() if isinstance(obj, smach.State)]
-    label2state = smach.StateMachine.get_children(smach.StateMachine._currently_opened_container())
-    #Do smach.StateMachine._currently_opened_container().get_children()["FIND_CROWD_CONTAINER"].get_children() recursively to get all labels but5 namespace them to their parent.
-    state2label = {state:label for label,state in label2state.iteritems()}
-    state_designator_relations = [] #List of (state, designator)-tuples
-    
-    # import ipdb; ipdb.set_trace()
-    for state in states:
-        for slot_key, slot_value in state.__dict__.iteritems():
-            if slot_value in designators: #Dunno which is faster/simpler: I can also lookup which __slots__ are instance fo designator again
-                name = state2label.get(state, state) #Get the abel of state, if not possible, just default to ugly __repr__
-                state_designator_relations += [(name, slot_value, slot_key, slot_value.resolve_type)]
-
-    import pprint
-    pprint.pprint(resolve_types)
-    print "-" * 10
-    pprint.pprint(state_designator_relations)
-    import ipdb; ipdb.set_trace()
-
 
 # ----------------------------------------------------------------------------------------------------
 
