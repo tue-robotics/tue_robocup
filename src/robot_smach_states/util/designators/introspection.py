d5d2ab60321e3cd95e3cf7004bb33ee7d675ee8c
Loy van Beek <loy.vanbeek@gmail.com>
2015-08-06 22:23:01 +0200

    I can finally make some useful looking graphs using this command: rosrun challenge_person_recognition person_recognition.py amigo ; kill -9 %1; dot designators.dot -T png -o designators.png && eog designators.png

--- src/robot_smach_states/util/designators/introspection.py
+++ src/robot_smach_states/util/designators/introspection.py
@@ -8,14 +8,29 @@ import pprint
 
 __author__ = 'loy'
 
+def flatten(tree, parentname=None, sep="."):
+    flat = []
+    for branchname, branch in tree.get_children().iteritems():
+        if isinstance(branch, smach.StateMachine):
+            flat.extend(flatten(branch, parentname=branchname, sep=sep))
+        else:
+            name = parentname+sep+branchname if parentname else branchname
+            flat += [(name, branch)]
+    # print flat
+    return flat
+
 def analyse_designators():
 
     designators = [obj for obj in gc.get_objects() if isinstance(obj, Designator)]
     resolve_types = {desig:desig.resolve_type for desig in designators}
 
-    states = [obj for obj in gc.get_objects() if isinstance(obj, smach.State)]
-    label2state = smach.StateMachine.get_children(smach.StateMachine._currently_opened_container())
-    #Do smach.StateMachine._currently_opened_container().get_children()["FIND_CROWD_CONTAINER"].get_children() recursively to get all labels but5 namespace them to their parent.
+    # states = [obj for obj in gc.get_objects() if isinstance(obj, smach.State)]
+    # label2state = smach.StateMachine.get_children(smach.StateMachine._currently_opened_container())
+    
+    #Do smach.StateMachine._currently_opened_container().get_children()["FIND_CROWD_CONTAINER"].get_children() recursively to get all labels but5 namespace them to their parent
+    label2state = dict(flatten(smach.StateMachine._currently_opened_container()))
+    states = label2state.values()
+
     state2label = {state:label for label,state in label2state.iteritems()}
     state_designator_relations = [] #List of (state, designator)-tuples
     
@@ -24,13 +39,17 @@ def analyse_designators():
 
     # import ipdb; ipdb.set_trace()
     for state in states:
-        for designator_role, desig in state.__dict__.iteritems():
-            if desig in designators: #Dunno which is faster/simpler: I can also lookup which __slots__ are instance fo designator again
-                statelabel = state2label.get(state, state) #Get the abel of state, if not possible, just default to ugly __repr__
+        for designator_role, desig in state.__dict__.iteritems(): #Iterate the self.xxx members of each state
+            # If the member is also a designator, then process it.
+            if desig in designators: #Dunno which is faster/simpler: I can also lookup which of __dict__ are instance fo designator again
+                statelabel = state2label.get(state, state) #Get the label of state, if not possible, just default to ugly __repr__
                 state_designator_relations += [(statelabel, desig, designator_role, desig.resolve_type)]
-                dot.edge(   str(desig).replace("=", "_"), 
+
+                desig_name = desig.__class__.__name__ + "@" + str(hex(id(desig)))
+
+                dot.edge(   desig_name.replace("=", "_"), 
                             str(statelabel).replace("=", "_"), 
-                            label="{} [{}]".format(str(designator_role), str(desig.resolve_type)))
+                            label="{} [{}]".format(str(designator_role), desig.resolve_type.__name__))
 
     pprint.pprint(resolve_types)
     print "-" * 10
