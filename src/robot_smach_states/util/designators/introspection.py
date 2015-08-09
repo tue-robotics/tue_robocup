71663bef6a502442e39a430cfc3e713f2851901e
Loy van Beek <loy.vanbeek@gmail.com>
2015-08-09 16:38:26 +0200

    Clean up of introspection

--- src/robot_smach_states/util/designators/introspection.py
+++ src/robot_smach_states/util/designators/introspection.py
@@ -11,71 +11,30 @@ import pprint
 
 __author__ = 'loy'
 
-def flatten(tree, parentname=None, sep="."):
-    flat = []
-    for branchname, branch in tree.get_children().iteritems():
-        if isinstance(branch, smach.StateMachine):
-            flat.extend(flatten(branch, parentname=branchname, sep=sep))
-        else:
-            name = parentname+sep+branchname if parentname else branchname
-            flat += [(name, branch)]
-    # print flat
-    return flat
+class DesignatorUsage(object):
+    def __init__(self, parent, designator, role):
+        self.parent = parent
+        self.designator = designator
+        self.role = role
 
-def analyse_designators():
+    def ad_graphviz_edge(self, graph):
+        pass
 
-    designators = [obj for obj in gc.get_objects() if isinstance(obj, Designator)]
-    resolve_types = {desig:desig.resolve_type for desig in designators}
+class DesignatorUsedInState(DesignatorUsage):
+    def add_graphviz_edge(self, graph):
+        desig_name = format_designator(self.designator)
 
-    # states = [obj for obj in gc.get_objects() if isinstance(obj, smach.State)]
-    # label2state = smach.StateMachine.get_children(smach.StateMachine._currently_opened_container())
-    
-    #Do smach.StateMachine._currently_opened_container().get_children()["FIND_CROWD_CONTAINER"].get_children() recursively to get all labels but5 namespace them to their parent
-    label2state = dict(flatten(smach.StateMachine._currently_opened_container()))
-    states = label2state.values()
+        graph.edge(gv_safe(desig_name), gv_safe(self.parent), label=gv_safe(self.role))
 
-    state2label = {state:label for label,state in label2state.iteritems()}
-    state_designator_relations = []  # List of (state, designator, resolve_type)-tuples
-    parent_child_relations = []  # List of (parent-designator, child-designator, role-of-child, resolve_type)-tuples
-    
-    from graphviz import Digraph
-    dot = Digraph(comment='Designators')
+class DesignatorUsedInDesignator(DesignatorUsage):
+    def add_graphviz_edge(self, graph):
+        parent_name = format_designator(self.parent)
+        desig_name = format_designator(self.designator)
 
-    # import ipdb; ipdb.set_trace()
-    for state in states:
-        for designator_role, desig in state.__dict__.iteritems(): #Iterate the self.xxx members of each state
-            # If the member is also a designator, then process it.
-            if desig in designators: #Dunno which is faster/simpler: I can also lookup which of __dict__ are instance fo designator again
-                statelabel = state2label.get(state, state) #Get the label of state, if not possible, just default to ugly __repr__
-                state_designator_relations += [(statelabel, desig, designator_role, desig.resolve_type)]
+        graph.edge(gv_safe(desig_name), gv_safe(parent_name), label=gv_safe(self.role))
 
-                desig_name = format_designator(desig)
-
-                dot.edge(   desig_name.replace("=", "_"), 
-                            str(statelabel).replace("=", "_"), 
-                            label=str(designator_role))
-
-    for parent_designator in designators:
-        for child_role, child_designator in parent_designator.__dict__.iteritems(): #Iterate the self.xxx members of each designator
-            # If the member is also a designator, then process it.
-            if child_designator in designators: #Dunno which is faster/simpler: I can also lookup which of __dict__ are instance fo designator again
-                parent_child_relations += [(parent_designator, child_designator, child_role, child_designator.resolve_type)]
-
-                parent_name = format_designator(parent_designator)
-                child_name = format_designator(child_designator)
-
-                dot.edge(   child_name.replace("=", "_"),
-                            parent_name.replace("=", "_"),
-                            label=str(child_role))
-
-    pprint.pprint(resolve_types)
-    print "-" * 10
-    pprint.pprint(state_designator_relations)
-    print "-" * 10
-
-    dot.save('designators.dot')
-    dot.render('designators.png')
-
+def gv_safe(string):
+    return str(string).replace("=", "_")
 
 def format_designator(desig):
     resolve_type_format = desig.resolve_type
@@ -93,4 +52,55 @@ def format_designator(desig):
                                                                     cls=desig.__class__.__name__,
                                                                     addr=hex(id(desig)),
                                                                     resolve_type=resolve_type_format)
-    return desig_name
+
+def flatten(tree, parentname=None, sep="."):
+    flat = []
+    for branchname, branch in tree.get_children().iteritems():
+        if isinstance(branch, smach.StateMachine) or isinstance(branch, smach.Iterator):
+            flat.extend(flatten(branch, parentname=branchname, sep=sep))
+        else:
+            name = parentname+sep+branchname if parentname else branchname
+            flat += [(name, branch)]
+    # print flat
+    return flat
+
+def analyse_designators(statemachine=None, statemachine_name=""):
+
+    designators = [obj for obj in gc.get_objects() if isinstance(obj, Designator)]
+    resolve_types = {desig:desig.resolve_type for desig in designators}
+
+    # import ipdb; ipdb.set_trace()
+    if not statemachine:
+        statemachine = smach.StateMachine._currently_opened_container()
+    label2state = dict(flatten(statemachine, sep="\n."))
+    states = label2state.values()
+
+    state2label = {state:label for label,state in label2state.iteritems()}
+
+    usages = []
+
+    for state in states:
+        statelabel = state2label.get(state, state) #Get the label of state, if not possible, just default to ugly __repr__
+        for designator_role, desig in state.__dict__.iteritems(): #Iterate the self.xxx members of each state
+            # If the member is also a designator, then process it.
+            if desig in designators: #Dunno which is faster/simpler: I can also lookup which of __dict__ are instance fo designator again
+                usages += [DesignatorUsedInState(statelabel, desig, designator_role)]
+
+
+    for parent_designator in designators:
+        for child_role, child_designator in parent_designator.__dict__.iteritems(): #Iterate the self.xxx members of each designator
+            # If the member is also a designator, then process it.
+            if child_designator in designators: #Dunno which is faster/simpler: I can also lookup which of __dict__ are instance fo designator again
+                usages += [DesignatorUsedInDesignator(parent_designator, child_designator, child_role)]
+
+    from graphviz import Digraph
+    dot = Digraph(comment=statemachine_name+' Designators')
+
+    for usage in usages:
+        usage.add_graphviz_edge(dot)
+
+    dot.save(statemachine_name+'_designators.dot')
+    dot.render(statemachine_name+'_designators.png')
+
+
+    # return desig_name
