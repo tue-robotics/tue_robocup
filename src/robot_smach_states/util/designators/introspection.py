145b9eafc4da86b49e3ff3888cc386cbd957343b
Loy van Beek <loy.vanbeek@gmail.com>
2015-08-10 20:04:29 +0200

    Pep8-ed code

--- src/robot_smach_states/util/designators/introspection.py
+++ src/robot_smach_states/util/designators/introspection.py
@@ -1,13 +1,18 @@
 #! /usr/bin/env python
+"""
+Introspection for designators.
+Usage: pass a Smach state-machine to analyse_designators.
 
-import rospy
+A .dot and .png will be saved to the current directory, showing the relations and composition of states,
+state_machines and designators.
+"""
+
 import smach
 from robot_smach_states.util.designators.core import Designator
 
-"""
-"""
 
 __author__ = 'loy'
+
 
 class DesignatorUsage(object):
     def __init__(self, parent, designator, role):
@@ -15,11 +20,13 @@ class DesignatorUsage(object):
         self.designator = designator
         self.role = role
 
+
 class DesignatorUsedInState(DesignatorUsage):
     def add_graphviz_edge(self, graph):
         desig_name = format_designator(self.designator)
 
         graph.edge(gv_safe(desig_name), gv_safe(self.parent), label=gv_safe(self.role))
+
 
 class DesignatorUsedInDesignator(DesignatorUsage):
     def add_graphviz_edge(self, graph):
@@ -28,8 +35,10 @@ class DesignatorUsedInDesignator(DesignatorUsage):
 
         graph.edge(gv_safe(desig_name), gv_safe(parent_name), label=gv_safe(self.role))
 
+
 def gv_safe(string):
     return str(string).replace("=", "_")
+
 
 def format_designator(desig):
     resolve_type_format = desig.resolve_type
@@ -43,25 +52,26 @@ def format_designator(desig):
     else:
         resolve_type_format = desig.resolve_type.__name__
 
-    desig_name = "{name}({cls}@{addr})\n<{resolve_type}>".format(   name=desig.name + "\n" if desig.name else "",
-                                                                    cls=desig.__class__.__name__,
-                                                                    addr=hex(id(desig)),
-                                                                    resolve_type=resolve_type_format)
+    desig_name = "{name}({cls}@{addr})\n<{resolve_type}>".format(name=desig.name + "\n" if desig.name else "",
+                                                                 cls=desig.__class__.__name__,
+                                                                 addr=hex(id(desig)),
+                                                                 resolve_type=resolve_type_format)
     return desig_name
+
 
 def flatten(tree, parentname=None, sep="."):
     flat = []
-    for branchname, branch in tree.get_children().iteritems():
+    for branch_name, branch in tree.get_children().iteritems():
         if isinstance(branch, smach.StateMachine) or isinstance(branch, smach.Iterator):
-            flat.extend(flatten(branch, parentname=branchname, sep=sep))
+            flat.extend(flatten(branch, parentname=branch_name, sep=sep))
         else:
-            name = parentname+sep+branchname if parentname else branchname
+            name = parentname + sep + branch_name if parentname else branch_name
             flat += [(name, branch)]
     # print flat
     return flat
 
-def analyse_designators(statemachine=None, statemachine_name=""):
 
+def analyse_designators(statemachine=None, statemachine_name=""):
     designators = Designator.instances
 
     if not statemachine:
@@ -69,32 +79,29 @@ def analyse_designators(statemachine=None, statemachine_name=""):
     label2state = dict(flatten(statemachine, sep="\n."))
     states = label2state.values()
 
-    state2label = {state:label for label,state in label2state.iteritems()}
+    state2label = {state: label for label, state in label2state.iteritems()}
 
     usages = []
 
     for state in states:
-        statelabel = state2label.get(state, state) #Get the label of state, if not possible, just default to ugly __repr__
-        for designator_role, desig in state.__dict__.iteritems(): #Iterate the self.xxx members of each state
+        state_label = state2label.get(state, state)  # Get the label of state but default to ugly __repr__
+        for designator_role, designator in state.__dict__.iteritems():  # Iterate the self.xxx members of each state
             # If the member is also a designator, then process it.
-            if desig in designators: #Dunno which is faster/simpler: I can also lookup which of __dict__ are instance fo designator again
-                usages += [DesignatorUsedInState(statelabel, desig, designator_role)]
-
+            if designator in designators:
+                usages += [DesignatorUsedInState(state_label, designator, designator_role)]
 
     for parent_designator in designators:
-        for child_role, child_designator in parent_designator.__dict__.iteritems(): #Iterate the self.xxx members of each designator
+        # Iterate the self.xxx members of each designator
+        for child_role, child_designator in parent_designator.__dict__.iteritems():
             # If the member is also a designator, then process it.
-            if child_designator in designators: #Dunno which is faster/simpler: I can also lookup which of __dict__ are instance fo designator again
+            if child_designator in designators:
                 usages += [DesignatorUsedInDesignator(parent_designator, child_designator, child_role)]
 
     from graphviz import Digraph
-    dot = Digraph(comment=statemachine_name+' Designators')
+    dot = Digraph(comment=statemachine_name + ' Designators')
 
     for usage in usages:
         usage.add_graphviz_edge(dot)
 
-    dot.save(statemachine_name+'_designators.dot')
-    dot.render(statemachine_name+'_designators.png')
-
-
-    # return desig_name
+    dot.save(statemachine_name + '_designators.dot')
+    dot.render(statemachine_name + '_designators.png')
