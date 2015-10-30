d52a9516801812b6692e4de57752931bb734be0f
Loy van Beek <loy.vanbeek@gmail.com>
2015-10-30 20:43:55 +0100

    No introserver, problem exist

--- src/robot_smach_states/util/startup.py
+++ src/robot_smach_states/util/startup.py
@@ -69,9 +69,9 @@ def startup(statemachine_creator, initial_state=None, robot_name='', challenge_n
                     "Overriding initial state with {}".format(initial_state))
                 executioner.set_initial_state(initial_state)
 
-            introserver = smach_ros.IntrospectionServer(
-                statemachine_creator.__name__, executioner, '/SM_ROOT_PRIMARY')
-            introserver.start()
+            # introserver = smach_ros.IntrospectionServer(
+            #     statemachine_creator.__name__, executioner, '/SM_ROOT_PRIMARY')
+            # introserver.start()
 
             # Run the statemachine
             outcome = executioner.execute()
