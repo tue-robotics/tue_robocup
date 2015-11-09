a39035604bf8552a199141b66c22b37f11e8326e
Loy <LoyVanBeek@users.noreply.github.com>
2015-11-09 10:16:11 +0100

    Opening door also for mockbot if there is no topic.
    TODO: Robot name should be a parameter to the script

--- test_tools/open_door.sh
+++ test_tools/open_door.sh
@@ -2,3 +2,4 @@
 
 #Find a topic thats a laser scan and publish a fake laser scan from laser_door_open.yaml on it. Keep latching this too
 rostopic pub $(rostopic find sensor_msgs/LaserScan) sensor_msgs/LaserScan -f laser_door_open.yaml -l
+rostopic pub "/mockbot/base_laser/scan" sensor_msgs/LaserScan -f laser_door_open.yaml -l
