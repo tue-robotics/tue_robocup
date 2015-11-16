b9e6068cd428437e63e1a2b7683a9f46c2faa25c
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-11-16 16:52:23 +0100

    temp(grab): Temporarily added new grab machine

--- src/robot_smach_states/manipulation/grab.py
+++ src/robot_smach_states/manipulation/grab.py
@@ -171,3 +171,106 @@ class Grab(smach.StateMachine):
             smach.StateMachine.add('GRAB', PickUp(robot, arm, item),
                                    transitions={'succeeded': 'done',
                                                 'failed':    'failed'})
+
+# ----------------------------------------------------------------------------------------------------
+
+class SjoerdsGrab(smach.State):
+
+    def __init__(self, robot, item_des, arm_des):
+        smach.State.__init__(self, outcomes=["done", 'failed'])
+        self._robot = robot
+        self.item_des = item_des
+        self.arm_des = arm_des
+
+    def execute(self, userdata=None):
+        entity = self.item_des.resolve()
+
+        self.fsm = NavigateToGrasp(self._robot, self.item_des, self.arm_des)
+        self.fsm.execute()
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # Resolve entity and arm
+
+        entity = self.item_des.resolve()
+        arm = self.arm_des.resolve()
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Open gripper (non-blocking)
+        arm.send_gripper_goal('open', timeout=0)
+
+        # Torso up (non-blocking)
+        self._robot.torso.high()
+
+        # Arm to position in a safe way
+        # arm._send_joint_trajectory([
+        #     [-0.1,-0.6,0.1,1.2,0.0,0.1,0.0],
+        #     [-0.1,-0.8,0.1,1.6,0.0,0.2,0.0],
+        #     [-0.1,-1.0,0.1,2.0,0.0,0.3,0.0],
+        #     [-0.1,-0.5,0.1,2.0,0.0,0.3,0.0],
+        #     ])#, timeout=20)      
+
+        arm._send_joint_trajectory([
+            [-0.1,-1.0,0.1,2.0,0.0,0.3,0.0]], timeout=rospy.Duration(0))  
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # Update the pose of the entity
+
+        # Make sure the head looks at the entity
+        pos = entity.pose.position
+        self._robot.head.look_at_point(msgs.PointStamped(pos.x, pos.y, pos.z, "/map"), timeout=10)
+
+        # This is needed because the head is not entirely still when the
+        # look_at_point function finishes
+        import time
+        time.sleep(1)
+
+        # Inspect the entity
+        segm_res = self._robot.ed.update_kinect("%s" % entity.id)
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # goal in map frame
+        goal_map = msgs.Point(0, 0, 0)
+
+        # Transform to base link frame
+        goal_bl = transformations.tf_transform(goal_map, entity.id, "/amigo/base_link", tf_listener=self._robot.tf_listener)
+        if goal_bl == None:
+            return 'failed'
+
+        # Pre-grasp
+        if not arm.send_goal(goal_bl.x, goal_bl.y, goal_bl.z, 0, 0, 0, frame_id="/amigo/base_link", timeout=20, pre_grasp=True, first_joint_pos_only=True):
+            print "Pre-grasp failed"
+            arm.reset()
+            arm.send_gripper_goal('close', timeout=0.01)
+            self._robot.head.cancel_goal()
+            return
+
+        # Grasp
+        if not arm.send_goal(goal_bl.x, goal_bl.y, goal_bl.z, 0, 0, 0, frame_id="/amigo/base_link", timeout=120, pre_grasp = True):
+            self._robot.speech.speak("I am sorry but I cannot move my arm to the object position", block=False)
+            print "Grasp failed"
+            arm.reset()
+            arm.send_gripper_goal('close', timeout=0.01)
+            self._robot.head.cancel_goal()
+            return "failed"
+
+        # Close gripper
+        arm.send_gripper_goal('close', timeout=5)
+
+        # Cancel the head goal
+        self._robot.head.cancel_goal()
+
+        # Lift
+        if not arm.send_goal( goal_bl.x, goal_bl.y, goal_bl.z + 0.1, 0.0, 0.0, 0.0, timeout=20, pre_grasp=False, frame_id="/amigo/base_link"):
+            print "Failed lift"
+
+        # Retract
+        if not arm.send_goal(max(0.18, goal_bl.x - 0.25), goal_bl.y, goal_bl.z + 0.1, 0.0, 0.0, 0.0, timeout=20, pre_grasp=False, frame_id="/amigo/base_link"):
+            print "Failed retract"
+
+        # Carrying pose
+        arm._send_joint_trajectory([[-0.1, -0.6, 0.2, 1.7, 0, 0.4, 0]])
+
+        return "done"
+
