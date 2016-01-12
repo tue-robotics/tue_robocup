49373b38e93c4347b7102d3ec56e15dd44e90a62
Rein Appeldoorn <reinzor@gmail.com>
2016-01-12 21:44:14 +0100

    updated knowledge file

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_navigation.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_navigation.py
@@ -1,10 +1,19 @@
 target1 = "living_room"
-target2 = "exit"
-target3 = "hallway"
-target4 = "hallway"
+target1_radius1 = 0.1
+target1_radius2 = 0.7
 
-radius1 = 0.1
-radius2 = 0.7
+target2 = "exit"
+target2_radius1 = 1.5
+target2_radius2 = 0.1
+target2_radius3 = 0.7
+
+target3 = "hallway"
+target3_radius1 = 0.1
+target3_radius2 = 0.7
+
+target4 = "hallway"
+target4_radius1 = 0.1
+target4_radius2 = 0.7
 
 exit1 = "exit"
 exit2 = "exit"
