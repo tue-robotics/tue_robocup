d0554d0201f1d8790207c67195f61662b3637137
Loy van Beek <loy.vanbeek@gmail.com>
2016-01-12 21:30:06 +0100

    Added another simple test case and some logging

--- src/robot_smach_states/util/visualization.py
+++ src/robot_smach_states/util/visualization.py
@@ -7,7 +7,14 @@ from robot_smach_states.util.designators.core import Designator, VariableWriter,
 from graphviz import Digraph
 
 def gv_safe(string):
-    return str(string).replace("=", "_")
+    return str(string).replace("=", "_").replace(" ", "_").replace(".", "_").replace(">", "").replace("<", "")
+
+from functools import wraps
+def make_calls_unique(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        pass
+
 
 class StateViz(object):
     def __init__(self, smach_obj, parent):
@@ -26,7 +33,7 @@ class StateViz(object):
         return name
 
     def get_node_identifier(self):
-        return "{}_{}".format(self.get_name(), gv_safe(self.smach_obj))
+        return str(id(self.smach_obj)) #"{}_{}".format(self.get_name(), gv_safe(self.smach_obj))
 
 class TransitionViz(object):
     def __init__(self, from_, to, label):
@@ -41,6 +48,10 @@ class TransitionViz(object):
             to_identifier = self.to.make_childviz(self.to.smach_obj.get_children()[
                 self.to.smach_obj._initial_state_label]).get_node_identifier()
 
+        if isinstance(self.from_, ContainerOutcomeViz):
+            to_identifier = self.to.get_node_identifier()
+
+        # print "TransitionViz: graph.edge({},{}, label={})".format(self.from_.get_node_identifier(), to_identifier, self.label)
         graph.edge(self.from_.get_node_identifier(),
                    to_identifier,
                    label=self.label)
@@ -59,7 +70,7 @@ class ContainerOutcomeViz(object):
         return self.name
 
     def get_node_identifier(self):
-        return "{}_{}".format(gv_safe(self.name), gv_safe(self.parent))
+        return "{}_{}".format(gv_safe(self.name), gv_safe(self.parent.get_node_identifier()))
 
 class ContainerViz(StateViz):
     def make_childviz(self, child):
@@ -79,18 +90,25 @@ class StateMachineViz(ContainerViz):
         self.parent = parent
 
     def add_to_graph(self, graph):
-        machine = Digraph(self.get_node_identifier())
-        machine.body.append('label = "{}"'.format(self.get_name()))
-        machine.body.append('color=blue')
+        my_subgraph = Digraph(self.get_name())
+        print "StateMachineViz.add_to_graph({}) adding my_subgraph {} ".format(id(graph), id(my_subgraph))
 
-        for outcome in self.smach_obj._outcomes:
-            outcomeviz = ContainerOutcomeViz(outcome, self)
-            outcomeviz.add_to_graph(machine)
 
-        for childname, child in self.smach_obj.get_children().iteritems():
+        # for outcome in self.smach_obj._outcomes:
+        #     outcomeviz = ContainerOutcomeViz(outcome, self)
+        #     outcomeviz.add_to_graph(machine)
+
+        # for childname, child in self.smach_obj.get_children().iteritems():
+        #     childviz = self.make_childviz(child)
+
+        for childname in self.smach_obj._transitions.keys():
+            print "\t child {}".format(childname)
+            child = self.smach_obj.get_children()[childname]
             childviz = self.make_childviz(child)
+            childviz.add_to_graph(my_subgraph)
 
             for transition, to_name in self.smach_obj._transitions[childname].iteritems():
+                print "\t\t transition {} --{}--> {}".format(childname, transition, to_name)
                 if not to_name in self.smach_obj._outcomes:
                     # if to_name == "RANGE_ITERATOR": import ipdb; ipdb.set_trace()
                     if to_name == None:
@@ -101,12 +119,15 @@ class StateMachineViz(ContainerViz):
                 else:
                     to_viz = ContainerOutcomeViz(to_name, self)
 
-                to_viz.add_to_graph(machine)
+                to_viz.add_to_graph(my_subgraph)
 
+                # print "TransitionViz({}, {}, {})".format(childviz, to_viz, transition)
                 transitionviz = TransitionViz(childviz, to_viz, transition)
-                transitionviz.add_to_graph(machine)
-            childviz.add_to_graph(machine)
-        graph.subgraph(machine)
+                transitionviz.add_to_graph(my_subgraph)
+
+        my_subgraph.body.append('label = "{}"'.format(self.get_name()))
+        my_subgraph.body.append('color=blue')
+        graph.subgraph(my_subgraph)
 
     def get_name(self):
         if self.parent:
@@ -125,7 +146,6 @@ class IteratorViz(ContainerViz):
 
     def add_to_graph(self, graph):
         machine = Digraph()
-        machine.body.append('color=red')
 
         #import ipdb; ipdb.set_trace()
         for outcome in self.smach_obj._outcomes:
@@ -147,6 +167,8 @@ class IteratorViz(ContainerViz):
                 transitionviz = TransitionViz(childviz, to_viz, transition)
                 transitionviz.add_to_graph(machine)
             childviz.add_to_graph(machine)
+
+        machine.body.append('color=red')
         graph.subgraph(machine)
 
 visualization_classes = [type(None), StateViz, StateMachineViz, TransitionViz, ContainerOutcomeViz, IteratorViz]
@@ -157,8 +179,8 @@ def visualize(statemachine, statemachine_name, save_dot=False, fmt='png'):
     dot.graph_attr['label'] = statemachine_name
     dot.graph_attr['labelloc'] ="t"
 
-    #import ipdb; ipdb.set_trace()
     viz  = StateMachineViz(statemachine, None)
+    import ipdb; ipdb.set_trace()
     viz.add_to_graph(dot)
     #_visualize_machine("ROOT", statemachine, dot)
 
@@ -215,9 +237,73 @@ def testcase2():
                                 sublevel1,
                                 transitions={'Finished' :'Done',
                                              'Failed'   :'Aborted'})
-
     visualize(toplevel, "testcase2", save_dot=True)
 
+def testcase3():
+    import smach
+
+    toplevel = smach.StateMachine(outcomes=['Done'])
+    with toplevel:
+        @smach.cb_interface(outcomes=["succeeded"])
+        def execute(userdata):
+            return "succeeded"
+        smach.StateMachine.add('TEST1',
+                                smach.CBState(execute),
+                                transitions={'succeeded':'SUBLEVEL1'})
+
+        sublevel1 = smach.StateMachine(outcomes=['Finished'])
+        with sublevel1:
+            smach.StateMachine.add('SUBTEST1',
+                                    smach.CBState(execute),
+                                    transitions={'succeeded':'SUBTEST2'})
+            smach.StateMachine.add('SUBTEST2',
+                                    smach.CBState(execute),
+                                    transitions={'succeeded':'Finished'})
+
+        smach.StateMachine.add('SUBLEVEL1',
+                                sublevel1,
+                                transitions={'Finished' :'Done'})
+    visualize(toplevel, "testcase3", save_dot=True)
+
+def draw_subgraph():
+    g = Digraph('G')
+
+    c0 = Digraph('cluster_0')
+    c0.body.append('style=filled')
+    c0.body.append('color=lightgrey')
+    c0.node_attr.update(style='filled', color='white')
+    c0.edges([('a0', 'a1'), ('a1', 'a2'), ('a2', 'a3')])
+    c0.body.append('label = "process #1"')
+
+    c1 = Digraph('cluster_1')
+    c1.node_attr.update(style='filled')
+    c1.edges([('b0', 'b1'), ('b1', 'b2'), ('b2', 'b3')])
+    c1.body.append('label = "process #2"')
+    c1.body.append('color=blue')
+
+    g.subgraph(c0)
+    g.subgraph(c1)
+
+    g.edge('start', 'a0', lhead='cluster_1')
+    g.edge('start', 'b0', lhead='cluster_1')
+    g.edge('a1', 'b3')
+    g.edge('b2', 'a3')
+    g.edge('a3', 'a0')
+    g.edge('a3', 'end')
+    g.edge('b3', 'end')
+
+    g.node('start', shape='Mdiamond')
+    g.node('end', shape='Msquare')
+
+    g.save('draw_subgraph.dot')
+    g.render('draw_subgraph')
+
+    g.view()
+
+
 if __name__ == "__main__":
-    testcase1()
-    testcase2()
+    # testcase1()
+    # testcase2()
+    testcase3()
+
+    # draw_subgraph()
