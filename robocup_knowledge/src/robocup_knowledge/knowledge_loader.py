d0926d4d7625eb02c2ef2f40098f3cd38d66fea7
Loy van Beek <loy.vanbeek@gmail.com>
2016-02-02 20:57:42 +0100

    Only printing knowledge optionally

--- robocup_knowledge/src/robocup_knowledge/knowledge_loader.py
+++ robocup_knowledge/src/robocup_knowledge/knowledge_loader.py
@@ -1,6 +1,6 @@
 # Resolve the environment variable $ROBOT_ENV
 
-def load_knowledge(knowledge_item):
+def load_knowledge(knowledge_item, print_knowledge=False):
     import os, sys, imp
     _robot_env = os.environ.get('ROBOT_ENV')
 
@@ -15,12 +15,13 @@ def load_knowledge(knowledge_item):
 
         knowledge_attrs = [attr for attr in dir(knowledge) if not callable(attr) and not attr.startswith("__")]
 
-        print "====================================="
-        print "==          KNOWLEDGE              =="
-        print "====================================="
-        for attr in knowledge_attrs:
-            print "==> %s = %s" % (attr, str(getattr(knowledge, attr)))
-        print "====================================="
+        if print_knowledge:
+            print "====================================="
+            print "==          KNOWLEDGE              =="
+            print "====================================="
+            for attr in knowledge_attrs:
+                print "==> %s = %s" % (attr, str(getattr(knowledge, attr)))
+            print "====================================="
 
         return knowledge
 
