c29d0a130ac4d9c1e73ee2b9713aca403148bdcf
Rein Appeldoorn <reinzor@gmail.com>
2016-02-17 11:02:45 +0100

    Knowledge update, handles multiple segmentation area's now

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/r5cop_demo.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/r5cop_demo.py
@@ -1,4 +1,16 @@
 starting_point = "initial_pose"
 
-inspection_ids = ["cabinet", "dinnertable"]
+# required keys: entity_id (str), room_id (str), navigation_area (str), segment_areas (list)
+inspection_places = [
+    {"entity_id": "dinnertable",
+     "room_id": "living_room",
+     "navigate_area": "near",
+     "segment_areas": ["under", "on_top_of"]},
+
+    {"entity_id": "cabinet",
+     "room_id": "kitchen",
+     "navigate_area": "near",
+     "segment_areas": ["on_top_of"]},
+]
+
 known_types = ["coke", "fanta", "mentos", "bifrutas"]
