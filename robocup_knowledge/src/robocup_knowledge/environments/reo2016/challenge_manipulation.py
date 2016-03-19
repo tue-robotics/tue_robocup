371e5a58484074519af7c63f4f6b6c3675e6d2fc
Rein Appeldoorn <reinzor@gmail.com>
2016-03-19 14:06:57 +0100

    scaffold reo2016 env

---
+++ robocup_knowledge/src/robocup_knowledge/environments/reo2016/challenge_manipulation.py
@@ -0,0 +1,20 @@
+''' Cabinet used for the challenge '''
+cabinet = "bookcase"
+
+''' Shelves where objects might be '''
+object_shelves =[cabinet + "/shelf6", cabinet + "/shelf7", cabinet + "/shelf8"]
+
+''' Shelf where we will actually try to grasp '''
+grasp_shelf = cabinet + "/shelf6"
+
+''' Shelf where we will actually place stuff '''
+place_shelf = cabinet + "/shelf5"
+
+''' Room where everything will take place '''
+room = "hallway"
+
+'''Object types that can be recognized'''
+object_types = ['pure_milk', 'orange_juice', 'sponge', 'papaya_milk', 'apple', 'tomato_chips', 'lemon', 'toothpaste', 
+                 'chocolates', 'bowl', 'beer', 'toilet_paper', 'soap', 'plate', 'pear', 'lotion', 'water', 'cloth', 
+                 'green_tea', 'gram_soup', 'bubble_gum', 'bean_sauce', 'barbecue_chips', 'tray', 'coconut_cereals', 
+                 'egg_stars', 'honey_chips', 'coco_balls', 'biscuits']
