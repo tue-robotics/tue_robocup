e50158d31ca72147e5fa5de99f84a745717287fe
Rein Appeldoorn <reinzor@gmail.com>
2016-03-22 21:07:52 +0100

    look_at_designator optional argument

--- src/robot_smach_states/navigation/navigate_to_waypoint.py
+++ src/robot_smach_states/navigation/navigate_to_waypoint.py
@@ -15,15 +15,19 @@ import rospy
 # ----------------------------------------------------------------------------------------------------
 
 class NavigateToWaypoint(NavigateTo):
-    def __init__(self, robot, waypoint_designator, radius = 0.15):
+    def __init__(self, robot, waypoint_designator, radius = 0.15, look_at_designator=None):
         """@param waypoint_designator resolves to a waypoint stored in ED"""
         super(NavigateToWaypoint, self).__init__(robot)
 
         self.robot               = robot
 
         check_resolve_type(waypoint_designator, ed.msg.EntityInfo) #Check that the waypoint_designator resolves to an Entity
+        if look_at_designator is not None:
+            check_resolve_type(look_at_designator, ed.msg.EntityInfo) #Check that the look_at_designator resolves to an Entity
+
         self.waypoint_designator = waypoint_designator
         self.radius              = radius
+        self.look_at_designator = look_at_designator
 
     def generateConstraint(self):
         e = self.waypoint_designator.resolve()
@@ -46,6 +50,16 @@ class NavigateToWaypoint(NavigateTo):
                 return None
 
         pc = PositionConstraint(constraint="(x-%f)^2+(y-%f)^2 < %f^2"%(x, y, self.radius), frame="/map")
-        oc = OrientationConstraint(look_at=Point(x+10, y, 0.0), angle_offset=rz, frame="/map")
+
+        oc = None
+        if self.look_at_designator:
+            look_at_e = self.look_at_designator.resolve()
+            if look_at_e:
+                oc = OrientationConstraint(frame=look_at_e.id)
+            else:
+                rospy.logerr("NavigateToWaypoint::generateConstraint: No entity could be resolved from designator '%s'" % self.look_at_designator)
+
+        if not oc:
+            oc = OrientationConstraint(look_at=Point(x+10, y, 0.0), angle_offset=rz, frame="/map")
 
         return pc, oc
