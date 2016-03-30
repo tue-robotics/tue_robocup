b1e2c9b811f2e38dd237b6eaa0db4ab5e8eb310b
Ramon Wijnands <rayman747@hotmail.com>
2016-03-30 10:56:57 +0200

    update rips waypoints

--- robocup_knowledge/src/robocup_knowledge/environments/reo2016/challenge_rips.py
+++ robocup_knowledge/src/robocup_knowledge/environments/reo2016/challenge_rips.py
@@ -1,4 +1,9 @@
-starting_point = "initial_pose"
-exit_1 = "exit_1_rips"
-exit_2 = "exit_2_rips"
-exit_3 = "exit_3_rips"
+starting_point = "initial_pose_door_B"
+
+intermediate_1 = "rips1"
+intermediate_2 = "rips2"
+intermediate_3 = "rips3"
+
+exit_1 = "exit_door_A1"
+exit_2 = "exit_door_A2"
+exit_3 = "exit_door_A3"
