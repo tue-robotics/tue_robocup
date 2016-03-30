8d5a37646c8fb8af5803078be705afd32cde8197
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2016-03-30 20:46:07 +0200

    Loading challenge knowledge now does work

--- challenge_manipulation/src/add_entities.py
+++ challenge_manipulation/src/add_entities.py
@@ -35,9 +35,9 @@ if __name__ == "__main__":
     if env == "reo2016":
 
         # Bookcase
-        W.add_object("coke11", "sim-coke", 0.692, -3.940, 0.15)
-        W.add_object("coke12", "sim-coke", 0.752, -3.960, 0.8)
-        W.add_object("coke13", "sim-coke", 0.692, -3.940, 0.8)
-        W.add_object("coke14", "sim-coke", 0.652, -3.920, 0.8)
+        W.add_object("coke11", "sim-coke", 0.892, -4.440, 0.15)
+        W.add_object("coke12", "sim-coke", 0.952, -4.660, 0.8)
+        W.add_object("coke13", "sim-coke", 0.892, -4.440, 0.8)
+        W.add_object("coke14", "sim-coke", 0.752, -4.220, 0.8)
 
 
