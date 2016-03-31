346dc082c577f72b5f8ef4f66c08e319bdaa2ab5
Rein Appeldoorn <reinzor@gmail.com>
2016-03-31 14:38:05 +0200

    addded knowledge for nav challenge

---
+++ robocup_knowledge/src/robocup_knowledge/environments/reo2016/challenge_navigation.py
@@ -0,0 +1,24 @@
+target1 = "navigation_waypoint_1"
+target1_radius1 = 0.1
+target1_radius2 = 0.7
+
+target2 = "navigation_waypoint_2"
+target2_radius1 = 1.5
+target2_radius2 = 0.1
+target2_radius3 = 0.7
+
+target3 = "navigation_waypoint_3"
+target3_radius1 = 0.1
+target3_radius2 = 0.7
+
+target4 = "entry_point_B"
+target4_radius1 = 0.1
+target4_radius2 = 0.7
+
+exit1 = "exit_door_B1"
+exit2 = "exit_door_B2"
+exit3 = "exit_door_B3"
+exit4 = "entry_point_B"
+
+starting_point = "initial_pose_door_A"
+rotation = 0
