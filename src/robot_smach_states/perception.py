14555d5c01ffd965a51fe21308706d580dff5622
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2016-03-31 18:15:05 +0200

    Added comments to constructor of LookAtArea

--- src/robot_smach_states/perception.py
+++ src/robot_smach_states/perception.py
@@ -42,6 +42,13 @@ class LookAtArea(State):
     """ Class to look at the center point of a specific area of an entity
     """
     def __init__(self, robot, entity, area, waittime=2.0):
+        """ Constructor
+        :param robot: robot object
+        :param entity: EdEntityDesignator with the area to look at
+        :param area: string with the area to look at
+        :param waittime: (optional) waittime (in seconds) between giving a head target and returning
+        :return:
+        """
         ds.check_type(entity, EntityInfo)
 
         State.__init__(self, locals(), outcomes=['succeeded', 'failed'])
