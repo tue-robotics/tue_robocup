0c096d5bf3f2f9d48a43dd63025957889c71df48
Sjoerd van den Dries <sjoerdvddries@gmail.com>
2016-04-01 12:09:02 +0200

    gpsr

--- robocup_knowledge/src/robocup_knowledge/environments/reo2016/challenge_gpsr.py
+++ robocup_knowledge/src/robocup_knowledge/environments/reo2016/challenge_gpsr.py
@@ -8,32 +8,8 @@ exit_waypoint = "exit_door_B1"
 
 rooms = common.rooms
 
-object_aliases = {"dinner table" : "dinnertable"}
+#object_aliases = {"dinner table" : "dinnertable"}
 
-# mapping from furniture to small objects that are on top of them (and can be grabbed)
-furniture_to_objects = {
-    "cabinet": [
-        "beer",
-        "bifrutas",
-        "coffee_pads",
-        "coke",
-        "deodorant"
-    ],
-
-    "dinnertable": [
-        "fanta",
-        "ice_tea",
-        "mentos",
-        "sprite",
-        "tea",
-        "teddy_bear",
-        "water",
-        "xylit24_spearmint",
-        "xylit24_white"],
-
-    "bed": []
-}
-
-_grab_locations = [o for o in common.locations if o["manipulation"] == "yes"]
-room_to_grab_locations = groupby(_grab_locations, lambda l: l['room'])
-room_to_grab_locations = {k: [l['name'] for l in g] for (k, g) in room_to_grab_locations}
+#_grab_locations = [o for o in common.locations if o["manipulation"] == "yes"]
+# room_to_grab_locations = groupby(_grab_locations, lambda l: l['room'])
+# room_to_grab_locations = {k: [l['name'] for l in g] for (k, g) in room_to_grab_locations}
