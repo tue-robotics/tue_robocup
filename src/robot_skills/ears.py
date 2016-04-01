7e2c50181dda1d40539d08e43404aa7738cae6c4
Rein Appeldoorn <reinzor@gmail.com>
2016-04-01 21:56:47 +0200

    Revert "return None if result is empty"
    This reverts commit b8bbade81144de35b26731df4a3a65d2e6b0b2bf.

--- src/robot_skills/ears.py
+++ src/robot_skills/ears.py
@@ -69,9 +69,6 @@ class Ears:
         if hasattr(self._post_hook, '__call__'):
             self._post_hook()
 
-        if answer.result == "":
-            answer = None
-
         return answer
 
 if __name__ == "__main__":
