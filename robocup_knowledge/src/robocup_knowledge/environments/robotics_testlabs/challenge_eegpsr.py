3e02cfb1472298b97f485df564e641f4e42e258f
Sjoerd van den Dries <sjoerdvddries@gmail.com>
2016-05-18 00:10:32 +0200

    update: updated robocup knowledge for (ee)gpsr

---
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_eegpsr.py
@@ -0,0 +1,11 @@
+from itertools import groupby
+from robocup_knowledge import knowledge_loader
+common = knowledge_loader.load_knowledge("common")
+
+initial_pose = "initial_pose_door_B"  # initial pose
+starting_pose = "eegpsr_starting_pose"  # Designated pose to wait for commands
+exit_waypoint = "exit_door_B1"
+
+rooms = common.rooms + ["entrance", "exit"]
+
+translations = { "bookcase" : "bocase" }
--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/common.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/common.py
@@ -4,37 +4,37 @@ female_names = ["Anna","Beth","Carmen","Jennifer","Jessica","Kimberly","Kristina
 male_names = ["Alfred","Charles","Daniel","James","John","Luis","Paul","Richard","Robert","Steve"]
 names = female_names + male_names
 
-# NEXT TIME: locations should be rooms
-locations = [ 'kitchen', 'livingroom', 'hall', 'bedroom', 'office']
-
 # This dict holds all locations
-# fill in: [{'room':'', 'category': '', 'location_name':'', 'manipulation':''}]
-all_locations = [{'room':'livingroom', 'category': 'shelf', 'location_name':'cupboard','manipulation':'yes'},
-{'room':'livingroom', 'category': 'table',      'location_name':'couchtable','manipulation':'yes'},
-{'room':'livingroom', 'category': 'shelf',      'location_name':'cabinet',   'manipulation':'yes'},
-{'room':'livingroom', 'category': 'appliance',  'location_name':'tv',        'manipulation':'no'},
-{'room':'livingroom', 'category': 'seat',       'location_name':'couch',     'manipulation':'no'},
-{'room':'kitchen', 'category': '',          'location_name':'kitchencounter','manipulation':'yes'},
-{'room':'kitchen', 'category': 'appliance', 'location_name':'stove',         'manipulation':'yes'},
-{'room':'kitchen', 'category': 'appliance', 'location_name':'fridge',        'manipulation':'yes'},
-{'room':'kitchen', 'category': 'appliance', 'location_name':'sink',          'manipulation':'yes'},
-{'room':'kitchen', 'category': 'utility',   'location_name':'trashbin',      'manipulation':'no'},
-{'room':'kitchen', 'category': 'table',     'location_name':'bartable',      'manipulation':'yes'},
-{'room':'hall', 'category': 'table',    'location_name':'small_table',   'manipulation':'yes'},
-{'room':'hall', 'category': 'utility',  'location_name':'coathanger',    'manipulation':'no'},
-{'room':'bedroom', 'category': 'seat',  'location_name':'bed',                   'manipulation':'yes'},
-{'room':'bedroom', 'category': 'table', 'location_name':'left_bedside_table',    'manipulation':'yes'},
-{'room':'bedroom', 'category': 'table', 'location_name':'right_bedside_table',   'manipulation':'yes'},
-{'room':'office', 'category': 'table', 'location_name':'counter',        'manipulation':'yes'},
-{'room':'office', 'category': 'shelf', 'location_name':'left_bookcase',  'manipulation':'yes'},
-{'room':'office', 'category': 'shelf', 'location_name':'right_bookcase', 'manipulation':'yes'},
-{'room':'office', 'category': 'table', 'location_name':'dinnertable',    'manipulation':'yes'},
-{'room':'office', 'category': 'table', 'location_name':'desk',           'manipulation':'yes'}]
+locations = [
+    {'room':'livingroom', 'category': 'shelf', 'name':'cupboard','manipulation':'yes'},
+    {'room':'livingroom', 'category': 'table',      'name':'couchtable','manipulation':'yes'},
+    {'room':'kitchen', 'category': 'shelf',      'name':'cabinet',   'manipulation':'yes'},
+    {'room':'livingroom', 'category': 'appliance',  'name':'tv',        'manipulation':'no'},
+    {'room':'livingroom', 'category': 'seat',       'name':'couch',     'manipulation':'no'},
+    {'room':'kitchen', 'category': '',          'name':'kitchencounter','manipulation':'yes'},
+    {'room':'kitchen', 'category': 'appliance', 'name':'stove',         'manipulation':'yes'},
+    {'room':'kitchen', 'category': 'appliance', 'name':'fridge',        'manipulation':'yes'},
+    {'room':'kitchen', 'category': 'appliance', 'name':'sink',          'manipulation':'yes'},
+    {'room':'kitchen', 'category': 'utility',   'name':'trashbin',      'manipulation':'no'},
+    {'room':'kitchen', 'category': 'table',     'name':'bartable',      'manipulation':'yes'},
+    {'room':'hallway', 'category': 'table',    'name':'small_table',   'manipulation':'yes'},
+    {'room':'hallway', 'category': 'utility',  'name':'coathanger',    'manipulation':'no'},
+    {'room':'bedroom', 'category': 'seat',  'name':'bed',                   'manipulation':'yes'},
+    {'room':'bedroom', 'category': 'table', 'name':'left_bedside_table',    'manipulation':'yes'},
+    {'room':'bedroom', 'category': 'table', 'name':'right_bedside_table',   'manipulation':'yes'},
+    {'room':'office', 'category': 'table', 'name':'counter',        'manipulation':'yes'},
+    {'room':'office', 'category': 'shelf', 'name':'left_bookcase',  'manipulation':'yes'},
+    {'room':'office', 'category': 'shelf', 'name':'right_bookcase', 'manipulation':'yes'},
+    {'room':'livingroom', 'category': 'table', 'name':'dinnertable',    'manipulation':'yes'},
+    {'room':'office', 'category': 'table', 'name':'desk',           'manipulation':'yes'}
+]
 
-location_rooms = list(set([ o["room"] for o in all_locations ]))
-location_categories = list(set([ o["category"] for o in all_locations ]))
-location_names = list(set([ o["location_name"] for o in all_locations ]))
-location_manipulatable = list(set([ o["manipulation"] for o in all_locations ]))
+location_rooms = list(set([ o["room"] for o in locations ]))
+location_categories = list(set([ o["category"] for o in locations ]))
+location_names = list(set([ o["name"] for o in locations ]))
+location_manipulatable = list(set([ o["manipulation"] for o in locations ]))
+
+rooms = location_rooms
 
 objects = [
 {'category': 'decoration',  'placement': 'couchtable',  'group': 'known',   'name': 'candle'},
@@ -79,6 +79,16 @@ object_groups = list(set([ o["group"] for o in objects ]))
 object_placements = list(set([ o["placement"] for o in objects ]))
 object_known_objects = list(set([ o["name"] for o in objects ]))
 
+category_locations = {
+}
+
+inspect_areas = {
+}
+
+inspect_positions = {
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 ''' colors from printing on screen '''
 class bcolors:
@@ -112,3 +122,98 @@ def make_prints(name):
         print prefix + bcolors.WARNING + sentence + bcolors.ENDC
 
     return printOk, printError, printWarning
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+def is_location(location):
+    for loc in locations:
+        if loc["name"] == location:
+            return True
+    return False
+
+def get_room(location):
+    for loc in locations:
+        if loc["name"] == location:
+            return loc["room"]
+    return None
+
+def get_inspect_areas(location):
+    if location in inspect_areas:
+        return inspect_areas[location]
+    else:
+        return ["on_top_of"]
+
+def get_inspect_position(location, area=""):
+    if location in inspect_positions and area in inspect_positions[location]:
+        return inspect_positions[location][area]
+    else:
+        return "in_front_of"
+
+def is_pick_location(location):
+    for loc in locations:
+        if loc["name"] == location and loc["manipulation"] == "yes":
+            return True
+    return False
+
+def is_place_location(location):
+    for loc in locations:
+        if loc["name"] == location and (loc["manipulation"] == "yes" or loc["manipulation"] == "only_putting"):
+            return True
+    return False
+
+def get_locations(room=None, pick_location=None, place_location=None):
+    return [loc["name"] for loc in locations
+                if (room == None or loc["room"] == room) and \
+                   (pick_location == None or pick_location == is_pick_location(loc["name"])) and \
+                   (place_location == None or place_location == is_place_location(loc["name"]))]
+
+def get_objects(category=None):
+    return [obj["name"] for obj in objects 
+                if category == None or category == obj["category"]]
+
+def get_object_category(obj):
+    for o in objects:
+        if o["name"] == obj:
+            return o["category"]
+    return None
+
+# Returns (location, area_name)
+def get_object_category_location(obj_cat):
+    for o in objects:
+        if o["category"] == obj_cat:
+            return (o["placement"], "on_top_of")
+    return None
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -     
+
+if __name__ == "__main__":
+    print "\n-----------------------------------------------------------------------------"
+    for obj in get_objects():
+        cat = get_object_category(obj)
+        (location, area_name) = get_object_category_location(cat)
+        print "object '{}'".format(obj)
+        print "    category: '{}'".format(cat)
+        print "    found '{} {}'".format(area_name, location)
+
+    print "\n-----------------------------------------------------------------------------"
+    for loc in get_locations():
+        print "location '{}', room: '{}'".format(loc, get_room(loc))
+
+    print "\n-----------------------------------------------------------------------------"
+    print "Pick locations:"
+    for loc in get_locations(pick_location=True):
+        print "    {}".format(loc)
+
+    print "\n-----------------------------------------------------------------------------"
+    print "Place locations:"
+    for loc in get_locations(place_location=True):
+        print "    {}".format(loc)
+
+
+    print "\n-----------------------------------------------------------------------------"
+    print "None-manipulation locations:"
+    for loc in get_locations(pick_location=False, place_location=False):
+        print "    {}".format(loc)       
+
+
+
