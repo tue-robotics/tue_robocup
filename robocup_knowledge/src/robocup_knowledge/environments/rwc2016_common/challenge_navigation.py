78ceea604a0924df79534e6e887154805524b54a
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2016-06-28 22:26:47 +0200

    Updated navigation knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2016_common/challenge_navigation.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2016_common/challenge_navigation.py
@@ -1,30 +1,30 @@
-target1 = "livingroom"
+target1 = "navigation_target1"
 target1_radius1 = 0.1
 target1_radius2 = 0.7
 
-target2_pre = "exit_pre"
+target2_pre = "navigation_target2_pre"
 target2_pre_radius1 = 0.1
-target2 = "exit"
+target2 = "navigation_target2"
 target2_radius1 = 0.1
 target2_radius2 = 0.7
 target2_obstacle_radius = 0.6
 
-target3 = "hallway"
+target3 = "navigation_target3"
 target3_radius1 = 0.1
 target3_radius2 = 0.7
 
-target_door = "door_navigation"
-door_id = "door1"
+target_door = "navigation_door"
+door_id = "navigation_door"  # What does this do?
 target_door_radius = 0.1
 
-target4 = "hallway"
+target4 = "navigation_target4"
 target4_radius1 = 0.1
 target4_radius2 = 0.7
 
-exit1 = "exit"
-exit2 = "exit"
-exit3 = "exit"
-exit4 = "exit"
+exit1 = "navigation_exit1"
+exit2 = "navigation_exit2"
+exit3 = "navigation_exit3"
+exit4 = "navigation_exit4"
 
 starting_point = "initial_pose"
 rotation = 0
