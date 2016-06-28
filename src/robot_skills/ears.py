49292edde0d6dfad327b13e09113361943daac42
Rein Appeldoorn <reinzor@gmail.com>
2016-06-28 15:35:43 +0200

    removed print example, included in HMI now

--- src/robot_skills/ears.py
+++ src/robot_skills/ears.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 import rospy
-import re, random
 from dragonfly_speech_recognition.msg import Choice
 from dragonfly_speech_recognition.srv import GetSpeech, GetSpeechRequest
 
@@ -18,38 +17,11 @@ class Ears:
         self._pre_hook = pre_hook
         self._post_hook = post_hook
 
-    def _print_example(self, req_spec, req_choices):
-        # Copy request
-        example = "(%s)" % req_spec
-
-        # Pick random group if available
-        while re.search('\([^\)]+\)', example):
-            options = re.findall('\([^\(\)]+\)', example)
-            for option in options:
-                example = example.replace(option, random.choice(option[1:-1].split("|")), 1)
-
-        # Fetch all the residual choices
-        choices = re.findall("<([^<>]+)>", example)
-
-        # Parse the choices in the ending result :)
-        for c in choices:
-            for req_c in req_choices:
-                if req_c == c:
-                    value = random.choice(req_choices[req_c])
-                    example = example.replace("<%s>"%c, value)
-
-        rospy.loginfo("Example of what the robot can hear: \x1b[1;43m'{}'\x1b[0m".format(example))
-
     #Function listens explained on wiki: http://servicerobot.cstwiki.wtb.tue.nl/index.php?title=Using_the_dragonfly_speech_recognition
     def recognize(self, spec, choices={}, time_out=rospy.Duration(10)):
 
         if hasattr(self._pre_hook, '__call__'):
             self._pre_hook()
-
-        try:
-            self._print_example(spec, choices)
-        except:
-            pass
 
         answer = self._hmi.old_query(spec, choices, timeout=time_out.to_sec())
         if answer:
