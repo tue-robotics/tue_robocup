dcfce986798e19e603c70838d73f5a4ab6097593
Rein Appeldoorn <reinzor@gmail.com>
2016-06-28 22:25:46 +0200

    integrated locations

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2016_common/common.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2016_common/common.py
@@ -3,3 +3,89 @@
 female_names = ["Emma", "Taylor", "Sophia", "Isabella", "Ava", "Robin", "Emily", "Angel", "Madison", "Charlotte"]
 male_names = ["Noah", "Liam", "Mason", "Jacob", "William", "Ethan", "Michael", "Alexander", "James", "Daniel"]
 names = female_names + male_names
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+locations = []
+category_locations = {}
+inspect_areas = {}
+inspect_positions = {}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+rooms = []
+grab_locations = []
+put_locations = []
+
+
+def is_location(location):
+    for loc in locations:
+        if loc["name"] == location:
+            return True
+    return False
+
+
+def get_room(location):
+    for loc in locations:
+        if loc["name"] == location:
+            return loc["room"]
+    return None
+
+
+def get_inspect_areas(location):
+    if location in inspect_areas:
+        return inspect_areas[location]
+    else:
+        return ["on_top_of"]
+
+
+def get_inspect_position(location, area=""):
+    if location in inspect_positions and area in inspect_positions[location]:
+        return inspect_positions[location][area]
+    else:
+        return "in_front_of"
+
+
+def is_pick_location(location):
+    for loc in locations:
+        if loc["name"] == location and loc["manipulation"] == "yes":
+            return True
+    return False
+
+
+def is_place_location(location):
+    for loc in locations:
+        if loc["name"] == location and (loc["manipulation"] == "yes" or loc["manipulation"] == "only_putting"):
+            return True
+    return False
+
+
+def get_locations(room=None, pick_location=None, place_location=None):
+    return [loc["name"] for loc in locations if (room is None or loc["room"] == room) and
+            (pick_location is None or is_pick_location(loc["name"])) and
+            (place_location is None or is_place_location(loc["name"]))]
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+objects = []
+object_names = []
+object_categories = []
+
+
+def get_objects(category=None):
+    return [obj["name"] for obj in objects
+                if category == None or category == obj["category"]]
+
+
+def get_object_category(obj):
+    for o in objects:
+        if o["name"] == obj:
+            return o["category"]
+    return None
+
+
+# Returns (location, area_name)
+def get_object_category_location( obj_cat):
+    location = category_locations[obj_cat].keys()[0]
+    area_name = category_locations[obj_cat].values()[0]
+    return location, area_name
--- robocup_knowledge/src/robocup_knowledge/environments/rwc2016a/common.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2016a/common.py
@@ -1,1 +1,53 @@
 from robocup_knowledge.environments.rwc2016_common.common import *
+
+# Keep same reference
+locations.extend([
+    {"name": "bed", "room": "bedroom", "location_category": "", "manipulation": "no"},
+    {"name": "desk", "room": "bedroom", "location_category": "snacks", "manipulation": "yes"},
+    {"name": "bedside", "room": "bedroom", "location_category": "candies", "manipulation": "yes"},
+    {"name": "bar", "room": "kitchen", "location_category": "", "manipulation": "yes"},
+    {"name": "sink", "room": "kitchen", "location_category": "containers", "manipulation": "yes"},
+    {"name": "cupboard", "room": "kitchen", "location_category": "drinks", "manipulation": "yes"},
+    {"name": "sideshelf", "room": "kitchen", "location_category": "food", "manipulation": "yes"},
+    {"name": "tv_stand", "room": "dining_room", "location_category": "", "manipulation": "yes"},
+    {"name": "dining_table", "room": "dining_room", "location_category": "", "manipulation": "yes"},
+    {"name": "bookcase", "room": "dining_room", "location_category": "", "manipulation": "yes"},
+    {"name": "living_shelf", "room": "living_room", "location_category": "", "manipulation": "yes"},
+    {"name": "living_table", "room": "living_room", "location_category": "", "manipulation": "yes"},
+    {"name": "drawer", "room": "living_room", "location_category": "toiletries", "manipulation": "yes"},
+    {"name": "cabinet", "room": "corridor", "location_category": "", "manipulation": "yes"}
+])
+
+rooms = list(set([o["room"] for o in locations]))
+grab_locations = list(set([o["name"] for o in locations if o["manipulation"] == "yes"]))
+put_locations = list(set([o["name"] for o in locations if o["manipulation"] != "no"]))
+
+if __name__ == "__main__":
+    print "\n-----------------------------------------------------------------------------"
+    for obj in get_objects():
+        cat = get_object_category(obj)
+        print "object '{}'".format(obj)
+        (location, area_name) = get_object_category_location(cat)
+
+        print "    category: '{}'".format(cat)
+        print "    found '{} {}'".format(area_name, location)
+
+    print "\n-----------------------------------------------------------------------------"
+    for loc in get_locations():
+        print "location '{}', room: '{}'".format(loc, get_room(loc))
+
+    print "\n-----------------------------------------------------------------------------"
+    print "Pick locations:"
+    for loc in get_locations(pick_location=True):
+        print "    {}".format(loc)
+
+    print "\n-----------------------------------------------------------------------------"
+    print "Place locations:"
+    for loc in get_locations(place_location=True):
+        print "    {}".format(loc)
+
+
+    print "\n-----------------------------------------------------------------------------"
+    print "None-manipulation locations:"
+    for loc in get_locations(pick_location=False, place_location=False):
+        print "    {}".format(loc)
