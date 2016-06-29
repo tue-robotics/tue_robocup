579ed54085fde102c15d3251f177789e9fdf80fc
Rein Appeldoorn <reinzor@gmail.com>
2016-06-29 20:38:58 +0200

    Added waiting for cup

--- challenge_person_recognition/src/challenge_person_recognition.py
+++ challenge_person_recognition/src/challenge_person_recognition.py
@@ -21,10 +21,25 @@ class ChallengePersonRecognition(smach.StateMachine):
         #  -----------------------------------------------------------------
 
         with self:
-            smach.StateMachine.add( 'INITIALIZE',
-                                    states.Initialize(robot),
-                                    transitions={'initialized': 'AWAIT_START',
-                                                 'abort': 'Aborted'})
+            smach.StateMachine.add('INITIALIZE',
+                                   states.Initialize(robot),
+                                   transitions={'initialized': 'INSTRUCT_WAIT_FOR_DOOR',
+                                                'abort': 'Aborted'})
+
+            smach.StateMachine.add("INSTRUCT_WAIT_FOR_DOOR",
+                                   states.Say(robot, ["Hi there, I will now wait until you remove the cup",
+                                                      "I'm waiting for you to remove the cup"], block=False),
+                                   transitions={"spoken": "WAIT_FOR_DOOR"})
+
+            smach.StateMachine.add("WAIT_FOR_DOOR",
+                                   states.WaitForDoorOpen(robot, timeout=10),
+                                   transitions={"closed": "DOOR_CLOSED",
+                                                "open": "AWAIT_START"})
+
+            smach.StateMachine.add("DOOR_CLOSED",
+                                   states.Say(robot, ["I am waiting for you to remove the cup",
+                                                      "I'd start, if you remove the cup from my laser"]),
+                                   transitions={"spoken": "WAIT_FOR_DOOR"})
 
             smach.StateMachine.add("AWAIT_START",
                                    states.AskContinue(robot),
