18877714ce81a015343b1c1d5f6cf5ac45ef4d7e
Sjoerd van den Dries <sjoerdvddries@gmail.com>
2016-06-30 12:33:37 +0200

    update(gpsr): knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2016a/challenge_gpsr.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2016a/challenge_gpsr.py
@@ -5,6 +5,6 @@ initial_pose = "initial_pose_door_a"  # initial pose
 starting_pose = "gpsr_meeting_point"  # Designated pose to wait for commands
 exit_waypoint = "exit_door_B1"
 
-rooms = common.rooms + ["entrance", "exit"]
+rooms = common.rooms + ["entrance", "exit", "office"]
 
 translations = { "bookcase" : "bocase" }
