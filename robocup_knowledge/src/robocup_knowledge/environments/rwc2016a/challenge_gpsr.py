55f0679d38b664c5198a6a5e43041886736018bf
Sjoerd van den Dries <sjoerdvddries@gmail.com>
2016-07-01 08:37:36 +0200

    better gpsr

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2016a/challenge_gpsr.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2016a/challenge_gpsr.py
@@ -1,7 +1,7 @@
 from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
-initial_pose = "initial_pose_door_a"  # initial pose
+initial_pose = "initial_pose_door_c"  # initial pose
 starting_pose = "gpsr_meeting_point"  # Designated pose to wait for commands
 exit_waypoint = "exit_door_B1"
 
