770c01466fcf4bb7a084f6b1a7af2ee95bda3164
Rein Appeldoorn <reinzor@gmail.com>
2016-07-02 11:15:18 +0200

    neater

--- challenge_open/src/sergio_challenge_open.py
+++ challenge_open/src/sergio_challenge_open.py
@@ -8,8 +8,13 @@ if __name__ == "__main__":
 
     robot = Robot()
 
-    robot.speech.speak("No ids specified, I will do them all", block=False)
-    ids = [e.id for e in robot.ed.get_entities() if e.has_shape and "sergio" not in e.id and "walls" not in e.id and "floor" not in e.id]
+    robot.speech.speak("Here is Sergio! Let's show some navigation!", block=False)
+    robot.speech.speak("My knowledge is based on AMIGO's view of the world!", block=False)
+    robot.base.force_drive(0.25, 0, 0, 5.0)  # x, y, z, time in seconds
+
+    exclude_list = ["amigo", "sergio", "walls", "floor", "root"]
+
+    ids = [e.id for e in robot.ed.get_entities() if e.has_shape and not any(ex in e.id for ex in exclude_list)]
     random.shuffle(ids)
 
     print "IDS:", ids
@@ -18,7 +23,7 @@ if __name__ == "__main__":
 
         robot.speech.speak("I am going to navigate to the %s" % id, block=False)
 
-        machine = robot_smach_states.NavigateToSymbolic(robot, {robot_smach_states.util.designators.EntityByIdDesignator(robot, id=id): "near"},
+        machine = robot_smach_states.NavigateToSymbolic(robot, {robot_smach_states.util.designators.EntityByIdDesignator(robot, id=id): "in_front_of"},
                                                         robot_smach_states.util.designators.EntityByIdDesignator(robot, id=id))
 
         result = machine.execute()
