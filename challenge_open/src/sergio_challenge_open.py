29fff0a61afe6ba52a51e939b4d39fc25c9c55c5
Rein Appeldoorn <reinzor@gmail.com>
2016-07-02 18:24:15 +0200

    keep looping

--- challenge_open/src/sergio_challenge_open.py
+++ challenge_open/src/sergio_challenge_open.py
@@ -10,25 +10,24 @@ if __name__ == "__main__":
 
     robot.speech.speak("Here is Sergio! Let's show some navigation!", block=False)
     robot.speech.speak("My knowledge is based on AMIGO's view of the world!", block=False)
-    robot.base.force_drive(0.25, 0, 0, 5.0)  # x, y, z, time in seconds
+    robot.base.force_drive(0.25, 0, 0, 8.0)  # x, y, z, time in seconds 2meters
 
-    exclude_list = ["amigo", "sergio", "walls", "floor", "root"]
+    exclude_list = ["door", "amigo", "sergio", "walls", "floor", "root"]
 
     ids = [e.id for e in robot.ed.get_entities() if e.has_shape and not any(ex in e.id for ex in exclude_list)]
-    random.shuffle(ids)
 
-    print "IDS:", ids
+    while not rospy.is_shutdown():
 
-    for id in ids:
+	    random.shuffle(ids)
+        
+	    print "IDS:", ids
 
-        robot.speech.speak("I am going to navigate to the %s" % id, block=False)
+	    for id in ids:
 
-        machine = robot_smach_states.NavigateToSymbolic(robot, {robot_smach_states.util.designators.EntityByIdDesignator(robot, id=id): "in_front_of"},
-                                                        robot_smach_states.util.designators.EntityByIdDesignator(robot, id=id))
+		machine = robot_smach_states.NavigateToSymbolic(robot, {robot_smach_states.util.designators.EntityByIdDesignator(robot, id=id): "in_front_of"},
+								robot_smach_states.util.designators.EntityByIdDesignator(robot, id=id))
 
-        result = machine.execute()
+		result = machine.execute()
 
-        if result == "arrived":
-            robot.speech.speak("I arrived at the %s" % id, block=True)
-        else:
-            robot.speech.speak("I failed to navigate to the %s" % id, block=True)
+		if result == "arrived":
+		    robot.speech.speak("I arrived at the %s" % id, block=True)
