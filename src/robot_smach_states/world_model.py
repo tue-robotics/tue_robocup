ed4279f0ac609d9df86d0370aeccd3a3f9e252d0
Rein Appeldoorn <reinzor@gmail.com>
2016-11-23 14:48:43 +0100

    Removed perception and person detection

--- src/robot_smach_states/world_model.py
+++ src/robot_smach_states/world_model.py
@@ -18,24 +18,6 @@ def _color_info(string):
 
 # ----------------------------------------------------------------------------------------------------
 
-class SetPlugins(smach.State):
-    def __init__(self, robot, enable=None, disable=None):
-        smach.State.__init__(self, outcomes=["done"])
-        self.enable = enable
-        self.disable = disable
-        self.robot = robot
-
-    def execute(self, userdata=None):
-        if self.disable:
-            self.robot.ed.disable_plugins(self.disable)
-
-        if self.enable:
-            self.robot.ed.enable_plugins(self.enable)
-
-        return 'done'
-
-
-
 '''
     Initialize world model with a certain configuration.
     Set perception mode to non-continuos and disable laser_integration.
@@ -46,11 +28,6 @@ class InitializeWorldModel(smach.State):
         self.robot = robot
 
     def execute(self, userdata=None):
-        # TODO: the state was crashing when calling these arguments,
-        #       ASK SJOERD HOW TO USE THIS NOW!
-        # self.robot.ed.configure_kinect_segmentation(continuous=False)
-        # self.robot.ed.configure_perception(continuous=False)
-        # self.robot.ed.disable_plugins(plugin_names=["laser_integration"])
         self.robot.ed.reset()
 
         return 'done'
