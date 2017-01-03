d6de71368c37833ea9c8b3d828d336268bd3d802
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-01-03 22:27:20 +0100

    feat(Shape) Worked on shape stuff

---
+++ robot_skills/src/robot_skills/util/shape.py
@@ -0,0 +1,43 @@
+# Robot skills
+from robot_skills.util.kdl_conversions import point_msg_to_kdl_vector
+
+
+class Shape(object):
+    """ Represents shape properties """
+    pass
+
+
+class RightPrism(Shape):
+    """ Represents a right prism, i.e., a  prism in which the joining edges and faces are perpendicular to the base
+    faces. This is typical for the shapes resulting from the convex hull, z min and z max of EntityInfo
+
+    """
+    def __init__(self, convex_hull, z_min, z_max):
+        """ Constructor
+
+        :param convex_hull: list with kdl Vectors representing the vertices of the convex hull. N.B.: these should only
+         contain x and y values, the z-values are not relevant (the height information is contained in the z_min
+         and z_max parameters).
+        :param z_min: float minimum height [m] # ToDo: w.r.t.???
+        :param z_max: float maximum height [m] # ToDo: w.r.t.???
+        """
+        super(RightPrism, self).__init__()
+        self._convex_hull = convex_hull
+        self._z_min = z_min
+        self._z_max = z_max
+
+
+def shape_from_entity_info(e):
+    """ Creates a shape from the convex_hull, z_min and z_max of the EntityInfo object. If no convex hull is present,
+    an empty Shape object is returned
+
+    :param e: ed.msg.EntityInfo
+    :return: Shape
+    """
+    # Check if we have a convex hull
+    if not e.convex_hull:
+        return Shape()
+
+    return RightPrism(convex_hull=[point_msg_to_kdl_vector(p) for p in e.convex_hull],
+                      z_min=e.z_min,
+                      z_max=e.z_max)
