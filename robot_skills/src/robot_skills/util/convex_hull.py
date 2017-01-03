28852de90ce7f1b57ae5bd1b4d8c83a16048a941
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-01-03 21:05:30 +0100

    feat(Entity) Added Entity, Area and ConvexHull classes

---
+++ robot_skills/src/robot_skills/util/convex_hull.py
@@ -0,0 +1,15 @@
+class ConvexHull(object):
+    """ Holds a convex hull
+
+    """
+    pass
+
+
+def convex_hull_from_entity_info(e):
+    """ Creates a ConvexHull from the convex_hull, z_min and z_max of the EntityInfo object
+
+    :param e: ed.msg.EntityInfo
+    :return: ConvexHull
+    """
+    # ToDo: fill
+    return ConvexHull
---
+++ robot_skills/src/robot_skills/util/entity.py
@@ -0,0 +1,47 @@
+# ROS
+import PyKDL as kdl
+
+from robot_skills.src.robot_skills.util.area import areas_from_entity_info_data
+from robot_skills.src.robot_skills.util.convex_hull import convex_hull_from_entity_info
+
+
+class Entity(object):
+    """ Holds all data concerning entities
+
+    """
+    def __init__(self, identifier, object_type, frame_id, pose, convex_hull, areas, super_types):
+        """ Constructor
+
+        :param identifier: string with the id of this entity
+        :param object_type: string with the type of this entity
+        :param frame_id: frame id w.r.t. which the pose is defined
+        :param pose: kdl frame with the pose of this entity
+        :param convex_hull: ConvexHull
+        :param areas: dict mapping strings to Areas
+        :param super_types: list with strings representing super types in an ontology of object types
+        """
+        self.id = identifier
+        self.type = object_type
+        self.frame_id = frame_id
+        self.pose = pose
+        self.convex_hull = convex_hull
+        self.areas = areas
+        self.super_types = super_types
+
+
+def from_entity_info(e):
+    """ Converts ed.msg.EntityInfo to an Entity
+
+    :param e: ed.msg.EntityInfo
+    :return: Entity
+    """
+    identifier = e.id
+    object_type = e.type
+    frame_id = "/map"  # ED has all poses in map
+    # ToDo: pose
+    pose = kdl.Frame()
+    convex_hull = convex_hull_from_entity_info(e)
+    areas = areas_from_entity_info_data(e.data)
+    super_types = e.types
+    return Entity(identifier=identifier, object_type=object_type, frame_id=frame_id, pose=pose, convex_hull=convex_hull,
+                  areas=areas, super_types=super_types)
