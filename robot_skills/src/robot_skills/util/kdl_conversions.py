d8bd26bc53e5eca01289c21ae6a6f7cf8dcd851f
Loy van Beek <loy.vanbeek@gmail.com>
2017-01-30 20:25:13 +0100

    Add doctests for kdl_conversions

--- robot_skills/src/robot_skills/util/kdl_conversions.py
+++ robot_skills/src/robot_skills/util/kdl_conversions.py
@@ -44,13 +44,13 @@ def kdlRotationToQuaternionMsg(rotation):
     :rtype: geometry_msgs.msg.Quaternion
 
     >>> rot = kdl.Rotation.Quaternion(1, 0, 0, 0)
-    >>> quat = kdlRotationToQuaternionMsg(rot)
-    >>> (quat.x, quat.y, quat.z, quat.w)
+    >>> quat_msg = kdlRotationToQuaternionMsg(rot)
+    >>> (quat_msg.x, quat_msg.y, quat_msg.z, quat_msg.w)
     (1.0, 0.0, 0.0, 0.0)
     >>> from math import pi
     >>> rot = kdl.Rotation.RPY(pi/2, 0, 0)
-    >>> quat = kdlRotationToQuaternionMsg(rot)
-    >>> (quat.x, quat.y, quat.z, quat.w)
+    >>> quat_msg = kdlRotationToQuaternionMsg(rot)
+    >>> (quat_msg.x, quat_msg.y, quat_msg.z, quat_msg.w)
     (0.7071067811865475, 0.0, 0.0, 0.7071067811865476)
     """
     return gm.Quaternion(*rotation.GetQuaternion())
@@ -61,6 +61,11 @@ def quaternionMsgToKdlRotation(quaternion):
     :param quaternion: Rotation to be converted
     :type quaternion: geometry_msgs.msg.Quaternion
     :rtype: PyKDL.Rotation
+
+    >>> quat_msg = gm.Quaternion(1.0, 0.0, 0.0, 1.0)
+    >>> rot = quaternionMsgToKdlRotation(quat_msg)
+    >>> rot.GetRPY()
+    (1.5707963267948966, -0.0, 0.0)
     """
     return kdl.Rotation.Quaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w)
 
@@ -68,8 +73,15 @@ def poseMsgToKdlFrame(pose):
     """
     Convert a geometry_msgs.msg.Pose message to a ROS PyKDL.Frame object
     :param pose: Pose to be converted
-    :type pose: geometry_msgs.msg.Quaternion
-    :rtype: PyKDL.Rotation
+    :type pose: geometry_msgs.msg.Pose
+    :rtype: PyKDL.Frame
+
+    >>> pose = gm.Pose(gm.Point(1, 2, 3), gm.Quaternion(1, 0, 0, 0))
+    >>> frame = poseMsgToKdlFrame(pose)
+    >>> frame.p
+    [           1,           2,           3]
+    >>> frame.M.GetQuaternion()
+    (1.0, 0.0, 0.0, 0.0)
     """
     rot = kdl.Rotation.Quaternion(pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
     trans = kdl.Vector(pose.position.x ,pose.position.y, pose.position.z)
@@ -79,8 +91,15 @@ def kdlFrameToPoseMsg(frame):
     """
     Convert a ROS PyKDL.Frame object to a geometry_msgs.msg.Pose message
     :param frame: Frame to be converted
-    :type frame: PyKDL.Rotation
-    :rtype: geometry_msgs.msg.Quaternion
+    :type frame: PyKDL.Frame
+    :rtype: geometry_msgs.msg.Pose
+
+    >>> frame = kdl.Frame(kdl.Rotation.Quaternion(1, 0, 0, 0), kdl.Vector(1, 2, 3))
+    >>> pose = kdlFrameToPoseMsg(frame)
+    >>> (pose.position.x, pose.position.y, pose.position.z)
+    (1.0, 2.0, 3.0)
+    >>> (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+    (1.0, 0.0, 0.0, 0.0)
     """
     pose = gm.Pose()
     pose.position = kdlVectorToPointMsg(frame.p)
