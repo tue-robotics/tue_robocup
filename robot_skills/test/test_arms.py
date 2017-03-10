171d564cf1466f2f5728f7425899949c8fa6bcde
Loy van Beek <loy.vanbeek@gmail.com>
2017-03-10 19:56:47 +0100

    Add basic test for arms.

--- robot_skills/test/test_arms.py
+++ robot_skills/test/test_arms.py
@@ -28,17 +28,30 @@ if not robot.leftArm.operational:
 
 goal1 = kdl_conversions.kdlFrameStampedFromXYZRPY(0.192, 0.125, 0.748, 0, 0, 0, "/"+robot.robot_name+"/base_link")
 
-robot.leftArm.send_goal(goal1)
+if not robot.leftArm.send_goal(goal1):
+    robot.speech.speak("Could not reach arm goal pose")
 robot.leftArm.wait_for_motion_done()
 
-robot.leftArm.send_gripper_goal("open")
-robot.speech.speak("My left hand is now open")
+if robot.leftArm.send_gripper_goal("open"):
+    robot.speech.speak("My left hand is now open")
+else:
+    robot.speech.speak("Could not open left hand")
 robot.leftArm.wait_for_motion_done()
 
-robot.leftArm.send_gripper_goal("close")
-robot.speech.speak("And closed")
-
+if robot.leftArm.send_gripper_goal("close"):
+    robot.speech.speak("Now my left hand is closed")
+else:
+    robot.speech.speak("Could not close left hand")
 robot.leftArm.wait_for_motion_done()
+
+
+for config in robot.leftArm.default_configurations.keys():
+    robot.speech.speak("Arm going to {p} {postfix}".format(p=config, postfix="pose" if "pose" not in config else ""))
+    if not robot.leftArm.send_joint_goal(config):
+        robot.speech.speak("Could not reach {p} {postfix}".format(p=config, postfix="pose" if "pose" not in config else ""))
+    robot.leftArm.wait_for_motion_done()
+    robot.leftArm.reset()
+    robot.leftArm.wait_for_motion_done()
 
 # TODO: Now check that the hand frame is within some tolerance to the desired goal.
 
