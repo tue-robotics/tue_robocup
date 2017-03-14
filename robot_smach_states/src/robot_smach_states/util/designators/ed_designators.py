5b3d701877d6a34f04d50043fad8100b5f0f68fa
Loy van Beek <loy.vanbeek@gmail.com>
2017-03-14 20:24:46 +0100

    Use frame_stamped.frame_id and not the implicit /map

--- robot_smach_states/src/robot_smach_states/util/designators/ed_designators.py
+++ robot_smach_states/src/robot_smach_states/util/designators/ed_designators.py
@@ -328,7 +328,7 @@ class EmptySpotDesignator(Designator):
             y = frame_stamped.frame.p.y()
             ro = "(x-%f)^2+(y-%f)^2 < %f^2"%(x, y, radius+0.075)
             ri = "(x-%f)^2+(y-%f)^2 > %f^2"%(x, y, radius-0.075)
-            pos_constraint = PositionConstraint(constraint=ri+" and "+ro, frame="/map")
+            pos_constraint = PositionConstraint(constraint=ri+" and "+ro, frame=frame_stamped.frame_id)
 
             plan_to_poi = self.robot.base.global_planner.getPlan(pos_constraint)
 
