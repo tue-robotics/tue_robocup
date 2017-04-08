30bcca3fa277325bf89b0b313d2631c9fc26cade
Loy van Beek <loy.vanbeek@gmail.com>
2017-04-08 12:10:58 +0200

    Cleanup world model test

--- robot_skills/test/test_worldmodel.py
+++ robot_skills/test/test_worldmodel.py
@@ -31,16 +31,19 @@ robot_loc = robot.base.get_location()  # This is just Frame, TODO make this retu
 here_vs = kdl_conversions.FrameStamped(robot_loc, "/map").extractVectorStamped()
 radius = 2
 close_entities = robot.ed.get_entities(center_point=here_vs, radius=radius)  # Get all the entities within radius of the robot
-robot.speech.speak("There are {count} entities within {radius} meters of my base".format(count=len(close_entities), radius=radius))
+robot.speech.speak("There are {count} entities within {radius} meters of my base"
+                   .format(count=len(close_entities), radius=radius))
 
 if not len(close_entities) < len(all_entities):
     robot.speech.speak("Oops, there should be less close entities than there are entities in the world")
     failed_actions += ["get_entities with center_point and radius"]
 
-type = "trashbin"
+# Get all the entities of some type within radius of the robot
+query_type = "trashbin"
 radius_2 = 10
-close_entities_of_type = robot.ed.get_entities(type=type, center_point=here_vs, radius=radius_2)  # Get all the entities of some type within radius of the robot
-robot.speech.speak("There are {count} {type}s within {radius} meters of my base".format(count=len(close_entities_of_type), radius=radius_2, type=type))
+close_entities_of_type = robot.ed.get_entities(type=query_type, center_point=here_vs, radius=radius_2)
+robot.speech.speak("There are {count} {type}s within {radius} meters of my base"
+                   .format(count=len(close_entities_of_type), radius=radius_2, type=query_type))
 
 #######################
 # Test Closest Entity #
@@ -49,13 +52,14 @@ robot.speech.speak("There are {count} {type}s within {radius} meters of my base"
 closest = robot.ed.get_closest_entity()
 robot.speech.speak("The closest entity is {id}, of type {type}".format(id=closest.id[:6], type=closest.type))
 
-# import ipdb; ipdb.set_trace()
-closest2 = robot.ed.get_closest_entity(center_point=kdl.Vector(0, 0, 0), radius=2.0)  # TODO: center_point should also be a VectorStamped
-robot.speech.speak("The closest entity to the center of the arena is {id}, of type {type}".format(id=closest2.id[:6], type=closest2.type))
+# TODO: center_point should also be a VectorStamped
+closest2 = robot.ed.get_closest_entity(center_point=kdl.Vector(0, 0, 0), radius=2.0)
+robot.speech.speak("The closest entity to the center of the arena is {id}, of type {type}"
+                   .format(id=closest2.id[:6], type=closest2.type))
 
-closest3 = robot.ed.get_closest_entity(type=type, center_point=kdl.Vector(0, 0, 0), radius=10.0)
-robot.speech.speak("The closest {type} to the center of the arena is {id}".format(id=closest3.id, type=type))
-if closest3.type != type:
+closest3 = robot.ed.get_closest_entity(type=query_type, center_point=kdl.Vector(0, 0, 0), radius=10.0)
+robot.speech.speak("The closest {type} to the center of the arena is {id}".format(id=closest3.id, type=query_type))
+if closest3.type != query_type:
     failed_actions += ["get_closest_entity with type, center_point and radius"]
 
 
