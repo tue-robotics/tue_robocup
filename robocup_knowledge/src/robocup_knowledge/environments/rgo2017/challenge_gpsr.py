f91d89f4195dd959760cc3418ef919bc20a8b5e8
Rein Appeldoorn <reinzor@gmail.com>
2017-05-04 17:55:25 +0200

    wip(): Knowledge grammars

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2017/challenge_gpsr.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2017/challenge_gpsr.py
@@ -22,6 +22,11 @@ rooms = common.rooms + ["entrance", "exit"]
 grammar_target = "T"
 
 grammar = """
+##############################################################################
+#
+# Actions
+#
+##############################################################################
 T[{actions : <A1>}] -> C[A1]
 #T[{actions : <A1, A2>}] -> C[A1] and C[A2]
 #T[{actions : <A1, A2, A3>}] -> C[A1] C[A2] and C[A3]
@@ -30,47 +35,83 @@ C[{A}] -> VP[A]
 
 ##############################################################################
 #
-# Verbs
+# Verbs & shared stuff
 #
 ##############################################################################
-VBTAKE -> bring | take
-VBPLACE -> put | place
-VBBRING -> bring | give
-VBDELIVER -> VBBRING | deliver
-VBTAKE -> get | grasp | take | pick up
-VBSPEAK -> tell | say
-VBGOPL["navigate-to"] -> go to | navigate to
-VBGOR -> VBGOPL| enter
-VBFIND -> find | locate | look for
-VBGUIDE -> guide | escort | take | lead | accompany
-VBFOLLOW -> follow | go after | come after
+V_SAY -> tell | say | speak
+V_TAKE -> bring | take
+V_PLACE -> put | place
+V_BRING -> give | bring | hand | deliver | take | carry | transport
+V_TAKE -> get | grasp | take | pick up
+V_SPEAK -> tell | say
+V_FIND -> find | locate | look for
+V_GUIDE -> guide | escort | take | lead | accompany
+V_FOLLOW -> follow | go after | come after
 
+DET -> the | a
+"""
+
+grammar += """
 ##############################################################################
 #
 # Bring
 #
 ##############################################################################
-VP = VBTAKE BRING_ENTITIES to the BRING_LOCATIONS
-VP = VBPLACE the BRING_ENTITIES on the {placement 2}
-VP = $vbbring me the $object
-VP = $vbdeliver the $object to $someone
-VP = $takefrom to the {placement 2}
-VP = $goplace, $vbfind the $object, and ($delivme | $delivat)
-VP = $goplace, $vbfind the $object, and $place
+# VP = V_TAKE BRING_ENTITIES to the BRING_LOCATIONS
+# VP = V_PLACE the BRING_ENTITIES on the {placement 2}
+# VP = $V_bring me the $object
+# VP = $V_deliver the $object to $someone
+# VP = $takefrom to the {placement 2}
+# VP = $goplace, $V_find the $object, and ($delivme | $delivat)
+# VP = $goplace, $V_find the $object, and $place
 
 # TO BE FILLED IN BY THE KNOWLEDGE / SEMANTICS
 BRING_ENTITIES -> coke | fanta
+"""
 
+grammar += """
 ##############################################################################
 #
 # Navigate
 #
 ##############################################################################
-VP["action": A, "entity": X] -> VBGOPL[A] the NAV_LOCATIONS[X]
+V_GOPL -> go to | navigate to
+V_GOR -> V_GOPL | enter
 
+VP["action": "navigate-to", "entity": {"id": X}] -> V_GOR the ROOMS[X]
+VP["action": "navigate-to", "entity": {"id": X}] -> V_GOPL the LOCATIONS[X]
 """
 
-for loc in common.get_locations():
-    grammar += '\nNAV_LOCATIONS[{"id": %s}] -> %s' % (loc, loc)
+for room in common.rooms:
+    grammar += '\nROOMS[%s] -> the %s' % (room, room)
 
-print grammar
+for loc in common.get_locations():
+    grammar += '\nLOCATIONS[%s] -> the %s' % (loc, loc)
+
+grammar += """
+##############################################################################
+#
+# SAY
+#
+##############################################################################
+VP["action": "say", "sentence": X] -> SAY_SENTENCE[X]
+
+SAY_SENTENCE["ROBOT_NAME"] -> V_SAY your name
+SAY_SENTENCE["TIME"] -> V_SAY the time | V_SAY what time it is | V_SAY what time is it
+SAY_SENTENCE["my team is tech united"] -> V_SAY the name of your team
+SAY_SENTENCE["DAY_OF_MONTH"] -> V_SAY the day of the month
+SAY_SENTENCE["DAY_OF_WEEK"] -> V_SAY the day of the week
+SAY_SENTENCE["TODAY"] -> V_SAY what day is today | V_SAY me what day it is | V_SAY the date
+SAY_SENTENCE["TOMORROW"] -> V_SAY what day is tomorrow
+"""
+
+if __name__ == "__main__":
+    from grammar_parser.cfgparser import CFGParser
+    grammar_parser = CFGParser.fromstring(grammar)
+    sentence = grammar_parser.get_random_sentence("T")
+
+    print "Parsing sentence:\n\n{}\n\n".format(sentence)
+
+    result = grammar_parser.parse("T", sentence)
+
+    print "Result:\n\n{}".format(result)
