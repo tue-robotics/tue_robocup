e95e7a83ea46a55a1943a3610784f0d177168ba5
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-05-06 20:26:49 +0200

    fix(Final) Initial pose bug

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2017/challenge_final.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2017/challenge_final.py
@@ -4,4 +4,4 @@ from robocup_knowledge import knowledge_loader
 # Load the common knowledge
 common = knowledge_loader.load_knowledge("common")
 
-starting_point = "initial_pose"
+initial_pose = "initial_pose"
