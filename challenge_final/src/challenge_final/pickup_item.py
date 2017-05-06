16c8da412a3640ad78fe5f2ea5c76907b326b10c
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-05-07 00:07:35 +0200

    wip(Final) Added PickupItem state

---
+++ challenge_final/src/challenge_final/pickup_item.py
@@ -0,0 +1,26 @@
+# ROS
+import smach
+import robot_smach_states as states
+from challenge_storing_groceries.manipulate_machine import DefaultGrabDesignator, GrabSingleItem
+
+
+class PickupItem(smach.StateMachine):
+    """ State machine to pick up an item. Inspects the entity that has been pointed at and grabs the item on top
+     of it
+    """
+    def __init__(self, robot, furniture_designator):
+        """ Constructor
+        :param robot: robot object
+        """
+        smach.StateMachine.__init__(self, outcomes=["succeeded", "failed"])
+
+        with self:
+            smach.StateMachine.add("INSPECT_FURNITURE",
+                                   states.Inspect(robot, furniture_designator),
+                                   transitions={"done": "PICKUP_FROM_FURNITURE",
+                                                "failed": "PICKUP_FROM_FURNITURE"})
+
+            smach.StateMachine.add("PICKUP_FROM_FURNITURE",  # Pickup the thing standing on the furniture
+                                   GrabSingleItem(robot, DefaultGrabDesignator(furniture_designator, "on_top_of")),
+                                   transitions={"succeeded": "succeeded",
+                                                "failed": "failed"})
