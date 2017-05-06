e97bbf6539cda40d473eb601e31ff935f0717b74
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-05-06 10:51:15 +0200

    cleanup(Follow) Removed commented stuff

--- robot_smach_states/src/robot_smach_states/navigation/follow_operator.py
+++ robot_smach_states/src/robot_smach_states/navigation/follow_operator.py
@@ -446,11 +446,8 @@ class FollowOperator(smach.State):
                 i = 0
 
             self._robot.head.wait_for_motion_done()
-            # print "Trying to detect faces..."
-            # rospy.logerr("ed.detect_persons() method disappeared! This was only calling the face recognition module and we are using a new one now!")
-            # rospy.logerr("I will return an empty detection list!")
 
-            # detections is a list of Recognitions
+            # raw_detections is a list of Recognitions
             # a recognition constains a CategoricalDistribution
             # a CategoricalDistribution is a list of CategoryProbabilities
             # a CategoryProbability has a label and a float
@@ -470,55 +467,11 @@ class FollowOperator(smach.State):
             else:
                 best_detection = None
                 recovered_operator = None
-            # if not detections:
-            #     detections = []
-            # best_score = -0.5 # TODO: magic number
-            # best_detection = None
-            # for d in detections:
-            #     print "name: %s" % d.name
-            #     print "score: %f" % d.name_score
-            #     if d.name == self._operator_name and d.name_score > best_score:
-            #         best_score = d.name_score
-            #         best_detection = d
-            #
-            #     if not d.name:
-            #         best_detection = None
-            #         break
 
             if best_detection:
 
-                print "Best detection: {}".format(best_detection)
-
-                #depth_image = self._robot.head.get_depth_image()
+                # print "Best detection: {}".format(best_detection)
                 roi = best_detection.roi
-                # # Resolution of depth image is half of the resolution of the rgb image
-                # x_min = roi.x_offset / 2
-                # x_width = roi.width / 2
-                # y_min = roi.y_offset / 2
-                # y_width = roi.height / 2
-                #
-                # # Take the inner part of the ROI
-                # xdmin = x_min + x_width / 3
-                # xdmax = x_min + 2 * x_width / 3
-                # ydmin = y_min + y_width / 3
-                # ydmax = y_min + 2 * y_width / 3
-                #
-                # # Get the average depth
-                # t = 0.0  # Total
-                # c = 0.0  # Counter
-                # for x in range(xdmin, xdmax):
-                #     for y in range(ydmin, ydmax):
-                #         t += depth_image.data[y * depth_image.width + x]
-                #         c += 1.0
-                # depth = t/c
-
-
-                # print "Trying to find closest laser entity to face"
-                # print "best detection frame id: %s"%best_detection.pose.header.frame_id
-                # operator_pos = geometry_msgs.msg.PointStamped()
-                # operator_pos.header.stamp = best_detection.pose.header.stamp
-                # operator_pos.header.frame_id = best_detection.pose.header.frame_id
-                # operator_pos.point = best_detection.pose.pose.position
 
                 try:
                     operator_pos_kdl = self._robot.head.project_roi(roi=roi, frame_id="map")
