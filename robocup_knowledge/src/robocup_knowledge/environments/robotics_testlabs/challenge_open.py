c95488a7f8487611936f157841ce30036c7497b3
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-05-20 16:53:17 +0200

    open of RGO2017 working again

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_open.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_open.py
@@ -1,23 +1,23 @@
-from itertools import groupby
 from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
-initial_pose = "initial_pose_door_B"  # initial pose
-starting_point = "eegpsr_starting_pose"  # Designated pose to wait for commands
-exit_waypoint = "exit_door_B1"
-ask_waypoint = "eegpsr_starting_pose"
+# initial_pose = "initial_pose_door_B"  # initial pose
+# exit_waypoint = "exit_door_B1"
+# starting_point = "gpsr_meeting_point"  # Designated pose to wait for commands
+starting_point = "initial_pose"
+ask_waypoint = "gpsr_meeting_point"
 
-translations = { "bookcase" : "bocase" }
+translations = {"bookcase": "bocase"}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # We have no rooms, because we're mapping:
+#
+# rooms = []
+# common.rooms = []
+#
+# def get_room(location):
+#     return None
 
-rooms = []
-common.rooms = []
-
-def get_room(location):
-    return None
-~
 # required keys: entity_id (str), room_id (str), navigation_area (str), segment_areas (list)
 inspection_places = [
     {"entity_id": "dinner_table",
@@ -30,13 +30,19 @@ inspection_places = [
      "navigate_area": "in_front_of",
      "segment_areas": ["on_top_of"]},
 
-    {"entity_id": "nightstand",
-     "room_id": "bedroom",
+    {"entity_id": "cabinet",
+     "room_id": "hallway",
      "navigate_area": "in_front_of",
      "segment_areas": ["on_top_of"]},
 
     {"entity_id": "bookcase",
      "room_id": "livingroom",
      "navigate_area": "in_front_of",
-     "segment_areas": ["shelf1", "shelf2", "shelf3", "shelf4", "shelf5"]},
+     "segment_areas": ["shelf2", "shelf3", "shelf4", "shelf5"]},
 ]
+
+grammar = "T[X] -> PLACES[X]"
+grammar_target = "T"
+for place in inspection_places:
+    grammar += "\nPLACES[%s] -> %s" % (place['entity_id'], place['entity_id'])
+    grammar += "\nPLACES[%s] -> %s" % (place['entity_id'], place['room_id'])
