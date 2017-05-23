f6faf62ae8740fe15fc17cb09a9cb46cdbc9b100
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-05-23 22:45:33 +0200

    fix knowledge open

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_open.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_open.py
@@ -1,22 +1,11 @@
 from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
-# initial_pose = "initial_pose_door_B"  # initial pose
-# exit_waypoint = "exit_door_B1"
-# starting_point = "gpsr_meeting_point"  # Designated pose to wait for commands
 starting_point = "initial_pose"
 ask_waypoint = "gpsr_meeting_point"
 
 translations = {"bookcase": "bocase"}
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# We have no rooms, because we're mapping:
-#
-# rooms = []
-# common.rooms = []
-#
-# def get_room(location):
-#     return None
 
 # required keys: entity_id (str), room_id (str), navigation_area (str), segment_areas (list)
 inspection_places = [
@@ -25,13 +14,13 @@ inspection_places = [
      "navigate_area": "near",
      "segment_areas": ["on_top_of"]},
 
-    {"entity_id": "battery_table",
-     "room_id": "workshop",
+    {"entity_id": "hallway_table",
+     "room_id": "hallway",
      "navigate_area": "in_front_of",
      "segment_areas": ["on_top_of"]},
 
     {"entity_id": "cabinet",
-     "room_id": "hallway",
+     "room_id": "kitchen",
      "navigate_area": "in_front_of",
      "segment_areas": ["on_top_of"]},
 
