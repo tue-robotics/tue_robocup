08fa84f2c295db6427d562777751af1cea10006b
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-06-11 11:52:39 +0200

    userdata=None

--- challenge_open/src/handle_detected_entities.py
+++ challenge_open/src/handle_detected_entities.py
@@ -16,7 +16,7 @@ class SelectEntity(smach.State):
         self._entity_classifications_designator = entitity_classifications_designator
         self._selected_entity_designator = selected_entity_designator
 
-    def execute(self, userdata):
+    def execute(self, userdata=None):
 
         # Try to pop item from entities_ids_designator
         try:
