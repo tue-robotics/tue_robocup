116fdec92dde99441635d256807b4a4d4bd10c8e
Sam Alexandrov <alexandrov.samuil@gmail.com>
2017-06-13 22:14:25 +0200

    Added scaffolding for set_a_table challenge and action plan in the ReadMe file

--- challenge_set_a_table/README.md
+++ challenge_set_a_table/README.md
@@ -9,13 +9,38 @@ amigo-free-mode
 rosrun challenge_set_a_table challenge_set_a_table.py amigo
 ```
 
+## CHALLENGE PLAN
+
+	# PART I
+	- hear: "Set the table!"
+	+ say: "What should I serve, master?"
+	- set the table:
+		- grab objects from their default place (6 objects)
+		- open door to grab objects???
+		- place the object to the table
+	- serve the meal: (2 options out of cereal and milk, sandwiches, toasts, Coffee and bread)
+		- pour milk in the bowl??? (We can bring our own food)
+		- pour cereals in the bowl???
+	- correct object positions:
+		- inspect the table
+		- move the objects to their original places???
+
+	# PART II
+	- hear: "Clean the table!"
+	- clear up the table:
+		- grab objects from table
+		- place the object to their default place
+	- clean the table:
+		- detect spots and spills???
+		- clean them using a cleaning cloth???
+
 ## DONE
 
--
+- scaffolding
+- planning
 
 ## TODO
 
-1. Plan - what is possible for execution
-2. Make scaffold with dummy functions
-3. Check for ready available functions and implement them
-4. Make / ask someone for missing functions
+1. Check what of the plan is possible
+2. Check for ready available functions and implement them
+3. Make the missing functions
--- challenge_set_a_table/src/challenge_set_a_table.py
+++ challenge_set_a_table/src/challenge_set_a_table.py
@@ -7,11 +7,14 @@ import os
 import datetime
 import math
 
-from robot_smach_states import Initialize, Say, WaitTime
+from robot_smach_states import Initialize, Say
 from robot_smach_states.util.startup import startup
 import robot_smach_states.util.designators as ds
 
-#from challenge_set_a_table_states import detect
+# Still empty
+#from challenge_set_a_table_states import clean_table
+#from challenge_set_a_table_states import fetch_command
+#from challenge_set_a_table_states import set_table
 
 
 class ChallengeSetATable(smach.StateMachine):
@@ -19,14 +22,49 @@ class ChallengeSetATable(smach.StateMachine):
         smach.StateMachine.__init__(self, outcomes=['Done','Aborted'])
 
         with self:
+            #Part I: Set a table
             smach.StateMachine.add('INITIALIZE',
+                                   Initialize(robot),
+                                   transitions={'initialized': 'FETCH_COMMAND_I',
+                                                'abort': 'Aborted'})            
+            smach.StateMachine.add('FETCH_COMMAND_I', # Hear "set the table"
+                                   Initialize(robot),
+                                   transitions={'initialized': 'ASK_FOR_MEAL',
+                                                'abort': 'Aborted'})
+            smach.StateMachine.add('ASK_FOR_MEAL',
+                                   Say(robot, "What should I serve, master?"),
+                                   transitions={'spoken': 'SET_THE_TABLE'})
+
+            smach.StateMachine.add('SET_THE_TABLE', # Set the table (bring the objects to the table)
+                                   Initialize(robot),
+                                   transitions={'initialized': 'SERVE_MEAL',
+                                                'abort': 'Aborted'})
+            smach.StateMachine.add('SERVE_MEAL', # Serve the meal (pour milk into the bowl)
+                                   Initialize(robot),
+                                   transitions={'initialized': 'CORRECT_OBJECT_POSITIONS',
+                                                'abort': 'Aborted'})
+            smach.StateMachine.add('CORRECT_OBJECT_POSITIONS', # Inspect table and correct the moved objects
+                                   Initialize(robot),
+                                   transitions={'initialized': 'FETCH_COMMAND_II',
+                                                'abort': 'Aborted'})
+
+            #Part II: Clean the table
+            smach.StateMachine.add('FETCH_COMMAND_II', #Hear "clear up the table"
+                                   Initialize(robot),
+                                   transitions={'initialized': 'AUFRAEUMEN',
+                                                'abort': 'Aborted'})
+            smach.StateMachine.add('AUFRAEUMEN', # Clear up the table (bring the objects to their default location)
+                                   Initialize(robot),
+                                   transitions={'initialized': 'CLEAN_THE_TABLE',
+                                                'abort': 'Aborted'})
+            smach.StateMachine.add('CLEAN_THE_TABLE', # Inspect for spots and spills and clean them
                                    Initialize(robot),
                                    transitions={'initialized': 'END_CHALLENGE',
                                                 'abort': 'Aborted'})
-       
+            
             # End
             smach.StateMachine.add('END_CHALLENGE',
-                                   Say(robot, "My work here is done, goodbye!"),
+                                   Say(robot, "I am finally free!"),
                                    transitions={'spoken': 'Done'})
 
             ds.analyse_designators(self, "set_a_table")
