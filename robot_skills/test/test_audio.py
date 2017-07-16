4125eae15e50cc1c4d10d9b7e0730c61092bcd3c
Loy van Beek <loy.vanbeek@gmail.com>
2017-07-16 13:55:46 +0200

    Fix #321

--- robot_skills/test/test_audio.py
+++ robot_skills/test/test_audio.py
@@ -36,32 +36,48 @@ places = ['bedroom','livingroom', 'hallway', 'kitchen']
 object_category = [ 'drink', 'snack', 'food', 'cleaningstuff']
 location_category = ['utensil','shelf','seating','table','trashbin','appliance']
 actions = ['transport','bring','carry','bring','get','give','point','look','detect','find','move','go','navigate']
-sides = ["left","right","front","back"]
-numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-restaurant_locations = ["ordering location","food location", "drink location"]
+restaurant_grammar = """O[P] -> ORDER[P] | can i have a ORDER[P] | i would like ORDER[P] | can i get ORDER[P] | could i have ORDER[P] | may i get ORDER[P] | bring me ORDER[P]
+ORDER[OO] -> COMBO[OO] | BEVERAGE[OO]
+BEVERAGE[{"beverage": B}] -> BEV[B]
+BEVERAGE[{"beverage": B}] -> DET BEV[B]
+COMBO[{"food1": F1, "food2": F2}] -> FOOD[F1] and FOOD[F2] | FOOD[F1] with FOOD[F2]
+COMBO[{"food1": F1, "food2": F2}] -> DET FOOD[F1] and FOOD[F2] | DET FOOD[F1] with FOOD[F2]
+COMBO[{"food1": F1, "food2": F2}] -> FOOD[F1] and DET FOOD[F2] | FOOD[F1] with DET FOOD[F2]
+COMBO[{"food1": F1, "food2": F2}] -> DET FOOD[F1] and DET FOOD[F2] | DET FOOD[F1] with DET FOOD[F2]
+DET -> a | an
+BEV['coke'] -> coke[B]
+BEV['fanta'] -> fanta[B]
+FOOD['pizza'] -> pizza[B]
+BEV['steak'] -> steak[B]"""
 
-def ask(question, spec, choices):
+
+def ask(question, grammar, target):
     s.speak(question)
-    print "Choices are: " + str(choices)
-    r = e.recognize(spec, choices)
-
+    print "Grammar: " + str(grammar)
+    r = robot.hmi.query(question, grammar, target)
 
     if r:
-        s.speak("You said: %s"%r.result)
+        s.speak("You said: %s"%r.sentence)
     else:
         s.speak("I don't understand, sorry")
 
     time.sleep(1)
 
+
 while not rospy.is_shutdown():
-    ask("What would you like to drink?", "<drink>", {"drink":drinks})
+    ask("What would you like to drink?", "T -> " + "|".join(drinks), "T")
 
-    ask("Ask me a EGPSR question", "<action> (me) (a|an|the) <object> (from|of) (the|a|an) <location> (to|of|from) (the|a|an) <location2> ", {"action":actions,"object":object_category,"location":location_category+places,"location2":location_category+places})
+    # ask("Ask me a EGPSR question", "<action> (me) (a|an|the) <object> (from|of) (the|a|an) <location> (to|of|from) (the|a|an) <location2> ",
+    #     {"action":actions,
+    #      "object":object_category,
+    #      "location":location_category+places,
+    #      "location2":location_category+places})
 
-    ask(random.choice(["What is your name?","Hey, how should I call you?"]), "<name>", {"name":names})
+    ask(random.choice(["What is your name?", "Hey, how should I call you?"]),
+            "Name -> " +"|".join(names), "Name")
 
     for i in range(0,4):
-        ask("Waiting for restaurant speech command","(<number> <side>|<location>)", {"side":sides,"number":numbers,"location":restaurant_locations})
+        ask("What can I get for you?", restaurant_grammar, "O")
 
 
 
