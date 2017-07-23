096b7978b73df4fb66bcd524e719818b0be3fafc
Rokus Ottervanger <rokusottervanger@gmail.com>
2017-07-24 01:43:31 +0200

    fix(knowledge/gpsr): typo in answer-question grammar

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_gpsr.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_gpsr.py
@@ -232,7 +232,7 @@ follow_action = "follow", {"location-from": {""}, "location-to": {}, "target": {
 ##############################################################################
 
 grammar += """
-VP["action": "answer_question"] -> answer a question
+VP["action": "answer-question"] -> answer a question
 """
 
 ##############################################################################
