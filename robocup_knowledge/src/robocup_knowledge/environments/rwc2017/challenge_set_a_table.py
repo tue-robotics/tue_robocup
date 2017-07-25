0df4ea5d82abf726353dbe916d695ee18734c6ab
Loy van Beek <loy.vanbeek@gmail.com>
2017-07-26 08:54:57 +0900

    wip(SetTable) Created get order class

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2017/challenge_set_a_table.py
@@ -0,0 +1,13 @@
+from robocup_knowledge import knowledge_loader
+common = knowledge_loader.load_knowledge("common")
+
+initial_pose = "ToDo"  # ""initial_pose"
+starting_pose = "ToDo"  # "gpsr_meeting_point"
+
+cupboard = "ToDo"  # "cabinet"
+cupboard_surface = "ToDo"  # "on_top_of"
+
+table = "ToDo"  # "dinner_table"
+table_surface = "ToDo"  # "on_top_of"
+
+breakfast_options = ["curry", "corn"]
