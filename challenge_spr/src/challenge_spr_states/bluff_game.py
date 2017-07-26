968d15f2dba3183190df012d1921ca2d02af749b
Rein Appeldoorn <reinzor@gmail.com>
2017-07-26 10:09:14 +0200

    feat(spr): Turning based on SSL

--- challenge_spr/src/challenge_spr_states/bluff_game.py
+++ challenge_spr/src/challenge_spr_states/bluff_game.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import threading
 
 import roslib
 import rospy
@@ -19,53 +20,65 @@ from robocup_knowledge import load_knowledge
 
 from riddle_game import hear, answer
 
+# def _turn_to_closest_entity(robot):
+#     # Reset the world model just to be sure
+#     robot.ed.reset()
+#
+#     operator = None
+#     while not operator:
+#         operator = robot.ed.get_closest_entity(radius=1.9, center_point=robot.base.get_location().extractVectorStamped())
+#         print operator
+#         if not operator:
+#             vth = 0.5
+#             th = 3.1415 / 10
+#             print "Turning %f radians with force drive" % th
+#             robot.base.force_drive(0, 0, vth, th / vth)
+#
+#     robot.base.force_drive(0, 0, 0, 0.5)
+#
+#     # Turn towards the operator
+#     current = robot.base.get_location()
+#     robot_th = current.frame.M.GetRPY()[2]  # Get the Yaw, rotation around Z
+#     desired_th = math.atan2(operator._pose.p.y() - current.frame.p.y(),
+#                             operator._pose.p.x() - current.frame.p.x())
+#
+#     # Calculate params
+#     th = desired_th - robot_th
+#     if th > 3.1415:
+#         th -= 2 * 3.1415
+#     if th < -3.1415:
+#         th += 2 * 3.1415
+#     vth = 0.5
+#
+#     # Turn
+#     robot.base.force_drive(0, 0, (th / abs(th)) * vth, abs(th) / vth)
 
-
-def _turn_to_closest_entity(robot):
-    # Reset the world model just to be sure
-    robot.ed.reset()
-
-    operator = None
-    while not operator:
-        operator = robot.ed.get_closest_entity(radius=1.9, center_point=robot.base.get_location().extractVectorStamped())
-        print operator
-        if not operator:
-            vth = 0.5
-            th = 3.1415 / 10
-            print "Turning %f radians with force drive" % th
-            robot.base.force_drive(0, 0, vth, th / vth)
-
-    robot.base.force_drive(0, 0, 0, 0.5)
-
-    # Turn towards the operator
-    current = robot.base.get_location()
-    robot_th = current.frame.M.GetRPY()[2]  # Get the Yaw, rotation around Z
-    desired_th = math.atan2(operator._pose.p.y() - current.frame.p.y(),
-                            operator._pose.p.x() - current.frame.p.x())
-
-    # Calculate params
-    th = desired_th - robot_th
-    if th > 3.1415:
-        th -= 2 * 3.1415
-    if th < -3.1415:
-        th += 2 * 3.1415
-    vth = 0.5
-
-    # Turn
-    robot.base.force_drive(0, 0, (th / abs(th)) * vth, abs(th) / vth)
 
 def turn_to_closest_entity(robot):
 
-    print "Last talker id: " + robot.hmi.last_talker_id
+    rospy.sleep(0.5)
 
-    # Calculate params
-    if "dragonfly_speech_recognition" not in robot.hmi.last_talker_id:
-        # TUrn
-        vth = 0.5
-        th = 3.1415 * 2 / 5
-        robot.base.force_drive(0, 0, (th / abs(th)) * vth, abs(th) / vth)
+    start = rospy.Time.now()
+    yaw = None
 
-    # _turn_to_closest_entity(robot)
+    # Try to find a source for a couple of seconds
+    while not yaw and (rospy.Time.now() - start).to_sec() < 6:
+        yaw = robot.ssl.get_last_yaw()
+        rospy.sleep(0.05)
+
+    # If we did not find a yaw, just default
+    if yaw is None:
+        yaw = math.pi * 2 / 5
+
+    if yaw > math.pi:
+        yaw -= 2 * math.pi
+    if yaw < -math.pi:
+        yaw += 2 * math.pi
+
+    rospy.loginfo("I should turn %.2f rad now", yaw)
+
+    vyaw = 0.5
+    robot.base.force_drive(0, 0, (yaw / abs(yaw)) * vyaw, abs(yaw) / vyaw)
 
 
 class HearQuestion(smach.State):
@@ -79,9 +92,12 @@ class HearQuestion(smach.State):
 
         self.robot.head.look_at_standing_person()
 
+        t = threading.Thread(target=turn_to_closest_entity, args=(self.robot,))
+        t.start()
+
         res = hear(self.robot, time_out=self.time_out)
 
-        turn_to_closest_entity(self.robot)
+        t.join()
 
         return answer(self.robot, res, crowd_data)
 
