4001c220af053b011e046988d98586fea41055d6
Loy van Beek <loy.vanbeek@gmail.com>
2017-07-26 12:01:45 +0900

    wip(Knowledge) Updated knowledge of storing groceries

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2017/challenge_storing_groceries.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2017/challenge_storing_groceries.py
@@ -16,6 +16,9 @@ cabinet_amcl = "kitchen_shelf"
 # cabinet_poses = [FrameStamped(frame=kdl.Frame(kdl.Rotation(), kdl.Vector(0.144, 3.574, 0.0)), frame_id="map"),
 #                  FrameStamped(frame=kdl.Frame(kdl.Rotation.RPY(0.0, 0.0, 1.570796), kdl.Vector(2.271, -1.258, 0.0)),
 #                               frame_id="map")]
+cabinet_poses = [FrameStamped(frame=kdl.Frame(kdl.Rotation.RPY(0.0, -0.0, 0.1292036732),
+                                              kdl.Vector( 2.39, -4.125,0)),
+                              frame_id="map")]
 object_shelves = ["shelf2", "shelf3", "shelf4", "shelf5"]
 object_types = [obj["name"] for obj in common.objects]
 
@@ -25,11 +28,15 @@ room = "kitchen"
 #
 # Placing
 default_place_entity = cabinet_amcl
-default_place_area = "shelf3"  # ToDo: update
+default_place_area = "shelf3"
 #
 # # Table pose
 # table_poses = [FrameStamped(frame=kdl.Frame(kdl.Rotation.RPY(0.0, 0.0, -0.5 * math.pi), kdl.Vector(1.0, 4.0, 0.0)),
 #                             frame_id="map"),
 #                FrameStamped(frame=kdl.Frame(kdl.Rotation.RPY(0.0, 0.0, math.pi), kdl.Vector(3.0, 0.0, 0.0)),
 #                             frame_id="map")]
-# entity_poses = zip(cabinet_poses, table_poses)
+# N.B.: rotated 180 deg to have in front of at the right side
+table_poses = [FrameStamped(frame=kdl.Frame(kdl.Rotation.RPY(0.0, -0.0, -1.57079632679),
+                                            kdl.Vector(4.16, -2.86, 0)),
+                            frame_id="map")]
+entity_poses = zip(cabinet_poses, table_poses)
