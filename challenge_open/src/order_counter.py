5d8262ba43d9df5772e8d49e83570e24d51d6d55
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-07-28 13:44:24 +0900

    fix(OrderCounter) Fixed Tim's bugs

--- challenge_open/src/order_counter.py
+++ challenge_open/src/order_counter.py
@@ -103,9 +103,9 @@ class OrderCounter(smach.State):
 
         # Play sound if the number of people changed
         if count > self._number_of_thirsty_people:
-			self.robot.speech.speak("ping")
-		elif count > self._number_of_thirsty_people:
-			self.robot.speech.speak("pong")
+            self.robot.speech.speak("ping")
+        elif count < self._number_of_thirsty_people:
+            self.robot.speech.speak("pong")
 
         # Remember the number of thirsty people
         self._number_of_thirsty_people = count
