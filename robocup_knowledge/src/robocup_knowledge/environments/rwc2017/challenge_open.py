bf07c7f962bf1668b31477a6f9142f894c00d4d8
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-07-28 18:52:58 +0900

    feat(Open) Added stuff to statemachine

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2017/challenge_open.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2017/challenge_open.py
@@ -1,2 +1,13 @@
+# Initial pose
 initial_pose = "initial_pose"
+
+# Sound source localization
 ssl_waypoint = "living_room"
+
+# Raytrace demo
+raytrace_waypoint = "laser_demo_waypoint"  # ToDo: add
+raytrace_breakout_id = "sofa"
+
+# Openpose demo
+order_counter_waypoint = "order_counter_waypoint"   # ToDo: add
+
