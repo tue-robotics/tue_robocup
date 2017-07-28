5c94ced429da19f46179d5b02efe1410289cf5e9
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-07-28 14:05:22 +0900

    feat(Restaurant) Added navigation fallbacks

--- challenge_restaurant/src/challenge_restaurant/restaurant.py
+++ challenge_restaurant/src/challenge_restaurant/restaurant.py
@@ -85,6 +85,21 @@ class Restaurant(smach.StateMachine):
                                    states.NavigateToWaypoint(robot=robot, waypoint_designator=kitchen_designator,
                                                              radius=0.15),
                                    transitions={'arrived': 'RECITE_ORDER',
+                                                'unreachable': 'SAY_NAVIGATE_TO_KITCHEN_FALLBACK',
+                                                'goal_not_defined': 'SAY_NAVIGATE_TO_KITCHEN_FALLBACK'})
+
+            smach.StateMachine.add('SAY_NAVIGATE_TO_KITCHEN_FALLBACK',
+                                   states.Say(robot, "Help, how do I get there?"),
+                                   transitions={'spoken': 'TURN_AROUND_KITCHEN_FALLBACK'})
+
+            smach.StateMachine.add('TURN_AROUND_KITCHEN_FALLBACK',
+                                   states.Turn(robot, radians=math.pi),
+                                   transitions={'turned': 'NAVIGATE_TO_KITCHEN_FALLBACK'})
+
+            smach.StateMachine.add('NAVIGATE_TO_KITCHEN_FALLBACK',
+                                   states.NavigateToWaypoint(robot=robot, waypoint_designator=kitchen_designator,
+                                                             radius=0.20),
+                                   transitions={'arrived': 'RECITE_ORDER',
                                                 'unreachable': 'RECITE_ORDER',
                                                 'goal_not_defined': 'RECITE_ORDER'})
 
@@ -103,6 +118,21 @@ class Restaurant(smach.StateMachine):
                                                 'preempted': 'STOP'})
 
             smach.StateMachine.add('BRING_OBJECTS',
+                                   states.NavigateToObserve(robot=robot, entity_designator=caller_designator,
+                                                            radius=0.7),
+                                   transitions={'arrived': 'SAY_OBJECTS',
+                                                'unreachable': 'SAY_BRING_OBJECTS_FALLBACK',
+                                                'goal_not_defined': 'RETURN_TO_START'})
+
+            smach.StateMachine.add('SAY_BRING_OBJECTS_FALLBACK',
+                                   states.Say(robot, "Help, how do I get there?"),
+                                   transitions={'spoken': 'TURN_AROUND_BRING_OBJECTS_FALLBACK'})
+
+            smach.StateMachine.add('TURN_AROUND_BRING_OBJECTS_FALLBACK',
+                                   states.Turn(robot, radians=2 * math.pi),
+                                   transitions={'turned': 'BRING_OBJECTS_FALLBACK'})
+
+            smach.StateMachine.add('BRING_OBJECTS_FALLBACK',
                                    states.NavigateToObserve(robot=robot, entity_designator=caller_designator,
                                                             radius=0.7),
                                    transitions={'arrived': 'SAY_OBJECTS',
