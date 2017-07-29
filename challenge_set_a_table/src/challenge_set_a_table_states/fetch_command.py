006035ca0bb6d59474f765d2f10de8b4308ba8c4
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2017-07-29 12:28:29 +0900

    Catch exceptions

--- challenge_set_a_table/src/challenge_set_a_table_states/fetch_command.py
+++ challenge_set_a_table/src/challenge_set_a_table_states/fetch_command.py
@@ -53,7 +53,9 @@ def fetch(robot, time_out=15.0, task="set"):
 
         except hmi.TimeoutException:
             i += 1
-
+        except Exception as e:
+            rospy.logerr('exception: %s', e)
+            return "heard"
     return "not_heard"
 
 
@@ -157,6 +159,8 @@ class GetBreakfastOrder(smach.State):
 
             except hmi.TimeoutException:
                 pass
+            except Exception as e:
+                rospy.logerr('exception: %s', e)
 
         # If nothing has been heard, make a guess
         self._recite(self.options[0])
