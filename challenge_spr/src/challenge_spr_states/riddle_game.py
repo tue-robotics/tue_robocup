b875249031c034e0c4d1389c9993a71e490542e8
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-10-07 11:48:54 +0200

    update style

--- challenge_spr/src/challenge_spr_states/riddle_game.py
+++ challenge_spr/src/challenge_spr_states/riddle_game.py
@@ -27,8 +27,9 @@ DEFAULT_HEAR_TIME = 15.0
 #
 ##############################################################################
 
+
 class HearAndAnswerQuestions(smach.State):
-    '''
+    """
     Robot hears and answers questions from the riddle game in SPR challenge.
 
     Variables:
@@ -36,7 +37,7 @@ class HearAndAnswerQuestions(smach.State):
 
     Outputs:
         done: answered all questions
-    '''
+    """
     def __init__(self, robot, num_questions=1, hear_time=DEFAULT_HEAR_TIME):
         smach.State.__init__(self, outcomes=["done"], input_keys=['crowd_data'])
         self.robot = robot
@@ -63,20 +64,22 @@ class HearAndAnswerQuestions(smach.State):
 #
 ##############################################################################
 
+
 def hear(robot, hear_time):
-    '''
+    """
     Robots hears a question
 
     Output:
         question, if understood, else None
-    '''
+    """
     try:
         return robot.hmi.query('Question?', knowledge.grammar, 'T', timeout=hear_time)
     except TimeoutException:
         return None
 
+
 def answer(robot, res, crowd_data):
-    '''
+    """
     Robot answers the heard question
 
     Arguments:
@@ -85,7 +88,7 @@ def answer(robot, res, crowd_data):
 
     Outputs:
         True: if successfully answered the question, else False
-    '''
+    """
     if not res:
         return False
 
@@ -108,7 +111,7 @@ def answer(robot, res, crowd_data):
                    ('return_color',     answer_object_color),
                    ('compare_category', answer_compare_objects_categories),
                    ('count_object',     answer_count_objects_in_category),
-                  ]
+                   ]
 
     for action in res.semantics['actions']:
         try:
@@ -129,11 +132,14 @@ def answer(robot, res, crowd_data):
     robot.speech.speak("The answer is %s" % answer)
     return True
 
+
 def answer_random_gender(action):
     return random.choice(["A male", "A female"])
 
+
 def answer_predefined_questions(action):
     return action['solution']
+
 
 def answer_count_people(action, crowd_data):
     crowd_properties = [('people', 'crowd_size'),
@@ -152,6 +158,7 @@ def answer_count_people(action, crowd_data):
             return 'In the crowd are %d %s' % (crowd_data[property_value], property_name)
     return 'I dont know'
 
+
 def answer_placement_location(action):
     entity = action['entity']
     locations = [loc for loc in common_knowledge.locations if loc['name'] == entity]
@@ -160,6 +167,7 @@ def answer_placement_location(action):
         return 'The %s can be found in the %s' % (entity, room)
     else:
         return 'I dont know that object'
+
 
 def answer_count_placement(action):
     entity = action['entity']
@@ -184,11 +192,13 @@ def answer_find_objects(action):
     else:
         return 'I should find %s but I dont know %s' % entity
 
+
 def answer_find_category(action):
     entity = action['entity']
     loc, area_name = common_knowledge.get_object_category_location(entity)
     return 'You can find the %s on the %s' % (entity, loc)
-    
+
+
 def answer_object_category(action):
     entity = action['entity']
     objects = [obj for obj in common_knowledge.objects if obj['name'] == entity]
@@ -198,6 +208,7 @@ def answer_object_category(action):
     else:
         return 'I should name a category but I dont know what is %s' % entity
 
+
 def answer_object_color(action):
     entity = action['entity']
     objects = [obj for obj in common_knowledge.objects if obj['name'] == entity]
@@ -206,6 +217,7 @@ def answer_object_color(action):
         return 'The color of %s is %s' % (entity, col)
     else:
         return 'I should name the color but I dont know %s' % entity
+
 
 def answer_compare_objects_categories(action):
     entity_a = action['entity_a']
@@ -221,19 +233,21 @@ def answer_compare_objects_categories(action):
             return 'These objects belong to different categories'
     else:
         return 'I dont know these objects'
-    
+
+
 def answer_count_objects_in_category(action):
     entity = action['entity']
     objects = [obj for obj in common_knowledge.objects if obj['category'] == entity]
     if len(objects) > 0:
         count = len(objects)
         return 'The number of objects in category %s is %i' % (entity, count)
-        
+
 ##############################################################################
 #
 # Standalone testing:
 #
 ##############################################################################
+
 
 class TestRiddleGame(smach.StateMachine):
     def __init__(self, robot):
