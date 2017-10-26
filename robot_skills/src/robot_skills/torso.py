1ebe4a380fe618e8958b9c43ba3f8cdbefa5a70c
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-10-26 14:21:07 +0200

    let torso use robot_part.load_param & adding docstrings
    some pep8

--- robot_skills/src/robot_skills/torso.py
+++ robot_skills/src/robot_skills/torso.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 import threading
 
-import actionlib
 import control_msgs.msg
 import rospy
 import trajectory_msgs.msg
@@ -14,43 +13,78 @@ from .util import concurrent_util
 
 class Torso(RobotPart):
     def __init__(self, robot_name, tf_listener):
+        """
+        constructor
+        :param robot_name: robot_name
+        :param tf_listener: tf_server.TFClient()
+        """
         super(Torso, self).__init__(robot_name=robot_name, tf_listener=tf_listener)
 
-        self.joint_names = rospy.get_param('/'+self.robot_name+'/skills/torso/joint_names')
-        self.default_configurations = rospy.get_param('/'+self.robot_name+'/skills/torso/default_configurations')
-        self.default_tolerance = rospy.get_param('/'+self.robot_name+'/skills/torso/default_tolerance')
+        self.joint_names = self.load_param('skills/torso/joint_names')
+        self.default_configurations = self.load_param('skills/torso/default_configurations')
+        self.default_tolerance = self.load_param('/skills/torso/default_tolerance')
         self.lower_limit = self.default_configurations['lower_limit']
         self.upper_limit = self.default_configurations['upper_limit']
 
         # Init action client
-        self.ac_move_torso = self.create_simple_action_client('/'+self.robot_name+'/body/joint_trajectory_action',
+        self.ac_move_torso = self.create_simple_action_client('/' + self.robot_name + '/body/joint_trajectory_action',
                                                               control_msgs.msg.FollowJointTrajectoryAction)
 
         # Init joint measurement subscriber
-        self.torso_sub = rospy.Subscriber('/'+self.robot_name+'/sergio/torso/measurements', JointState, self._receive_torso_measurement)
+        self.torso_sub = rospy.Subscriber('/' + self.robot_name + '/sergio/torso/measurements', JointState,
+                                          self._receive_torso_measurement)
 
     def close(self):
-        rospy.loginfo("Torso cancelling all goals on close")
+        """
+        Cancels all active goals for the torso
+        :return: no return
+        """
+        try:
+            rospy.loginfo("Torso cancelling all goals on close")
+        except AttributeError:
+            print "Torso cancelling all goals on close. Rospy is already deleted."
+
         self.ac_move_torso.cancel_all_goals()
 
-    def send_goal(self, configuration, timeout=0.0, tolerance = []):
+    def send_goal(self, configuration, timeout=0.0, tolerance=[]):
+        """
+        Send a named joint goal (pose) defined in the parameter default_configurations to the torso
+        :param configuration: name of configuration, configuration should be loaded as parameter
+        :param timeout: timeout in seconds; in case of 0.0, not waiting for motion done
+        :param tolerance: list of position tolerances with the length equal to the number of joints
+        :return: True or False, False in case of nonexistent configuration or failed execution
+        """
         if configuration in self.default_configurations:
             return self._send_goal(self.default_configurations[configuration], timeout=timeout, tolerance=tolerance)
         else:
             rospy.logwarn('Default configuration {0} does not exist'.format(configuration))
             return False
 
-    def _send_goal(self, torso_pos, timeout=0.0, tolerance = []):
+    def _send_goal(self, torso_pos, timeout=0.0, tolerance=[]):
+        """
+        Send a joint goal to the torso
+        :param torso_pos: list of joint positions with the length equal to the number of joints
+        :param timeout: timeout in seconds; in case of 0.0, not waiting for motion done
+        :param tolerance: list of position tolerances with the length equal to the number of joints
+        :return: True or False, False in case of nonexistent configuration or failed execution
+        """
         rospy.logdebug("Send torso goal {0}, timeout = {1}".format(torso_pos, timeout))
 
         if len(torso_pos) != len(self.joint_names):
-            rospy.logwarn('Length of desired torso pos {0} does not correspond with number of joints {1}'.format(len(torso_pos), len(self.joint_names)))
+            rospy.logwarn(
+                'Length of desired torso pos {0} does not correspond with number of joints {1}'.format(
+                    len(torso_pos),
+                    len(self.joint_names)))
             return False
 
         ''' Check limits '''
         for i in range(0, len(self.joint_names)):
             if torso_pos[i] < self.lower_limit[i] or torso_pos[i] > self.upper_limit:
-                rospy.logwarn("Desired position {0} for joint {1} exceeds limits [{2}, {3}]".format(torso_pos[i], self.joint_names[i], self.lower_limit[i], self.upper_limit[i]))
+                rospy.logwarn("Desired position {0} for joint {1} exceeds limits [{2}, {3}]".format(torso_pos[i],
+                                                                                                    self.joint_names[i],
+                                                                                                    self.lower_limit[i],
+                                                                                                    self.upper_limit[
+                                                                                                        i]))
                 return False
 
         torso_goal = control_msgs.msg.FollowJointTrajectoryGoal()
@@ -59,7 +93,7 @@ class Torso(RobotPart):
         torso_goal_point.positions = torso_pos
         torso_goal.trajectory.points.append(torso_goal_point)
 
-        for i in range(0,len(self.joint_names)):
+        for i in range(0, len(self.joint_names)):
             goal_tolerance = control_msgs.msg.JointTolerance()
             goal_tolerance.name = self.joint_names[i]
             if len(tolerance) == len(self.joint_names):
@@ -70,44 +104,63 @@ class Torso(RobotPart):
 
         rospy.logdebug("Sending torso_goal: {0}".format(torso_goal))
 
-        import time; time.sleep(0.01)  # This is dangerous now we are change the Ts of the TrajectoryActionLib
+        import time
+        time.sleep(0.01)  # This is dangerous now we are changing the Ts of the TrajectoryActionLib
 
-                                        # This is necessary: the rtt_actionlib in the hardware seems
-                                        # to only have a queue size of 1 and runs at 1000 hz. This
-                                        # means that if two goals are send approximately at the same
-                                        # time (e.g. an arm goal and a torso goal), one of the two
-                                        # goals probably won't make it. This sleep makes sure the
-                                        # goals will always arrive in different update hooks in the
-                                        # hardware TrajectoryActionLib server.
+        # This is necessary: the rtt_actionlib in the hardware seems
+        # to only have a queue size of 1 and runs at 1000 hz. This
+        # means that if two goals are send approximately at the same
+        # time (e.g. an arm goal and a torso goal), one of the two
+        # goals probably won't make it. This sleep makes sure the
+        # goals will always arrive in different update hooks in the
+        # hardware TrajectoryActionLib server.
 
         self.ac_move_torso.send_goal(torso_goal)
 
         if timeout == 0.0:
             return True
         else:
-            return self.wait(timeout)
+            return self.wait_for_motion_done(timeout)
 
     def high(self):
-        """ Sends the torso to its upper limit """
+        """
+        Sends the torso to its upper limit
+        :return: True or False
+        """
         return self._send_goal(self.upper_limit)
 
     def medium(self):
+        """
+        Sends the torso to the middle position of each joint
+        :return: True or False
+        """
         goal = []
         # ToDo: make nice
         for i in range(0, len(self.joint_names)):
-            goal.append(self.lower_limit[i]+(self.upper_limit[i]-self.lower_limit[i])/2)
+            goal.append(self.lower_limit[i] + (self.upper_limit[i] - self.lower_limit[i]) / 2)
         return self._send_goal(goal)
 
     def low(self):
-        """ Sends the torso to its lower limit """
+        """
+        Sends the torso to its lower limit
+        :return: True or False
+        """
         return self._send_goal(self.lower_limit)
 
     def reset(self):
+        """
+        Sends the torso to the reset pose
+        :return:
+        """
         return self.send_goal('reset')
 
     def cancel_goal(self):
+        """
+        Cancels all active goals for the torso; Doing the same as close()
+        :return: no return
+        """
         self.ac_move_torso.cancel_goal()
-        #return True
+        # return True
 
     def wait_for_motion_done(self, timeout=10, cancel=False):
         """ Waits until all action clients are done
@@ -138,8 +191,16 @@ class Torso(RobotPart):
 
     @concurrent_util.synchronized(_lock)
     def _receive_torso_measurement(self, jointstate):
-        """@param jointstate JointState"""
+        """
+        Callback function of joint measurement subscriber
+        :param jointstate: JointState
+        :return: no return
+        """
         self.current_position = jointstate
 
     def get_position(self):
+        """
+        Get the current joint positions
+        :return: list of current positions
+        """
         return self.current_position.position
