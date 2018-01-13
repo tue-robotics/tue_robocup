19fe113183afb56d677fb01710b40cefb0d93b97
Sam Alexandrov <alexandrov.samuil@gmail.com>
2018-01-13 11:08:02 +0100

    Removed unnecessary spaces

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_help_me_carry.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_help_me_carry.py
@@ -5,23 +5,22 @@ time_out_seconds = 60.0
 time_out_seconds_door = 120.0
 
 commands = {
-    'follow':               ['follow',              # 'Robot, follow me'
-                             'follow me'],          
-    'remember':             ['remember location',   # 'Remember, this location is the car'
-                             'here is the car',     
-                             'stop following',      # 'Stop following me'
-                             'stop following me']}     
-                             
+    'follow': ['follow',  # 'Robot, follow me'
+               'follow me'],
+    'remember': ['remember location',  # 'Remember, this location is the car'
+                 'here is the car',
+                 'stop following',  # 'Stop following me'
+                 'stop following me']}
+
 # add or get from ~/ros/kinetic/system/src/ed_object_models/models/robotics_testlabs/model.yaml
 waypoints = {
-    'dinner_table':         {'id':   'dinner_table',
-                             'radius':  1},
-    'kitchen':              {'id':   'bookcase_waypoint',
-                             'radius':  1},
-    }
+    'dinner_table': {'id': 'dinner_table',
+                     'radius': 1},
+    'kitchen': {'id': 'bookcase_waypoint',
+                'radius': 1}}
 
-waypoint_car = {'id':      'car',
-                'radius':  1}
+waypoint_car = {'id': 'car',
+                'radius': 1}
 
 default_item = 'coke'
 default_place = 'cabinet'
