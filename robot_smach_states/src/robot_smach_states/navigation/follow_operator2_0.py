dd6259c31b60841534eed4909ce6a4b8b3e64d76
Josja Geijsberts <j.geijsberts@student.tue.nl>
2018-02-03 10:21:32 +0100

    small update

--- robot_smach_states/src/robot_smach_states/navigation/follow_operator2_0.py
+++ robot_smach_states/src/robot_smach_states/navigation/follow_operator2_0.py
@@ -15,11 +15,12 @@ class LearnOperator(smach.State):
 
     def execute(self, userdata=None):
         num_detections = 0
-        while num_detections < 2:
+        while num_detections < 2:   #  Should eventually be increased, 2 is too small
             var = raw_input("Did I successfully detect you? Yes/No: ")
             if var == "Yes":
                 num_detections += 1
         print ("Detected operator successfully 2 times, start following...")
+
         # The operator should be added to the breadcrumb list here.
         return 'follow'
 
@@ -31,13 +32,18 @@ class Track(smach.State):  # Updates the breadcrumb path
                              input_keys=['track_buffer_in'],
                              output_keys=['track_buffer_out'])
         self.counter = 0
+        # self.track_buffer_in = track_buffer_in
 
     def execute(self, userdata):
         if self.counter == 4:
-            userdata.track_buffer_in.append(self.counter)
+            userdata.track_buffer_out = userdata.track_buffer_in.append(self.counter)
+            print ("New breadcrumb added to buffer")
+            # print userdata.track_buffer_out
+            self.counter = 0
             return 'no_track'
-        userdata.track_buffer_out = userdata.track_buffer_in
+        userdata.track_buffer_out.append(userdata.track_buffer_in)
         self.counter += 1
+        print (".")
         return 'track'
 
 
@@ -46,19 +52,25 @@ class FollowBread(smach.State):
         smach.State.__init__(self,
                              outcomes=['follow_bread', 'no_follow_bread'],
                              input_keys=['followbread_buffer_in'],
-                             output_keys=['followbread_buffer_out'])
-        self.counter = 0
+                             output_keys=['followbread_buffer_out']
+                             )
+        # self.counter = 0
         # self.navigation_data = [0]
 
     def execute(self, userdata):
-        if self.counter == 5:
-            return 'no_follow_bread'
-        self.counter += 1
-        print userdata.followbread_buffer_in
-        # while len(userdata.followbread_buffer_in) > 0:
-        #     self.navigation_data.append(userdata.followbread_buffer_in.pop())
+        # if self.counter == 5:
+        #     return 'no_follow_bread'
+        # self.counter += 1
+
+        if len(userdata.followbread_buffer_in) is not None:
+            print userdata.followbread_buffer_in
+            # userdata.followbread_buffer_in.popleft()
+        # userdata.followbread_buffer_out = userdata.followbread_buffer_in
+
+        # print userdata.followbread_buffer_out
+
         # print self.navigation_data
-        userdata.followbread_buffer_out = userdata.followbread_buffer_in
+        # userdata.followbread_buffer_out = userdata.followbread_buffer_in
         return 'follow_bread'
 
 
@@ -90,11 +102,15 @@ class Recovery(smach.State):
 def setup_statemachine(robot):
     sm_top = smach.StateMachine(outcomes=['Done', 'Aborted', 'Failed'])
 
+    # sm_top.userdata.buffer = collections.deque()  #  Als buffer ook in Learn Operator moet komen
+
     with sm_top:
 
         smach.StateMachine.add('LEARN_OPERATOR', LearnOperator(),
                                transitions={'follow': 'CON_FOLLOW'})
 
+        #  remapping={'learn_op_buffer_in': 'buffer', 'learn_op_buffer_out': 'buffer'}
+        #  Buffer should be initialized in Learn Operator since the first breadcrumb is found here
         smach.StateMachine.add('ASK_FINALIZE', AskFinalize(),
                                transitions={'follow': 'CON_FOLLOW',
                                             'Done': 'Done'})
@@ -103,20 +119,32 @@ def setup_statemachine(robot):
                                             'follow': 'CON_FOLLOW'})
 
         sm_con = smach.Concurrence(outcomes=['recover_operator', 'ask_finalize', 'keep_following'],
-                                    default_outcome='keep_following',
-                                    outcome_map={'ask_finalize':
-                                                 {'FOLLOWBREAD': 'no_follow_bread', 'TRACK': 'track'},
-                                                 'recover_operator': {'FOLLOWBREAD': 'no_follow_bread', 'TRACK': 'no_track'}})
+                                   default_outcome='keep_following',
+                                   outcome_map={'ask_finalize': {'FOLLOWBREAD': 'no_follow_bread',
+                                                                 'TRACK': 'track'},
+                                                'recover_operator': {'FOLLOWBREAD': 'no_follow_bread',
+                                                                     'TRACK': 'no_track'}})
 
         sm_con.userdata.buffer = collections.deque()
+
         with sm_con:
-            smach.Concurrence.add('FOLLOWBREAD', FollowBread(), remapping={'followbread_buffer_in': 'buffer', 'followbread_buffer_out': 'buffer'})
-            smach.Concurrence.add('TRACK', Track(), remapping={'track_buffer_in':'buffer', 'track_buffer_out': 'buffer'})
+            smach.Concurrence.add('FOLLOWBREAD', FollowBread(), remapping={'followbread_buffer_in': 'buffer',
+                                                                            'followbread_buffer_out': 'buffer'})
+
+            smach.Concurrence.add('TRACK', Track()
+                                   , remapping={'track_buffer_in': 'buffer',
+                                                                'track_buffer_out': 'buffer'}
+                                  )
 
         smach.StateMachine.add('CON_FOLLOW', sm_con,
                                transitions={'recover_operator': 'RECOVERY',
                                             'ask_finalize': 'ASK_FINALIZE',
-                                            'keep_following': 'CON_FOLLOW'})
+                                            'keep_following': 'CON_FOLLOW'},
+                               # remapping={#'track_buffer_in': 'buffer',
+                               #            #'track_buffer_out': 'buffer',
+                               #            'followbread_buffer_in': 'buffer',
+                               #            'followbread_buffer_out': 'buffer'}
+                               )
 
         return sm_top
 
