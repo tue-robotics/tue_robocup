cb89fcc406fb148f1663abb3ba72d49629a422db
Janno Lunenburg <jannolunenburg@gmail.com>
2018-04-25 23:46:32 +0200

    cleanup(robot_skills/wm)Pep8ified

--- robot_skills/src/robot_skills/world_model_ed.py
+++ robot_skills/src/robot_skills/world_model_ed.py
@@ -27,7 +27,8 @@ class Navigation(RobotPart):
 
     def get_position_constraint(self, entity_id_area_name_map):
         try:
-            res = self._get_constraint_srv(entity_ids=[ k for k in entity_id_area_name_map ], area_names=[ v for k,v in entity_id_area_name_map.iteritems() ])
+            res = self._get_constraint_srv(entity_ids=[k for k in entity_id_area_name_map],
+                                           area_names=[v for k, v in entity_id_area_name_map.iteritems()])
         except Exception, e:
             rospy.logerr(e)
             return None
@@ -54,17 +55,17 @@ class ED(RobotPart):
         self._ed_reset_srv = self.create_service_client('/%s/ed/reset' % robot_name, ed_msgs.srv.Reset)
         self._ed_get_image_srv = self.create_service_client('/%s/ed/kinect/get_image' % robot_name,
                                                             ed_sensor_integration.srv.GetImage)
-        self._ed_ray_trace_srv = self.create_service_client('/%s/ed/ray_trace' % robot_name, ed_sensor_integration.srv.RayTrace)
+        self._ed_ray_trace_srv = self.create_service_client('/%s/ed/ray_trace' % robot_name,
+                                                            ed_sensor_integration.srv.RayTrace)
 
         self._tf_listener = tf_listener
 
         self.navigation = Navigation(robot_name, tf_listener)
 
-        self._marker_publisher = rospy.Publisher("/" + robot_name + "/ed/simple_query",  visualization_msgs.msg.Marker,
+        self._marker_publisher = rospy.Publisher("/" + robot_name + "/ed/simple_query", visualization_msgs.msg.Marker,
                                                  queue_size=10)
 
         self.robot_name = robot_name
-
 
     def wait_for_connections(self, timeout):
         """ Waits for the connections until they are connected
@@ -81,13 +82,15 @@ class ED(RobotPart):
         self._publish_marker(center_point, radius)
 
         center_point_in_map = center_point.projectToFrame("/map", self._tf_listener)
-        query = SimpleQueryRequest(id=id, type=type, center_point=kdl_vector_to_point_msg(center_point_in_map.vector), radius=radius)
+        query = SimpleQueryRequest(id=id, type=type, center_point=kdl_vector_to_point_msg(center_point_in_map.vector),
+                                   radius=radius)
 
         try:
-            entity_infos= self._ed_simple_query_srv(query).entities
+            entity_infos = self._ed_simple_query_srv(query).entities
             entities = map(from_entity_info, entity_infos)
         except Exception, e:
-            rospy.logerr("ERROR: robot.ed.get_entities(id=%s, type=%s, center_point=%s, radius=%s)" % (id, type, str(center_point), str(radius)))
+            rospy.logerr("ERROR: robot.ed.get_entities(id=%s, type=%s, center_point=%s, radius=%s)" % (
+            id, type, str(center_point), str(radius)))
             rospy.logerr("L____> [%s]" % e)
             return []
 
@@ -95,7 +98,7 @@ class ED(RobotPart):
 
     def get_closest_entity(self, type="", center_point=None, radius=0):
         if not center_point:
-            center_point = VectorStamped(x=0, y=0, z=0, frame_id="/"+self.robot_name+"/base_link")
+            center_point = VectorStamped(x=0, y=0, z=0, frame_id="/" + self.robot_name + "/base_link")
 
         entities = self.get_entities(type=type, center_point=center_point, radius=radius)
 
@@ -117,7 +120,7 @@ class ED(RobotPart):
 
     def get_closest_room(self, center_point=None, radius=0):
         if not center_point:
-            center_point = VectorStamped(x=0, y=0, z=0, frame_id="/"+self.robot_name+"/base_link")
+            center_point = VectorStamped(x=0, y=0, z=0, frame_id="/" + self.robot_name + "/base_link")
 
         return self.get_closest_entity(type="room", center_point=center_point, radius=radius)
 
@@ -134,14 +137,16 @@ class ED(RobotPart):
         entities = self.get_entities(type="", center_point=center_point, radius=radius)
 
         # HACK
-        entities = [ e for e in entities if e.shape and e.type == "" and e.id.endswith("-laser") ]
+        entities = [e for e in entities if e.shape and e.type == "" and e.id.endswith("-laser")]
 
         if len(entities) == 0:
             return None
 
         # Sort by distance
         try:
-            entities = sorted(entities, key=lambda entity: entity.distance_to_2d(center_point.projectToFrame("/%s/base_link"%self.robot_name, self._tf_listener).vector)) # TODO: adjust for robot
+            entities = sorted(entities, key=lambda entity: entity.distance_to_2d(
+                center_point.projectToFrame("/%s/base_link" % self.robot_name,
+                                            self._tf_listener).vector))  # TODO: adjust for robot
         except:
             print "Failed to sort entities"
             return None
@@ -197,7 +202,8 @@ class ED(RobotPart):
 
             Z, Y, X = frame_stamped.frame.M.GetEulerZYX()
             t = frame_stamped.frame.p
-            json_entity += ', "pose": { "x": %f, "y": %f, "z": %f, "X": %f, "Y": %f, "Z": %f }' % (t.x(), t.y(), t.z(), X, Y, Z)
+            json_entity += ', "pose": { "x": %f, "y": %f, "z": %f, "X": %f, "Y": %f, "Z": %f }' % (
+            t.x(), t.y(), t.z(), X, Y, Z)
 
         if flags or add_flags or remove_flags:
             json_entity += ', "flags": ['
@@ -211,10 +217,10 @@ class ED(RobotPart):
                     if not isinstance(flag, dict):
                         print "update_entity - Error: flags need to be a list of dicts or a dict"
                         return False
-                    for k,v in flag.iteritems():
+                    for k, v in flag.iteritems():
                         if not first:
                             json_entity += ','
-                        json_entity += '{"%s":"%s"}' % (k,v)
+                        json_entity += '{"%s":"%s"}' % (k, v)
                         first = False
 
             for flag in add_flags:
@@ -231,7 +237,7 @@ class ED(RobotPart):
 
             json_entity += ']'
 
-        json = '{"entities":[{%s}]}'%json_entity
+        json = '{"entities":[{%s}]}' % json_entity
         rospy.logdebug(json)
 
         return self._ed_update_srv(request=json)
@@ -256,8 +262,8 @@ class ED(RobotPart):
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    def get_closest_possible_person_entity(self, type="", center_point=VectorStamped(), radius=0, room = ""):
-        #if isinstance(center_point, PointStamped):
+    def get_closest_possible_person_entity(self, type="", center_point=VectorStamped(), radius=0, room=""):
+        # if isinstance(center_point, PointStamped):
         #    center_point = self._transform_center_point_to_map(center_point)
         # ToDo: check frame ids
 
@@ -265,7 +271,7 @@ class ED(RobotPart):
 
         # HACK
         # entities = [ e for e in entities if e.convex_hull and e.type == "" and 'possible_human' in e.flags ]
-        entities = [ e for e in entities if e.is_a('possible_human') ]
+        entities = [e for e in entities if e.is_a('possible_human')]
 
         if len(entities) == 0:
             return None
@@ -300,7 +306,7 @@ class ED(RobotPart):
         # Save the image (logging)
         self.save_image(path_suffix=area_description.replace(" ", "_"))
 
-        res = self._ed_kinect_update_srv(area_description = area_description, background_padding = background_padding)
+        res = self._ed_kinect_update_srv(area_description=area_description, background_padding=background_padding)
         if res.error_msg:
             rospy.logerr("Could not segment objects: %s" % res.error_msg)
 
@@ -322,16 +328,16 @@ class ED(RobotPart):
         if res.error_msg:
             rospy.logerr("While classifying entities: %s" % res.error_msg)
 
-
         posteriors = [dict(zip(distr.values, distr.probabilities)) for distr in res.posteriors]
 
         # Filter on types if types is not None
-       	return [ClassificationResult(_id, exp_val, exp_prob, distr) for _id, exp_val, exp_prob, distr
-       				in zip(res.ids, res.expected_values, res.expected_value_probabilities, posteriors) if types is None or exp_val in types]
+        return [ClassificationResult(_id, exp_val, exp_prob, distr) for _id, exp_val, exp_prob, distr
+                in zip(res.ids, res.expected_values, res.expected_value_probabilities, posteriors) if
+                types is None or exp_val in types]
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    def save_image(self, path = "", path_suffix = "", filename = ""):
+    def save_image(self, path="", path_suffix="", filename=""):
         import os
         import time
 
@@ -390,7 +396,8 @@ class ED(RobotPart):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     def _transform_center_point_to_map(self, pointstamped):
-        point_in_map = transformations.tf_transform(pointstamped.point, pointstamped.header.frame_id, "/map", self._tf_listener)
+        point_in_map = transformations.tf_transform(pointstamped.point, pointstamped.header.frame_id, "/map",
+                                                    self._tf_listener)
         return point_in_map
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
