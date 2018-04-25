25ae8a50d7c94ca75c550c52c2d23c19394d712d
Loy van Beek <loy.vanbeek@gmail.com>
2018-04-25 14:31:50 +0200

    Ignore parts of Sergio that the real robot is still lacking

--- robot_skills/src/robot_skills/sergio.py
+++ robot_skills/src/robot_skills/sergio.py
@@ -5,3 +5,5 @@ class Sergio(robot.Robot):
     """docstring for Sergio"""
     def __init__(self, wait_services=False):
         super(Sergio, self).__init__(robot_name="sergio", wait_services=False)
+
+        self.__ignored_parts = ["left_arm", "right_arm", "spindle"]
