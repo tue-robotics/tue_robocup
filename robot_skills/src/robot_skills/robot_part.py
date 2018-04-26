acb519cb25e0e8b0787ce3592fb92e083038b2c9
Loy van Beek <loy.vanbeek@gmail.com>
2018-04-26 11:26:09 +0200

    Correct part names :-( and warn for real when not ignored

--- robot_skills/src/robot_skills/robot_part.py
+++ robot_skills/src/robot_skills/robot_part.py
@@ -176,7 +176,7 @@ class RobotPart(object):
         diag = diagnostic_dict.get(self.__diagnostics_name, None)
 
         if not diag:
-            pass # rospy.logwarn('no diagnostic msg received for the %s' % self.__diagnostics_name)
+            rospy.logwarn('no diagnostic msg received for the %s' % self.__diagnostics_name)
         else:
             # TODO: diagnostic_msgs.DiagnosticStatus.OK instead of our own enumeration.
             # 0. Stale
