8fb717c7fe13ea81d29050dd2cb4c87959decc69
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-04-28 21:26:24 +0200

    (rgo2018) vieze_node take biggest face

--- challenge_final/scripts/dirty_node.py
+++ challenge_final/scripts/dirty_node.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, RegionOfInterest
 from image_recognition_msgs.srv import Annotate, Recognize
 from image_recognition_msgs.msg import Annotation
+
+
+def filter_on_area(recognitions):
+    """
+    return the biggest roi
+
+    :param recognitions:
+    :return:
+    """
+    best_roi_area = 0
+    best_rec = RegionOfInterest()
+    for rec in recognitions:
+        roi_area = rec.roi.height * rec.roi.width
+        if roi_area > best_roi_area:
+            best_rec = rec
+    return best_rec
 
 
 class DirtyNode:
@@ -11,6 +27,7 @@ class DirtyNode:
         self._img_sub = rospy.Subscriber('photo_from_telegram', Image, self._image_cb)
         self._recognize_srv = rospy.ServiceProxy('face_recognition/recognize', Recognize)
         self._annotate_srv = rospy.ServiceProxy('face_recognition/annotate', Annotate)
+        rospy.loginfo("Starting the vieze node")
 
     def _image_cb(self, image):
         self.learn_person(image=image)
@@ -28,13 +45,14 @@ class DirtyNode:
         # recognitions = [r for r in raw_recognitions if r.roi.height > HEIGHT_TRESHOLD and r.roi.width > WIDTH_TRESHOLD]
         rospy.loginfo('found %d valid face(s)', len(recognitions))
 
-        if len(recognitions) != 1:
-            rospy.loginfo("Too many faces: {}".format(len(recognitions)))
-            return False
+        # if len(recognitions) != 1:
+        #     rospy.loginfo("Too many faces: {}".format(len(recognitions)))
+        #     return False
 
         if not recognitions:
             return False
-        recognition = recognitions[0]
+
+        recognition = filter_on_area(recognitions)
 
         rospy.loginfo('annotating that face as %s', name)
         try:
