dac0d6be6edff77c766d25759d4e3c060ee0c4f2
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-04-28 20:50:49 +0200

    Dirty_node annotating image as operator

---
+++ challenge_final/scripts/dirty_node.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from image_recognition_msgs.srv import Annotate, Recognize
+from image_recognition_msgs.msg import Annotation
+
+
+class DirtyNode:
+    def __init__(self):
+        self._img_sub = rospy.Subscriber('photo_from_telegram', Image, self._image_cb)
+        self._recognize_srv = rospy.ServiceProxy('face_recognition/recognize', Recognize)
+        self._annotate_srv = rospy.ServiceProxy('face_recognition/annotate', Annotate)
+
+    def _image_cb(self, image):
+        self.learn_person(image=image)
+
+    def learn_person(self, image, name='operator'):
+        # HEIGHT_TRESHOLD = 88
+        # WIDTH_TRESHOLD = 88
+
+        try:
+            recognitions = self._recognize_srv(image=image).recognitions
+        except rospy.ServiceException as e:
+            rospy.logerr('annotate failed: {}'.format(e))
+            return False
+        # raw_recognitions = self._recognize_srv(image=image).recognitions
+        # recognitions = [r for r in raw_recognitions if r.roi.height > HEIGHT_TRESHOLD and r.roi.width > WIDTH_TRESHOLD]
+        rospy.loginfo('found %d valid face(s)', len(recognitions))
+
+        if len(recognitions) != 1:
+            rospy.loginfo("Too many faces: {}".format(len(recognitions)))
+            return False
+
+        if not recognitions:
+            return False
+        recognition = recognitions[0]
+
+        rospy.loginfo('annotating that face as %s', name)
+        try:
+            self._annotate_srv(image=image, annotations=[Annotation(label=name, roi=recognition.roi)])
+        except rospy.ServiceException as e:
+            rospy.logerr('annotate failed: {}'.format(e))
+            return False
+
+        return True
+
+
+if __name__ == '__main__':
+
+    rospy.init_node("dirty_node")
+
+    node = DirtyNode()
+
+    rospy.spin()
