77d35c26c7568466b63b5be1fe5686b49e7559ed
Sam Alexandrov <alexandrov.samuil@gmail.com>
2018-05-22 20:21:25 +0200

    Updated ReadMe and added small corrections to the spr files and knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_spr.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_spr.py
@@ -143,16 +143,18 @@ LH -> lighter | heavier
 
 Q["action" : "find_object", "entity" : O] -> where can i find DET OBJECT_NAMES[O]
 Q["action" : "find_category", "entity" : C] -> where can i find DET OBJECT_CATEGORIES[C]
+
 Q["action" : "return_category", "entity" : O] -> to WHATWHICH category belong the OBJECT_NAMES[O]
 Q["action" : "return_color", "entity" : O] -> whats the color of the OBJECT_NAMES[O]
+
 Q["action" : "compare_category", "entity_a" : O, "entity_b" : A] -> do DET OBJECT_NAMES[O] and DET OBJECT_NAMES[A] belong to the same category
 Q["action" : "compare_sizes", "entity_a" : O, "entity_b" : A] -> between DET OBJECT_NAMES[O] and DET OBJECT_NAMES[A] which one is SB
 Q["action" : "compare_weight", "entity_a" : O, "entity_b" : A] -> between DET OBJECT_NAMES[O] and DET OBJECT_NAMES[A] which one is LH
-Q["action" : "count_object", "entity" : C] -> how many OBJECT_CATEGORIES[C] there are
 
-NQ["action" : "count_object", "entity" : C, "location" : Y] -> how many OBJECT_CATEGORIES[C] are in the PLACEMENTS_AND_BEACONS[Y]
-NQ["action" : "count_object", "entity" : O, "location" : Y] -> how many OBJECT_NAMES[O] are in the PLACEMENTS_AND_BEACONS[Y]
-NQ["action" : "category_at_loc", "location" : Y] -> what objects are stored in the PLACEMENTS_AND_BEACONS[Y]
+Q["action" : "count_object_cat", "entity" : C] -> how many OBJECT_CATEGORIES[C] there are
+
+NQ["action" : "count_object_loc", "location" : Y] -> how many objects are in the PLACEMENTS_AND_BEACONS[Y] | what objects are stored in the PLACEMENTS_AND_BEACONS[Y]
+NQ["action" : "count_object_cat_loc", "entity" : C, "location" : Y] -> how many OBJECT_CATEGORIES[C] are in the PLACEMENTS_AND_BEACONS[Y]
 '''
 
 ##############################################################################
