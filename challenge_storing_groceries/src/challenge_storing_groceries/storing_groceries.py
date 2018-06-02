da1408d19890074cb37b6073ee83a0c0da57ab51
Loy van Beek <loy.vanbeek@gmail.com>
2018-06-02 11:14:19 +0200

    Remove obsolete outcommented code

--- challenge_storing_groceries/src/challenge_storing_groceries/storing_groceries.py
+++ challenge_storing_groceries/src/challenge_storing_groceries/storing_groceries.py
@@ -67,7 +67,6 @@ class StoringGroceries(smach.StateMachine):
 
                 # Update designators
                 cabinet.id_ = closest_workspace.place_entity_conf.entity_id
-                # room.id_ = ENTITY_POSES[0][4] # TODO: no usages besides this line and instantiation?
 
                 # Update manipulate machine
                 manipulate_machine.table_designator.id_         = closest_workspace.grasp_entity_conf.entity_id
