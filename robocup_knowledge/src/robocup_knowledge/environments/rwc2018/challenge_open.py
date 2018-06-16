49726458290af012a93336405327849503feed11
Sam Alexandrov <alexandrov.samuil@gmail.com>
2018-06-16 21:35:04 +0200

    added the files for the challenges in Montreal

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2018/challenge_open.py
@@ -0,0 +1,10 @@
+# OPEN CHALLENGE KNOWLEDGE FILE RWC2018
+
+# TU/e Robotics
+from robocup_knowledge import knowledge_loader
+
+# Load the common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+initial_pose = "initial_pose"
+meeting_point = "gpsr_meeting_point"
