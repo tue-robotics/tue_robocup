ccae6929e854882401e464bf0864ca90281e6312
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-06-19 18:23:12 -0400

    door is in corridor

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2018/common.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2018/common.py
@@ -22,7 +22,7 @@ locations = [
     {'name': 'end_table',       'room': 'living_room',      'category': 'table',        'manipulation': 'yes'},
     {'name': 'couch',           'room': 'living_room',      'category': 'utility',      'manipulation': 'no'},
     {'name': 'bookcase',        'room': 'living_room',      'category': 'shelf',        'manipulation': 'yes'},
-    {'name': 'door',            'room': 'living_room',      'category': 'utility',      'manipulation': 'no'}
+    {'name': 'door',            'room': 'corridor',      'category': 'utility',      'manipulation': 'no'}
 ]
 
 location_rooms = list(set([ o["room"] for o in locations ]))
