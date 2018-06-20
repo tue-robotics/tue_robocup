24a94bddb9e7be7e92f5e63b1fd1a79ff0c0d360
Ramon Wijnands <rayman747@hotmail.com>
2018-06-19 22:26:38 -0400

    Tuning on the robot

--- challenge_dishwasher/src/challenge_dishwasher/custom_place.py
+++ challenge_dishwasher/src/challenge_dishwasher/custom_place.py
@@ -1,9 +1,12 @@
 import rospy
+from geometry_msgs.msg import Twist, Vector3, PoseStamped, Quaternion
 import tf2_geometry_msgs
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from robot_skills.amigo import Amigo
 from robot_smach_states import NavigateToSymbolic
 from robot_smach_states.util.designators import EdEntityDesignator, Designator
 from smach import StateMachine, cb_interface, CBState
+from challenge_dishwasher.open_dishwasher import ControlToPose, ControlParameters
 
 
 _ = tf2_geometry_msgs
@@ -15,13 +18,25 @@ class CustomPlace(StateMachine):
 
         @cb_interface(outcomes=['done'])
         def pre_place_pose(ud):
-            arm.resolve()._send_joint_trajectory([[0, 0.2519052373022729913, 0.7746500794619434, 1.3944848321343395,
-                                                   -1.829999276180074, 0.6947045024700284, 0.1889253710114966]],
+            arm.resolve()._send_joint_trajectory([[-0.92, 0.044, 0.80, 0.297, 0.934, -0.95, 0.4]],
                                                  timeout=rospy.Duration(0))
+            arm.resolve().wait_for_motion_done()
             return 'done'
 
         @cb_interface(outcomes=['done'])
         def move_closer(ud):
+            goal_pose = PoseStamped()
+            goal_pose.header.stamp = rospy.Time.now()
+            goal_pose.header.frame_id = dishwasher_id
+            goal_pose.pose.orientation = Quaternion(*quaternion_from_euler(0, 0, 1.9656259917))
+            goal_pose.pose.position.x = -4.55638664735
+            goal_pose.pose.position.y = 4.99959353359
+            ControlToPose(robot, goal_pose, ControlParameters(0.5, 1.0, 0.3, 0.3, 0.3, 0.01, 0.1)).execute({})
+
+            arm.resolve()._send_joint_trajectory([[-0.92, 0.044, 0.80, 0.497, 0.934, -0.95, 0.4]],
+                                                 timeout=rospy.Duration(0))
+            robot.torso._send_goal([0.35])
+            robot.torso.wait_for_motion_done()
             return 'done'
 
         @cb_interface(outcomes=['done'])
@@ -80,5 +95,5 @@ if __name__ == '__main__':
     robot.leftArm.reset()
     robot.torso.reset()
 
-    sm = TestCustomPlace(robot, 'dishwasher', 'in_front_of')
+    sm = TestCustomPlace(robot, 'dishwasher', 'to_the_side_of')
     sm.execute()
