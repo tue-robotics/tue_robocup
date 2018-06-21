87d373ec535680e376f0d61a07fa53e87e51c4be
Loy van Beek <loy.vanbeek@gmail.com>
2018-06-20 23:25:13 -0400

    Remove scripts from setup.py because there are no scripts

--- challenge_dishwasher/setup.py
+++ challenge_dishwasher/setup.py
@@ -3,7 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['challenge_dishwasher'],
-    scripts=['scripts/challenge_dishwasher'],
     package_dir={'': 'src'}
 )
 
