a43ab7ee0e73d5f31540c89bf7912fd3ba25f47b
josja_geijsberts <j.geijsberts@student.tue.nl>
2018-08-03 16:23:53 +0200

    changed hero.py to bridge software

--- robot_skills/src/robot_skills/hero.py
+++ robot_skills/src/robot_skills/hero.py
@@ -1,5 +1,8 @@
 import robot
 
+import rospy
+import geometry_msgs.msg
+
 from collections import OrderedDict
 
 
@@ -9,9 +12,14 @@ class Hero(robot.Robot):
         super(Hero, self).__init__(robot_name="hero", wait_services=wait_services)
 
         self._ignored_parts = ["leftArm", "rightArm", "torso", "spindle", "head"]
+       
+        # remap topics in base   
+        self.parts['base']._cmd_vel = rospy.Publisher('/hsrb/command_velocity', geometry_msgs.msg.Twist, queue_size=10)
+	
+	#rename joint names
+	self.parts['leftArm'].joint_names = self.parts['leftArm'].load_param('skills/arm/joint_names')	
 
         # This is still very ugly, because there is a lof of double code, but atleast it is only in sergio.
-        self.parts['leftArm'] = robot.arms.FakeArm(self.robot_name, self.tf_listener, side="left")
         self.parts['rightArm'] = robot.arms.FakeArm(self.robot_name, self.tf_listener, side="right")
 
         for partname, bodypart in self.parts.iteritems():
