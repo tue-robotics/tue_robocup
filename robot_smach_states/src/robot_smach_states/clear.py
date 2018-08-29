0ee9cda0d753fc58860a72725f1454471f0f94e3
Josja Geijsberts <j.geijsberts@student.tue.nl>
2018-08-29 09:38:37 +0200

    added clear smach state ready for first test

---
+++ robot_smach_states/src/robot_smach_states/clear.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+
+# ROS
+import rospy
+import smach
+import tf2_ros
+
+# TU/e Robotics
+from robot_skills.util.entity import Entity
+
+import robot_smach_states as states
+from robot_smach_states.util.designators import check_type
+from robot_smach_states.util.designators import VariableDesignator, EdEntityDesignator
+
+class isitclear(smach.StateMachine):
+    """
+    Check if there are entities on the object in the world model
+    """
+
+    def __init__(self,
+                 robot,
+                 objectIDsDes):
+        smach.State.__init__(self, outcomes=['clear', 'not_clear'])
+        self._robot = robot
+        self._object_designator = objectIDsDes
+
+    def execute(self, userdata=None):
+        ids_to_select_from = [e.id for e in self._candidate_entities_designator.resolve()]
+        rospy.logdebug('list of entities to select from: {}'.format(ids_to_select_from))
+
+        return 'clear'
+
+
+class Clear(smach.Statemachine):
+    def __init__(self, robot, source_location, source_searchArea="on_top_of", source_navArea, target_location, target_placeArea="on_top_of", target_navArea):
+        """
+        Let the given robot move to a location and remove all entities from that table one at a time
+        :param robot: Robot to use
+        :param source_location: Location which will be cleared
+        :param target_location: Location where the objects will be placed
+        :return:
+        """
+        smach.StateMachine.__init__(self, outcomes=['done', 'failed'])
+
+        # Check types or designator resolve types
+        #check_type(source_location, Entity)
+        #check_type(target_location, Entity)
+
+        segmented_entities_designator = VariableDesignator([], resolve_type=[ClassificationResult])
+
+        with self:
+            smach.StateMachine.add('INSPECT_SOURCE_ENTITY',
+                                   states.world_model.Inspect(robot=robot,
+                                                              entityDes=source_location,
+                                                              objectIDsDes=segmented_entities_designator,
+                                                              searchArea=source_searchArea,
+                                                              navigation_area=source_navArea,
+                                   transitions={'done': 'CHECK_IF_ENTITY_FOUND',
+                                                'failed': 'inspect_failed'})
+
+            smach.StateMachine.add('DETERMINE_IF_CLEAR',
+                                   isitclear(robot=robot,
+                                             objectIDsDes=segmented_entities_designator),
+                                   transitions={'clear': 'done',
+                                                'not_clear': 'MOVE_OBJECT'})
+
+            smach.StateMachine.add("MOVE OBJECT",
+                                   move_to_thingy(),
+                                   transitions={"done": "INSPECT_%d" % next_i})
