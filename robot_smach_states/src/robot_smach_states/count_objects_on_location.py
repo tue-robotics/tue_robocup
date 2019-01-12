c066c606e6e36fce480b326150c7642205adbb7a
Lars Janssen <janslar@hotmail.com>
2019-01-12 15:29:19 +0100

    adding docstrings, pep8tifying, removing redudant code

--- robot_smach_states/src/robot_smach_states/count_objects_on_location.py
+++ robot_smach_states/src/robot_smach_states/count_objects_on_location.py
@@ -8,12 +8,16 @@ from robot_skills.classification_result import ClassificationResult
 
 
 class CountObjectsOnLocation(smach.State):
-    def __init__(self, robot, location, segmented_objects_designator, num_objects_designator, segmentation_area='on_top_of', object_type='', threshold=0.0):
+    def __init__(self, robot, location, segmented_objects_designator, num_objects_designator,
+                 segmentation_area='on_top_of', object_type='', threshold=0.0):
         """ Constructor
 
         :param robot: robot object
         :param location: Where to look for objects?
-        :param num_objects_designator: a VariableDesignator(resolve_type=int).writeable() that will store the number of objects
+        :param segmented_objects_designator: a VariableDesignator (resolve_type=ClassificationResult) that stores the
+            classification of an object
+        :param num_objects_designator: a VariableDesignator(resolve_type=int).writeable() that will store the number
+            of objects
         :param segmentation_area: string defining where the objects are w.r.t. the entity, default = on_top_of
         :param threshold: float for classification score. Entities whose classification score is lower are ignored
             (i.e. are not added to the segmented_entity_ids_designator)
@@ -31,26 +35,20 @@ class CountObjectsOnLocation(smach.State):
         self.segmented_objects_designator = segmented_objects_designator
 
     def execute(self, userdata=None):
-        # res = self.robot.ed.update_kinect("{} {}".format(self.segmentation_area, self.location))
-        # entities = self.segmented_objects_designator.resolve()
-        # segmented_object_ids = entities.new_ids + entities.updated_ids
         object_classifications = self.segmented_objects_designator.resolve()
 
         rospy.loginfo("Segmented %d objects" % len(object_classifications))
         if object_classifications:
-            # object_classifications = self.robot.ed.classify(ids=segmented_object_ids)
 
             for idx, obj in enumerate(object_classifications):
-                rospy.loginfo("   - Object {i} is a '{t}' (prob: {p}, ID: {id})".format(i=idx, t=obj.type, id=obj.id, p=obj.probability))
+                rospy.loginfo("   - Object {i} is a '{t}' (prob: {p}, ID: {id})".format(i=idx,t=obj.type,
+                                                                                        id=obj.id, p=obj.probability))
 
-            over_threshold = object_classifications
-            if self.threshold:
-                over_threshold = [obj for obj in object_classifications if obj.probability >= self.threshold]
+            over_threshold = [obj for obj in object_classifications if obj.probability >= self.threshold]
 
             dropped = {obj.id: obj.probability for obj in object_classifications if obj.probability < self.threshold}
-            rospy.loginfo("Dropping {l} entities due to low class. score (< {th}): {dropped}"
+            rospy.debug("Dropping {l} entities due to low class. score (< {th}): {dropped}"
                           .format(th=self.threshold, dropped=dropped, l=len(dropped)))
-
             object_classifications = over_threshold
 
             list_objects = [obj for obj in object_classifications if obj.type == self.object_type.resolve()]
@@ -66,6 +64,14 @@ class CountObjectsOnLocation(smach.State):
 
 class InspectAndCount(smach.StateMachine):
     def __init__(self, robot, where_to_count_designator, type_to_count_designator, count_designator):
+        """
+        :param robot: robot object
+        :param where_to_count_designator: Where to look for objects?
+        :param type_to_count_designator: a VariableDesignator (resolve_type=ClassificationResult) that stores the
+            classification of an object
+        :param count_designator:  VariableDesignator(resolve_type=int).writeable() that will store the number
+            of objects
+        """
         smach.StateMachine.__init__(self, outcomes=['Done', 'Aborted'])
 
         entities = ds.VariableDesignator([], resolve_type=[ClassificationResult])
