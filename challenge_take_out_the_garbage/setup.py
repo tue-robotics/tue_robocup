3471d741caffe76bc3dc38eb813c93088131cd7b
Rokus Ottervanger <rokusottervanger@gmail.com>
2019-02-05 22:34:10 +0100

    Fixed setup file, so it is executable

--- challenge_take_out_the_garbage/setup.py
+++ challenge_take_out_the_garbage/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     # #  don't do this unless you want a globally visible script
     # scripts=['bin/myscript'],
-    packages=['challenge_example'],
+    packages=['challenge_take_out_the_garbage'],
     package_dir={'': 'src'}
 )
 
