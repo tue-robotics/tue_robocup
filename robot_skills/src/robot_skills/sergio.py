074d61a686d84d7571de79b20d8b779929d18194
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-02-05 19:23:52 +0100

    cleanup of robot_skills

--- robot_skills/src/robot_skills/sergio.py
+++ robot_skills/src/robot_skills/sergio.py
@@ -26,9 +26,9 @@ class Sergio(robot.Robot):
         self.add_body_part('hmi', api.Api(self.robot_name, self.tf_listener,
                                           lambda: self.lights.set_color_colorRGBA(lights.LISTENING),
                                           lambda: self.lights.set_color_colorRGBA(lights.RESET)))
-        self..add_body_part('ears', ears.Ears(self.robot_name, self.tf_listener,
-                                              lambda: self.lights.set_color_colorRGBA(lights.LISTENING),
-                                              lambda: self.lights.set_color_colorRGBA(lights.RESET)))
+        self.add_body_part('ears', ears.Ears(self.robot_name, self.tf_listener,
+                                             lambda: self.lights.set_color_colorRGBA(lights.LISTENING),
+                                             lambda: self.lights.set_color_colorRGBA(lights.RESET)))
 
         self.add_body_part('ebutton', ebutton.EButton(self.robot_name, self.tf_listener))
 
