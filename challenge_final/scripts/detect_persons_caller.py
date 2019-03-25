e352c63b80527d5b0ffbbf8e3bb5a78d074919be
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-03-25 11:37:38 +0100

    some cleaning up of challenge_final

--- challenge_final/scripts/detect_persons_caller.py
+++ challenge_final/scripts/detect_persons_caller.py
@@ -7,7 +7,7 @@ from sensor_msgs.msg import Image
 def callback(img):
     try:
         detect_persons(image=img)
-    except rospy.ServiceException, e:
+    except rospy.ServiceException as e:
         rospy.logerr("Service call failed: %s" % e)
     else:
         rospy.loginfo("Succesfully called detect persons :)")
@@ -16,8 +16,8 @@ def callback(img):
 
 rospy.init_node('detect_persons_caller')
 rospy.Subscriber("/amigo/top_kinect/rgb/image", Image, callback, queue_size=1)
-print "Waiting for amigo/person_detection/detect_persons"
+print("Waiting for amigo/person_detection/detect_persons")
 rospy.wait_for_service('/amigo/person_detection/detect_persons')
 detect_persons = rospy.ServiceProxy('/amigo/person_detection/detect_persons', GetPersons)
-print "Spinning ..."
+print("Spinning ...")
 rospy.spin()
