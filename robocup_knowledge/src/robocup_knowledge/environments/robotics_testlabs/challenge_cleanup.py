f5e98f21efad3aaece641cfd5f9df41ddcee66bb
Peter van Dooren <pepevandooren@hotmail.com>
2019-04-16 20:42:56 +0200

    knowledge from the r5cop demo

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_cleanup.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_cleanup.py
@@ -1,12 +1,30 @@
-from robocup_knowledge import knowledge_loader
-common = knowledge_loader.load_knowledge("common")
-
 starting_point = "initial_pose"
 
-intermediate_1 = "registration_table1"
-intermediate_2 = "registration_table2"
-intermediate_3 = "registration_table3"
+# required keys: entity_id (str), room_id (str), navigation_area (str), segment_areas (list)
+inspection_places = [
+    {"entity_id": "dinner_table",
+     "room_id": "livingroom",
+     "navigate_area": "near",
+     "segment_areas": ["on_top_of"]},
 
-exit_1 = "exit_1_rips"
-exit_2 = "exit_2_rips"
-exit_3 = "exit_3_rips"
+    {"entity_id": "cabinet",
+     "room_id": "kitchen",
+     "navigate_area": "near",
+     "segment_areas": ["on_top_of"]},
+]
+
+known_types = [
+    "coke",
+    "fanta",
+    "mentos",
+    "bifrutas",
+    "beer",
+    "coffee_pads",
+    "deodorant",
+    "ice_tea",
+    "sprite",
+    "tea",
+    "water",
+    "xylit24_spearmint",
+    "xylit24_white"]
+
