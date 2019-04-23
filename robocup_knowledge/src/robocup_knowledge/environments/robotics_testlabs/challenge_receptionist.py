e2ceb6e826e1f4fef71848355ecffe9ee3f1f889
Loy van Beek <loy.vanbeek@gmail.com>
2019-04-23 20:54:40 +0200

    Move common knowledge in receptionist to common knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_receptionist.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_receptionist.py
@@ -8,11 +8,3 @@ starting_point = "initial_pose"
 waypoint_door = {'id': 'entry_door', 'radius': 0.5}
 
 waypoint_livingroom = {'id': 'livingroom', 'radius': 0.5}
-
-default_target_radius = 0.2
-
-drink_names = [obj['name'] for obj in common.objects if obj['category'] == 'drink']
-
-drink_spec = "T['drink': O] -> OPTIONS[O]\n\n"
-for dn in drink_names:
-    drink_spec += "OPTIONS['{drink}'] -> {drink}\n".format(drink=dn)
