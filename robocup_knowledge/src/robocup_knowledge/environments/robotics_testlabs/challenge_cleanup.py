f00c62792956a4b26336014938cc804022d0212c
Henk van Rooy <h.w.a.m.v.rooy@tue.nl>
2019-04-23 19:59:37 +0200

    local knowlwdge update

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_cleanup.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_cleanup.py
@@ -1,6 +1,12 @@
 from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
+"""
+Local knowledge info needed:
+The room to search has enough waypoints to cover the complete area.
+This means waypoints on the floor, and ob
+"""
+
 starting_point = "initial_pose"
 
 # required keys: entity_id (str), room_id (str), navigation_area (str), segment_areas (list)
