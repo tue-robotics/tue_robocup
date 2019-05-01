a8bff58d8e94214f6298896b83fc7bffa050f1a0
Sam Alexandrov <alexandrov.samuil@gmail.com>
2019-05-01 20:57:42 +0200

    Added serving drinks challenge knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_serving_drinks.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_serving_drinks.py
@@ -1,2 +1,7 @@
 # SERVING DRINKS KNOWLEDGE FILE RGO2019
 
+# Bar where the drinks are located
+bar_id = "bar"
+
+# Room where the party takes place, i.e., where the robot should look for people
+room_id = "living_room"
