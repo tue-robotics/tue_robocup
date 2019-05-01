b496730053bc0f2c29737e52afd440a4e41288ac
Sam Alexandrov <alexandrov.samuil@gmail.com>
2019-05-01 20:58:09 +0200

    Added take out the garbage challenge knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_take_out_the_garbage.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_take_out_the_garbage.py
@@ -1,2 +1,11 @@
 # TAKE OUT THE GARBAGE KNOWLEDGE FILE RGO2019
 
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+starting_point = "initial_pose"
+trashbin_id = "trashbin"
+trashbin_id2 = "trashbin2"
+drop_zone_id = "operator_table"
