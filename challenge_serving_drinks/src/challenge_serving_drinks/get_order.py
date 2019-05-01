4d9840a70e41a5b308350911480b09363046779e
Sam Alexandrov <alexandrov.samuil@gmail.com>
2019-05-01 21:00:15 +0200

    Serving drings update
    Added class DetectWaving and updated challenge execution

--- challenge_serving_drinks/src/challenge_serving_drinks/get_order.py
+++ challenge_serving_drinks/src/challenge_serving_drinks/get_order.py
@@ -1,5 +1,6 @@
 # System
 import random
+import math
 
 # ROS
 import rospy
@@ -7,10 +8,14 @@ import smach
 
 # TU/e Robotics
 import robot_smach_states as states
+import PyKDL as kdl
 from hmi import TimeoutException
 from robocup_knowledge import knowledge_loader
 from robot_skills.robot import Robot
 from robot_smach_states.util.designators import EdEntityDesignator, VariableDesignator
+from robot_skills.util.kdl_conversions import frame_stamped, VectorStamped
+from tue_msgs.msg import People
+from Queue import Queue, Empty
 
 # Knowledge
 COMMON_KNOWLEDGE = knowledge_loader.load_knowledge("common")
@@ -143,6 +148,124 @@ class AskDrink(smach.State):
         return speech_result.semantics
 
 
+class DetectWaving(smach.State):
+    """ Waits for waving person.
+
+    This is based on the 'WaitForCustomer' class of the the restaurant challenge. Might
+    be nice to merge these two.
+    """
+    def __init__(self, robot, caller_id, kitchen_designator):
+        """ Constructor
+
+        :param robot: robot object
+        """
+        smach.State.__init__(self, outcomes=['succeeded', 'aborted'])
+        self._robot = robot
+        self._caller_id = caller_id
+        self._kitchen_designator = kitchen_designator
+        self._people_sub = rospy.Subscriber(robot.robot_name + '/persons', People, self.people_cb, queue_size=1)
+        self.people_queue = Queue(maxsize=1)
+
+    def people_cb(self, persons):
+        if persons.people:
+            rospy.logdebug('Received %d persons in the people cb', len(persons.people))
+        self.people_queue.put(persons)
+
+    def execute(self):
+
+        self._robot.head.reset()
+        self._robot.head.wait_for_motion_done()
+
+        self._robot.speech.speak("Please wave if you want me to bring you something")
+
+        head_samples = 20
+        look_distance = 3.0
+        look_angles = [0,
+                       0,
+                       0,
+                       10,
+                       -10,
+                       20,
+                       -20]
+
+        self.clear_queue()
+
+        waving_persons = []
+        i = 0
+        while not rospy.is_shutdown() and not waving_persons:
+            header, waving_persons = self.wait_for_waving_person(head_samples=head_samples)
+
+            angle = look_angles[i % len(look_angles)]
+            rospy.loginfo('Still waiting... looking at %d degrees', angle)
+            angle = math.radians(angle)
+            head_goal = VectorStamped(x=look_distance * math.cos(angle),
+                                      y=look_distance * math.sin(angle), z=1.3,
+                                      frame_id="/%s/base_link" % self._robot.robot_name)
+            self._robot.head.look_at_point(head_goal)
+            i += 1
+
+        if not waving_persons:
+            return 'aborted'
+
+        rospy.loginfo('waving persons: %s', waving_persons)
+        if len(waving_persons) > 1:
+            rospy.logwarn('using the first person')
+
+        point = waving_persons[0].position
+        pose = frame_stamped(header.frame_id, point.x, point.y, point.z)
+        rospy.loginfo('update customer position to %s', pose)
+        self._robot.ed.update_entity(id=self._caller_id, frame_stamped=pose, type="waypoint")
+
+        # look at the barman
+        kitchen_entity = self._kitchen_designator.resolve()
+        target_pose = kitchen_entity._pose
+        head_target_kdl = target_pose * kdl.Vector(20.0, 0.0, 0.0)
+        head_target = VectorStamped(x=head_target_kdl.x(), y=head_target_kdl.y(), z=head_target_kdl.z(),
+                                    frame_id="/map")
+        # pose = kitchen_entity.pose.extractVectorStamped()
+        # pose.vector[2] = 1.5
+
+        self._robot.head.look_at_point(head_target)
+        self._robot.head.wait_for_motion_done()
+
+        return 'succeeded'
+
+    def clear_queue(self):
+        while True:
+            try:
+                self.people_queue.get_nowait()
+            except Empty:
+                # There is probably an old measurement blocking in the callback thread, also remove that one
+                self.people_queue.get()
+                return
+
+    def wait_for_waving_person(self, head_samples):
+        waving_persons = []
+        for i in range(0, head_samples):
+            if rospy.is_shutdown():
+                return
+
+            people_received = self.wait_for_cb()
+            rospy.logdebug('Got sample %d with seq %s', i, people_received.header.seq)
+            for person in people_received.people:
+                if {'RWave', 'LWave'}.intersection(set(person.tags)):
+                    waving_persons.append(person)
+
+            if waving_persons:
+                break
+        return people_received.header, waving_persons
+
+    def wait_for_cb(self):
+        timeout = 1
+
+        people_received = People()
+        while not rospy.is_shutdown() and not people_received.people:
+            try:
+                return self.people_queue.get(timeout=timeout)
+            except Empty:
+                rospy.logwarn('No people message received within %d seconds', timeout)
+
+
 class GetOrder(smach.StateMachine):
     """ Gets an order. If succeeded, the person_designator and drink_designator are filled and can be used in subsequent
     states.
@@ -161,11 +284,44 @@ class GetOrder(smach.StateMachine):
         with self:
 
             # For now:
-            # * Ask person to step in front
-            # * Wait (for fixed period/until operator is detected) for operator to step in front of the robot
-            # Future ToDo's, replace by:
+            # * Say that everyone looks like having a drink
             # * Ask person to wave
             # * Drive to waiving person (a la restaurant)
+            # * If not waving person (for fixed period/until operator is detected):
+                # * Ask person to step in front
+                # * Wait (for fixed period/until operator is detected) for operator to step in front of the robot
+
+            smach.StateMachine.add(
+                "ASK_FOR_WAVING",
+                states.Say(
+                    robot=robot,
+                    sentence="Looks like everyone has a drink",
+                    look_at_standing_person=True),
+                transitions={"spoken": "WAIT_FOR_WAVING"}
+            )
+
+            smach.StateMachine.add(
+                "WAIT_FOR_WAVING",
+                DetectWaving(robot, caller_id, kitchen_designator),
+                transitions={'succeeded': 'SAY_I_HAVE_SEEN',
+                             'aborted': 'ASK_STEP_IN_FRONT'}
+            )
+
+            smach.StateMachine.add(
+                'SAY_I_HAVE_SEEN',
+                states.Say(robot, 'I have seen a waving person, I will be there shortly!'),
+                transitions={"spoken": 'NAVIGATE_TO_WAVING'}
+            )
+
+            smach.StateMachine.add(
+                'NAVIGATE_TO_WAVING',
+                states.NavigateToObserve(
+                    robot=robot,
+                    entity_designator=caller_designator,
+                    radius=1.1),
+                transitions={'arrived': 'LEARN_OPERATOR',
+                            'unreachable': 'ASK_STEP_IN_FRONT',
+                            'goal_not_defined': 'WAIT_FOR_WAVING'})
 
             smach.StateMachine.add(
                 "ASK_STEP_IN_FRONT",
@@ -196,7 +352,6 @@ if __name__ == "__main__":
     # Test code
 
     import sys
-    import robot_smach_states.util.designators as ds
 
     rospy.init_node('get_drinks')
 
@@ -208,7 +363,7 @@ if __name__ == "__main__":
     elif robot_name == 'hero':
         from robot_skills.hero import Hero as Robot
     else:
-        print "unknown robot"
+        print("unknown robot")
         sys.exit()
 
     _robot = Robot()
