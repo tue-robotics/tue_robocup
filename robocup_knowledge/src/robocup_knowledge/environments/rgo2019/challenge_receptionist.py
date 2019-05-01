60ad7c581bacac13171beeaff6bb9412ef87dd66
Sam Alexandrov <alexandrov.samuil@gmail.com>
2019-05-01 20:57:10 +0200

    Added receptionist challenge knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_receptionist.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_receptionist.py
@@ -1,2 +1,12 @@
 # RECEPTIONIST KNOWLEDGE FILE RGO2019
 
+from robocup_knowledge import knowledge_loader
+common = knowledge_loader.load_knowledge("common")
+
+operator_name = "john"
+
+starting_point = "initial_pose"
+
+waypoint_door = {'id': 'entry_door', 'radius': 0.5}
+
+waypoint_livingroom = {'id': 'livingroom', 'radius': 0.5}
