066d4e92a5ac4fdede006fae97973cdb2bd1e66c
Sam Alexandrov <alexandrov.samuil@gmail.com>
2019-05-02 20:04:26 +0200

    knowledge[serving_drinks] updated bar_id with existing entity in the world model

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_serving_drinks.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_serving_drinks.py
@@ -1,7 +1,7 @@
 # SERVING DRINKS KNOWLEDGE FILE RGO2019
 
 # Bar where the drinks are located
-bar_id = "kitchen_table"
+bar_id = "desk"
 
 # Room where the party takes place, i.e., where the robot should look for people
 room_id = "living_room"
