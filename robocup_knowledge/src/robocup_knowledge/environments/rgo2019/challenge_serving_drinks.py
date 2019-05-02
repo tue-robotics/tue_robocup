8e7e83d38a7ff2b1a6b56c2f757ef230a2889a6d
Sam Alexandrov <alexandrov.samuil@gmail.com>
2019-05-02 09:09:29 +0200

    Added initial pose/waypoints

--- robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_serving_drinks.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rgo2019/challenge_serving_drinks.py
@@ -5,3 +5,11 @@ bar_id = "bar"
 
 # Room where the party takes place, i.e., where the robot should look for people
 room_id = "living_room"
+
+# Default guest
+operator_name = "default_guest"
+
+# Starting location
+starting_point = "initial_pose"
+waypoint_door = {"id": "entry_door", "radius": 0.5}
+waypoint_livingroom = {"id": "livingroom", "radius": 0.5}
