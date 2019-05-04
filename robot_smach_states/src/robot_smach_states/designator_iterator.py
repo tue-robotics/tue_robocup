1e05d38dc77ea33bf736461f62234bef31515328
Loy van Beek <loy.vanbeek@gmail.com>
2019-05-04 23:06:56 +0200

    Create state to iterate over collection designator

---
+++ robot_smach_states/src/robot_smach_states/designator_iterator.py
@@ -0,0 +1,54 @@
+import smach
+
+import robot_smach_states.util.designators.core as ds
+from robot_smach_states.util.designators import is_writeable
+
+
+class IterateDesignator(smach.State):
+    def __init__(self, collection_des, element_des):
+        """Iterate over a designator that resolves to a collection
+
+        :param collection_des: Designator with a iterable resolve_type
+        :param element_des: Writeable designator with a resolve_type that should match the element type of the collection
+        """
+        smach.State.__init__(self, outcomes=['next', 'stop_iteration'])
+
+        is_writeable(element_des)
+
+        assert hasattr(collection_des.resolve_type, '__iter__')
+        assert collection_des.resolve_type[0] == element_des.resolve_type
+
+        self.collection_des = collection_des
+        self.element_des = element_des
+
+        self._current_elements = None
+
+    def execute(self, userdata=None):
+        if self._current_elements is None:
+            self._current_elements = iter(self.collection_des.resolve())
+
+        try:
+            self.element_des.write(next(self._current_elements))
+            return 'next'
+        except StopIteration:
+            return 'stop_iteration'
+
+
+if __name__ == "__main__":
+    collection_des = ds.Designator(['a', 'b', 'c'])
+    element_des = ds.VariableDesignator(resolve_type=str)
+
+    iterator = IterateDesignator(collection_des, element_des.writeable)
+
+    import ipdb; ipdb.set_trace()
+    assert iterator.execute() == 'next'
+    assert element_des.resolve() == 'a'
+
+    assert iterator.execute() == 'next'
+    assert element_des.resolve() == 'b'
+
+    assert iterator.execute() == 'next'
+    assert element_des.resolve() == 'c'
+
+    assert iterator.execute() == 'stop_iteration'
+
