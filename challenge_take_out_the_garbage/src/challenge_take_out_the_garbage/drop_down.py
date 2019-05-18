547765cf4328c0194d9020b3d4aae9e4eff1a668
Kevin Dang <kevin.acnowlogia@gmail.com>
2019-05-18 15:33:07 +0200

    Added some docstrings and cleaned up statemachine

--- challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/drop_down.py
+++ challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/drop_down.py
@@ -1,18 +1,23 @@
 # ROS
-import rospy
 import smach
 
 # TU/e
-import robot_skills
 import robot_smach_states as states
 import robot_smach_states.util.designators as ds
-import robot_smach_states.manipulation as manipulation
-from robot_skills.arms import PublicArm
-
 from robot_skills.util.kdl_conversions import FrameStamped
 
+
 class dropPoseDesignator(ds.Designator):
+    """
+    Designator to resolver the drop pose
+    """
     def __init__(self, robot, drop_height, name):
+        """
+
+        :param robot: robot object
+        :param drop_height: height to drop the object from
+        :param name: name of pose
+        """
         super(dropPoseDesignator, self).__init__(resolve_type=FrameStamped, name=name)
 
         self._robot = robot
@@ -31,44 +36,13 @@ class dropPoseDesignator(ds.Designator):
 
         return FrameStamped(frame, "/map")
 
-class PlaceSingleItem(smach.State):
-    """ Tries to place an object. A 'place' statemachine is constructed dynamically since this makes it easier to
-     build a statemachine (have we succeeded in grasping the objects?)"""
-    def __init__(self, robot, place_designator, item_designator):
-        """ Constructor
-
-        :param robot: robot object
-        :param place_designator: Designator that resolves to the pose to place at. E.g. an EmptySpotDesignator
-        """
-        smach.State.__init__(self, outcomes=["succeeded", "failed"])
-
-        self._robot = robot
-        self._place_designator = place_designator
-        self._item_designator = item_designator
-
-    def execute(self, userdata=None):
-
-        # Try to place the object
-        # ToDo: Change back when arm can be set to occupied in pick up
-        arm_designator = ds.OccupiedArmDesignator(robot=self._robot, arm_properties={})
-        # arm_designator = ds.UnoccupiedArmDesignator(self._robot, {}, name="empty_arm_designator")
-        sm = states.Place(robot=self._robot, item_to_place=self._item_designator, place_pose=self._place_designator,
-                          place_volume="on_top_of", arm=arm_designator)
-        result = sm.execute()
-
-        # If failed, do handover to human in order to continue
-        if result != "done":
-            sm = states.HandoverToHuman(robot=self._robot, arm_designator=arm_designator)
-            sm.execute()
-
-        return "succeeded" if result == "done" else "failed"
-
 
 class DropDownTrash(smach.StateMachine):
-
+    """
+    State that makes the robot go to the drop zone and drop the trash bag there
+    """
     def __init__(self, robot, trash_designator, drop_designator):
         """
-
         :param robot: robot object
         :param trash_designator: EdEntityDesignator designating the trash
         :param drop_designator: EdEntityDesignator designating the collection zone
@@ -76,15 +50,25 @@ class DropDownTrash(smach.StateMachine):
         smach.StateMachine.__init__(self, outcomes=["succeeded", "failed", "aborted"])
 
         drop_area_pose = dropPoseDesignator(robot, 0.6, "drop_pose")
+        arm_designator = ds.OccupiedArmDesignator(robot=robot, arm_properties={})
+        self._trash_designator = trash_designator
+        self._drop_designator = drop_designator
 
         with self:
             smach.StateMachine.add("GO_TO_COLLECTION_ZONE",
-                                   states.NavigateToObserve(robot, drop_designator),
-                                   transitions={"arrived": "PLACE_ITEM",
+                                   states.NavigateToObserve(robot, self._drop_designator),
+                                   transitions={"arrived": "PLACE_TRASH",
                                                 "goal_not_defined": "aborted",
                                                 "unreachable": "failed"})
 
-            smach.StateMachine.add("PLACE_ITEM", PlaceSingleItem(robot=robot, place_designator=drop_area_pose,
-                                                                 item_designator=trash_designator),
+            smach.StateMachine.add("PLACE_TRASH",
+                                   states.Place(robot=robot, item_to_place=trash_designator,
+                                                place_pose=drop_area_pose, place_volume="on_top_of",
+                                                arm=arm_designator),
+                                   transitions={"done": "succeeded",
+                                                "failed": "HANDOVER"})
+
+            smach.StateMachine.add("HANDOVER",
+                                   states.HandoverToHuman(robot=robot, arm_designator=arm_designator),
                                    transitions={"succeeded": "succeeded",
                                                 "failed": "failed"})
