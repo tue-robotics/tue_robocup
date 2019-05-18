08b739e300e64948ca02341b24166e4987567439
Kevin Dang <kevin.acnowlogia@gmail.com>
2019-05-18 15:35:29 +0200

    Added docstrings, clean up code and add the handover state if grab fails

--- challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/pick_up.py
+++ challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/pick_up.py
@@ -3,13 +3,13 @@ import rospy
 import smach
 
 # TU/e
-import robot_skills
 import robot_smach_states as states
 import robot_smach_states.util.designators as ds
+import robot_smach_states.manipulation as manipulation
+from robot_skills.arms import PublicArm
 
 from ed_msgs.msg import EntityInfo
-
-from robot_skills.util.kdl_conversions import FrameStamped, kdl_frame_from_XYZRPY
+from robot_skills.util.kdl_conversions import FrameStamped
 
 
 class GetTrashBin(smach.State):
@@ -17,15 +17,18 @@ class GetTrashBin(smach.State):
     Get the entity of the trashbin in FrameStamped and save it to get the orientation of the fixed point in world model
     """
     def __init__(self, robot, trashbin):
+        """
+        :param robot: robot object
+        :param trashbin: EdEntityDesignator designating the trash bin
+        """
         smach.State.__init__(self, outcomes=["succeeded", "failed"])
         self._robot = robot
         self._trashbin = trashbin
 
     def execute(self, userdata=None):
         e = self._trashbin.resolve()
-        # get original entity pose
-        print("{}".format(type(self._robot.ed.get_entity(id=e.id)._pose)))
 
+        # get original entity pose
         frame_original = self._robot.ed.get_entity(id=e.id)._pose
 
         # inspect and update entity
@@ -34,9 +37,8 @@ class GetTrashBin(smach.State):
         frame_updated = self._robot.ed.get_entity(id=e.id)._pose
 
         # update entity with original orientation
-        test = frame_updated
-        test.M = frame_original.M
-        new_frame = FrameStamped(test, "map")
+        frame_updated.M = frame_original.M
+        new_frame = FrameStamped(frame_updated, "map")
 
         # new_frame.header.stamp
         print("{}".format(new_frame))
@@ -46,11 +48,16 @@ class GetTrashBin(smach.State):
         else:
             return "failed"
 
+
 class GrabTrash(smach.State):
     """
     Set the arm in the prepare grasp position so it can safely go to the grasp position
     """
     def __init__(self, robot, arm_designator):
+        """
+        :param robot: robot object
+        :param arm_designator: arm designator resolving to the arm with which to grab
+        """
         smach.State.__init__(self, outcomes=['succeeded', 'failed'])
 
         self._robot = robot
@@ -98,23 +105,73 @@ class GrabTrash(smach.State):
         arm.send_joint_goal('reset')
         arm.wait_for_motion_done()
 
-        # ToDo: the arm should have an occupied state, so that an occupied designator can be used in drop down
-
         handed_entity = EntityInfo(id="trash")
         arm.occupied_by = handed_entity
 
         return "succeeded"
 
 
-class PickUpTrash(smach.StateMachine):
+class HandoverFromHuman(smach.StateMachine):
+    '''
+    State that enables low level grab reflex. Besides a robot object, needs
+    an arm and an entity to grab, which is either one from ed through the
+    grabbed_entity_designator or it is made up in the
+    CloseGripperOnHandoverToRobot state and given the grabbed_entity_label
+    as id.
+    '''
+    def __init__(self, robot, arm_designator, grabbed_entity_label="", grabbed_entity_designator=None, timeout=15, arm_configuration="handover_to_human"):
+        """
+        Hold up hand to accept an object and close hand once something is inserted
+        :param robot: Robot with which to execute this behavior
+        :param arm_designator: ArmDesignator resolving to arm accept item into
+        :param grabbed_entity_label: What ID to give a dummy item in case no grabbed_entity_designator is supplied
+        :param grabbed_entity_designator: EntityDesignator resolving to the accepted item. Can be a dummy
+        :param timeout: How long to hold hand over before closing without anything
+        :param arm_configuration: Which pose to put arm in when holding hand up for the item.
+        """
+        smach.StateMachine.__init__(self, outcomes=['succeeded','failed','timeout'])
 
+        ds.check_type(arm_designator, PublicArm)
+        if not grabbed_entity_designator and grabbed_entity_label == "":
+            rospy.logerr("No grabbed entity label or grabbed entity designator given")
+
+        with self:
+            smach.StateMachine.add("POSE", manipulation.ArmToJointConfig(robot, arm_designator, arm_configuration),
+                                   transitions ={'succeeded': 'OPEN_BEFORE_INSERT','failed':'OPEN_BEFORE_INSERT'})
+
+            smach.StateMachine.add(
+                'OPEN_BEFORE_INSERT',
+                manipulation.SetGripper(
+                    robot=robot,
+                    arm_designator=arm_designator,
+                    gripperstate=manipulation.GripperState.OPEN),
+                transitions={'succeeded': 'SAY1',
+                             'failed': 'SAY1'}
+            )
+
+            smach.StateMachine.add("SAY1", states.Say(robot,'Please hand over the object by putting the top of the bag between'
+                                                     ' my grippers and push firmly into my camera.'),
+                            transitions={'spoken': 'CLOSE_AFTER_INSERT'})
+
+            smach.StateMachine.add( 'CLOSE_AFTER_INSERT', manipulation.CloseGripperOnHandoverToRobot(robot,
+                                                                                        arm_designator,
+                                                                                        grabbed_entity_label=grabbed_entity_label,
+                                                                                        grabbed_entity_designator=grabbed_entity_designator,
+                                                                                        timeout=timeout),
+                                transitions={'succeeded'    :   'succeeded',
+                                             'timeout'      :   'timeout',
+                                             'failed'       :   'failed'})
+
+
+class PickUpTrash(smach.StateMachine):
+    """
+    State that makes the robot go to the correct position in front of trash bin and grabs the trash
+    """
     def __init__(self, robot, trashbin_designator, arm_designator):
         """
-
         :param robot: robot object
         :param trashbin_designator: EdEntityDesignator designating the trashbin
-        :param trash_designator: EdEntityDesignator designating the trash
-        :param drop_designator: EdEntityDesignator designating the collection zone
+        :param arm_designator: arm designator resolving to the arm with which to grab
         """
         smach.StateMachine.__init__(self, outcomes=["succeeded", "failed", "aborted"])
 
@@ -122,11 +179,11 @@ class PickUpTrash(smach.StateMachine):
             smach.StateMachine.add("GO_BIN",
                                    states.NavigateToSymbolic(robot, {trashbin_designator: 'in_front_of'},
                                                              trashbin_designator),
-                                   transitions={"arrived": "PICK_UP_TRASH",
+                                   transitions={"arrived": "GET_BIN_POSITION",
                                                 "goal_not_defined": "aborted",
                                                 "unreachable": "failed"})
 
-            smach.StateMachine.add("PICK_UP_TRASH", GetTrashBin(robot=robot, trashbin=trashbin_designator),
+            smach.StateMachine.add("GET_BIN_POSITION", GetTrashBin(robot=robot, trashbin=trashbin_designator),
                                    transitions={"succeeded": "GO_TO_NEW_BIN",
                                                 "failed": "failed"})
 
@@ -134,10 +191,33 @@ class PickUpTrash(smach.StateMachine):
                                    states.NavigateToObserve(robot, trashbin_designator, radius=0.4),
                                    transitions={"arrived": "PREPARE_AND_GRAB",
                                                 "goal_not_defined": "aborted",
-                                                "unreachable": "failed"})
+                                                "unreachable": "ASK_HANDOVER"})
 
             smach.StateMachine.add("PREPARE_AND_GRAB", GrabTrash(robot=robot, arm_designator=arm_designator),
                                    transitions={"succeeded": "succeeded",
-                                                "failed": "failed"})
+                                                "failed": "ASK_HANDOVER"})
+
+            # Ask human to handover the trash bag
+            smach.StateMachine.add("ASK_HANDOVER", HandoverFromHuman(robot=robot, arm_designator=arm_designator,
+                                                                     grabbed_entity_label='thrash'),
+                                   transitions={"succeeded": "LOWER_ARM",
+                                                "failed": "failed",
+                                                "timeout": "TIMEOUT"})
+
+            arm_occupied_designator = ds.OccupiedArmDesignator(robot=robot, arm_properties={})
+
+            smach.StateMachine.add("LOWER_ARM", states.ArmToJointConfig(robot=robot,
+                                                                        arm_designator=arm_occupied_designator,
+                                                                        configuration="reset"),
+                                   transitions={"succeeded": "RECEIVED_TRASH_BAG",
+                                                "failed": "RECEIVED_TRASH_BAG"})
+
+            smach.StateMachine.add("RECEIVED_TRASH_BAG", states.Say(robot, "I received the thrash bag. I will throw"
+                                                                           " it away, please move away.", block=True),
+                                   transitions={'spoken': 'succeeded'})
+
+            smach.StateMachine.add("TIMEOUT", states.Say(robot, "I have not received anything, so I will stop",
+                                                         block=False),
+                                   transitions={'spoken': "failed"})
 
 
