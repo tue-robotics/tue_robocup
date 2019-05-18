9c9f506fa0f1386dcbdfbac7a305bb87ab109874
Kevin Dang <kevin.acnowlogia@gmail.com>
2019-05-18 16:33:55 +0200

    Add knowledge for robotics testlabs

---
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_take_out_the_garbage.py
@@ -0,0 +1,11 @@
+# TAKE OUT THE GARBAGE KNOWLEDGE FILE ROBOTICS_TESTLABS
+
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+starting_point = "initial_pose"
+trashbin_id = "trashbin"
+trashbin_id2 = "trashbin"
+drop_zone_id = "drop_area"
