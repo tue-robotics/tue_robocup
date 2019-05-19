c5547f6509029eaa4646923d4733d1b45073c736
Lars Janssen <janslar@hotmail.com>
2019-05-19 11:21:50 +0200

    Fixes(gpsr): shorten speech and reintroduce the tc_fuckup time

--- challenge_gpsr/src/gpsr.py
+++ challenge_gpsr/src/gpsr.py
@@ -141,11 +141,11 @@ class ConversationEngineWithHmi(ConversationEngine):
         self.robot.reset()
         self.robot.head.look_at_standing_person()
 
-        self.robot.speech.speak("Please trigger me by stating my name directly into the microphone.", block=True)
+        self.robot.speech.speak("Please trigger me by stating my name directly into the microphone after the ping.", block=True)
 
         self.wait_to_be_called()
 
-        self.robot.speech.speak("What can I do for you? State your command loudly directly into my microphone, but please wait for the ping!.", block=True)
+        self.robot.speech.speak("Please state your command clearly into the microphone after the ping!.", block=True)
 
         while not rospy.is_shutdown():
             try:
@@ -153,7 +153,7 @@ class ConversationEngineWithHmi(ConversationEngine):
                                                            grammar=grammar,
                                                            target=target)
                 if not self.is_text_valid_input(sentence):
-                    self._say_to_user("I don't understand what you're saying, please rephrase. Speak loudly and clearly after the ping.")
+                    self._say_to_user("I don't understand what you're saying, please try again.")
                     continue
 
                 self.timeout_count = 0
@@ -164,6 +164,7 @@ class ConversationEngineWithHmi(ConversationEngine):
                 if correct:
                     # Pass the heard sentence to the conv.engine. This parses it again, but fuck efficiency for now
                     self.user_to_robot_text(sentence)
+                    rospy.sleep(self._tc_fuckup_time)
                     break
             except (hmi.TimeoutException , hmi.GoalNotSucceededException) as e:
                 rospy.logwarn("HMI failed when getting command: {}" .format(e))
@@ -193,7 +194,7 @@ class ConversationEngineWithHmi(ConversationEngine):
                     rospy.logwarn("[GPSR] Timeout_count: {}".format(self.timeout_count))
 
     def heard_correct(self, sentence):
-        self.robot.speech.speak('I heard %s, is this correct? Please speak loudly into the microphone and move away afterwards!' % sentence)
+        self.robot.speech.speak('I heard %s, is this correct? Please speak loudly into the microphone!' % sentence)
         try:
             if 'no' == self.robot.hmi.query('', 'T -> yes | no', 'T').sentence:
                 self.robot.speech.speak('Sorry, please try again')
