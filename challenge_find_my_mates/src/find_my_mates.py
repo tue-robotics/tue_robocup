f9afec30cb3d823dfcff6f46fea8d46d807b6f6c
Josja Geijsberts <j.geijsberts@student.tue.nl>
2019-05-21 21:30:31 +0200

    implemented review comments.

--- challenge_find_my_mates/src/find_my_mates.py
+++ challenge_find_my_mates/src/find_my_mates.py
@@ -29,7 +29,7 @@ class FindPeople(State):
         :param search_timeout: (float) maximum time the robot is allowed to search
         :param look_distance: (float) robot only considers laser entities within this radius
         """
-        State.__init__(self, outcomes=['Done', 'Aborted', 'Failed'])
+        State.__init__(self, outcomes=['done', 'aborted', 'failed'])
 
         self._robot = robot
         self._search_timeout = search_timeout
@@ -45,22 +45,21 @@ class FindPeople(State):
 
         start_time = rospy.Time.now()
 
-        look_distance = 2.0  # magic number 4
         look_angles = [f * math.pi / d if d != 0 else 0.0 for f in [-1, 1] for d in [0, 6, 4, 2.3]]  # Magic numbers
-        head_goals = [kdl_conversions.VectorStamped(x=look_distance * math.cos(angle),
-                                                    y=look_distance * math.sin(angle), z=1.7,
+        head_goals = [kdl_conversions.VectorStamped(x=self._look_distance * math.cos(angle),
+                                                    y=self._look_distance * math.sin(angle), z=1.7,
                                                     frame_id="/%s/base_link" % self._robot.robot_name)
                       for angle in look_angles]
-        head_goals.extend([kdl_conversions.VectorStamped(x=2.0 * look_distance * math.cos(angle),
-                                                         y=2.0 * look_distance * math.sin(angle), z=1.7,
+        head_goals.extend([kdl_conversions.VectorStamped(x=2.0 * self._look_distance * math.cos(angle),
+                                                         y=2.0 * self._look_distance * math.sin(angle), z=1.7,
                                                          frame_id="/%s/base_link" % self._robot.robot_name)
                            for angle in look_angles])
-        n = 0 # This represents the number of detected people, once four people are detected, this state is exited. 
+        n = 0 # This represents the number of detected people, once four people are detected, this state is exited.
         i = 0
 
         while (rospy.Time.now() - start_time).to_sec() < self._search_timeout:
             if self.preempt_requested():
-                return 'Aborted'
+                return 'aborted'
 
             self._robot.head.look_at_point(head_goals[i])
             i += 1
@@ -69,8 +68,6 @@ class FindPeople(State):
             self._robot.head.wait_for_motion_done()
             raw_detections = self._robot.perception.detect_faces()
 
-            # if not raw_detections:
-            #     continue
             if raw_detections:
                 for detection in raw_detections:
                     roi = detection.roi
@@ -80,6 +77,7 @@ class FindPeople(State):
                         rospy.logerr("head.project_roi failed: %s", e)
                         continue  # move onto next detection
 
+                    # ToDo: This should be the ED people detector
                     detected_person = self._robot.ed.get_closest_laser_entity(radius=self._look_distance,
                                                                               center_point=person_pos_kdl)
 
@@ -93,24 +91,23 @@ class FindPeople(State):
                             _pose.extractVectorStamped() - detected_person.pose.extractVectorStamped() > self._min_dist:
                             self._people[n] = detected_person
 
-                    if len(self._people) == (n+1):
-                        rospy.loginfo(
-                            "I found someone at {}".format(self._people[n].pose.extractVectorStamped(),
-                                                           block=False))
+                            rospy.loginfo(
+                                "I found someone at {}".format(self._people[n].pose.extractVectorStamped(),
+                                                               block=False))
 
-                        self._robot.ed.update_entity(
-                            id="person" + str(n),
-                            frame_stamped=kdl_conversions.FrameStamped(kdl.Frame(person_pos_kdl.vector), "map"),
-                            type="waypoint")
+                            self._robot.ed.update_entity(
+                                id="person" + str(n),
+                                frame_stamped=kdl_conversions.FrameStamped(kdl.Frame(person_pos_kdl.vector), "map"),
+                                type="waypoint")
 
-                        n += 1
+                            n += 1
+
                     if len(self._people) > 3:
                         self._robot.head.close()
-                        return 'Done'
+                        return 'done'
 
         self._robot.head.close()
-        # rospy.sleep(2.0)
-        return 'Done'
+        return 'done'
 
 
 class IdentifyPeople(State):
@@ -119,7 +116,7 @@ class IdentifyPeople(State):
     """
 
     def __init__(self, robot):
-        State.__init__(self, outcomes=['Done', 'Aborted', 'Failed'])
+        State.__init__(self, outcomes=['done', 'aborted', 'failed'])
         self._robot = robot
 
     def execute(self, userdata=None):
@@ -145,14 +142,13 @@ class IdentifyPeople(State):
                 self._robot.speech.speak(
                     "I don't understand, I expected a command like " + ", ".join(challenge_knowledge.names))
             try:
-                print("Detecting this person")
+                rospy.loginfo("Detecting this person")
                 # detect person -> update person entity with certain attributes
             except:
-                print("Detecting failed.")
+                rospy.loginfo("Detecting failed.")
                 continue
                 # catch exceptions
-            # ask name and update entity
-        return 'Done'
+        return 'done'
 
 
 class ReportPeople(State):
@@ -161,7 +157,7 @@ class ReportPeople(State):
     """
 
     def __init__(self, robot):
-        State.__init__(self, outcomes=['Done', 'Aborted', 'Failed'])
+        State.__init__(self, outcomes=['done', 'aborted', 'failed'])
         self._robot = robot
         self._room_entity = self._robot.ed.get_entity(id=ROOM_ID)
 
@@ -177,15 +173,17 @@ class ReportPeople(State):
             if self._room_entity.in_volume(kdl_conversions.VectorStamped(vector=item._pose.p), "in"):
                 furniture_entities_room.append(item)
         closest_entity = []
-        for person in person_entities:
-            distance_closest = 10.0
-            temp_entity_near = furniture_entities_room[0]
-            for entity in furniture_entities_room:
-                distance = entity.distance_to_2d(person.position)
-                if distance < distance_closest:
-                    temp_entity_near = entity
-                    distance_closest = distance
-            closest_entity.append(temp_entity_near)
+
+        if len(furniture_entities_room)>0:
+            for person in person_entities:
+                distance_closest = furniture_entities_room[0].distance_to_2d(person.position)
+                temp_entity_near = furniture_entities_room[0]
+                for entity in furniture_entities_room:
+                    distance = entity.distance_to_2d(person.position)
+                    if distance < distance_closest:
+                        temp_entity_near = entity
+                        distance_closest = distance
+                closest_entity.append(temp_entity_near)
 
         self._robot.speech.speak('I found many people but none were as pretty as you.', block=True)
         people_descriptions = {}
@@ -195,6 +193,7 @@ class ReportPeople(State):
             unique_property = None
             properties = [prop for prop in dir(person) if not prop.startswith('__')]
             attributes = dict()
+            # ToDo: should be updated to contain the new person attributes
             for prop in properties:
                 temp_prop = [getattr(person, prop) for person in person_entities]
                 c = Counter(temp_prop)
@@ -217,8 +216,7 @@ class ReportPeople(State):
                 sentence += "and the person was wearing a mostly {} shirt.".format(getattr(person, unique_property))
 
             people_descriptions[getattr(person, 'name')] = sentence
-            if i > 3:
-                break
+
         n = 0
         while n < 3:
             self._robot.speech.speak('Which mate would you like me to describe?', block=True)
@@ -235,45 +233,10 @@ class ReportPeople(State):
                     n += 1
             else:
                 self._robot.speech.speak(
-                    "I don't understand, I expected a command like " + ", ".join(challenge_knowledge.names))
+                    "I don't understand, I expected a name like " + ", ".join(challenge_knowledge.names))
                 continue
 
-        return 'Done'
-
-
-class AskNames(State):
-    """
-    Ask for which three people to describe
-    """
-
-    def __init__(self, robot):
-        State.__init__(self, outcomes=['Done', 'Aborted', 'Failed'])
-        self._robot = robot
-        self._tries = 5
-        self._time_out = 30
-
-    def execute(self, userdata=None):
-        self._robot.head.look_at_standing_person()
-        self._robot.speech.speak("Which mates would you like me to find?", block=True)
-
-        try:
-            result = self._robot.hmi.query('What names?', 'T -> ' + ' | '.join(challenge_knowledge.names), 'T',
-                                           timeout=self._time_out)
-            command_recognized = result.sentence
-        except TimeoutException:
-            command_recognized = None
-        if command_recognized == "":
-            self._robot.speech.speak("I am still waiting for a command and did not hear anything")
-        elif command_recognized in challenge_knowledge.names:
-            if command_recognized == "no":
-                self._robot.speech.speak("OK")
-            else:
-                self._robot.speech.speak("OK, I find {}".format(command_recognized))
-            return 'Done'
-        else:
-            self._robot.speech.speak(
-                "I don't understand, I expected a command like " + ", ".join(challenge_knowledge.names))
-            return 'Failed'
+        return 'done'
 
 
 def setup_statemachine(robot):
@@ -286,9 +249,9 @@ def setup_statemachine(robot):
         #                  transitions={'initialized': 'INIT_POSE',
         #                               'abort': 'aborted'})
 
-        StateMachine.add('INIT_POSE', SetInitialPose(robot, STARTING_POINT), transitions={'done': 'WAIT_TIME',
+        StateMachine.add('INIT_POSE', SetInitialPose(robot, STARTING_POINT), transitions={'done': 'GO_TO_SEARCH_POSE',
                                                                                           'preempted': 'aborted',
-                                                                                          'error': 'WAIT_TIME'})
+                                                                                          'error': 'GO_TO_SEARCH_POSE'})
 
         # StateMachine.add('START_CHALLENGE',
         #                  StartChallengeRobust(robot, initial_pose=STARTING_POINT, door=False),
@@ -303,14 +266,14 @@ def setup_statemachine(robot):
                                       'unreachable': 'LOCATE_PEOPLE'})
 
         # locate three (or all four) people
-        StateMachine.add('LOCATE_PEOPLE', FindPeople(robot), transitions={'Done': 'IDENTIFY_PEOPLE',
-                                                                          'Aborted': 'done',
-                                                                          'Failed': 'LOCATE_PEOPLE'})
+        StateMachine.add('LOCATE_PEOPLE', FindPeople(robot), transitions={'done': 'IDENTIFY_PEOPLE',
+                                                                          'aborted': 'done',
+                                                                          'failed': 'LOCATE_PEOPLE'})
 
         # drive past all thee people and fill their description
-        StateMachine.add('IDENTIFY_PEOPLE', IdentifyPeople(robot), transitions={'Done': 'GO_BACK_TO_OPERATOR',
-                                                                                'Aborted': 'done',
-                                                                                'Failed': 'GO_BACK_TO_OPERATOR'})
+        StateMachine.add('IDENTIFY_PEOPLE', IdentifyPeople(robot), transitions={'done': 'GO_BACK_TO_OPERATOR',
+                                                                                'aborted': 'done',
+                                                                                'failed': 'GO_BACK_TO_OPERATOR'})
 
         # drive back to the operator to describe the mates
         StateMachine.add('GO_BACK_TO_OPERATOR',
@@ -326,9 +289,9 @@ def setup_statemachine(robot):
                                       'unreachable': 'GO_BACK_TO_OPERATOR'})
 
         # check how to uniquely define them
-        StateMachine.add('REPORT_PEOPLE', ReportPeople(robot), transitions={'Done': 'done',
-                                                                            'Aborted': 'done',
-                                                                            'Failed': 'failed'})
+        StateMachine.add('REPORT_PEOPLE', ReportPeople(robot), transitions={'done': 'done',
+                                                                            'aborted': 'done',
+                                                                            'failed': 'failed'})
 
     return sm
 
