e18c293131318731e8acf8fc3693cac7c0e70314
Loy van Beek <loy.vanbeek@gmail.com>
2019-06-01 17:13:38 +0200

    Fix ausage of a missing variable

--- robot_smach_states/src/robot_smach_states/util/startup.py
+++ robot_smach_states/src/robot_smach_states/util/startup.py
@@ -63,7 +63,7 @@ def startup(statemachine_creator, statemachine_args = (), initial_state=None, ro
             rospy.loginfo("Setting initial pose of (%f,%f,%f)", x, y, yaw)
             robot.base.set_initial_pose(x, y, yaw)
         except:
-            rospy.logerr("Failed to parse initial pose x, y, yaw from %s" % init_pose)
+            rospy.logerr("Failed to parse initial pose x, y, yaw")
 
     introserver = None
 
