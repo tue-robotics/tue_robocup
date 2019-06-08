b03b0175d9e6dd7c5cbe0e1ba0a40281e4adffd6
Peter van Dooren <pepevandooren@hotmail.com>
2019-06-08 11:36:22 +0200

    added state to raise the robot for inspect

---
+++ challenge_take_out_the_garbage/README.md
@@ -0,0 +1,19 @@
+Before Testing:
+1: Be on the proper branches:
+	- on ed_object_models -> branch: rgo2019_challenge_take_out_the_garbage
+	- on challange_take_out_the_garbage -> branch: rgo2019_challenge_take_out_the_garbage
+2: The drop area where the robot is going to drop the bag is the left side of the dinner table.
+   So make sure that area is clear of any objects!
+
+Instructions for testing:
+1: Set the robot in the initial position, so in front of the imaginary door (in front of the sofa block)
+2: Run challenge
+3: Press emergency button when it is going to grab the trash bag! or remove bin.
+   The distance is not tuned yet for this trash bin, so it will probably go wrong!!
+   
+ToDo's:
+- add second trash bin, if we have a second trash bin.
+  For now the second trash bin is the same as the first one.
+- tune the correct distance for this trash bin
+
+
---
+++ challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/drop_down.py
@@ -0,0 +1,73 @@
+# ROS
+import smach
+
+# TU/e
+import robot_smach_states as states
+import robot_smach_states.util.designators as ds
+from robot_skills.util.kdl_conversions import FrameStamped
+
+
+class dropPoseDesignator(ds.Designator):
+    """
+    Designator to resolver the drop pose
+    """
+    def __init__(self, robot, drop_height, name):
+        """
+        :param robot: robot object
+        :param drop_height: height to drop the object from
+        :param name: name of pose
+        """
+        super(dropPoseDesignator, self).__init__(resolve_type=FrameStamped, name=name)
+
+        self._robot = robot
+        self._drop_height = drop_height
+
+    def _resolve(self):
+        frame = None
+
+        # Query ed
+        try:
+            frame = self._robot.ed.get_entity(id="drop_area")._pose
+        except:
+            return None
+
+        frame.p.z(self._drop_height)
+
+        return FrameStamped(frame, "/map")
+
+
+class DropDownTrash(smach.StateMachine):
+    """
+    State that makes the robot go to the drop zone and drop the trash bag there
+    """
+    def __init__(self, robot, trash_designator, drop_designator):
+        """
+        :param robot: robot object
+        :param trash_designator: EdEntityDesignator designating the trash
+        :param drop_designator: EdEntityDesignator designating the collection zone
+        """
+        smach.StateMachine.__init__(self, outcomes=["succeeded", "failed", "aborted"])
+
+        drop_area_pose = dropPoseDesignator(robot, 0.6, "drop_pose")
+        arm_designator = ds.OccupiedArmDesignator(robot=robot, arm_properties={})
+        self._trash_designator = trash_designator
+        self._drop_designator = drop_designator
+
+        with self:
+            smach.StateMachine.add("GO_TO_COLLECTION_ZONE",
+                                   states.NavigateToObserve(robot, self._drop_designator),
+                                   transitions={"arrived": "PLACE_TRASH",
+                                                "goal_not_defined": "aborted",
+                                                "unreachable": "failed"})
+
+            smach.StateMachine.add("PLACE_TRASH",
+                                   states.Place(robot=robot, item_to_place=trash_designator,
+                                                place_pose=drop_area_pose, place_volume="on_top_of",
+                                                arm=arm_designator),
+                                   transitions={"done": "succeeded",
+                                                "failed": "HANDOVER"})
+
+            smach.StateMachine.add("HANDOVER",
+                                   states.HandoverToHuman(robot=robot, arm_designator=arm_designator),
+                                   transitions={"succeeded": "succeeded",
+                                                "failed": "failed"})
