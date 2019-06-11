8238080ed8fd19468422d6f6a6278528e7acf632
Janno Lunenburg <jannolunenburg@gmail.com>
2019-06-11 21:57:23 +0200

    refactor(robot_smach_states)get_directions is now a free function

--- robot_smach_states/src/robot_smach_states/human_interaction/give_directions.py
+++ robot_smach_states/src/robot_smach_states/human_interaction/give_directions.py
@@ -26,8 +26,30 @@ class Side(enum.Enum):
 DirectionItem = namedtuple("DirectionItem", ["object_id", "room_id", "side"])
 
 
+def _get_entity_pose_in_path(point, next_point, entity_pose):
+    # type: (kdl.Vector, kdl.Vector, kdl.Frame) -> kdl.Frame
+    """
+    Computes the entity pose w.r.t. the virtual frame that is spanned by the two points
+    :param point: First point in fixed frame
+    :type point: kdl.Vector
+    :param next_point: Next point in fixed frame
+    :type next_point: kdl.Vector
+    :param entity_pose: (kdl.Frame) Entity pose in fixed frame
+    :type entity_pose: kdl.Frame
+    :return: Entity pose in virtual 'path' frame
+    :rtype: kdl.Frame
+    """
+    # Determine a 6D path pose
+    path_pose = create_frame_from_points(point, next_point)
+
+    # Transform entity pose into path frame
+    entity_pose_path = path_pose.Inverse() * entity_pose
+
+    return entity_pose_path
+
+
 def get_directions(robot, entity_designator, x_threshold=0.75, y_threshold=1.5):
-    # type: (Robot, Designator, float, float) -> None
+    # type: (Robot, Designator, float, float) -> list
     """
     Computes a list of named tuples of the furniture objects that are passed and in which rooms these are on which side
     of the operator when moving towards a certain entity
@@ -42,9 +64,138 @@ def get_directions(robot, entity_designator, x_threshold=0.75, y_threshold=1.5):
     :param y_threshold: if the entity is closer than this distance in y-direction w.r.t. the path frame
     it is considered 'passed'
     :type y_threshold: float
-    :return:
+    :return: (list(DirectionItem)) directions the operator should follow
+    :raises: (AssertionError, RuntimeError)
     """
-    pass
+    # Get the constraints for the global planner
+    nav_constraints = OrderedDict()
+    target_entity = entity_designator.resolve()  # type: Entity
+    rospy.loginfo("Resolved to Entity: {}".format(target_entity.id))
+    if not target_entity:
+        raise RuntimeError("Cannot give directions if I don't know where to go")
+
+    if target_entity.is_a("room"):
+        possible_areas = ["in"]
+    else:
+        possible_areas = ["in_front_of", "near"]
+    for area in possible_areas:
+        nav_constraints[area] = NavigateToSymbolic.generate_constraint(
+            robot=robot,
+            entity_designator_area_name_map={entity_designator: area},
+            entity_lookat_designator=entity_designator)
+
+    if not nav_constraints:
+        raise RuntimeError("Cannot give directions if I don't know where to go")
+
+    # Call the global planner for the shortest path to this entity
+    path = None
+    for name, nav_con in nav_constraints.iteritems():
+        path = robot.base.global_planner.getPlan(position_constraint=nav_con[0])
+        if path is not None:
+            break
+
+    if path is None:
+        raise RuntimeError("I'm sorry but I don't know how to get to the {}".format(target_entity.id))
+
+    # Convert the path to a list of kdl Vectors
+    assert (all([p.header.frame_id.endswith("map") for p in path])), "Not all path poses are defined w.r.t. 'map'"
+    kdl_path = [point_msg_to_kdl_vector(p.pose.position) for p in path]
+
+    # Get all entities
+    entities = robot.ed.get_entities()
+    furniture_entities = [entity for entity in entities if entity.is_a("furniture")]
+    room_entities = [room for room in entities if room.type == "room"]
+
+    # Match the furniture entities to rooms
+    furniture_entities_room = {room: [] for room in room_entities}  # maps room entities to furniture entities
+    for item in furniture_entities:  # type: Entity
+
+        try:
+            room = get_room(room_entities, item._pose.p)
+            furniture_entities_room[room].append(item)
+            rospy.loginfo("{} ({}) is in the {}".format(item.id, item._pose.p, room.id))
+        except RuntimeError:
+            rospy.logwarn("{} ({}) not in any room".format(item.id, item._pose.p))
+            # continue
+
+    # Match the path to rooms
+    passed_room_ids = []  # Will contain the ids of the rooms that are passed
+    kdl_path_rooms = []  # Will contain tuples (kdl.Vector, Entity) with a waypoint and the room this waypoint is in
+    for position in kdl_path:
+
+        try:
+            room = get_room(room_entities, position)
+        except RuntimeError:
+            continue
+        kdl_path_rooms.append((position, room))
+        if room.id not in passed_room_ids:
+            passed_room_ids.append(room.id)
+
+    # With this information: start creating the text for the robot
+    # ToDo: kdl_path_rooms should not be empty
+    result = []
+    start_room_id = ""
+    if kdl_path_rooms:
+        start_room_id = kdl_path_rooms[0][1].id
+        result.append(DirectionItem(None, start_room_id, None))
+
+    # We need to remember the 'previous' room id so the robot can mention when the next room is entered
+    prev_room_id = start_room_id
+
+    # Keep track of the ids of the entities that are passed so that the robot doesn't mention any entity twice
+    passed_ids = []
+    for (position, room), (next_position, _) in zip(kdl_path_rooms[:-1],
+                                                    kdl_path_rooms[1:]):
+
+        # Check if the room has changed
+        if prev_room_id != room.id:
+            prev_room_id = room.id
+
+        furniture_objects = furniture_entities_room[room]  # Furniture objects of the room in which this waypoint
+        # is situated
+        to_add = []  # will contain tuples (str, distance, str) with the entity id, the distance between this
+        # waypoint and the center of this entity (in x-direction) and the side where the entity is (w.r.t. the path)
+        reached_target = False
+        for entity in furniture_objects:  # type: Entity
+            rospy.logdebug("\tEntity: {}".format(room.id))
+
+            # Check if in passed ids
+            if entity.id in passed_ids:
+                rospy.logdebug("Skipping {}, already in passed ids".format(room.id))
+                continue
+
+            # Compute the pose of the entity w.r.t. the 'path'
+            entity_pose_path = _get_entity_pose_in_path(position, next_position, entity.pose.frame)
+
+            # Check the distance
+            if abs(entity_pose_path.p.x()) < x_threshold and abs(entity_pose_path.p.y()) < y_threshold:
+                rospy.logdebug("Appending {}: ({}, {})".format(
+                    entity.id, entity_pose_path.p.x(), entity_pose_path.p.y()))
+
+                # side = "left" if entity_pose_path.p.y() >= 0.0 else "right"
+                side = Side.LEFT if entity_pose_path.p.y() >= 0.0 else Side.RIGHT
+
+                to_add.append((entity.id, entity_pose_path.p.y(), side))
+
+        # Sort the list based on the distance
+        to_add.sort(key=lambda id_distance_tuple: id_distance_tuple[1])
+
+        # Add all items to the list
+        for entity_id, _, side in to_add:
+            if entity_id == target_entity.id:
+                reached_target = True
+                break
+            else:
+                result.append(DirectionItem(entity_id, room.id, side.value))
+
+            passed_ids.append(entity_id)
+
+        if reached_target:
+            break
+
+    result.append(DirectionItem(target_entity.id, None, None))
+    rospy.loginfo("Result: {}".format(result))
+    return result
 
 
 def sentence_from_directions(directions):
@@ -113,185 +264,23 @@ class GiveDirections(smach.State):
         self._y_threshold = y_threshold
 
     def execute(self, userdata=None):
-        # Get the constraints for the global planner
-        nav_constraints = OrderedDict()
-        goal_entity = self._entity_designator.resolve()  # type: Entity
-        rospy.loginfo("Resolved to Entity: {}".format(goal_entity.id))
-        if not goal_entity:
-            rospy.logerr("Cannot give directions if I don't know where to go")
-            self._robot.speech.speak("I'm sorry but I don't know where you want to go", mood="sad")
-            return "failed"
-        if goal_entity.is_a("room"):
-            possible_areas = ["in"]
-        else:
-            possible_areas = ["in_front_of", "near"]
-        for area in possible_areas:
-            nav_constraints[area] = NavigateToSymbolic.generate_constraint(
-                robot=self._robot,
-                entity_designator_area_name_map={self._entity_designator: area},
-                entity_lookat_designator=self._entity_designator)
 
-        if not nav_constraints:
-            rospy.logerr("Cannot give directions if I don't know where to go")
-            self._robot.speech.speak("I'm sorry but I don't know where you want to go", mood="sad")
-            return "failed"
-
-        # Resolve the entity: we'll need it later
-        target_entity = self._entity_designator.resolve()  # type: Entity
-
-        # Call the global planner for the shortest path to this entity
-        path = None
-        for name, nav_con in nav_constraints.iteritems():
-            path = self._robot.base.global_planner.getPlan(position_constraint=nav_con[0])
-            if path is not None:
-                break
-
-        if path is None:
-            rospy.logerr("No path to {}".format(target_entity.id))
-            self._robot.speech.speak("I'm sorry but I don't know how to get to the {}".format(target_entity.id))
-            return "failed"
-
-        # Convert the path to a list of kdl Vectors
-        assert(all([p.header.frame_id.endswith("map") for p in path])), "Not all path poses are defined w.r.t. 'map'"
-        kdl_path = [point_msg_to_kdl_vector(p.pose.position) for p in path]
-
-        # Get all entities
-        entities = self._robot.ed.get_entities()
-        furniture_entities = [entity for entity in entities if entity.is_a("furniture")]
-        room_entities = [room for room in entities if room.type == "room"]
-
-        # Log the time we start iterating
         t_start = rospy.Time.now()
 
-        # Match the furniture entities to rooms
-        furniture_entities_room = {room: [] for room in room_entities}  # maps room entities to furniture entities
-        for item in furniture_entities:  # type: Entity
+        try:
+            directions = get_directions(self._robot, self._entity_designator, self._x_threshold, self._y_threshold)
+        except (AssertionError, RuntimeError) as e:
+            rospy.logerr("Get directions failed: {}".format(e.message))
+            self._robot.speech.speak("I'm sorry but {}".format(e.message))
+            return "failed"
 
-            try:
-                room = get_room(room_entities, item._pose.p)
-                furniture_entities_room[room].append(item)
-                rospy.loginfo("{} ({}) is in the {}".format(item.id, item._pose.p, room.id))
-            except RuntimeError:
-                rospy.logwarn("{} ({}) not in any room".format(item.id, item._pose.p))
-                # continue
+        rospy.logdebug("Getting directions took {} seconds".format((rospy.Time.now() - t_start).to_sec()))
 
-        # Match the path to rooms
-        passed_room_ids = []  # Will contain the ids of the rooms that are passed
-        kdl_path_rooms = []  # Will contain tuples (kdl.Vector, Entity) with a waypoint and the room this waypoint is in
-        for position in kdl_path:
-
-            try:
-                room = get_room(room_entities, position)
-            except RuntimeError:
-                continue
-            kdl_path_rooms.append((position, room))
-            if room.id not in passed_room_ids:
-                passed_room_ids.append(room.id)
-
-        # With this information: start creating the text for the robot
-        # ToDo: kdl_path_rooms should not be empty
-        sentence = ""
-        result = []
-        start_room_id = ""
-        if kdl_path_rooms:
-            start_room_id = kdl_path_rooms[0][1].id
-            sentence += "We are now in the {}.\n".format(start_room_id)
-            result.append(DirectionItem(None, start_room_id, None))
-
-        # We need to remember the 'previous' room id so the robot can mention when the next room is entered
-        prev_room_id = start_room_id
-
-        # Keep track of the ids of the entities that are passed so that the robot doesn't mention any entity twice
-        passed_ids = []
-        for (position, room), (next_position, _) in zip(kdl_path_rooms[:-1],
-                                                        kdl_path_rooms[1:]):
-
-            # Check if the room has changed
-            if prev_room_id != room.id:
-                sentence += "You enter the {}.\n".format(room.id)
-                prev_room_id = room.id
-
-            furniture_objects = furniture_entities_room[room]  # Furniture objects of the room in which this waypoint
-            # is situated
-            to_add = []  # will contain tuples (str, distance, str) with the entity id, the distance between this
-            # waypoint and the center of this entity (in x-direction) and the side where the entity is (w.r.t. the path)
-            reached_target = False
-            for entity in furniture_objects:  # type: Entity
-                rospy.logdebug("\tEntity: {}".format(room.id))
-
-                # Check if in passed ids
-                if entity.id in passed_ids:
-                    rospy.logdebug("Skipping {}, already in passed ids".format(room.id))
-                    continue
-
-                # Compute the pose of the entity w.r.t. the 'path'
-                entity_pose_path = self.get_entity_pose_in_path(position, next_position, entity.pose.frame)
-
-                # Check the distance
-                if abs(entity_pose_path.p.x()) < self._x_threshold and abs(entity_pose_path.p.y()) < self._y_threshold:
-                    rospy.logdebug("Appending {}: ({}, {})".format(
-                        entity.id, entity_pose_path.p.x(), entity_pose_path.p.y()))
-
-                    # side = "left" if entity_pose_path.p.y() >= 0.0 else "right"
-                    side = Side.LEFT if entity_pose_path.p.y() >= 0.0 else Side.RIGHT
-
-                    to_add.append((entity.id, entity_pose_path.p.y(), side))
-
-            # Sort the list based on the distance
-            to_add.sort(key=lambda id_distance_tuple: id_distance_tuple[1])
-
-            # Add all items to the list
-            for entity_id, _, side in to_add:
-                if entity_id == target_entity.id:
-                    reached_target = True
-                    break
-                else:
-                    sentence += "You walk by the {} on your {}.\n".format(entity_id, side.value)
-                    result.append(DirectionItem(entity_id, room.id, side.value))
-
-                passed_ids.append(entity_id)
-
-            if reached_target:
-                break
-
-        sentence += "You have now reached the {}.\n".format(target_entity.id)
-        result.append(DirectionItem(target_entity.id, room.id, None))
-        rospy.loginfo("Result: {}".format(result))
-
-        rospy.loginfo("Directions computation took {} seconds".format((rospy.Time.now() - t_start).to_sec()))
-
-        new_sentence = sentence_from_directions(result)
-        rospy.loginfo("New sentence: {}".format(new_sentence))
+        sentence = sentence_from_directions(directions)
 
         self._robot.speech.speak(sentence)
 
-        # ToDo's:
-        # * Improve texts
-        # * Break up this execute method into more (standalone/static) methods to improve readability and re-usability
-
         return "succeeded"
-
-    @staticmethod
-    def get_entity_pose_in_path(point, next_point, entity_pose):
-        # type: (kdl.Vector, kdl.Vector, kdl.Frame) -> kdl.Frame
-        """
-        Computes the entity pose w.r.t. the virtual frame that is spanned by the two points
-        :param point: First point in fixed frame
-        :type point: kdl.Vector
-        :param next_point: Next point in fixed frame
-        :type next_point: kdl.Vector
-        :param entity_pose: (kdl.Frame) Entity pose in fixed frame
-        :type entity_pose: kdl.Frame
-        :return: Entity pose in virtual 'path' frame
-        :rtype: kdl.Frame
-        """
-        # Determine a 6D path pose
-        path_pose = create_frame_from_points(point, next_point)
-
-        # Transform entity pose into path frame
-        entity_pose_path = path_pose.Inverse() * entity_pose
-
-        return entity_pose_path
 
 
 def create_frame_from_points(p0, p1):
