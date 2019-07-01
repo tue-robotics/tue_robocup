95f712dbbbed8634f4745dc077f336b19774795e
Loy van Beek <loy.vanbeek@gmail.com>
2019-07-01 16:28:25 +1000

    Document challenge_knowledge variables

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_receptionist.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_receptionist.py
@@ -5,10 +5,10 @@ operator_name = "john"
 
 starting_point = "initial_pose"
 
-waypoint_door = {'id': 'door2', 'radius': 0.5}
+waypoint_door = {'id': 'door_opening_right_start', 'radius': 0.5}  # Somewhere facing the door to where ppl enter
 
-waypoint_livingroom = {'id': 'livingroom', 'radius': 0.5}
+waypoint_livingroom = {'id': 'livingroom', 'radius': 0.5}  # From where to find John
 
-sitting_room = 'hallway'
+sitting_room = 'hallway'  # Where people will be guided to sit and the robot will find a seat
 
-seats = ['hallway_table']
+seats = ['hallway_table']  # List of seats to check for empty-ness
