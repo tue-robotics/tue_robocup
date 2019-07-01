5157d5299cc81950ef33692a09a280a5ebd8fc9a
Arpit Aggarwal <ar13pit@gmail.com>
2019-07-01 10:27:01 +1000

    [robot_smach_states] Forked FindPerson from find_person_in_room
    changed interface

---
+++ robot_smach_states/src/robot_smach_states/human_interaction/find_person.py
@@ -0,0 +1,138 @@
+#! /usr/bin/env python
+
+# System
+import math
+import sys
+
+# ROS
+import PyKDL as kdl
+import geometry_msgs
+import rospy
+import smach
+
+# TU/e Robotics
+import robot_smach_states as states
+import robot_smach_states.util.designators as ds
+from robot_skills.util import kdl_conversions
+
+
+class FindPerson(smach.State):
+    """
+    Smach state to find a person. The robot looks around and tries to recognize all faces in view.
+    """
+
+    def __init__(self, robot, properties=None, query_entity_designator=None,
+                 result_designator=None, look_distance=1.0, speak=False,
+                 strict=True, nearest=False, attempts=1, search_timeout=60):
+        """ Initialization method
+        :param robot: robot api object
+        :param properties: (dict) keyvalue pair of the properties a person must
+            possess. None as a value for a property would search for all possible
+            values of the property.
+        :param query_entity_designator: An entity designator to match all found
+            people to
+        :param result_designator: A designator to write the search result to
+        :param look_distance: (float) The distance (radius) which the robot must look at
+        :param speak: (bool) If True, the robot will speak while trying to find
+            a person
+        :param strict: (bool) If True then only people with all specified
+            properties is returned else all people with at least one true property
+        :param nearest: (bool) whether or not to discard recognitions based on the label
+        :param attempts: (int) Max number of attempts
+        :param search_timeout: (float) maximum time the robot is allowed to search
+        """
+        smach.State.__init__(self, outcomes=['found', 'failed'])
+
+        self._robot = robot
+
+        self._properties = properties
+        self._query_entity_designator = query_entity_designator
+        self._look_distance = look_distance
+        self._speak = speak
+        self._strict = strict
+        self._nearest = nearest
+
+        self._search_timeout = search_timeout
+
+        if result_designator:
+            ds.is_writeable(result_designator)
+        self._result_designator = result_designator
+
+    def execute(self, userdata=None):
+        look_angles = None
+        if not self._properties:
+            look_angles = [0]
+        else:
+            look_angles = [f * math.pi / d if d != 0 else 0.0 for f in [-1, 1] for d in [0, 6, 4, 2.3]]  # Magic numbers
+            try:
+                person_label = self._properties["id"]
+                person_label = person_label.resolve() if hasattr(person_label, 'resolve') else person_label
+
+                rospy.loginfo("Trying to find {}".format(person_label))
+                if self._speak:
+                    self._robot.speech.speak(
+                            "{}, please look at me while I am looking for you".format(
+                            person_label),
+                        block=False)
+            except:
+                pass
+
+        start_time = rospy.Time.now()
+
+        head_goals = [kdl_conversions.VectorStamped(x=self._look_distance * math.cos(angle),
+                                                    y=self._look_distance * math.sin(angle),
+                                                    z=1.5,
+                                                    frame_id="/%s/base_link" % self._robot.robot_name)
+                      for angle in look_angles]
+
+        i = 0
+
+        rate = rospy.Rate(2)
+        while (rospy.Time.now() - start_time).to_sec() < self._search_timeout and not rospy.is_shutdown():
+            if self.preempt_requested():
+                return 'failed'
+
+            self._robot.head.look_at_point(head_goals[i])
+            i += 1
+            if i == len(head_goals):
+                i = 0
+            self._robot.head.wait_for_motion_done()
+
+            self._image_data = self._robot.perception.get_rgb_depth_caminfo()
+            success, found_people_ids = self._robot.ed.detect_people(*self._image_data)
+            found_people = [self._robot.ed.get_entity(id) for id in found_people_ids]
+            found_names = {person.id: person for person in found_people}
+
+            found_person = None
+
+            if self._discard_other_labels:
+                found_person = found_names.get(person_label, None)
+            else:
+                # find which of those is closest
+                robot_pose = self._robot.base.get_location()
+                found_person = min(found_people, key=lambda person: person.pose.frame.p - robot_pose.frame.p)
+
+            if self._room:
+                room_entity = self._robot.ed.get_entity(id=self._room)
+                if not room_entity.in_volume(found_person.pose.extractVectorStamped(), 'in'):
+                    # If the person is not in the room we are looking for, ignore the person
+                    rospy.loginfo("We found a person '{}' but was not in desired room '{}' so ignoring that person"
+                                  .format(found_person.id, room_entity.id))
+                    found_person = None
+
+            if found_person:
+                rospy.loginfo("I found {} who I assume is {} at {}".format(found_person.id, person_label, found_person.pose.extractVectorStamped(), block=False))
+                self._robot.speech.speak("I think I found {}.".format(person_label, block=False))
+                self._robot.head.close()
+
+                if self._found_entity_designator:
+                    self._found_entity_designator.write(found_person)
+
+                return 'found'
+            else:
+                rospy.logwarn("Could not find {}".format(person_label))
+                rate.sleep()
+
+        self._robot.head.close()
+        rospy.sleep(2.0)
+        return 'failed'
