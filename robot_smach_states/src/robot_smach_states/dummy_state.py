00e5ac28ddcb77262d39cfcbe05c93017e113b56
Arpit Aggarwal <ar13pit@gmail.com>
2019-07-02 09:20:45 +1000

    [robot_smach_state] Added test for dummy state

--- robot_smach_states/src/robot_smach_states/dummy_state.py
+++ robot_smach_states/src/robot_smach_states/dummy_state.py
@@ -1,4 +1,6 @@
 import smach
+import rospy
+
 import robot_smach_states.util.designators as ds
 
 __all__ = ["DummyState"]
@@ -8,17 +10,38 @@ class DummyState(smach.State):
     """
     DummyState to which returns the input outcome
     """
+
     def __init__(self, result_designator):
         """
         :param result_designator: (VariableDesignator) or (str) The result to
             be returned
         """
-        super(DummyState, self).__init__(outcomes=["succeeded", "failed", "aborted"])
-        ds.check_type(result_designator, "str")
-        self.result = result_designator.resolve() if hasattr(result_designator,"resolve") else result_designator
+        super(DummyState,
+              self).__init__(outcomes=["succeeded", "failed", "aborted"])
+        ds.check_type(result_designator, str)
+        self.result = result_designator.resolve() if hasattr(
+            result_designator, "resolve") else result_designator
 
     def execute(self, userdata=None):
         if self.result in self._outcomes:
             return self.result
         else:
+            rospy.loginfo("Outcome '{}' is invalid. Aborting.".format(self.result))
             return "aborted"
+
+
+if __name__ == "__main__":
+    rospy.init_node('dummy_state_machine')
+
+    sm = smach.StateMachine(outcomes=['outcome1', 'outcome2', 'outcome3'])
+
+    with sm:
+        sm.add('FOO',
+               DummyState("yay"),
+               transitions={
+                   'succeeded': 'outcome1',
+                   'failed': 'outcome2',
+                   'aborted': 'outcome3'
+               })
+
+    sm.execute()
