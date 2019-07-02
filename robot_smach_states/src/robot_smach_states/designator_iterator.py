5a5dcf303af20dd40a6fdbe2dd52d0ca8155cc52
Loy van Beek <loy.vanbeek@gmail.com>
2019-07-02 14:06:36 +1000

    Check for collection is None in IterateDesignator

--- robot_smach_states/src/robot_smach_states/designator_iterator.py
+++ robot_smach_states/src/robot_smach_states/designator_iterator.py
@@ -12,7 +12,7 @@ class IterateDesignator(smach.State):
         The collection is resolved on the fist time the state is called AND after is was exhausted.
 
         Once the collection is exhausted, the outcome will be 'stop_iteration'.
-        The next timee the state is executed, the collection will be resolved again
+        The next time the state is executed, the collection will be resolved again
 
         :param collection_des: Designator with a iterable resolve_type
         :param element_des: Writeable designator with a resolve_type that should match the element type of the collection
@@ -60,6 +60,10 @@ class IterateDesignator(smach.State):
     def execute(self, userdata=None):
         if self._current_elements is None:
             collection = self.collection_des.resolve()
+            if collection is None:
+                rospy.loginfo("Collection is None".format(collection))
+                return 'stop_iteration'
+
             rospy.loginfo("Current elements: {}".format(collection))
             self._current_elements = iter(collection)
 
