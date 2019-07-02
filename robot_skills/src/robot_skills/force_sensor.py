3916e4842f278dadb95721ba74047e08152ba8cd
Rein Appeldoorn <reinzor@gmail.com>
2019-07-02 14:08:22 +1000

    feat(robot): Force sensor move down edge up detection

---
+++ robot_skills/src/robot_skills/force_sensor.py
@@ -0,0 +1,55 @@
+import numpy as np
+import rospy
+from geometry_msgs.msg import WrenchStamped
+
+
+class TimeOutException(Exception):
+    pass
+
+
+class ForceSensor(object):
+    def __init__(self, topic, force_norm_threshold=5):
+        """
+        Class for conveniently using a force sensor
+
+        :param topic: Topic to use for WrenchStamped measurement
+        :param force_norm_threshold: Edge up if norm exceeds this value (defaults to 100)
+        """
+        self._topic = topic
+
+        self._calibrated_msg = None
+        self._edge_up = False
+        self._force_norm_threshold = force_norm_threshold
+
+    def _wrench_callback(self, msg):
+        def _detect_edge_up(calibrated_msg, msg):
+            def _norm(v):
+                return np.linalg.norm(np.array([v.x, v.y, v.z]))
+
+            calibrated_force_norm = _norm(calibrated_msg.wrench.force)
+            force_norm = _norm(msg.wrench.force)
+
+            rospy.loginfo("Force norm: %.2f", force_norm - calibrated_force_norm)
+
+            return abs(force_norm - calibrated_force_norm) > self._force_norm_threshold
+
+        if self._calibrated_msg is None:
+            self._calibrated_msg = msg
+        else:
+            self._edge_up = _detect_edge_up(self._calibrated_msg, msg)
+
+    def wait_for_edge_up(self, timeout=10.0):
+        """
+        Returns if an edge up event is detected. Will raise a TimeOutException if no edge up is detected within timeout.
+        :param timeout: Edge up wait patience
+        """
+        self._calibrated_msg = None
+        subscriber = rospy.Subscriber(self._topic, WrenchStamped, self._wrench_callback, queue_size=1)
+
+        end_time = rospy.Time.now() + rospy.Duration.from_sec(timeout)
+        while not rospy.is_shutdown() and not self._edge_up:
+            if rospy.Time.now() > end_time:
+                raise TimeOutException("Force sensor edge up detection timeout of {} exceeded".format(timeout))
+            rospy.Rate(10).sleep()
+        subscriber.unregister()
+        rospy.loginfo("Edge up detected!")
