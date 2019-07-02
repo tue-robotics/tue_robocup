8fb9420b02835fce1e110e91e1d24831ccf4bb9b
Lars Janssen <janslar@hotmail.com>
2019-07-03 00:31:46 +1000

    Feature(challenge_knowlegde): update in cleanup, take_out_the_garbage, common

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2019/challenge_take_out_the_garbage.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2019/challenge_take_out_the_garbage.py
@@ -1,4 +1,4 @@
-# TAKE OUT THE GARBAGE KNOWLEDGE FILE ROBOTICS_TESTLABS
+# TAKE OUT THE GARBAGE KNOWLEDGE FILE RWC2019
 # TU/e Robotics
 from robocup_knowledge import knowledge_loader
 
@@ -8,5 +8,5 @@ common = knowledge_loader.load_knowledge("common")
 starting_point = "initial_pose"
 trashbin_id = "trash_bin"
 trashbin_id2 = "trash_bin"
-drop_zone_id = "operator_table"
+drop_zone_id = "registration_table2"
 
