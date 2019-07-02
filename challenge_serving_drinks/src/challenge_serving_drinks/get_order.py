74cb6c3f8fc4fc59eaa1b87a1855a30e67dc2e6d
Arpit Aggarwal <ar13pit@gmail.com>
2019-07-02 10:47:34 +1000

    [serving_drinks] Added dummy state for missing functionality

--- challenge_serving_drinks/src/challenge_serving_drinks/get_order.py
+++ challenge_serving_drinks/src/challenge_serving_drinks/get_order.py
@@ -40,31 +40,46 @@ class GetOrder(smach.StateMachine):
             caller_designator = ds.EdEntityDesignator(robot=robot, id=caller_id, name="caller_des")
 
             # Detect - people holding drinks and people without drinks  #ToDo: implement!
+            smach.StateMachine.add("SAY_PEOPLE_WITHOUT_DRINKS",
+                                   states.Say(robot=robot, sentence="Trying to find people without a drink",
+                                              look_at_standing_person=True),
+                                   transitions={"spoken": "FIND_PERSON_WITHOUT_DRINK"})
+
+            # TODO: Change DummyState to actual state
+            smach.StateMachine.add("FIND_PERSON_WITHOUT_DRINK",
+                                   states.DummyState(robot=robot,
+                                                     result_designator="failed",
+                                                     caller_id=caller_id),
+                                   transitions={"succeeded": "SAY_I_HAVE_SEEN",
+                                                "failed": "ASK_FOR_WAVING",
+                                                "aborted": "ASK_FOR_WAVING"})
 
             # Detect fallback - detect waving people
             smach.StateMachine.add("ASK_FOR_WAVING",
-                                   states.Say(robot=robot, sentence="Looks like everyone has a drink",
+                                   states.Say(robot=robot,
+                                              sentence="Could not find people without a drink. Please wave if you want me to bring you something",
                                               look_at_standing_person=True),
-                                   transitions={"spoken": "ASK_STEP_IN_FRONT"})  # ToDo: transition to WAIT_FOR_WAVING
+                                   transitions={"spoken": "WAIT_FOR_WAVING"})
 
             smach.StateMachine.add("WAIT_FOR_WAVING",
                                    DetectWaving(robot=robot, caller_id=caller_id),
                                    transitions={"succeeded": "SAY_I_HAVE_SEEN",
                                                 "aborted": "ASK_STEP_IN_FRONT"})
 
+            # Navigate to person who wants to place an order
             smach.StateMachine.add("SAY_I_HAVE_SEEN",
                                    states.Say(robot=robot,
-                                              sentence="I have seen a waving person, I will be there shortly!",
+                                              sentence="Found person who may want to place an order. I will be there shortly!",
                                               look_at_standing_person=True),
-                                   transitions={"spoken": "NAVIGATE_TO_WAVING"})
+                                   transitions={"spoken": "NAVIGATE_TO_PERSON"})
 
             # Navigate to waving people
-            smach.StateMachine.add("NAVIGATE_TO_WAVING",
+            smach.StateMachine.add("NAVIGATE_TO_PERSON",
                                    states.NavigateToObserve(robot=robot, entity_designator=caller_designator,
                                                             radius=1.1),
                                    transitions={"arrived": "LEARN_NAME",
                                                 "unreachable": "ASK_STEP_IN_FRONT",
-                                                "goal_not_defined": "WAIT_FOR_WAVING"})
+                                                "goal_not_defined": "SAY_PEOPLE_WITHOUT_DRINKS"})
 
             # Detect waving people fallback - ask operator in front
             smach.StateMachine.add("ASK_STEP_IN_FRONT",
@@ -84,7 +99,7 @@ class GetOrder(smach.StateMachine):
             # Ask operator for his name fallback
             smach.StateMachine.add("LEARN_NAME_FALLBACK",
                                    states.Say(robot=robot,
-                                              sentence="Sorry, I did not get your name, I'll just call you john",
+                                              sentence="Sorry, I did not get your name, I'll just call you John",
                                               look_at_standing_person=True),
                                    transitions={"spoken": "LEARN_OPERATOR"})
 
