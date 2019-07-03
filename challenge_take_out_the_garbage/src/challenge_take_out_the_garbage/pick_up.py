ce0be8d56837f1781fc66e3176d25ad76de32535
Lars Janssen <janslar@hotmail.com>
2019-07-04 00:25:25 +1000

    Fixes(pick_up): fixes based on tests and final teamleader meeting input

--- challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/pick_up.py
+++ challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/pick_up.py
@@ -5,6 +5,8 @@ import smach
 from geometry_msgs.msg import WrenchStamped
 
 # TU/e
+from smach import cb_interface, CBState
+
 import robot_smach_states as states
 import robot_smach_states.manipulation as manipulation
 from robot_skills.arms import PublicArm
@@ -12,6 +14,8 @@ import robot_smach_states.util.designators as ds
 from challenge_take_out_the_garbage.control_to_trash_bin import ControlToTrashBin
 
 from ed_msgs.msg import EntityInfo
+
+from robot_skills.force_sensor import TimeOutException
 from robot_skills.util.kdl_conversions import FrameStamped
 
 
@@ -19,6 +23,7 @@ class MeasureForce(object):
     """
     Measure the three forces in the arm
     """
+
     def __init__(self, robot):
         """
 
@@ -27,7 +32,7 @@ class MeasureForce(object):
 
     def get_force(self):
         # ft_sensor_topic = '/'+self._robot.robot_name+'/wrist_wrench/raw'
-        ft_sensor_topic = '/'+self._robot.robot_name+'/wrist_wrench/compensated'
+        ft_sensor_topic = '/' + self._robot.robot_name + '/wrist_wrench/compensated'
 
         force_grab = rospy.wait_for_message(ft_sensor_topic, WrenchStamped)
 
@@ -42,6 +47,7 @@ class GetTrashBin(smach.State):
     """
     Get the entity of the trashbin in FrameStamped and save it to get the orientation of the fixed point in world model
     """
+
     def __init__(self, robot, trashbin):
         """
         :param robot: robot object
@@ -61,7 +67,7 @@ class GetTrashBin(smach.State):
         frame_original = self._robot.ed.get_entity(id=e.id)._pose
 
         # inspect and update entity
-        states.look_at_segmentation_area(self._robot,  self._robot.ed.get_entity(id=e.id), 'on_top_of')
+        states.look_at_segmentation_area(self._robot, self._robot.ed.get_entity(id=e.id), 'on_top_of')
         self._robot.ed.update_kinect("{} {}".format('on_top_of', e.id))
         frame_updated = self._robot.ed.get_entity(id=e.id)._pose
 
@@ -81,6 +87,7 @@ class GrabTrash(smach.State):
     """
     Set the arm in the prepare grasp position so it can safely go to the grasp position
     """
+
     def __init__(self, robot, arm_designator, try_num=3, minimal_weight=0.1):
         """
         :param robot: robot object
@@ -103,17 +110,13 @@ class GrabTrash(smach.State):
         gravitation = 9.81
         try_current = 0
 
-        # Send to grab trash pose
-        arm.send_joint_goal('grab_trash_bag')
-        arm.wait_for_motion_done()
-
         measure_force = MeasureForce(self._robot)
         # To be able to determine the weight of the object the difference between the default weight and the weight with
         # the object needs to be taken. Note that initially the weight with the object is set to the default weight to
         # be able to enter the while loop below.
         arm_weight = measure_force.get_force()
         arm_with_object_weight = arm_weight
-        weight_object = numpy.linalg.norm(numpy.subtract(arm_weight, arm_with_object_weight))/gravitation
+        weight_object = numpy.linalg.norm(numpy.subtract(arm_weight, arm_with_object_weight)) / gravitation
 
         while weight_object < self._minimal_weight and try_current < self._try_num:
             if try_current == 0:
@@ -138,8 +141,15 @@ class GrabTrash(smach.State):
             arm.wait_for_motion_done()
 
             # Go down and grab
-            self._robot.torso.send_goal("grab_trash_down")
-            self._robot.torso.wait_for_motion_done()
+            try:
+                arm.move_down_until_force_sensor_edge_up()
+            except TimeOutException:
+                rospy.logwarn("No forces were felt, however no action is taken!")
+                pass
+
+            # self._robot.torso.send_goal("grab_trash_down")
+            # self._robot.torso.wait_for_motion_done()
+            # rospy.sleep(3)
             arm.send_gripper_goal('close', max_torque=1.0)
             arm.wait_for_motion_done()
 
@@ -177,7 +187,9 @@ class HandoverFromHumanFigure(smach.StateMachine):
     CloseGripperOnHandoverToRobot state and given the grabbed_entity_label
     as id.
     """
-    def __init__(self, robot, arm_designator, grabbed_entity_label="", grabbed_entity_designator=None, timeout=15, arm_configuration="handover_to_human"):
+
+    def __init__(self, robot, arm_designator, grabbed_entity_label="", grabbed_entity_designator=None, timeout=15,
+                 arm_configuration="handover_to_human"):
         """
         Hold up hand to accept an object and close hand once something is inserted
         :param robot: Robot with which to execute this behavior
@@ -187,7 +199,7 @@ class HandoverFromHumanFigure(smach.StateMachine):
         :param timeout: How long to hold hand over before closing without anything
         :param arm_configuration: Which pose to put arm in when holding hand up for the item.
         """
-        smach.StateMachine.__init__(self, outcomes=['succeeded','failed','timeout'])
+        smach.StateMachine.__init__(self, outcomes=['succeeded', 'failed', 'timeout'])
 
         ds.check_type(arm_designator, PublicArm)
         if not grabbed_entity_designator and grabbed_entity_label == "":
@@ -216,19 +228,20 @@ class HandoverFromHumanFigure(smach.StateMachine):
                                    transitions={'succeeded': 'CLOSE_AFTER_INSERT'})
 
             smach.StateMachine.add('CLOSE_AFTER_INSERT', manipulation.CloseGripperOnHandoverToRobot(robot,
-                                                                                        arm_designator,
-                                                                                        grabbed_entity_label=grabbed_entity_label,
-                                                                                        grabbed_entity_designator=grabbed_entity_designator,
-                                                                                        timeout=timeout),
-                                   transitions={'succeeded'    :   'succeeded',
-                                                'timeout'      :   'failed',
-                                                'failed'       :   'failed'})
+                                                                                                    arm_designator,
+                                                                                                    grabbed_entity_label=grabbed_entity_label,
+                                                                                                    grabbed_entity_designator=grabbed_entity_designator,
+                                                                                                    timeout=timeout),
+                                   transitions={'succeeded': 'succeeded',
+                                                'timeout': 'failed',
+                                                'failed': 'failed'})
 
 
 class PickUpTrash(smach.StateMachine):
     """
     State that makes the robot go to the correct position in front of trash bin and grabs the trash
     """
+
     def __init__(self, robot, trashbin_designator, arm_designator):
         """
         :param robot: robot object
@@ -239,12 +252,31 @@ class PickUpTrash(smach.StateMachine):
         place_pose_designator = ds.EmptySpotDesignator(robot, trashbin_designator)
 
         with self:
+            @cb_interface(outcomes=['done'])
+            def _joint_goal(_):
+                arm = arm_designator.resolve()
+                if not arm:
+                    rospy.logerr("Could not resolve arm")
+                    return "failed"
+                # Send to grab trash pose
+                arm.send_joint_goal('grab_trash_bag')
+                arm.wait_for_motion_done()
+                return 'done'
+
+            smach.StateMachine.add("JOINT_GOAL",
+                                   CBState(_joint_goal),
+                                   transitions={'done': 'GO_BIN'})
+
             smach.StateMachine.add("GO_BIN",
                                    states.NavigateToPlace(robot=robot, place_pose_designator=place_pose_designator,
                                                           arm_designator=arm_designator),
                                    transitions={"arrived": "GET_BIN_POSITION",
                                                 "goal_not_defined": "aborted",
                                                 "unreachable": "failed"})
+
+            smach.StateMachine.add("OPEN_DOOR_PLEASE",
+                                   states.Say(robot, "Can you please open the door for me? It seems blocked!"),
+                                   transitions={"spoken": "GET_BIN_POSITION"})
 
             smach.StateMachine.add("GET_BIN_POSITION", GetTrashBin(robot=robot, trashbin=trashbin_designator),
                                    transitions={"succeeded": "GO_TO_NEW_BIN",
@@ -267,7 +299,7 @@ class PickUpTrash(smach.StateMachine):
 
             # Ask human to handover the trash bag
             smach.StateMachine.add("ASK_HANDOVER", HandoverFromHumanFigure(robot=robot, arm_designator=arm_designator,
-                                                                     grabbed_entity_label='thrash'),
+                                                                           grabbed_entity_label='thrash'),
                                    transitions={"succeeded": "LOWER_ARM",
                                                 "failed": "failed",
                                                 "timeout": "TIMEOUT"})
@@ -293,13 +325,14 @@ if __name__ == '__main__':
     import os
     import robot_smach_states.util.designators as ds
     from robot_skills import Hero
+
     rospy.init_node(os.path.splitext("test_" + os.path.basename(__file__))[0])
     hero = Hero()
     hero.reset()
 
     arm = ds.UnoccupiedArmDesignator(hero, {})
 
-    #sm = HandoverFromHumanFigure(hero, arm, grabbed_entity_label='trash')
-    #sm.execute()
+    # sm = HandoverFromHumanFigure(hero, arm, grabbed_entity_label='trash')
+    # sm.execute()
 
     GrabTrash(hero, arm, 100, 2).execute()
