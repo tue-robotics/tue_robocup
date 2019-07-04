dc98fa54658ca5e44e16850aeec85e7fa5a568cc
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-07-04 13:00:54 +1000

    Fix cleanup for robotics testlabs

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_cleanup.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/challenge_cleanup.py
@@ -22,6 +22,34 @@ cleaning_locations = [
 
 grammar_target = "T"
 
-grammar = "T -> kitchen"
-grammar += "\nT -> livingroom"
-grammar += "\nT -> hallway"
+grammar = ""
+for room in common.location_rooms:
+    grammar += "\nT[{0}] -> {0}".format(room)
+
+category_grammar = """
+T[P] -> CATEGORY[P] | it is a CATEGORY[P] | the category is CATEGORY[P]
+"""
+for l in common.category_locations:
+    category_grammar += "\nCATEGORY[{}] -> {}".format(l, l.replace('_', ' '))
+
+category_grammar += "\nCATEGORY[{}] -> {}".format("trash", "trash".replace('_', ' '))
+
+
+if __name__ == "__main__":
+    print("Clean-up Grammar:\n\n{}\n\n".format(category_grammar))
+
+    from grammar_parser.cfgparser import CFGParser
+
+    import sys
+    grammar_parser = CFGParser.fromstring(category_grammar)
+
+    if len(sys.argv) > 2:
+        sentence = " ".join(sys.argv[2:])
+    else:
+        sentence = grammar_parser.get_random_sentence("T")
+
+    print("Parsing sentence:\n\n{}\n\n".format(sentence))
+
+    result = grammar_parser.parse("T", sentence)
+
+    print("Result:\n\n{}".format(result))
