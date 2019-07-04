b2cc73fb2f7f564634b415ca5ea78a637c6bcda8
Loy van Beek <loy.vanbeek@gmail.com>
2019-07-04 15:47:27 +1000

    Busy with find_my_mates

--- challenge_find_my_mates/src/find_my_mates.py
+++ challenge_find_my_mates/src/find_my_mates.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import rospy
-import math
-import PyKDL as kdl
 from robot_smach_states.util import startup
-from smach import StateMachine, State
-from robot_smach_states import NavigateToWaypoint, SetInitialPose, Initialize, WaitTime, StartChallengeRobust, Say
-from robot_smach_states.util.designators import EntityByIdDesignator
 from robot_skills.util import kdl_conversions
 from hmi import TimeoutException
 from collections import Counter
 from robocup_knowledge import load_knowledge
+
+import robot_smach_states as states
+import smach
+from robot_skills.util.entity import Entity
+import robot_smach_states.util.designators as ds
+from challenge_find_my_mates.identify_people import IdentifyPeople
 
 challenge_knowledge = load_knowledge('challenge_find_my_mates')
 
@@ -18,158 +19,21 @@ ROOM_ID = challenge_knowledge.room
 SEARCH_POINT = challenge_knowledge.search_point
 
 
-class FindPeople(State):
-    """
-    Locate three people in the provided room
-    """
-
-    def __init__(self, robot, search_timeout=60, look_distance=2.0, min_dist=0.5):
-        """ Initialization method
-        :param robot: robot api object
-        :param search_timeout: (float) maximum time the robot is allowed to search
-        :param look_distance: (float) robot only considers laser entities within this radius
-        """
-        State.__init__(self, outcomes=['done', 'aborted', 'failed'])
-
-        self._robot = robot
-        self._search_timeout = search_timeout
-        self._look_distance = look_distance
-        self._min_dist = min_dist
-        self._people = []
-
-    def execute(self, userdata=None):
-        rospy.loginfo("Trying to find my mates")
-        self._robot.head.look_at_standing_person()
-        self._robot.speech.speak("Please look at me while I am looking for you",
-                                 block=False)
-
-        start_time = rospy.Time.now()
-
-        look_angles = [f * math.pi / d if d != 0 else 0.0 for f in [-1, 1] for d in [0, 6, 4, 2.3]]  # Magic numbers
-        head_goals = [kdl_conversions.VectorStamped(x=self._look_distance * math.cos(angle),
-                                                    y=self._look_distance * math.sin(angle), z=1.7,
-                                                    frame_id="/%s/base_link" % self._robot.robot_name)
-                      for angle in look_angles]
-        head_goals.extend([kdl_conversions.VectorStamped(x=2.0 * self._look_distance * math.cos(angle),
-                                                         y=2.0 * self._look_distance * math.sin(angle), z=1.7,
-                                                         frame_id="/%s/base_link" % self._robot.robot_name)
-                           for angle in look_angles])
-        n = 0 # This represents the number of detected people, once four people are detected, this state is exited.
-        i = 0
-
-        while (rospy.Time.now() - start_time).to_sec() < self._search_timeout:
-            if self.preempt_requested():
-                return 'aborted'
-
-            self._robot.head.look_at_point(head_goals[i])
-            i += 1
-            if i == len(head_goals):
-                i = 0
-            self._robot.head.wait_for_motion_done()
-            raw_detections = self._robot.perception.detect_faces()
-
-            if not raw_detections:
-                continue
-
-            for detection in raw_detections:
-                roi = detection.roi
-                try:
-                    person_pos_kdl = self._robot.perception.project_roi(roi=roi, frame_id="map")
-                except Exception as e:
-                    rospy.logerr("head.project_roi failed: %s", e)
-                    continue  # move onto next detection
-
-                # ToDo: This should be the ED people detector
-                detected_person = self._robot.ed.get_closest_laser_entity(radius=self._look_distance,
-                                                                          center_point=person_pos_kdl)
-
-                room_entity = self._robot.ed.get_entity(id=ROOM_ID)
-                if not room_entity.in_volume(detected_person.pose.extractVectorStamped(), 'in'):
-                    detected_person = None
-
-                add_person = True
-                for person in self._people:
-                    new_person = (person.pose.frame.p - detected_person.pose.frame.p).Norm() > self._min_dist
-                    person_not_operator = (self._robot.ed.get_entities(id=STARTING_POINT).pose.frame.p - detected_person.pose.frame.p).Norm() > self._min_dist
-                    if not new_person or not person_not_operator:
-                        add_person = False
-                        break
-
-                if add_person:
-                    self._people[n] = detected_person
-
-                    rospy.loginfo(
-                        "I found someone at {}".format(self._people[n].pose.extractVectorStamped(),
-                                                       block=False))
-
-                    self._robot.ed.update_entity(
-                        id="person" + str(n),
-                        frame_stamped=kdl_conversions.FrameStamped(kdl.Frame(person_pos_kdl.vector), "map"),
-                        type="waypoint")
-
-                    n += 1
-
-                if len(self._people) > 3:
-                    self._robot.head.close()
-                    return 'done'
-
-        self._robot.head.close()
-        return 'done'
-
-
-class IdentifyPeople(State):
-    """
-    Navigate to all three people and determine their attributes
-    """
-
-    def __init__(self, robot):
-        State.__init__(self, outcomes=['done', 'aborted', 'failed'])
-        self._robot = robot
-
-    def execute(self, userdata=None):
-        entities = self._robot.ed.get_entities()
-        person_entities = [entity for entity in entities if (entity.is_a("waypoint") and entity.id.startswith("person"))]
-        for person in person_entities:
-            NavigateToWaypoint(self._robot, EntityByIdDesignator(self._robot, id=person.id), radius=0.7)
-            self._robot.head.look_at_standing_person()
-            self._robot.speech.speak("Please tell me your name.", block=True)
-
-            try:
-                result = self._robot.hmi.query('What names?', 'T -> ' + ' | '.join(challenge_knowledge.names), 'T',
-                                               timeout=self._time_out)
-                command_recognized = result.sentence
-            except TimeoutException:
-                command_recognized = None
-            if command_recognized == "":
-                self._robot.speech.speak("I could not hear your name, please tell me your name.")
-            elif command_recognized in challenge_knowledge.names:
-                self._robot.speech.speak(
-                    "OK, I understand your name is {}, but I will think of a nice nickname.".format(command_recognized))
-            else:
-                self._robot.speech.speak(
-                    "I don't understand, I expected one of the following names: " + ", ".join(challenge_knowledge.names))
-            try:
-                rospy.loginfo("Detecting this person")
-                # detect person -> update person entity with certain attributes
-            except:
-                rospy.loginfo("Detecting failed.")
-                continue
-                # catch exceptions
-        return 'done'
-
-
-class ReportPeople(State):
+class ReportPeople(smach.State):
     """
     Form the sentences for all three people and say that sentence
     """
 
-    def __init__(self, robot):
-        State.__init__(self, outcomes=['done', 'aborted', 'failed'])
+    def __init__(self, robot, identified_people_designator):
+        smach.State.__init__(self, outcomes=['done', 'aborted', 'failed'])
         self._robot = robot
-        self._room_entity = self._robot.ed.get_entity(id=ROOM_ID)
+
+        ds.check_type(identified_people_designator, [Entity])
+        self._identified_people_designator = identified_people_designator
 
     def execute(self, userdata=None):
-        entities = self._robot.ed.get_entities()
+        self._room_entity = self._robot.ed.get_entity(id=ROOM_ID)
+        entities = self._identified_people_designator.resolve()
         person_entities = [entity for entity in entities if entity.is_a("person")]
 
         furniture_entities = [entity for entity in entities if entity.is_a("furniture")]
@@ -247,56 +111,69 @@ class ReportPeople(State):
 
 
 def setup_statemachine(robot):
-    sm = StateMachine(outcomes=['done', 'failed', 'aborted'])
+    sm = smach.StateMachine(outcomes=['done', 'failed', 'aborted'])
+
+    found_people_designator = ds.VariableDesignator(resolve_type=[Entity], name='found_people_designator')
+    identified_people_designator = ds.VariableDesignator(resolve_type=[Entity], name='identified_people_designator')
 
     with sm:
 
-        # StateMachine.add('INITIALIZE',
+        # smach.StateMachine.add('INITIALIZE',
         #                  Initialize(robot),
         #                  transitions={'initialized': 'INIT_POSE',
         #                               'abort': 'aborted'})
 
-        StateMachine.add('INIT_POSE', SetInitialPose(robot, STARTING_POINT), transitions={'done': 'GO_TO_SEARCH_POSE',
+        smach.StateMachine.add('INIT_POSE', states.SetInitialPose(robot, STARTING_POINT), transitions={'done': 'GO_TO_SEARCH_POSE',
                                                                                           'preempted': 'aborted',
                                                                                           'error': 'GO_TO_SEARCH_POSE'})
 
-        # StateMachine.add('START_CHALLENGE',
+        # smach.StateMachine.add('START_CHALLENGE',
         #                  StartChallengeRobust(robot, initial_pose=STARTING_POINT, door=False),
         #                  transitions={'Done': 'GO_TO_SEARCH_POSE',
         #                               'Aborted': 'aborted',
         #                               'Failed': 'WAIT_TIME'})
 
-        StateMachine.add('GO_TO_SEARCH_POSE',
-                         NavigateToWaypoint(robot, EntityByIdDesignator(robot, id=SEARCH_POINT), radius=0.4),
+        smach.StateMachine.add('GO_TO_SEARCH_POSE',
+                         states.NavigateToWaypoint(robot, ds.EntityByIdDesignator(robot, id=SEARCH_POINT), radius=0.4),
                          transitions={'arrived': 'LOCATE_PEOPLE',
                                       'goal_not_defined': 'LOCATE_PEOPLE',
                                       'unreachable': 'LOCATE_PEOPLE'})
 
         # locate three (or all four) people
-        StateMachine.add('LOCATE_PEOPLE', FindPeople(robot), transitions={'done': 'IDENTIFY_PEOPLE',
-                                                                          'aborted': 'done',
-                                                                          'failed': 'LOCATE_PEOPLE'})
+        smach.StateMachine.add('LOCATE_PEOPLE',
+                               states.FindPeople(robot,
+                                                 found_people_designator=found_people_designator.writeable,
+                                                 query_entity_designator=ds.EntityByIdDesignator(robot, id=ROOM_ID),
+                                                 attempts=2),
+                               transitions={'done': 'IDENTIFY_PEOPLE',
+                                            'aborted': 'done',
+                                            'failed': 'LOCATE_PEOPLE'})
 
         # drive past all thee people and fill their description
-        StateMachine.add('IDENTIFY_PEOPLE', IdentifyPeople(robot), transitions={'done': 'GO_BACK_TO_OPERATOR',
-                                                                                'aborted': 'done',
-                                                                                'failed': 'GO_BACK_TO_OPERATOR'})
+        smach.StateMachine.add('IDENTIFY_PEOPLE',
+                               IdentifyPeople(robot,
+                                              found_people_designator=found_people_designator,
+                                              identified_people_designator=identified_people_designator.writeable),
+                               transitions={'done': 'GO_BACK_TO_OPERATOR',
+                                            'aborted': 'done',
+                                            'failed': 'GO_BACK_TO_OPERATOR'})
 
         # drive back to the operator to describe the mates
-        StateMachine.add('GO_BACK_TO_OPERATOR',
-                         NavigateToWaypoint(robot, EntityByIdDesignator(robot, id=STARTING_POINT), radius=0.7),
+        smach.StateMachine.add('GO_BACK_TO_OPERATOR',
+                         states.NavigateToWaypoint(robot, ds.EntityByIdDesignator(robot, id=STARTING_POINT), radius=0.7),
                          transitions={'arrived': 'REPORT_PEOPLE',
                                       'goal_not_defined': 'REPORT_PEOPLE',
                                       'unreachable': 'GO_TO_OPERATOR_MORE_ROOM'})
 
-        StateMachine.add('GO_TO_OPERATOR_MORE_ROOM',
-                         NavigateToWaypoint(robot, EntityByIdDesignator(robot, id=STARTING_POINT), radius=1.2),
+        smach.StateMachine.add('GO_TO_OPERATOR_MORE_ROOM',
+                         states.NavigateToWaypoint(robot, ds.EntityByIdDesignator(robot, id=STARTING_POINT), radius=1.2),
                          transitions={'arrived': 'REPORT_PEOPLE',
                                       'goal_not_defined': 'REPORT_PEOPLE',
                                       'unreachable': 'GO_BACK_TO_OPERATOR'})
 
         # check how to uniquely define them
-        StateMachine.add('REPORT_PEOPLE', ReportPeople(robot), transitions={'done': 'done',
+        smach.StateMachine.add('REPORT_PEOPLE', ReportPeople(robot, identified_people_designator),
+                               transitions={'done': 'done',
                                                                             'aborted': 'done',
                                                                             'failed': 'failed'})
 
