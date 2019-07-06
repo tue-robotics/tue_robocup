987d894bf2a1d6ee3af357d6f515123490f156f4
Loy van Beek <loy.vanbeek@gmail.com>
2019-07-06 16:05:29 +1000

    Sending images to Telegram works

--- challenge_final/src/challenge_final/select_image_by_letter.py
+++ challenge_final/src/challenge_final/select_image_by_letter.py
@@ -1,10 +1,12 @@
 import sys
 import cv2
+from threading import Event
 
 # ROS
 import rospy
 import smach
 import cv_bridge
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
 # Robot skills
@@ -14,13 +16,23 @@ import robot_smach_states as states
 
 
 class SelectImageByLetter(smach.State):
-    def __init__(self, robot):
+    def __init__(self, robot, question='Which do you want?', timeout=60):
         smach.State.__init__(self,
-                             outcomes=['selected', 'failed'],
+                             outcomes=['succeeded', 'failed'],
                              input_keys=['label2image'],
                              output_keys=['selected_label'])
 
-    def execute(self, ud):
+        self._timeout = timeout
+        self._received = Event()
+        self._selection = ''
+
+        self._question = question
+
+        self._image_pub = rospy.Publisher('image_from_ros', Image, queue_size=10)
+        self._text_pub = rospy.Publisher('message_from_ros', String, queue_size=10)
+        self._text_sub = rospy.Subscriber('message_to_ros', String, self._handle_reply)
+
+    def execute(self, user_data):
         # Get a dict {str: Image}
         # Publish these to telegram:
         #   image 1
@@ -31,7 +43,32 @@ class SelectImageByLetter(smach.State):
         #  Receive the desired label
         # Return the label to the next state
 
+        self._received = Event()
+        self._selection = None
+
+        rate = rospy.Rate(2)
+
+        self._text_pub.publish(self._question)
+        for label, image_msg in user_data['label2image'].items():
+            # The order in which these images are received matters! text should be below the
+            self._image_pub.publish(image_msg)
+            rate.sleep()
+            self._text_pub.publish(label)
+            rate.sleep()
+
+        self._text_pub.publish("Please pick something from one of the pictures and send me the label below")
+
+        if self._received.wait(self._timeout):
+            user_data['selected_label'] = self._selection
+            return 'succeeded'
+
         return 'failed'
+
+    def _handle_reply(self, msg):
+        # type: (String) -> None
+        self._selection = msg.data
+
+        self._received.set()
 
 if __name__ == '__main__':
     from robot_skills import get_robot
@@ -40,21 +77,26 @@ if __name__ == '__main__':
         robot_name = sys.argv[1]
 
         rospy.init_node('test_find_person_in_room')
-        _robot = get_robot(robot_name)
+        _robot = None # get_robot(robot_name)
 
         bridge = cv_bridge.CvBridge()
 
         sm = SelectImageByLetter(_robot)
 
-        image_paths = sys.argv[1:]
+        image_paths = sys.argv[2:]
         labels = ['a', 'b', 'c', 'd', 'e']
 
         ud = {'label2image': {}}
-        for label, image_path in zip(labels, image_paths):
-            cv_image = cv2.imread(image_path)
 
-            ros_image = bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
-            ud['label2image'][label] = ros_image
+        for label, image_path in zip(labels, image_paths):
+            rospy.loginfo("Loading image for {}: {}".format(label, image_path))
+            try:
+                cv_image = cv2.imread(image_path)
+
+                ros_image = bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
+                ud['label2image'][label] = ros_image
+            except TypeError as type_err:
+                rospy.logerr("Could not load {}".format(image_path))
 
         sm.execute(ud)
 
