794cf997e9aff3034fe9eca51a63cf679106eb44
Janno Lunenburg <jannolunenburg@gmail.com>
2019-09-24 19:10:38 +0200

    Revert "Add __all__ to human_interaction and parents"
    This reverts commit c12579e99728165b5d58f9866da39bfaf2506137.

--- robot_smach_states/src/robot_smach_states/human_interaction/__init__.py
+++ robot_smach_states/src/robot_smach_states/human_interaction/__init__.py
@@ -1,7 +1,5 @@
 from detect_face import DetectFace
-from find_person_in_room import *
 from human_interaction import *
+from find_person_in_room import *
 from learn_operator import LearnOperator
 from sound_source_localization import *
-
-__all__ = ['detect_face', 'find_person_in_room', 'human_interaction', 'learn_operator', 'sound_source_localization']
