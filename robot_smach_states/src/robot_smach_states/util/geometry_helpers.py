7ddf5c1b28bfb07d4b57c80bf18d368d9481b7d2
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-09-24 19:27:37 +0200

    convert print to rospy log in geomtery helpers

--- robot_smach_states/src/robot_smach_states/util/geometry_helpers.py
+++ robot_smach_states/src/robot_smach_states/util/geometry_helpers.py
@@ -3,9 +3,11 @@ import math
 
 # ROS
 import PyKDL as kdl
+import rospy
 
 # TU/e Robotics
 from robot_skills.util.kdl_conversions import point_msg_to_kdl_vector
+
 
 def wrap_angle_pi(angle):
     """
@@ -64,7 +66,7 @@ def onTopOff(subject, container, ht=0.1):
     """
     ''' First: check if container actually has a convex hull '''
     if len(container.convex_hull) == 0:
-        print 'Error, entity {0} has no convex hull'.format(container.id)
+        rospy.logerr('Entity {0} has no convex hull'.format(container.id))
         return False
 
     ''' Second: turn points into KDL objects and offset '''
