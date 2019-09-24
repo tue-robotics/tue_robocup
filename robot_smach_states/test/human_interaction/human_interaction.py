90a94a78e251724afb15880cbd4c94aa6b7def00
Janno Lunenburg <jannolunenburg@gmail.com>
2019-09-24 21:40:34 +0200

    test(human_interaction)Cleaned up some stuff

--- robot_smach_states/test/human_interaction/human_interaction.py
+++ robot_smach_states/test/human_interaction/human_interaction.py
@@ -1,5 +1,4 @@
 # System
-import mock
 import unittest
 
 # TU/e Robotics
@@ -15,7 +14,7 @@ class TestSay(unittest.TestCase):
     def setUp(self):
         self.robot = Mockbot()
 
-    def test_say_formatted(self):
+    def test_say(self):
         # Simple test
         sentence = "This is an easy sentence"
         say = Say(self.robot, sentence=sentence)
@@ -23,29 +22,29 @@ class TestSay(unittest.TestCase):
         self.assertIn(sentence, self.robot.speech.speak.call_args_list[-1].args)
 
         # Provide a list with options that need to be formatted based on the resolution of a designator
-        options = ["Hey {a}"]
-        options_formatted = [o.format(a="hero") for o in options]
-        say = Say(self.robot, options, a=ds.VariableDesignator("hero"))
+        option = "Hey {a}"
+        formatted_option = option.format(a="hero")
+        say = Say(self.robot, [option], a=ds.VariableDesignator("hero"))
         self.assertEqual(say.execute(), "spoken")
-        self.assertIn(options_formatted[0], self.robot.speech.speak.call_args_list[-1].args)
+        self.assertIn(formatted_option, self.robot.speech.speak.call_args_list[-1].args)
 
         # Provide a designator that resolves to a sentence that is formatted with the resolution of a second designator
-        des = ds.VariableDesignator(options, resolve_type=[str])
+        des = ds.VariableDesignator([option], resolve_type=[str])
         say = Say(self.robot, des, a=ds.VariableDesignator("hero"))
         self.assertEqual(say.execute(), "spoken")
-        self.assertIn(options_formatted[0], self.robot.speech.speak.call_args_list[-1].args)
+        self.assertIn(formatted_option, self.robot.speech.speak.call_args_list[-1].args)
 
         # Provide a designator with options that do not need to be formatted and add an superfluous designator
-        options = ["Hey"]
-        des2 = ds.VariableDesignator(options)
+        option = "Hey"
+        des2 = ds.VariableDesignator([option])
         say = Say(self.robot, des2, a=ds.VariableDesignator("hero"))
         self.assertEqual(say.execute(), "spoken")
-        self.assertIn(options[0], self.robot.speech.speak.call_args_list[-1].args)
+        self.assertIn(option, self.robot.speech.speak.call_args_list[-1].args)
 
         # Provide a designator with options that do not need to be formatted
         say = Say(self.robot, des2)
         self.assertEqual(say.execute(), "spoken")
-        self.assertIn(options[0], self.robot.speech.speak.call_args_list[-1].args)
+        self.assertIn(option, self.robot.speech.speak.call_args_list[-1].args)
 
         # Provide a wrong keyword argument and check if it fails
         say = Say(self.robot, des, b=ds.VariableDesignator("hero"))
