61cdfb102fd69901174e02dc65dee0d15d725a28
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-09-25 22:23:01 +0200

    designator checks docstrings

--- robot_smach_states/src/robot_smach_states/util/designators/checks.py
+++ robot_smach_states/src/robot_smach_states/util/designators/checks.py
@@ -5,6 +5,16 @@ import core
 
 def check_resolve_type(designator, *allowed_types):
     """
+    Check if the resolve type of a designator is one of the allowed types. If the resolve type is a list, the internal
+    type is also checked.
+    Incorrect: []; [str, int]
+    Correct: [str]; [str], [int]
+    This check allows for type checking on construction of a statemachine, so runtime error can be prevented.
+    :param designator: Designator to check the resolve type of. In case of a list resolve type, one type in the list
+    needs to be defined.
+    :param allowed_types: Allowed resolve type of the designator. Allowed list types, also need to have an internal type
+    defined.
+
     >>> from robot_smach_states.util.designators.core import Designator
     >>> d1 = Designator("a", resolve_type=str, name='d1')
     >>> check_resolve_type(d1, str)
@@ -56,6 +66,14 @@ def check_resolve_type(designator, *allowed_types):
 
 def check_type(designator_or_value, *allowed_types):
     """
+    Check if the resolve type of a designator or a variable is one of the allowed types. If the type is a list, the internal
+    type is also checked.
+    Incorrect: []; [str, int]
+    Correct: [str]; [str], [int]
+    This check allows for type checking on construction of a statemachine, so runtime error can be prevented.
+    :param designator_or_value: Designator or variable to check the type of.
+    :param allowed_types: Allowed type of the designator or variable.
+
     >>> from robot_smach_states.util.designators.core import Designator
     >>> d = Designator("a", resolve_type=str)
     >>> check_type(d, str)
