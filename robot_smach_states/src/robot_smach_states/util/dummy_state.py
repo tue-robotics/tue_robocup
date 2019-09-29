513f3b3ee50bff9e751b58f7d3b74d585b417bf9
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-09-29 15:29:55 +0200

    Don't use super with Smach.State

--- robot_smach_states/src/robot_smach_states/util/dummy_state.py
+++ robot_smach_states/src/robot_smach_states/util/dummy_state.py
@@ -14,8 +14,7 @@ class DummyState(smach.State):
         :param result_designator: (VariableDesignator) or (str) The result to
             be returned
         """
-        super(DummyState,
-              self).__init__(outcomes=["succeeded", "failed", "aborted"])
+        smach.State.__init__(outcomes=["succeeded", "failed", "aborted"])
         ds.check_type(result_designator, str)
         self.result = result_designator.resolve() if hasattr(
             result_designator, "resolve") else result_designator
