64f5e10f35e294a2332f03bcc3e1f21b4b59623c
Janno Lunenburg <jannolunenburg@gmail.com>
2019-10-05 10:14:46 +0200

    test(demo)Created testfile

---
+++ challenge_demo/test/grammar_test.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+import unittest
+
+
+class GrammarTest(unittest.TestCase):
+
+    def test_grammar(self):
+        print("Yeehah")
+
+
+if __name__ == "__main__":
+    unittest.main()
