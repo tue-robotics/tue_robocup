74976f4b5effd72b089bd6e1d333fc2a0dc98fc9
Janno Lunenburg <jannolunenburg@gmail.com>
2019-10-15 19:35:01 +0200

    doc(demo)Added docstrings and typehints to grammar test

--- challenge_demo/test/grammar_test.py
+++ challenge_demo/test/grammar_test.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import collections
 import unittest
 
-from grammar_parser.cfgparser import CFGParser
+from grammar_parser.cfgparser import CFGParser, Conjunct, Option, Rule
 from robocup_knowledge import load_knowledge
 from robot_skills.mockbot import Mockbot
 from action_server.actions.action import ConfigurationResult
@@ -15,6 +15,10 @@ class GrammarTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        """
+        Loads knowledge of this challenge, constructs the parser and task manager and finds the 'rule' in the grammar
+        defining the actions
+        """
         cls.knowledge = load_knowledge('challenge_demo')
         cls.parser = CFGParser.fromstring(cls.knowledge.grammar)
         cls.task_manager = TaskManager(robot=Mockbot())
@@ -22,48 +26,55 @@ class GrammarTest(unittest.TestCase):
         # ToDo: Should we make everything recursive???
 
         # Rule corresponding to the grammar target
-        root_rule = cls.parser.rules[cls.knowledge.grammar_target]
+        root_rule = cls.parser.rules[cls.knowledge.grammar_target]  # type: Rule
 
         # In principle, a list of actions can be provided to the actions server
         # Therefore, we need to take to steps to list all actions
         # I.e., the following actions_list_target refers to the grammar target
         # containing a list of actions
         actions_list_target = ""
-        for option in root_rule.options:
+        for option in root_rule.options:  # type: Option
             if "actions" not in option.lsemantic:
                 continue
-            actions_list_target = option.conjuncts[0].name  # This is too hardcoded
+            actions_list_target = option.conjuncts[0].name  # type: str# This is too hardcoded
             break
 
         assert actions_list_target, "Cannot derive actions from grammar"
-        actions_list_rule = cls.parser.rules[actions_list_target]
+        actions_list_rule = cls.parser.rules[actions_list_target]  # type: Rule
         assert len(actions_list_rule.options) == 1, \
             "Don't know how to continue if the actions list rule has multiple options"
         assert len(actions_list_rule.options[0].conjuncts) == 1, \
             "Don't know how to continue if the actions options list rule has multiple conjunctions"
 
         # Now, we need to get all separate options
-        actions_target = actions_list_rule.options[0].conjuncts[0].name
+        actions_target = actions_list_rule.options[0].conjuncts[0].name  # type: str
 
-        cls.actions_rule = cls.parser.rules[actions_target]
+        cls.actions_rule = cls.parser.rules[actions_target]  # type: Rule
 
     def test_grammar(self):
+        """
+        Based on the actions rule, it recursively determines a viable option (e.g., 'go to the couch_table'), parses
+        this using the CFGParser and tries to configure the task manager based on the parse result. All results (even
+        if the configuration step with the task manager raises an exception) are stored in a result dict. Eventually,
+        all positive results are printed and if negative results have occurred, an AssertionError is raised with a
+        message containing all failed options.
+        """
 
         test_results = {}
-        for option in self.actions_rule.options:
+        for option in self.actions_rule.options:  # type: Option
 
             result_str = ""
 
             # For each conjunct, we want to have a viable option
-            for conjunct in option.conjuncts:
+            for conjunct in option.conjuncts:  # type: Conjunct
                 result_str += self._resolve_conjunct(conjunct.name)
-            result_str = result_str.rstrip(" ")
+            result_str = result_str.rstrip(" ")  # type: str
 
             # Now, we can parse the result string to get the action description
             actions_definition = self.parser.parse_raw(
                 target=self.knowledge.grammar_target,
                 words=result_str,
-            )
+            )  # type: dict
             self.assertIn(
                 "actions",
                 actions_definition,
@@ -113,11 +124,13 @@ class GrammarTest(unittest.TestCase):
             return target + " "
 
         result = ""
-        rule = cls.parser.rules[target]
+        rule = cls.parser.rules[target]  # type: Rule
         # For now, take the first available option
-        option = rule.options[0]
+        # If we want to make this method entirely recursive (from the root of the grammar), we might want to cook up
+        # something more generic.
+        option = rule.options[0]  # type: Option
 
-        for conjunct in option.conjuncts:
+        for conjunct in option.conjuncts:  # type: Conjunct
             if not conjunct.is_variable:
                 result += conjunct.name
                 result += " "
