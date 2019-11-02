18bfb09743db6fc0425f0b78464cbe6e8e914e5c
Peter van Dooren <pepevandooren@hotmail.com>
2019-11-02 16:19:51 +0100

    added tests for setGripper state

--- robot_smach_states/test/manipulation/handover_to_human.py
+++ robot_smach_states/test/manipulation/handover_to_human.py
@@ -37,5 +37,51 @@ class TestHandOverToHuman(unittest.TestCase):
         self.assertIsNone(self.robot.arms["leftArm"].occupied_by)
 
 
+class TestOpenGripper(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.robot = Mockbot()
+
+    def setUp(self):
+        self.arm_ds = ds.ArmDesignator(self.robot, {'required_arm_name': 'leftArm'})
+
+    def test_set_open(self):
+        state = states.SetGripper(self.robot, self.arm_ds, 'open')
+        self.assertEqual(state.execute(), "succeeded")
+        self.robot.arms["leftArm"].send_gripper_goal.assert_called_once_with('open', mock.ANY, max_torque=mock.ANY)
+
+
+class TestCloseGripper(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.robot = Mockbot()
+
+    def setUp(self):
+        self.arm_ds = ds.ArmDesignator(self.robot, {'required_arm_name': 'leftArm'})
+        self.entity = Entity("12345", "dummy", "/map",
+                                 None, None, {}, None, 0)
+        self.entity_ds = ds.VariableDesignator(self.entity)
+
+    def test_set_close(self):
+        state = states.SetGripper(self.robot, self.arm_ds, 'close', self.entity_ds)
+        self.assertEqual(state.execute(), "succeeded")
+        self.robot.arms["leftArm"].send_gripper_goal.assert_called_once_with('close', mock.ANY, max_torque=mock.ANY)
+        self.assertEqual(self.robot.arms["leftArm"].occupied_by, self.entity)
+
+
+class TestSetGripperFail(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.robot = Mockbot()
+
+    def setUp(self):
+        self.arm_ds = ds.ArmDesignator(self.robot, {'required_arm_name': 'leftArm'})
+
+    def test_invalid_arm_designator(self):
+        invalid_arm_ds = ds.ArmDesignator(self.robot, {'required_arm_name': 'there_is_no_arm_with_this_name'})
+        state = states.SetGripper(self.robot, invalid_arm_ds, 'close')
+        self.assertEqual(state.execute(), "failed")
+
+
 if __name__ == '__main__':
     unittest.main()
