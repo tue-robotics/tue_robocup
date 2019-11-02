c828edb4f7f30503cbe1086067ff09940492b72d
Lars Janssen <janslar@hotmail.com>
2019-11-02 15:38:25 +0100

    Fix(designator_iterator): remove unnecessary code

--- robot_smach_states/src/robot_smach_states/designator_iterator.py
+++ robot_smach_states/src/robot_smach_states/designator_iterator.py
@@ -63,7 +63,7 @@ class IterateDesignator(smach.State):
         if self._current_elements is None:
             collection = self.collection_des.resolve()
             if collection is None:
-                rospy.logwarn("Collection is None".format(collection))
+                rospy.logwarn("Collection is None")
                 return 'stop_iteration'
 
             rospy.loginfo("Current elements: {}".format(collection))
