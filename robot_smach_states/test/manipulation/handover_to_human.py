f44a29bd330c265b51261d63ffb6b6434f3f342e
Peter van Dooren <pepevandooren@hotmail.com>
2019-11-02 14:25:22 +0100

    added test for handover_to_human

---
+++ robot_smach_states/test/manipulation/handover_to_human.py
@@ -0,0 +1,43 @@
+import unittest
+import mock
+
+# datatypes
+import PyKDL as kdl
+
+# Robot Skills
+from robot_skills.mockbot import Mockbot
+from robot_skills.util.entity import Entity
+
+# Robot Smach States
+import robot_smach_states as states
+from robot_smach_states.util import designators as ds
+
+
+class TestHandOverToHuman(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.robot = Mockbot()
+
+    def setUp(self):
+        entity = Entity("12345", "dummy", "/map",
+                             None, None, {}, None, 0)
+        self.robot.arms["leftArm"].occupied_by = entity
+        self.arm_ds = ds.OccupiedArmDesignator(self.robot, {"required_goals": ['handover_to_human', 'reset']})
+
+    def test_handover_to_human(self):
+        state = states.HandoverToHuman(self.robot, self.arm_ds)
+        state.check_consistency()
+        self.assertIsNotNone(state.execute(), "Execute returned None, is container consistent?")
+
+        self.robot.arms["rightArm"].send_joint_goal.assert_not_called()
+        self.robot.arms["rightArm"].send_gripper_goal.assert_not_called()
+
+        self.robot.arms["leftArm"].send_joint_goal.assert_any_call('handover_to_human', max_joint_vel=mock.ANY, timeout=mock.ANY)
+        self.robot.arms["leftArm"].send_joint_goal.assert_any_call('reset', max_joint_vel=mock.ANY, timeout=mock.ANY)
+
+        self.robot.arms["leftArm"].send_gripper_goal.assert_any_call('open', mock.ANY, max_torque=mock.ANY)
+
+
+if __name__ == '__main__':
+    unittest.main()
