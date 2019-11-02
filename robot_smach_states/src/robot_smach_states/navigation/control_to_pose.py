d810705cf1fd12b14c25b68f2f35242a20275190
Lars Janssen <janslar@hotmail.com>
2019-11-02 11:46:53 +0100

    Fix(control_to_pose): add docstring

--- robot_smach_states/src/robot_smach_states/navigation/control_to_pose.py
+++ robot_smach_states/src/robot_smach_states/navigation/control_to_pose.py
@@ -30,7 +30,7 @@ def _get_yaw_from_quaternion_msg(msg):
     """
     Returns the yaw angle from a rotation in quaternion representation (msg)
     :param msg: The quaternion msg
-    :return: Yaw angle
+    :return: (float) Yaw angle in rad
     """
     orientation_list = [msg.x, msg.y, msg.z, msg.w]
     _, _, yaw = euler_from_quaternion(orientation_list)
