6e3b41d37c7e5f73eccfe354358ef00ea9d8b6b1
Lars Janssen <janslar@hotmail.com>
2019-11-08 20:35:07 +0100

    Initial commit guidance test

---
+++ robot_smach_states/test/navigation/test_guidance.py
@@ -0,0 +1,60 @@
+import unittest
+import PyKDL as kdl
+
+# Robot Skills
+from robot_skills.mockbot import Mockbot, Base
+from robot_skills.util.entity import Entity
+from robot_skills.util.volume import BoxVolume
+from robot_skills.util.kdl_conversions import FrameStamped
+
+# Robot Smach States
+from robot_smach_states.navigation.guidance import TourGuide, Guide
+
+
+class TestTourGuide(unittest.TestCase):
+    def setUp(self):
+        # simple rooms setup, all rooms are 5x5x3m
+        # 5 -------  ---------
+        # | kitchen | bedroom |
+        # 0---------5---------10
+
+        box1 = BoxVolume(kdl.Vector(0, 0, 0),
+                         kdl.Vector(5, 5, 3))
+
+        box2 = BoxVolume(kdl.Vector(0, 0, 0),
+                         kdl.Vector(1, 1, 0.5))
+
+        self._robot = Mockbot()
+        # self._robot.base.get_location = FrameStamped(kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(-3, 1, 0)), 'Hero')
+
+        entity1 = Entity("kitchen", "room", "/map", kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(0, 0, 0)), None,
+                         {"in": box1}, None, 0)
+        entity2 = Entity("bedroom", "room", "/map", kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(5, 0, 0)), None,
+                         {"in": box1}, None, 0)
+        entity3 = Entity("cabinet", "furniture", "/map", kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(4, 4, 0)), None,
+                         {"in": box2}, None, 0)
+        entity4 = Entity("bookcase", "furniture", "/map", kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(8, 1, 0)),
+                         None, {"in": box2}, None, 0)
+
+        self._robot.get_entities = [entity1, entity2, entity3, entity4]
+        self._room_entities = [entity1, entity2]
+
+    def test_get_room(self):
+        # position = FrameStamped(kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(-3, 1, 0)), 'Hero')
+        position = kdl.Vector(3, 1, 0)
+        test = TourGuide(self._robot)
+        room = test.get_room(position)
+        self.assertEqual(room.id, 'kitchen')
+
+    # def test_tour_guide(self):
+    #     test = TourGuide(self._robot
+    #     self.assertEqual(test.get_room(self._robot.base.get_location.frame.p), "kitchen")
+    #     self.assertEqual(test.describe_near_objects(), "On our {}")
+
+
+# class TestGuide(unittest.TestCase):
+#     def setUp(self):
+#         self.robot = Mockbot()
+#
+#     def test_guide(self):
+#         test = Guide(self.robot)
