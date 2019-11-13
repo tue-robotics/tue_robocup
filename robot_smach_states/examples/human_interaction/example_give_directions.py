447a98c26aeebd9ad33fd425825f766c71e18186
Lars Janssen <janslar@hotmail.com>
2019-11-13 08:41:39 +0100

    Fix(example_give_direction): integrate argparser

--- robot_smach_states/examples/human_interaction/example_give_directions.py
+++ robot_smach_states/examples/human_interaction/example_give_directions.py
@@ -1,22 +1,27 @@
 import rospy
-import sys
-import robot_smach_states.util.designators as ds
+import argparse
 
+# TU/e Robotics
 from robot_skills.get_robot import get_robot_from_argv
 from robot_smach_states.human_interaction import give_directions
+import robot_smach_states.util.designators as ds
+from robot_skills.get_robot import get_robot
 
 
 if __name__ == "__main__":
 
-    assert len(sys.argv) == 3, "Please provide the robot name and the entity id of the object to give directions to," \
-                               "e.g., 'python example_give_directions.py hero bed'"
+    parser = argparse.ArgumentParser(description="Starting from the current robot position and orientation give "
+                                                 "directions to the desired target object")
+    parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
+    parser.add_argument("target_entity_id", type=str, help="Entity id of the target object")
+    args = parser.parse_args()
 
     # Create node, robot and toggle interface
     rospy.init_node("test_give_directions")
-    r = get_robot_from_argv(1)
-    e_id = sys.argv[2]
+    r = get_robot(args.robot)
+    e_id = args.target_entity_id
 
     # Instantiate GuideToSymbolic machine
-    s = give_directions.GiveDirections(r,ds.EntityByIdDesignator(r, id=e_id))
+    s = give_directions.GiveDirections(r, ds.EntityByIdDesignator(r, id=e_id))
     # Execute the state
     s.execute()
