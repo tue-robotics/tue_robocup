573e964eb4d9e55433845f512e390b5eb4fe5573
Josja Geijsberts <j.geijsberts@student.tue.nl>
2019-12-10 21:20:03 +0100

    Added docstrings and removed comments

--- robot_smach_states/src/robot_smach_states/human_interaction/entity_from_operator_pose.py
+++ robot_smach_states/src/robot_smach_states/human_interaction/entity_from_operator_pose.py
@@ -1,15 +1,7 @@
-# ROS
-import os
-
 import rospy
 from geometry_msgs.msg import PoseStamped
-#  from robot_skills import Hero
-#  from robot_smach_states import Entity, VariableDesignator
-#  from robot_smach_states.util.designators import is_writeable
-#  from rospy import ServiceException
 from smach import State
 from std_msgs.msg import Header
-from ed_sensor_integration.srv import RayTraceResponse
 
 
 class GetFurnitureFromOperatorPose(State):
@@ -18,24 +10,22 @@ class GetFurnitureFromOperatorPose(State):
     """
 
     def __init__(self, robot, knowledge):
-        """ Initialization
+        """" Initialization
 
         :param robot: Robot API object
+        :param knowledge: available knowledge to be used for the furniture
         """
         State.__init__(self, outcomes=['succeeded', 'failed'])
         self._robot = robot
         self.operator = None
-        self.all_possible_furniture = [sub['name'] for sub in knowledge.locations]
-        # def __init__(self, robot, furniture_designator):
-        # State.__init__(self, outcomes=['done'], output_keys=["laser_dot"])
-        # is_writeable(furniture_designator)
+        self.all_possible_furniture = knowledge.location_names
 
     def execute(self, userdata=None):
         rospy.loginfo("I entered the execution state!")
         final_result = None
         while not rospy.is_shutdown() and final_result is None:
-            self._prep_operator() # output should be checked
-            if self._get_operator() is not None: # output should be checked
+            self._prep_operator()  # output should be checked
+            if self._get_operator() is not None:  # output should be checked
                 return 'failed'
             result = None
             while not rospy.is_shutdown() and result is None and self.operator is not None:
@@ -52,11 +42,8 @@ class GetFurnitureFromOperatorPose(State):
                     rospy.logerr("Could not get ray trace from closest person: {}".format(e))
                 rospy.sleep(1.0)
 
-            # result.intersection_point type: PointStamped
-            # result.entity_id: string
-            rospy.loginfo(
-                "There is a ray intersection with {i} at ({p.x:.4}, {p.y:.4}, {p.z:.4})".format(i=result.entity_id,
-                                                                                                p=result.intersection_point.point))
+            rospy.loginfo("There is a ray intersection with {i} at ({p.x:.4}, {p.y:.4}, {p.z:.4})".format(
+                i=result.entity_id, p=result.intersection_point.point))
 
             if result.entity_id in self.all_possible_furniture:
                 final_result = result
@@ -70,12 +57,9 @@ class GetFurnitureFromOperatorPose(State):
 
         self._robot.speech.speak("You pointed at %s" % final_result.entity_id)
         self._robot.get_arm().send_joint_goal("reset")
-        # Fill the designator and user data the furniture inspection
-        # furniture_designator.write(self._robot.ed.get_entity(final_result.entity_id))
-        # user_data['laser_dot'] = result.intersection_point
         return 'succeeded'
 
-    def _show_image(self, timeout=5.0):
+    def _show_image(self, timeout=5.0):  # could also use /hero/openpose/result_image as rgb (same msg type)
         rgb, depth, depth_info = self._robot.perception.get_rgb_depth_caminfo()
         self._robot.hmi.show_image_from_msg(rgb, timeout)
         return rgb, depth, depth_info
