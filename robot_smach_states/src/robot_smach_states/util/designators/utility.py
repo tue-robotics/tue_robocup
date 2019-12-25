3e2f972f687f01b7586e883c38a62d537de6d849
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-12-25 12:34:19 +0100

    (robot_smach_states) == None -> is None

--- robot_smach_states/src/robot_smach_states/util/designators/utility.py
+++ robot_smach_states/src/robot_smach_states/util/designators/utility.py
@@ -74,7 +74,7 @@ class LockingDesignator(Designator):
 
     def _resolve(self):
         if self._locked:
-            if self._current == None:
+            if self._current is None:
                 self._current = self.to_be_locked.resolve()
                 # rospy.loginfo("{0} locked to {1}".format(self, str(self._current)[:10]))
             return self._current
