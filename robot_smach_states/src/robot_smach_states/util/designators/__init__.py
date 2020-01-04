e86c8ae19db0d222e0b71d52a087bf9b3f90020a
Peter van Dooren <pepevandooren@hotmail.com>
2020-01-04 16:54:09 +0100

    fixed typo

--- robot_smach_states/src/robot_smach_states/util/designators/__init__.py
+++ robot_smach_states/src/robot_smach_states/util/designators/__init__.py
@@ -2,7 +2,7 @@ from core import *
 from checks import *
 from utility import *
 from ed_designators import *
-from place_designators import *
+from place_designator import *
 from arm import *
 from introspection import *
 from string_manipulation import *
