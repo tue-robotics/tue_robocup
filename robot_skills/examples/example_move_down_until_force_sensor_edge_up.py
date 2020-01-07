d1aa3579fb0ef97fbc6f67f143417055a9ca0453
Lars Janssen <janslar@hotmail.com>
2020-01-07 20:45:19 +0100

    Fix:add required argument

--- robot_skills/examples/example_move_down_until_force_sensor_edge_up.py
+++ robot_skills/examples/example_move_down_until_force_sensor_edge_up.py
@@ -4,6 +4,6 @@ from robot_skills.util.robot_constructor import robot_constructor
 
 rospy.init_node('test_mode_down_until_force_sensor_edge_up', anonymous=True)
 robot = robot_constructor("hero")
-arm = robot.get_arm()
+arm = robot.get_arm(force_sensor_required=True)
 
 arm.move_down_until_force_sensor_edge_up()
