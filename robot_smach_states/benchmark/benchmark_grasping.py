7dbeb05407e3a9b3d396c77bc23def0f4975e3db
Loy van Beek <loy.vanbeek@gmail.com>
2020-01-14 19:28:31 +0100

    Add options to grab anything

--- robot_smach_states/benchmark/benchmark_grasping.py
+++ robot_smach_states/benchmark/benchmark_grasping.py
@@ -27,12 +27,23 @@ if __name__ == "__main__":
                                                  "After the grasping is done, go back to the waypoint, "
                                                  "turn around and drop the item, "
                                                  "then turn around once more to be in the start configuration again")
-    parser.add_argument("cls", type=str, help="class of entity to grasp, eg. 'coke'")
-    parser.add_argument("support", type=str, help="ID of entity to grasp FROM ('grasp' entity is on-top-of this 'support' entity), eg. 'cabinet'")
-    parser.add_argument("waypoint", type=str, help="ID of waypoint for start and end position of the robot")
-    parser.add_argument("inspect_from_area", type=str, help="What area of the support-entity to inspect from", default='in_front_of')
-    parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
-    parser.add_argument("--output", default="grasp_benchmark.csv", help="Output of the benchmark results")
+    parser.add_argument("cls", type=str,
+                        help="class of entity to grasp, eg. 'coke' or '*' if you don't care")
+    parser.add_argument("support", type=str,
+                        help="ID of entity to grasp FROM ('grasp' entity is on-top-of this 'support' entity), eg. 'cabinet'")
+    parser.add_argument("waypoint", type=str,
+                        help="ID of waypoint for start and end position of the robot")
+    parser.add_argument("inspect_from_area", type=str,
+                        help="What area of the support-entity to inspect from", default='in_front_of')
+    parser.add_argument("--robot", default="hero",
+                        help="Robot name (amigo, hero, sergio)")
+    parser.add_argument("--output", default="grasp_benchmark.csv",
+                        help="Output of the benchmark results")
+    parser.add_argument("--strict-class", default=True,
+                        help="Only grasp an item if it matches the specified class. "
+                             "Pass --strict-class=false to also grab another class of items "
+                             "if none of the expected class is observed. "
+                             "Use this if you only care about grasping, not object recognition")
 
     args = parser.parse_args()
 
@@ -41,7 +52,7 @@ if __name__ == "__main__":
     with open(args.output, 'a+') as csv_file:
         reader = csv.DictReader(csv_file)
 
-        fields = ['robot', 'start_waypoint', 'class', 'id', 'inspect_start', 'inspect_end', 'inspect_duration', 'grab_start', 'grab_end', 'grab_duration']
+        fields = ['robot', 'start_waypoint', 'expected_class', 'observed_class', 'id', 'inspect_start', 'inspect_end', 'inspect_duration', 'grab_start', 'grab_end', 'grab_duration']
         results_writer = csv.DictWriter(csv_file, fieldnames=fields)
 
         if reader.fieldnames != results_writer.fieldnames:
@@ -57,7 +68,7 @@ if __name__ == "__main__":
         arm = ds.LockingDesignator(ds.UnoccupiedArmDesignator(robot, {}))
         arm.lock()
 
-        record = {'robot': args.robot, 'start_waypoint': args.waypoint, 'class': args.cls,
+        record = {'robot': args.robot, 'start_waypoint': args.waypoint, 'expected_class': args.cls,
                   'id': None, 'inspect_start': None, 'inspect_end': None, 'grab_start': None, 'grab_end': None}
 
         try:
@@ -77,9 +88,22 @@ if __name__ == "__main__":
 
             inspection_result = entity_ids.resolve()  # type: List[ClassificationResult]
             if inspection_result:
-                matching_entity_ids = [result.id for result in inspection_result if result.type == grasp_cls.resolve()]
-                if matching_entity_ids:
-                    selected_entity_id = matching_entity_ids[0]
+                if grasp_cls.resolve() == '*' or not args.strict_class:
+                    rospy.loginfo("Any item will match")
+                    matching_results = inspection_result
+                else:
+                    matching_results = [result for result in inspection_result if result.type == grasp_cls.resolve()]
+                    rospy.loginfo("Found {} items of class {}".format(len(matching_results), grasp_cls.resolve()))
+
+
+                if matching_results:
+                    if len(matching_results) > 1:
+                        rospy.logwarn("There are multiple items OK to grab, will select the first one")
+                        record['observed_class'] = ' '.join([result.type for result in matching_results])
+                    else:
+                        record['observed_class'] = matching_results[0].type
+                    selected_entity_id = matching_results[0].id
+
                     rospy.loginfo("Selected entity {} for grasping".format(selected_entity_id))
                     grasp_entity = ds.EdEntityDesignator(robot, id=selected_entity_id)
                     record['id'] = selected_entity_id
