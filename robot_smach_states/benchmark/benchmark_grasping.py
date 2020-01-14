4e066386ea3781a26e518c86f4efaa0ded51a913
Loy van Beek <loy.vanbeek@gmail.com>
2020-01-14 22:12:38 +0100

    Move implementation f single item benchmark to separate function

--- robot_smach_states/benchmark/benchmark_grasping.py
+++ robot_smach_states/benchmark/benchmark_grasping.py
@@ -23,6 +23,94 @@ try:
 except ImportError:
     pass
 
+def single_item(robot, results_writer, cls, support, waypoint, inspect_from_area=None, non_strict_class=False):
+    grasp_cls = ds.Designator(cls, name='grasp_cls')
+    support_entity = ds.EdEntityDesignator(robot, id=support, name='support_entity')
+    entity_ids = ds.VariableDesignator([], resolve_type=[ClassificationResult], name='entity_ids')
+    waypoint = ds.EdEntityDesignator(robot, id=waypoint, name='waypoint')
+
+    arm = ds.LockingDesignator(ds.UnoccupiedArmDesignator(robot, {}))
+    arm.lock()
+
+    record = {'robot': robot.robot_name, 'start_waypoint': waypoint, 'expected_class': cls,
+              'id': None}
+
+    try:
+        nav_to_start = NavigateToWaypoint(robot,
+                                          waypoint_designator=waypoint,
+                                          look_at_designator=support_entity)
+
+        assert nav_to_start.execute() == 'arrived', "I did not arrive at the start"
+
+        inspect = Inspect(robot=robot, entityDes=support_entity, objectIDsDes=entity_ids,
+                          navigation_area=inspect_from_area)
+
+        record['inspect_start'] = time.time()
+        assert inspect.execute() == 'done', "I could not inspect the support entity"
+        record['inspect_end'] = time.time()
+        record['inspect_duration'] = '{:3.3f}'.format(record['inspect_end'] - record['inspect_start'])
+
+        inspection_result = entity_ids.resolve()  # type: List[ClassificationResult]
+        if inspection_result:
+            if grasp_cls.resolve() == '*' or non_strict_class:
+                rospy.loginfo("Any item will match")
+                matching_results = inspection_result
+            else:
+                matching_results = [result for result in inspection_result if result.type == grasp_cls.resolve()]
+                rospy.loginfo("Found {} items of class {}".format(len(matching_results), grasp_cls.resolve()))
+
+            if matching_results:
+                if len(matching_results) > 1:
+                    rospy.logwarn("There are multiple items OK to grab, will select the last one")
+                    record['observed_class'] = ' '.join([result.type for result in matching_results])
+                else:
+                    record['observed_class'] = matching_results[-1].type
+                selected_entity_id = matching_results[-1].id
+
+                rospy.loginfo("Selected entity {} for grasping".format(selected_entity_id))
+                grasp_entity = ds.EdEntityDesignator(robot, id=selected_entity_id)
+                record['id'] = selected_entity_id[:6]
+
+                entity = grasp_entity.resolve()  # type: Entity
+                if entity:
+                    vector_stamped = entity.pose.extractVectorStamped()  # type: VectorStamped
+                    record['x'] = '{:.3f}'.format(vector_stamped.vector.x())
+                    record['y'] = '{:.3f}'.format(vector_stamped.vector.y())
+                    record['z'] = '{:.3f}'.format(vector_stamped.vector.z())
+
+                grab_state = Grab(robot, grasp_entity, arm)
+
+                record['grab_start'] = time.time()
+                assert grab_state.execute() == 'done', "I couldn't grasp"
+                record['grab_end'] = time.time()
+                record['grab_duration'] = '{:3.3f}'.format(record['grab_end'] - record['grab_start'])
+
+                assert nav_to_start.execute() == 'arrived', "I could not navigate back to the start"
+
+                rospy.logwarn("Robot will turn around to drop the {}".format(grasp_cls.resolve()))
+
+                force_drive = ForceDrive(robot, 0, 0, 1, 3.14)  # rotate 180 degs in pi seconds
+                force_drive.execute()
+
+                say_drop = Say(robot, sentence="I'm going to drop the item, please hold it!")
+                say_drop.execute()
+
+                drop_it = SetGripper(robot, arm_designator=arm, grab_entity_designator=grasp_entity)
+                drop_it.execute()
+
+                force_drive_back = ForceDrive(robot, 0, 0, -1, 3.14)  # rotate -180 degs in pi seconds
+                force_drive_back.execute()
+                nav_to_start.execute()
+            else:
+                raise AssertionError("No {} found".format(grasp_cls.resolve()))
+        else:
+            rospy.logerr("No entities found at all :-(")
+    except AssertionError as assertion_err:
+        say_fail = Say(robot, sentence=assertion_err.message + ", sorry")
+        say_fail.execute()
+    finally:
+        results_writer.writerow(record)
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Measure the time it takes to grasp an entity when starting from a given pose. "
@@ -67,90 +155,4 @@ if __name__ == "__main__":
 
         robot = get_robot(args.robot)
 
-        grasp_cls = ds.Designator(args.cls, name='grasp_cls')
-        support_entity = ds.EdEntityDesignator(robot, id=args.support, name='support_entity')
-        entity_ids = ds.VariableDesignator([], resolve_type=[ClassificationResult], name='entity_ids')
-        waypoint = ds.EdEntityDesignator(robot, id=args.waypoint, name='waypoint')
-
-        arm = ds.LockingDesignator(ds.UnoccupiedArmDesignator(robot, {}))
-        arm.lock()
-
-        record = {'robot': args.robot, 'start_waypoint': args.waypoint, 'expected_class': args.cls,
-                  'id': None}
-
-        try:
-            nav_to_start = NavigateToWaypoint(robot,
-                                              waypoint_designator=waypoint,
-                                              look_at_designator=support_entity)
-
-            assert nav_to_start.execute() == 'arrived', "I did not arrive at the start"
-
-            inspect = Inspect(robot=robot, entityDes=support_entity, objectIDsDes=entity_ids,
-                              navigation_area=args.inspect_from_area)
-
-            record['inspect_start'] = time.time()
-            assert inspect.execute() == 'done', "I could not inspect the support entity"
-            record['inspect_end'] = time.time()
-            record['inspect_duration'] = '{:3.3f}'.format(record['inspect_end'] - record['inspect_start'])
-
-            inspection_result = entity_ids.resolve()  # type: List[ClassificationResult]
-            if inspection_result:
-                if grasp_cls.resolve() == '*' or args.non_strict_class:
-                    rospy.loginfo("Any item will match")
-                    matching_results = inspection_result
-                else:
-                    matching_results = [result for result in inspection_result if result.type == grasp_cls.resolve()]
-                    rospy.loginfo("Found {} items of class {}".format(len(matching_results), grasp_cls.resolve()))
-
-
-                if matching_results:
-                    if len(matching_results) > 1:
-                        rospy.logwarn("There are multiple items OK to grab, will select the last one")
-                        record['observed_class'] = ' '.join([result.type for result in matching_results])
-                    else:
-                        record['observed_class'] = matching_results[-1].type
-                    selected_entity_id = matching_results[-1].id
-
-                    rospy.loginfo("Selected entity {} for grasping".format(selected_entity_id))
-                    grasp_entity = ds.EdEntityDesignator(robot, id=selected_entity_id)
-                    record['id'] = selected_entity_id[:6]
-
-                    entity = grasp_entity.resolve() # type: Entity
-                    if entity:
-                        vector_stamped = entity.pose.extractVectorStamped()  # type: VectorStamped
-                        record['x'] = '{:.3f}'.format(vector_stamped.vector.x())
-                        record['y'] = '{:.3f}'.format(vector_stamped.vector.y())
-                        record['z'] = '{:.3f}'.format(vector_stamped.vector.z())
-
-                    grab_state = Grab(robot, grasp_entity, arm)
-
-                    record['grab_start'] = time.time()
-                    assert grab_state.execute() == 'done', "I couldn't grasp"
-                    record['grab_end'] = time.time()
-                    record['grab_duration'] = '{:3.3f}'.format(record['grab_end'] - record['grab_start'])
-
-                    assert nav_to_start.execute() == 'arrived', "I could not navigate back to the start"
-
-                    rospy.logwarn("Robot will turn around to drop the {}".format(grasp_cls.resolve()))
-
-                    force_drive = ForceDrive(robot, 0, 0, 1, 3.14)  #rotate 180 degs in pi seconds
-                    force_drive.execute()
-
-                    say_drop = Say(robot, sentence="I'm going to drop the item, please hold it!")
-                    say_drop.execute()
-
-                    drop_it = SetGripper(robot, arm_designator=arm, grab_entity_designator=grasp_entity)
-                    drop_it.execute()
-
-                    force_drive_back = ForceDrive(robot, 0, 0, -1, 3.14)  #rotate -180 degs in pi seconds
-                    force_drive_back.execute()
-                    nav_to_start.execute()
-                else:
-                    raise AssertionError("No {} found".format(grasp_cls.resolve()))
-            else:
-                rospy.logerr("No entities found at all :-(")
-        except AssertionError as assertion_err:
-            say_fail = Say(robot, sentence=assertion_err.message+", sorry")
-            say_fail.execute()
-        finally:
-            results_writer.writerow(record)
+        single_item(robot, results_writer, args.cls, args.support, args.inspect_from_area, args.non_strict_class)
