934b1cb1567865dcc7977606101a1a66d41635ab
Lars Janssen <janslar@hotmail.com>
2020-01-18 23:08:13 +0100

    Fix(take_out_the_garbage): missing import

--- challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/take_out_the_garbage.py
+++ challenge_take_out_the_garbage/src/challenge_take_out_the_garbage/take_out_the_garbage.py
@@ -5,6 +5,7 @@ import rospy
 # Robot smach states
 import robot_smach_states as states
 from robocup_knowledge import load_knowledge
+import robot_smach_states.util.designators as ds
 from robot_skills import arms
 from challenge_take_out_the_garbage.pick_up import PickUpTrash
 from challenge_take_out_the_garbage.drop_down import DropDownTrash, DropTrash
