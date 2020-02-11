04cd8493f1f46925689cfc28a955c561f4d6b307
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-02-11 20:54:32 +0100

    (robot_smach_states) fix imports

--- robot_smach_states/src/robot_smach_states/navigation/__init__.py
+++ robot_smach_states/src/robot_smach_states/navigation/__init__.py
@@ -9,7 +9,8 @@ from .navigate_to_pose import *
 from .navigate_robust import NavigateRobust
 from .navigate_to_waypoint import *
 from .navigate_to_symbolic import *
-from .follow_operator import *
-from .follow_operator2_0 import FollowOperator2
 from .door_opening import *
 from .find import Find
+from .follow_operator import *
+from .follow_operator2_0 import FollowOperator2
+from .guidance import *
