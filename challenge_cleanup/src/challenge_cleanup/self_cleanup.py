6a1fe342b16907cd29a26162ceddde405703a322
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-02-11 21:03:26 +0100

    (cleanup) fix import

--- challenge_cleanup/src/challenge_cleanup/self_cleanup.py
+++ challenge_cleanup/src/challenge_cleanup/self_cleanup.py
@@ -6,6 +6,7 @@ from robot_smach_states.human_interaction import AskYesNo, HearOptionsExtra, Say
 from robot_smach_states.manipulation import ArmToJointConfig, Grab, Place
 from robot_smach_states.navigation import NavigateToPlace, NavigateToRoom
 from robot_smach_states.utility import WaitTime
+from robot_smach_states.world_model import Inspect
 from robot_skills.util.kdl_conversions import FrameStamped
 from robot_skills.util.entity import Entity
 import robot_smach_states.util.designators as ds
