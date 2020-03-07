ba78e0951dbcfed7ae4df61e4e841f2805f8adb4
Peter van Dooren <pepevandooren@hotmail.com>
2020-03-07 17:19:49 +0100

    refactor

--- challenge_storing_groceries/src/challenge_storing_groceries/storing_groceries.py
+++ challenge_storing_groceries/src/challenge_storing_groceries/storing_groceries.py
@@ -21,75 +21,77 @@ from open_door import OpenDoorMachine
 challenge_knowledge = load_knowledge('challenge_storing_groceries')
 
 
-class StoringGroceries(smach.StateMachine):
-    def __init__(self, robot):
-        smach.StateMachine.__init__(self, outcomes=['Done', 'Aborted', 'Failed'])
+def setup_statemachine(robot):
+    state_machine = smach.StateMachine(outcomes=['Done', 'Failed', 'Aborted'])
 
-        skip_door = rospy.get_param("~skip_door", True)
-        shelfDes = ds.EntityByIdDesignator(robot, id=challenge_knowledge.shelf)
-        tableDes = ds.EntityByIdDesignator(robot, id=challenge_knowledge.table)
+    skip_door = rospy.get_param("~skip_door", True)
+    shelfDes = ds.EntityByIdDesignator(robot, id=challenge_knowledge.shelf)
+    tableDes = ds.EntityByIdDesignator(robot, id=challenge_knowledge.table)
 
-        with self:
-            smach.StateMachine.add('START_CHALLENGE_ROBUST',
-                                   StartChallengeRobust(robot, challenge_knowledge.starting_point),
-                                   transitions={'Done': 'SKIP_DOOR',
-                                                'Aborted': 'Aborted',
-                                                'Failed': 'Failed'})
+    with state_machine:
 
-            smach.StateMachine.add("SKIP_DOOR",
-                                   CheckBool(skip_door),
-                                   transitions={'true': "NAV_TO_START",
-                                                'false': "OPEN_DOOR"})
+        smach.StateMachine.add('START_CHALLENGE_ROBUST',
+                               StartChallengeRobust(robot, challenge_knowledge.starting_point),
+                               transitions={'Done': 'SKIP_DOOR',
+                                            'Aborted': 'Aborted',
+                                            'Failed': 'Failed'})
 
-            # open the door of the cabinet
-            smach.StateMachine.add("OPEN_DOOR",
-                                   OpenDoorMachine(robot, shelfDes),
-                                   transitions={'succeeded': 'NAV_TO_START',
-                                                'failed': 'SAY_UNABLE_TO_OPEN_DOOR'})
+        smach.StateMachine.add("SKIP_DOOR",
+                               CheckBool(skip_door),
+                               transitions={'true': "NAV_TO_START",
+                                            'false': "OPEN_DOOR"})
 
-            smach.StateMachine.add('SAY_UNABLE_TO_OPEN_DOOR',
-                                   states.human_interaction.Say(robot, "I am unable to open the shelf door, "
-                                                                       "can you please open it for me?"),
-                                   transitions={'spoken': 'NAV_TO_START'})
+        # open the door of the cabinet
+        smach.StateMachine.add("OPEN_DOOR",
+                               OpenDoorMachine(robot, shelfDes),
+                               transitions={'succeeded': 'NAV_TO_START',
+                                            'failed': 'SAY_UNABLE_TO_OPEN_DOOR'})
 
-            # Inspect shelf
-            smach.StateMachine.add("NAV_TO_START",
-                                   states.navigation.NavigateToSymbolic(robot,
-                                                                        {shelfDes: "in_front_of"},
-                                                                        shelfDes),
-                                   transitions={'arrived': 'INSPECT_SHELVES',
-                                                'unreachable': 'INSPECT_SHELVES',
-                                                'goal_not_defined': 'INSPECT_SHELVES'})
+        smach.StateMachine.add('SAY_UNABLE_TO_OPEN_DOOR',
+                               states.human_interaction.Say(robot, "I am unable to open the shelf door, "
+                                                                   "can you please open it for me?"),
+                               transitions={'spoken': 'NAV_TO_START'})
 
-            smach.StateMachine.add("INSPECT_SHELVES",
-                                   InspectShelves(robot, shelfDes),
-                                   transitions={'succeeded': 'RANGE_ITERATOR',
-                                                'nothing_found': 'RANGE_ITERATOR',
-                                                'failed': 'RANGE_ITERATOR'})
+        # Inspect shelf
+        smach.StateMachine.add("NAV_TO_START",
+                               states.navigation.NavigateToSymbolic(robot,
+                                                                    {shelfDes: "in_front_of"},
+                                                                    shelfDes),
+                               transitions={'arrived': 'INSPECT_SHELVES',
+                                            'unreachable': 'INSPECT_SHELVES',
+                                            'goal_not_defined': 'INSPECT_SHELVES'})
 
-            # store items
-            # Begin setup iterator
-            single_item = ManipulateMachine(robot)
-            # The exhausted argument should be set to the prefered state machine outcome
-            range_iterator = smach.Iterator(outcomes=['succeeded', 'failed'],  # Outcomes of the iterator state
-                                            input_keys=[], output_keys=[],
-                                            it=lambda: range(5),
-                                            it_label='index',
-                                            exhausted_outcome='succeeded')
+        smach.StateMachine.add("INSPECT_SHELVES",
+                               InspectShelves(robot, shelfDes),
+                               transitions={'succeeded': 'RANGE_ITERATOR',
+                                            'nothing_found': 'RANGE_ITERATOR',
+                                            'failed': 'RANGE_ITERATOR'})
 
-            with range_iterator:
+        # store items
+        # Begin setup iterator
+        single_item = ManipulateMachine(robot)
+        # The exhausted argument should be set to the prefered state machine outcome
+        range_iterator = smach.Iterator(outcomes=['succeeded', 'failed'],  # Outcomes of the iterator state
+                                        input_keys=[], output_keys=[],
+                                        it=lambda: range(5),
+                                        it_label='index',
+                                        exhausted_outcome='succeeded')
 
-                smach.Iterator.set_contained_state('SINGLE_ITEM',
-                                                   single_item,
-                                                   loop_outcomes=['succeeded', 'failed'])
+        with range_iterator:
 
-            smach.StateMachine.add('RANGE_ITERATOR', range_iterator,
-                                   {'succeeded': 'AT_END',
-                                    'failed': 'Failed'})
-            # End setup iterator
+            smach.Iterator.set_contained_state('SINGLE_ITEM',
+                                               single_item,
+                                               loop_outcomes=['succeeded', 'failed'])
 
-            smach.StateMachine.add('AT_END',
-                                   states.human_interaction.Say(robot, "Goodbye"),
-                                   transitions={'spoken': 'Done'})
+        smach.StateMachine.add('RANGE_ITERATOR', range_iterator,
+                               {'succeeded': 'AT_END',
+                                'failed': 'Failed'})
+        # End setup iterator
 
-            ds.analyse_designators(self, "manipulation")
+        smach.StateMachine.add('AT_END',
+                               states.human_interaction.Say(robot, "Goodbye"),
+                               transitions={'spoken': 'Done'})
+
+        ds.analyse_designators(state_machine, "manipulation")
+
+    return state_machine
