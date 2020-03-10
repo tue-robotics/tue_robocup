9e12e56a5e4b0ec4ce0b364245bab356b8fd6181
Janno Lunenburg <jannolunenburg@gmail.com>
2020-03-10 21:49:57 +0100

    fix(smach)Grasppointdetermination: added check on frame ids

--- robot_smach_states/src/robot_smach_states/manipulation/grasp_point_determination.py
+++ robot_smach_states/src/robot_smach_states/manipulation/grasp_point_determination.py
@@ -39,9 +39,11 @@ class GraspPointDeterminant(object):
             return self._get_grasp_pose(entity, arm)
         # noinspection PyBroadException
         except Exception as e:
-            rospy.logwarn("Cannot compute grasp pose, defaulting to entity position and "
-                          "orientation the same as the robot")
+            rospy.logwarn("Cannot compute grasp pose ({}), defaulting to entity position and "
+                          "orientation the same as the robot".format(e.message))
             robot_pose_map = self._robot.base.get_location()  # type: FrameStamped
+            assert entity.frame_id == robot_pose_map.frame_id, "It is assumed that the entity frame id and the " \
+                                                               "world frame id are equal (typically 'map')"
             return FrameStamped(kdl.Frame(robot_pose_map.frame.M, entity.pose.frame.p), entity.frame_id)
 
     def _get_grasp_pose(self, entity, arm):
