b75d83f8098d6690b127b5730f1bf989679b67d6
Peter van Dooren <pepevandooren@hotmail.com>
2020-03-17 22:11:49 +0100

    added example waypoint constraints

--- robot_smach_states/examples/util/designators/navigation/example_waypoint_designator.py
+++ robot_smach_states/examples/util/designators/navigation/example_waypoint_designator.py
@@ -3,16 +3,16 @@ import argparse
 
 from robot_skills.get_robot import get_robot
 # Robot Smach States
-from robot_smach_states.util.designators.navigation.symbolic_constraints import SymbolicConstraintsDesignator
+from robot_smach_states.util.designators.navigation.waypoint_constraints import WayPointConstraintsDesignator
 from robot_smach_states.util.designators.ed_designators import EntityByIdDesignator
 
 if __name__ == "__main__":
     """
-    Example demonstrating how to use the SymbolicConstraintsDesignator
+    Example demonstrating how to use the WaypointConstraintsDesignator
     Make sure you have both (robot)-start and ED running.
     """
 
-    parser = argparse.ArgumentParser(description="Example symbolic constaints designator")
+    parser = argparse.ArgumentParser(description="Example waypoint constaints designator")
     parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
     args = parser.parse_args()
 
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     robot = get_robot(args.robot)
 
     # get an entitydesignator
-    entity = EntityByIdDesignator(robot, 'dinner_table', name='entity_designator')
+    wp = EntityByIdDesignator(robot, 'initial_pose', name='entity_designator')
 
     # create the constraints designator
-    nav1 = SymbolicConstraintsDesignator(robot, {entity: "in_front_of"}, name="symbolic designator")
+    nav1 = WayPointConstraintsDesignator(wp, name="waypoint designator")
 
     rospy.loginfo("Result of {}: {}".format(nav1.name, nav1.resolve()))
