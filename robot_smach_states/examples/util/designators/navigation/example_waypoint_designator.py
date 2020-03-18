0986d5f8a57e8419c952be505a38ce8cf40545ab
Peter van Dooren <pepevandooren@hotmail.com>
2020-03-18 10:59:47 +0100

    simplified example waypoint constraints and added test

--- robot_smach_states/examples/util/designators/navigation/example_waypoint_designator.py
+++ robot_smach_states/examples/util/designators/navigation/example_waypoint_designator.py
@@ -1,28 +1,28 @@
 import rospy
 import argparse
 
-from robot_skills.get_robot import get_robot
+from robot_skills.util.entity import Entity
+import PyKDL as kdl
 # Robot Smach States
 from robot_smach_states.util.designators.navigation.waypoint_constraints import WayPointConstraintsDesignator
-from robot_smach_states.util.designators.ed_designators import EntityByIdDesignator
+from robot_smach_states.util.designators.core import VariableDesignator
 
 if __name__ == "__main__":
     """
     Example demonstrating how to use the WaypointConstraintsDesignator
-    Make sure you have both (robot)-start and ED running.
     """
 
     parser = argparse.ArgumentParser(description="Example waypoint constaints designator")
-    parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
     args = parser.parse_args()
 
-    rospy.init_node("example_symbolic_constaints_designator")
-    robot = get_robot(args.robot)
+    rospy.init_node("example_waypoint_constaints_designator")
 
     # get an entitydesignator
-    wp = EntityByIdDesignator(robot, 'initial_pose', name='entity_designator')
+    pose = kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(1, 2, 0))
+    e = Entity("dummy_waypoint", "dummy_type", "/map", pose, None, None, None, None)
+    waypoint= VariableDesignator(e, name="entity designator")
 
     # create the constraints designator
-    nav1 = WayPointConstraintsDesignator(wp, name="waypoint designator")
+    nav1 = WayPointConstraintsDesignator(waypoint, name="waypoint designator")
 
     rospy.loginfo("Result of {}: {}".format(nav1.name, nav1.resolve()))
--- robot_smach_states/test/util/designators/navigation/test_pose_constraints.py
+++ robot_smach_states/test/util/designators/navigation/test_pose_constraints.py
@@ -1,30 +1,20 @@
 #! /usr/bin/env python
 import unittest
 
-from robot_skills.util.entity import Entity
-import PyKDL as kdl
-from robot_smach_states.util.designators.core import VariableDesignator
-
-from robot_smach_states.util.designators.navigation.look_at_constraints import LookAtConstraintsDesignator
+from robot_smach_states.util.designators.navigation.pose_constraints import PoseConstraintsDesignator
 
 
-class TestLookAtConstraintDesignator(unittest.TestCase):
+class TestPoseConstraintDesignator(unittest.TestCase):
     def test_resolve(self):
-        pose = kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(1, 2, 1.3))
-        e = Entity("dummy", "dummy_type", "/map", pose, None, None, None, None)
-        entity = VariableDesignator(e, name="entity designator")
-        n = LookAtConstraintsDesignator(entity, name="look at designator")
+        n = PoseConstraintsDesignator(0.5, 1.3, rz=None, radius=0.15, frame_id="/map", name="pose designator")
         constr = n.resolve()
-        self.assertIsNone(constr.pc)
-        self.assertIsNotNone(constr.oc)
+        self.assertIsNotNone(constr.pc)
+        self.assertIsNone(constr.oc)
 
     def test_resolve_offset(self):
-        pose = kdl.Frame(kdl.Rotation.RPY(0, 0, 0), kdl.Vector(1, 2, 1.3))
-        e = Entity("dummy", "dummy_type", "/map", pose, None, None, None, None)
-        entity = VariableDesignator(e, name="entity designator")
-        n = LookAtConstraintsDesignator(entity, 1.57, name="look at designator")
+        n = PoseConstraintsDesignator(0.5, 1.3, rz=-1.57, radius=0.15, frame_id="/map", name="pose designator")
         constr = n.resolve()
-        self.assertIsNone(constr.pc)
+        self.assertIsNotNone(constr.pc)
         self.assertIsNotNone(constr.oc)
 
 
