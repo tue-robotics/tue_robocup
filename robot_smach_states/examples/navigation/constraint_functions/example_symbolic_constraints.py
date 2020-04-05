65eb7e646451cd57d7dede6dd9c9dcfa5f9fc157
Peter van Dooren <pepevandooren@hotmail.com>
2020-04-05 10:52:41 +0200

    added room_constraints example

--- robot_smach_states/examples/navigation/constraint_functions/example_symbolic_constraints.py
+++ robot_smach_states/examples/navigation/constraint_functions/example_symbolic_constraints.py
@@ -3,7 +3,7 @@ import argparse
 
 from robot_skills.get_robot import get_robot
 # Robot Smach States
-from robot_smach_states.navigation.constraint_functions.symbolic_constraints import symbolic_constraint
+from robot_smach_states.navigation.constraint_functions.symbolic_constraints import symbolic_constraint, room_constraint
 from robot_smach_states.util.designators.ed_designators import EntityByIdDesignator
 
 if __name__ == "__main__":
@@ -22,8 +22,17 @@ if __name__ == "__main__":
     # get an entitydesignator
     entity = EntityByIdDesignator(robot, 'dinner_table', name='entity_designator')
 
-    # with rotation
+    # in front of the dinner table
     rospy.loginfo("symbolic constraint corresponding to 'in_front_of' the 'dinner_table':")
     pc, oc = symbolic_constraint(robot, {entity: "in_front_of"})
+    rospy.loginfo("pc becomes : {}".format(pc))
+    rospy.loginfo("oc becomes : {}".format(oc))
+
+    # get a roomdesignator
+    room = EntityByIdDesignator(robot, 'kitchen', name='room_designator')
+
+    # room constraint
+    rospy.loginfo("room constraint corresponding to 'kitchen':")
+    pc, oc = room_constraint(robot, room)
     rospy.loginfo("pc becomes : {}".format(pc))
     rospy.loginfo("oc becomes : {}".format(oc))
