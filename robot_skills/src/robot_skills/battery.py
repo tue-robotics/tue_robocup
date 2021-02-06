1aa76ba87373ec0701433059b8c68d25712ed70f
PetervDooren <PetervDooren@outlook.com>
2021-02-06 11:35:22 +0100

    add docstrings

--- robot_skills/src/robot_skills/battery.py
+++ robot_skills/src/robot_skills/battery.py
@@ -5,7 +5,15 @@ from robot_skills.robot_part import RobotPart
 
 
 class Battery(RobotPart):
+    """
+    Interface to a battery. This implementation assumes a recent measurement to be available
+    """
     def __init__(self, robot_name, tf_listener, location):
+        """
+        @param robot_name: name of the robot
+        @param tf_listener: tf_listener of the robot (needed because it is a robot part)
+        @param location: string indicating the location of the battery
+        """
         super(Battery, self).__init__(robot_name=robot_name, tf_listener=tf_listener)
         self.location = location
         self.battery_sub = rospy.Subscriber("battery", BatteryState, self.battery_callback)
@@ -17,12 +25,18 @@ class Battery(RobotPart):
 
     @property
     def percentage(self):
+        """"
+        Percentage of the battery expressed between 1 (full) and 0 (empty)
+        """
         if self.recent_msg is None:
             return None
         return self.recent_msg.percentage
 
     @property
     def is_charging(self):
+        """
+        boolean indicating whether or not the battery is charging
+        """
         if self.recent_msg is None:
             return None
         return self.recent_msg.power_supply_status == BatteryState.POWER_SUPPLY_STATUS_CHARGING
