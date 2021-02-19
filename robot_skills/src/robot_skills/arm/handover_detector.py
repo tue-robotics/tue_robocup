4798967381a81f72efc146197f5fa47e1fa4a8db
PetervDooren <PetervDooren@outlook.com>
2021-02-19 20:31:27 +0100

    rename side to name

--- robot_skills/src/robot_skills/arm/handover_detector.py
+++ robot_skills/src/robot_skills/arm/handover_detector.py
@@ -4,20 +4,21 @@ import PyKDL as kdl
 from robot_skills.robot_part import RobotPart
 from tue_manipulation_msgs.msg import GripperCommandGoal, GripperCommandAction
 
+
 class HandoverDetector(RobotPart):
     """
     Sensor functionality to detect when a handover is taking place
     """
-    def __init__(self, robot_name, tf_listener, side):
+    def __init__(self, robot_name, tf_listener, name):
         """
         constructor
 
         :param robot_name: robot_name
         :param tf_listener: tf_server.TFClient()
-        :param side: string used to identify the sensor
+        :param name: string used to identify the sensor
         """
         super(HandoverDetector, self).__init__(robot_name=robot_name, tf_listener=tf_listener)
-        self.side = side
+        self.name = name
 
     def handover_to_human(self, timeout=10):
         """
@@ -27,12 +28,12 @@ class HandoverDetector(RobotPart):
         :param timeout: timeout in seconds
         :return: True or False
         """
-        pub = rospy.Publisher('/' + self.robot_name + '/handoverdetector_' + self.side + '/toggle_robot2human',
+        pub = rospy.Publisher('/' + self.robot_name + '/handoverdetector_' + self.name + '/toggle_robot2human',
                               std_msgs.msg.Bool, queue_size=1, latch=True)
         pub.publish(std_msgs.msg.Bool(True))
 
         try:
-            rospy.wait_for_message('/' + self.robot_name + '/handoverdetector_' + self.side + '/result', std_msgs.msg.Bool,
+            rospy.wait_for_message('/' + self.robot_name + '/handoverdetector_' + self.name + '/result', std_msgs.msg.Bool,
                                    timeout)
             # print('/'+self.robot_name+'/handoverdetector_'+self.side+'/result')
             return True
@@ -49,12 +50,12 @@ class HandoverDetector(RobotPart):
         :param timeout: timeout in seconds
         :return: True or False
         """
-        pub = rospy.Publisher('/' + self.robot_name + '/handoverdetector_' + self.side + '/toggle_human2robot',
+        pub = rospy.Publisher('/' + self.robot_name + '/handoverdetector_' + self.name + '/toggle_human2robot',
                               std_msgs.msg.Bool, queue_size=1, latch=True)
         pub.publish(std_msgs.msg.Bool(True))
 
         try:
-            rospy.wait_for_message('/' + self.robot_name + '/handoverdetector_' + self.side + '/result', std_msgs.msg.Bool,
+            rospy.wait_for_message('/' + self.robot_name + '/handoverdetector_' + self.name + '/result', std_msgs.msg.Bool,
                                    timeout)
             # print('/'+self.robot_name+'/handoverdetector_'+self.side+'/result')
             return True
