0e1b8cc143bbc75beaaff0ab45cea9dcacb58df7
lars <janslar@hotmail.com>
2021-03-30 19:47:10 +0200

    Feature(arm_requirements): point_at

--- robot_smach_states/src/robot_smach_states/manipulation/point_at.py
+++ robot_smach_states/src/robot_smach_states/manipulation/point_at.py
@@ -7,7 +7,7 @@ import smach
 # TU/e Robotics
 from robot_skills.arm.arms import PublicArm
 from robot_skills.util.entity import Entity
-from ..util.designators import check_type
+from robot_smach_states.util.designators import check_type
 
 # System
 import numpy as np
@@ -15,6 +15,8 @@ import sys
 
 
 class PointAt(smach.State):
+    REQUIRED_ARM_PROPERTIES = {"required_goals": ['point_at'], }
+
     def __init__(self, robot, arm_designator, point_at_designator, look_at_designator=None):
         """
         Drive the robot back a little and move the designated arm to place the designated item at the designated pose
@@ -52,12 +54,10 @@ class PointAt(smach.State):
                 return "failed"
         else:
             look_at_ent = point_at_ent  # type: Entity
-
         arm = self._arm_designator.resolve()  # type: PublicArm
         if not arm:
             rospy.logerr("Could not resolve _arm_designator")
             return "failed"
-
         # TODO: make arm point at some pose
         vs = point_at_ent.pose.extractVectorStamped()  # VectorStamped
         vector_in_bs = vs.projectToFrame(self._robot.base_link_frame, self._robot.tf_listener)
@@ -69,7 +69,7 @@ class PointAt(smach.State):
         # vth is in radians/sec but we rotate for 1 s to that should equal $rotate_base in the end.
 
         maxvel = np.pi / 10.0  # Max rotation speed in rad/s
-        duration = abs(rotate_base/maxvel)  # duration of rotation, in s
+        duration = abs(rotate_base / maxvel)  # duration of rotation, in s
         vel = maxvel * np.sign(rotate_base)
         rospy.loginfo("Rotate base by {:.3f}deg. At {:.3f}deg/s this takes {}s".format(np.degrees(rotate_base),
                                                                                        np.degrees(vel),
@@ -85,9 +85,34 @@ class PointAt(smach.State):
         return 'succeeded'
 
 
+class PointAtSM(smach.StateMachine):
+
+    def __init__(self, robot, arm_designator, point_at_designator, look_at_designator=None):
+        """
+        Drive the robot back a little and move the designated arm to place the designated item at the designated pose
+
+        :param robot: Robot to execute state with
+        :param arm_designator : arm to place with, so Arm that holds entity_to_place, E.g. ArmHoldingEntityDesignator
+        :param point_at_designator: Designator that resolves to the pose to place at. E.g. EmptySpotDesignator
+        """
+        smach.StateMachine.__init__(self, outcomes=['done', 'failed'])
+
+        with self:
+            smach.StateMachine.add('RESOLVE_ARM', ResolveArm(arm_designator, self),
+                                   transitions={'succeeded': 'POINT_AT',
+                                                'failed': 'failed'})
+
+            smach.StateMachine.add('POINT_AT', PointAt(robot, arm_designator, point_at_designator),
+                                   transitions={'failed': 'failed',
+                                                'succeeded': 'done'})
+
+        # check_arm_requirements(self, robot)
+
+
 if __name__ == "__main__":
     from robot_skills import get_robot
-    from ..util.designators import EdEntityDesignator, UnoccupiedArmDesignator
+    from robot_smach_states.util.designators import EdEntityDesignator, UnoccupiedArmDesignator
+    from robot_smach_states.utility import ResolveArm
 
     if len(sys.argv) > 1:
         robot_name = sys.argv[1]
@@ -96,10 +121,10 @@ if __name__ == "__main__":
 
         rospy.init_node('test_follow_operator')
         robot = get_robot(robot_name)
-        sm = PointAt(robot,
-                     arm_designator=UnoccupiedArmDesignator(robot, {'required_goals':['point_at']}),
-                     point_at_designator=EdEntityDesignator(robot, id=point_at, name='point_at_des'),
-                     look_at_designator=EdEntityDesignator(robot, id=look_at, name='look_at_des'))
+        sm = PointAtSM(robot,
+                       arm_designator=UnoccupiedArmDesignator(robot).lockable(),
+                       point_at_designator=EdEntityDesignator(robot, id=point_at, name='point_at_des'),
+                       look_at_designator=EdEntityDesignator(robot, id=look_at, name='look_at_des'))
         sm.execute()
     else:
         print("Please provide robot name, point_at ID and look_at ID as argument.")
