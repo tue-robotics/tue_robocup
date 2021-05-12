a632d106341f42f64d0e35848edd7aee73d5222f
Raymond van de Ven <r.a.m.v.d.ven@student.tue.nl>
2021-05-12 12:15:54 +0200

    Back bumper added and double activation counter

--- robot_smach_states/examples/example_bumper.py
+++ robot_smach_states/examples/example_bumper.py
@@ -12,15 +12,27 @@ class BumperThing:
         self.robot = robot
         self.front_bumper_sub = rospy.Subscriber("hero/base_f_bumper_sensor", Bool, self.front_callback)
         self.front_bumper_active = False
+        self.back_bumper_sub = rospy.Subscriber("hero/base_b_bumper_sensor", Bool, self.front_callback)
+        self.back_bumper_active = False
 
     def front_callback(self, msg):
         rospy.loginfo(msg)
         self.front_bumper_active = msg.data
 
+    def back_callback(self, msg):
+        rospy.loginfo(msg)
+        self.back_bumper_active = msg.data
+
     def motion(self):
-        if self.ront_bumper_active:
+        if self.front_bumper_active and self.back_bumper_active :
+            rospy.loginfo("robot_stuck")
+            self.robot.base.force_drive(0, 0, 0, 1)
+        elif self.front_bumper_active:
             rospy.loginfo("driving_back")
             self.robot.base.force_drive(-0.1, 0, 0, 1)
+        elif self.back_bumper_active:
+            rospy.loginfo("driving_forward")
+            self.robot.base.force_drive(0.1, 0, 0, 1)
 
 
 if __name__ == "__main__":
