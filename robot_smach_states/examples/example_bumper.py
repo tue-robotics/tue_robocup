97fd9c0586322c7aff7b69104b5e6670a5759e71
Raymond van de Ven <r.a.m.v.d.ven@student.tue.nl>
2021-05-12 11:27:06 +0200

    added movement

--- robot_smach_states/examples/example_bumper.py
+++ robot_smach_states/examples/example_bumper.py
@@ -7,14 +7,19 @@ from std_msgs.msg import Bool
 from robot_skills.get_robot import get_robot
 
 
-def front_callback(data):
-    rospy.loginfo(data)
+class BumperThing:
+    def __init__(self, robot):
+        self.robot = robot
+        self.front_bumper_sub = rospy.Subscriber("hero/base_f_bumper_sensor", Bool, self.front_callback)
+
+    def front_callback(self, msg):
+        rospy.loginfo(msg)
+        if msg.data:
+            self.robot.base.force_drive(-0.1, 0, 0, 0.1)
 
 
 if __name__ == "__main__":
-
     rospy.init_node("test_grasping")
-
     robot = get_robot("hero")
-
-    front_bumper_sub = rospy.Subscriber("hero/base_f_bumper_sensor", Bool, front_callback)
+    bt = BumperThing(robot)
+    rospy.spin()
