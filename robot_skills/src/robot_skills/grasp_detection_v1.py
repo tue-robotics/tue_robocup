d2bf78e68a7f6df4d023f93425112bd7eed3a5f8
JeroenBerghs <j.c.a.berghs@student.tue.nl>
2021-05-18 20:53:49 +0200

    Made a start on the time dependency

--- robot_skills/src/robot_skills/grasp_detection_v1.py
+++ robot_skills/src/robot_skills/grasp_detection_v1.py
@@ -1,18 +1,33 @@
 import rospy
 from geometry_msgs.msg import WrenchStamped
 
+import time
+
 
 def callback(data):
-    rospy.loginfo("I felt: %s", data.wrench)
+    rospy.loginfo("I felt: \n %.s", data.wrench)
     threshold_torque_y = 0.1  # Threshold for the torque around y axis in [Nm]
-    object = False
+    object_present1 = False
+    object_present2 = False
+
     if data.wrench.torque.y >= threshold_torque_y:
-        object = True
-        print('Grasp: SUCCESSFUL')
+        object_present1 = True
+        print('Initial grasp: SUCCESSFUL')
+
+        time.sleep(2)
+
+        if data.wrench.torque.y >= threshold_torque_y and object_present1 == True:
+            object_present2 = True
+            print('Total grasp: SUCCESSFUL')
+        else:
+            object_present2 = False
+            print('Initial grasp: SUCCESSFUL, total grasp: FAILED')
+
     else:
-        object = False
-        print('Grasp: FAILED')
-    return object
+        object_present1 = False
+        print('Initial and total grasp: FAILED')
+
+    return object_present1 and object_present2
 
 
 def listener():
