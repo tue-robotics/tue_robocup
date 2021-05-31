15e14e0ced71663fd1f37009ba17bf94a9a5272b
JeroenBerghs <j.c.a.berghs@student.tue.nl>
2021-05-31 15:17:25 +0200

    Changed the line of rospy.init_mode()

--- robot_skills/examples/example_grasp_detector.py
+++ robot_skills/examples/example_grasp_detector.py
@@ -2,10 +2,10 @@ import rospy
 
 from robot_skills.arm.grasp_detector import GraspDetector
 
+rospy.init_node("example_grasp_detector")
+
 wrench_topic = "/hero/wrist_wrench/raw"  # topic to listen to
 gd = GraspDetector("Hero", None, wrench_topic)
-
-rospy.init_node("example_grasp_detector")
 
 while not rospy.is_shutdown():
     raw_input("Press enter to query the grasp detector")
