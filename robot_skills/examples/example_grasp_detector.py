8416416813b886c388db135ff0e5cca379cf0b17
JeroenBerghs <j.c.a.berghs@student.tue.nl>
2021-05-31 16:25:08 +0200

    Changed raw_input to input

--- robot_skills/examples/example_grasp_detector.py
+++ robot_skills/examples/example_grasp_detector.py
@@ -8,7 +8,7 @@ wrench_topic = "/hero/wrist_wrench/raw"  # topic to listen to
 gd = GraspDetector("Hero", None, wrench_topic)
 
 while not rospy.is_shutdown():
-    raw_input("Press enter to query the grasp detector")
+    input("Press enter to query the grasp detector")
     if gd.detect():
         print("robot is currently holding something!!!")
     else:
