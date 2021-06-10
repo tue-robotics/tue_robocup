93a9d8216297bd94e7e8d1a6fe56298bf4c4fa39
JeroenBerghs <j.c.a.berghs@student.tue.nl>
2021-06-10 11:27:03 +0200

    Minor changes

--- robot_skills/examples/example_grasp_detector.py
+++ robot_skills/examples/example_grasp_detector.py
@@ -7,11 +7,10 @@ rospy.init_node("example_grasp_detector")
 wrench_topic = "/hero/wrist_wrench/raw"  # topic to listen to
 gd = GraspDetector("Hero", None, wrench_topic)
 
-gd.start_recording()
-
 while not rospy.is_shutdown():
 
     input("Press enter to query the grasp detector")
+    gd.start_recording()
     if gd.detect():
         print("robot is currently holding something!!!", "Last T_y: {}".format(gd.torque_list[-1]))
     else:
