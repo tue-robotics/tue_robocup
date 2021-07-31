7c47bbf4d0088e3181880ad6f43525fbf488573d
Janno Lunenburg <jannolunenburg@gmail.com>
2021-07-31 16:08:05 +0200

    wip(smach_states)Added support for topological navigation with waypoints

--- robot_smach_states/test/navigation/test_topological_navigation.py
+++ robot_smach_states/test/navigation/test_topological_navigation.py
@@ -6,6 +6,8 @@ import unittest
 from topological_action_planner_msgs.msg import Edge, Node
 
 # Robot Smach States
+from robot_smach_states.navigation.navigate_to_symbolic import NavigateToSymbolic
+from robot_smach_states.navigation.navigate_to_waypoint import NavigateToWaypoint
 from robot_smach_states.navigation.navigation import NavigateTo
 from robot_smach_states.navigation.topological_navigation import convert_msgs_to_actions
 
@@ -29,11 +31,19 @@ class TestTopologicalNavigation(unittest.TestCase):
         msg.destination = destination_node
         actions = convert_msgs_to_actions(ROBOT, [msg])
         self.assertEquals(len(actions), 1, "Result should contain exactly one action")
-        self.assertTrue(isinstance(actions[0], NavigateTo), "Action should be a 'NavigateTo' action")
+        self.assertTrue(isinstance(actions[0], NavigateToSymbolic), "Action should be a 'NavigateToSymbolic' action")
 
-    @unittest.skip("Not yet implemented")
     def test_drive_waypoint(self):
-        pass
+        msg = Edge()
+        msg.action_type = Edge.ACTION_DRIVE
+        origin_node = Node(SOURCE_NODE_ID, SOURCE_NODE_AREA)
+        msg.origin = origin_node
+        destination_node = Node()
+        destination_node.entity = DESTINATION_NODE_ID
+        msg.destination = destination_node
+        actions = convert_msgs_to_actions(ROBOT, [msg])
+        self.assertEquals(len(actions), 1, "Result should contain exactly one action")
+        self.assertTrue(isinstance(actions[0], NavigateToWaypoint), "Action should be a 'NavigateToWaypoint' action")
 
     def test_open_door(self):
         pass
