da2d45d802e090e532b647ad4b2b8c047a6501eb
Arpit Aggarwal <ar13pit@gmail.com>
2021-08-01 15:20:49 +0200

    feat(challenge_move_obstacles): Rename module and fix behavior

--- challenge_move_obstacles/setup.py
+++ challenge_move_obstacles/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     # #  don't do this unless you want a globally visible script
     # scripts=['bin/myscript'],
-    packages=['challenge_move_obstacles'],
+    packages=['move_obstacles'],
     package_dir={'': 'src'}
 )
 
