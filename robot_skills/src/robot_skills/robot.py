2377399e40368a6929f3a419c57f73e1251f902f
Peter van Dooren <pepevandooren@hotmail.com>
2021-08-01 10:51:49 +0200

    fix import

--- robot_skills/src/robot_skills/robot.py
+++ robot_skills/src/robot_skills/robot.py
@@ -21,7 +21,7 @@ from sensor_msgs.msg import Image, JointState
 from std_msgs.msg import String
 
 # TU/e
-from topological_action_planner.srv import Plan
+from topological_action_planner_msgs.srv import Plan
 
 # Robot skills
 from .arm import arms
