6657a7eb1a90322354d40a6ac5ce45c7158386a3
Arpit Aggarwal <ar13pit@gmail.com>
2021-08-01 15:21:17 +0200

    feat(challenge_move_obstacles): Rename executable

--- challenge_move_obstacles/scripts/challenge_move_obstacles.py
+++ challenge_move_obstacles/scripts/challenge_move_obstacles.py
@@ -3,7 +3,7 @@
 import argparse
 import rospy
 from robot_smach_states.util.startup import startup
-from challenge_move_obstacles import MoveObstacles
+from move_obstacles import MoveObstacles
 
 
 def sm(robot, x, y, gdx, gdy):
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("robot", help="obstacle x position", type=str)
     parser.add_argument("x", help="obstacle x position", type=float)
     parser.add_argument("y", help="obstacle y position", type=float)
     parser.add_argument("gdx", help="manipulation pose x distance from obstacle", type=float)
@@ -24,4 +25,5 @@ if __name__ == "__main__":
     rospy.init_node('move_obstacles_exec')
 
     sm_args = (args.x, args.y, args.gdx, args.gdy)
-    startup(sm, statemachine_args=sm_args, challenge_name='move_obstacles')
+
+    startup(sm, statemachine_args=sm_args, challenge_name='move_obstacles', argv=["challenge_move_obstacles.py", args.robot])
