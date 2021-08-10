7f64a9502fe290d796216a944522fa8b819e696e
Janno Lunenburg <jannolunenburg@gmail.com>
2021-08-10 21:54:47 +0200

    wip(smach)Adding example for opening doors

---
+++ robot_smach_states/examples/manipulation/example_open_door.py
@@ -0,0 +1,26 @@
+# System
+import argparse
+
+# ROS
+import rospy
+
+# TU/e Robotics
+from robot_skills import get_robot
+
+# Robot Smach States
+from robot_smach_states.manipulation.open_door import PassDoor
+from robot_smach_states.util.designators import Designator, EntityByIdDesignator
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Test the topological navigation state")
+    parser.add_argument("-r", "--robot", default="hero", help="Robot name (amigo, hero, sergio)")
+    parser.add_argument("-e", "--entity", default="door", help="ID of the door to pass")
+    parser.add_argument("-a", "--area", default="in_front_of", help="Area of the entity to navigate to")
+    args = parser.parse_args()
+
+    # Create node and robot
+    rospy.init_node("test_pass_door")
+
+    robot = get_robot(args.robot)
