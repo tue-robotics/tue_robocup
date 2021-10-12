712c0cbee42e3cc4b2f1bcac864712e6c23d4c9b
shanki98 <shanki98@gmail.com>
2021-10-12 21:20:42 +0200

    Add Example ArmToJointConfig

---
+++ robot_smach_states/examples/manipulation/example_ArmToJointConfig.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#ROS
+import rospy
+import smach
+import argparse
+
+from robot_smach_states.manipulation import ArmToJointConfig
+from robot_smach_states.util.designators import ArmDesignator
+
+class TestArmToJointConfig(smach.State):
+    """StateMachine used to test the ArmToJointConfig in robot_smach_states"""
+    def __init__(self, robot):
+        """
+        :param robot: robot object used to test the arm (type:string)
+        """
+        super().__init__(outcomes=["succeeded", "failed"])
+        self.robot = robot
+        self.arm = ArmDesignator(self.robot)
+        with self:
+            self.add("MOVE_TO_GRASP",
+                     ArmToJointConfig(self.robot,self.arm,"prepare_grasp"),
+                     transitions = {"failed":"failed",
+                                    "succeeded":"RESET_ARM"}
+                     )
+            self.add("RESET_ARM",
+                     ArmToJointConfig(self.robot,self.arm,"reset"),
+                     transitions = {"failed":"failed",
+                                    "succeeded":"succeeded"}
+                     )
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Test moving Arm to a default configuration")
+    parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
+    args = parser.parse_args()
+
+    rospy.init_node("Test_Arm_to_Joint_config")
+    robot = get_robot(args.robot)
+
+    rospy.loginfo("Starting the StateMachine to Test Arm")
+    sm = TestArmToJointConfig(robot)
+    rospy.loginfo("Executing the StateMachine to Test Arm")
+    sm.execute()
+    rospy.loginfo("Done")
