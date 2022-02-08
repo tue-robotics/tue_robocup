f60d51db3481dd493851c57b7810445b1e9fa275
rodTU <P-ict0>
2022-02-08 22:07:53 +0100

    Removed recording functions

--- robot_skills/src/robot_skills/arm/active_grasp_detector.py
+++ robot_skills/src/robot_skills/arm/active_grasp_detector.py
@@ -1,57 +1,50 @@
 from __future__ import absolute_import
-
 import rospy
- 
 from sensor_msgs.msg import JointState
 
-
-# TU/e Robotics
 from robot_skills.robot_part import RobotPart
 
 
 class ActiveGraspDetector(RobotPart):
-	"""
-	Class for detecting whether or not the robot is holding something using ""
 
-	:param robot_name: Name of the robot
-	:param tf_buffer: tf2_ros.Buffer for use in RobotPart
-	:param wrench_topic: Topic to use for measurement
-	"""
+    def __init__(self, robot_name, tf_buffer, wrench_topic):
+    """
+    Class for detecting whether or not the robot is holding something
 
-	def __init__(self, robot_name, tf_buffer, wrench_topic):
-	        super(GraspDetector, self).__init__(robot_name=robot_name, tf_buffer=tf_buffer)
-	        self._topic = wrench_topic
-	        self.start_time = None
-	        self.threshold_difference = 0.075 # Values may vary on standby
+    :param robot_name: Name of the robot
+    :param tf_buffer: tf2_ros.Buffer for use in RobotPart
+    :param wrench_topic: Topic to use for measurement
+    """
+        
+        super(ActiveGraspDetector, self).__init__(robot_name=robot_name, tf_buffer=tf_buffer)
+        self._topic = wrench_topic
 
-	        # self.measuring_ideal ?
-	  	self.measuring_max = rospy.Duration(10.0) # seconds
+        self.wrench_sub = self.create_subscriber(self._topic, JointState, self._joint_callback, queue_size=1)
 
-	        self.sensor_list = []
-	        self.record = False
+        self.threshold_difference = 0.075 # Difference between base and final position
+        self.minimum_position = None # To be determined
 
-
-
-	def start_recording(self):
-	    """"
-	    Function that allows the start of recording the sensor data.
-	    :return: True in order to start the recording
-	    """
-	    rospy.loginfo('Start recording')
-
-	    # Reset starting time
-	    self.start_time = rospy.Time.now()
-
-	    # Resets the sensor list
-	    self.sensor_list = []
-
-	    self.record = True  # sets flag to start recording
+        self.current_position = None
+        self.position_difference = None
 
 
-	def detect(self):
-	    """
-	    If called returns it the robot is holding something or not
+    def _joint_callback(self, msg):
+        """
+        Will be executed every time a new message is received.
+        :param msg: input joint message
+        """
+        if msg.name == 'hand_motor_joint':
+            self.current_position = msg.position
 
-	    :return: True if the robot is holding something
-	    :return: False if it is not holding something
-	    """
+
+    def detect(self):
+        """
+        If called returns it the robot is holding something or not
+
+        :return: True if the robot is holding something
+        :return: False if it is not holding something
+        """
+
+        if self.current_position < minimum_position: # Hand not open enough to hold something
+            return False
+
