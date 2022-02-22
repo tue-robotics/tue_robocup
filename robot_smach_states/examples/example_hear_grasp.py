7b92ad1a3fd45e564a26c4c9feee0d02c9b1efb4
shanki98 <shanki98@gmail.com>
2022-02-22 22:02:17 +0100

    Updated robot.ed.get_closest_entity()

--- robot_smach_states/examples/example_hear_grasp.py
+++ robot_smach_states/examples/example_hear_grasp.py
@@ -9,17 +9,17 @@ from robot_smach_states.utility import Initialize
 class Hear(smach.State):
     def __init__(self, robot):
         smach.State.__init__(self, outcomes=['succeeded', 'failed', 'preempted'],
-                             output_keys=['heared'])
+                             output_keys=['heard'])
         self.robot = robot
 
     def execute(self, userdata):
         answer = self.robot.hmi.query(description='can i have a coke?',
                                       grammar="T -> DET coke \n DET -> a",
                                       target='T',
-                                      timeout=rospy.Duration(60).to_sec())
+                                      timeout=60)
         if answer:
             rospy.loginfo('I heard:' + answer.sentence)
-        userdata.heared = answer.sentence
+        userdata.heard = answer.sentence
         return 'succeeded'
 
 
@@ -35,9 +35,10 @@ class GetEntitysByType(smach.State):
 
         rospy.loginfo('I\'m going to search for {}'.format(type))
         # call robot.ed.get_closest_entity()
-        entity = {'id': 'coke-1'}
+        entity = self.robot.ed.get_closest_entity(etype=userdata.heard)
+        #entity = {'id': 'coke-1'}
 
-        userdata.entity = entity
+        userdata.entity = entsity
         return 'succeeded'
 
 
