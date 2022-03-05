5e975995f2a588685bcab08ef41222078a863360
shanki98 <shanki98@gmail.com>
2022-03-05 14:37:55 +0100

    Fixed example segment object

--- robot_smach_states/examples/example_segment_objects.py
+++ robot_smach_states/examples/example_segment_objects.py
@@ -7,14 +7,14 @@ from robot_skills.get_robot import get_robot
 
 # Robot Smach States
 from robot_smach_states.util import designators as ds
-from robot_smach_states import SegmentObjects
+from robot_smach_states.count_objects_on_location import CountObjectsOnLocation
 
 from robot_skills.classification_result import ClassificationResult
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Test if it can segment objects")
+    parser = argparse.ArgumentParser(description="Test if it can segment objects and count them")
     parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
     parser.add_argument("entity", default="dinner_table", help="Name of the entity on which to segment objects")
     args = parser.parse_args()
@@ -25,15 +25,18 @@ if __name__ == "__main__":
     location_name = args.entity
 
     rospy.loginfo("Creating location designator")
-    location_designator = ds.EntityByIdDesignator(robot, location_name)
+    location_designator = ds.EntityByIdDesignator(robot, uuid=None, name=location_name)
 
     rospy.loginfo("Creating found entity designator")
     segmented_entities_designator = ds.VariableDesignator([], resolve_type=[ClassificationResult])
+    rospy.loginfo("Creating numer of objects designator")
+    num_objects_designator = ds.VariableDesignator([], resolve_type=int)
+    rospy.loginfo("Creating segment and count objects state machine")
+    sm = CountObjectsOnLocation(robot, location_name, segmented_entities_designator, num_objects_designator.writeable,
+                                segmentation_area='on_top_of', object_type='', threshold=0.0)
 
-    rospy.loginfo("Creating segment objects state")
-    sm = SegmentObjects(robot, segmented_entities_designator.writeable, location_designator, "on_top_of")
+    rospy.loginfo("Executing segment and count objects state machine")
 
-    rospy.loginfo("Executing segment objects state")
     sm.execute()
 
     rospy.loginfo("Resulting classification result: \n {}".format(segmented_entities_designator.resolve()))
