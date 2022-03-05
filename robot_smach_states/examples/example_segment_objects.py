9f5b42051fec926fbcf59f6a3450eb59c3a3fdd8
shanki98 <shanki98@gmail.com>
2022-03-05 16:55:15 +0100

    Fixed changes requested

--- robot_smach_states/examples/example_segment_objects.py
+++ robot_smach_states/examples/example_segment_objects.py
@@ -17,23 +17,22 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Test if it can segment objects")
     parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
-    parser.add_argument("Entity", default="dinner_table", help="Name of the entity on which to segment objects")
+    parser.add_argument("entity", default="dinner_table", help="Name of the entity on which to segment objects")
 
     args = parser.parse_args()
 
     rospy.init_node("example_segment_objects")
 
     robot = get_robot(args.robot)
-    location_name = args.Entity
+    location_name = args.entity
 
     rospy.loginfo("Creating location designator")
-    location_designator = ds.EdEntityDesignator(robot, uuid = location_name)
+    location_designator = ds.EdEntityDesignator(robot, uuid=location_name)
     rospy.loginfo("Creating found entity designator")
     segmented_entities_designator = ds.VariableDesignator([], resolve_type=[ClassificationResult])
 
     rospy.loginfo("Creating segment objects state machine")
-    sm = SegmentObjects(robot, segmented_entities_designator.writeable, location_designator,
-                        segmentation_area="on_top_of", unknown_threshold=0.0, filter_threshold=0.0)
+    sm = SegmentObjects(robot, segmented_entities_designator.writeable, location_designator)
 
     rospy.loginfo("Executing segment objects state machine")
 
