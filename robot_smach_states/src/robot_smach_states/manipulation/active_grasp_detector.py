8acc8f60cada90ee7bcfe5e3d81a3531027a8a7a
P-ict0 <r.martin.nunez@student.tue.nl>
2022-03-15 20:38:30 +0100

    Fixed outcomes from smach state

--- robot_smach_states/src/robot_smach_states/manipulation/active_grasp_detector.py
+++ robot_smach_states/src/robot_smach_states/manipulation/active_grasp_detector.py
@@ -12,7 +12,7 @@ from robot_skills.arm.gripper_position_detector import GripperPositionDetector
 class ActiveGraspDetector(smach.State):
     REQUIRED_ARM_PROPERTIES = {"required_gripper_types": [GripperTypes.GRASPING], }
 
-    def __init__(self, robot, arm_designator, threshold_difference=0.075, minimum_position=0.2, max_torque=0.015):
+    def __init__(self, robot, arm_designator, threshold_difference=0.075, minimum_position=0.1, max_torque=0.15):
         """
         State for detecting whether the robot is holding something
 
@@ -22,7 +22,7 @@ class ActiveGraspDetector(smach.State):
         :param minimum_position: Minimum position to assume that the gripper is holding something
         :param max_torque: Max torque of the gripper to perform the test with
         """
-        smach.State.__init__(self, outcomes=['true', 'false', 'failed'])
+        smach.State.__init__(self, outcomes=['true', 'false', 'failed', 'Cannot determine'])
         self.robot = robot
 
         self.arm_designator = arm_designator
@@ -41,9 +41,13 @@ class ActiveGraspDetector(smach.State):
         first_position = arm._arm.gripper_position_detector.detect()  # First position of the gripper
 
         if first_position is None:
+            rospy.loginfo("Cannot retrieve first position")
             return 'failed'
-        else:
+        elif first_position < self.minimum_position:
+            rospy.loginfo("First position is {}".format(first_position))
+            return 'Cannot determine'
 
+        else:
             if not arm.gripper.send_goal('close', max_torque=self.max_torque):
                 rospy.logerr("Error while closing the gripper")  # Ignores the error but notifies it
 
@@ -51,8 +55,13 @@ class ActiveGraspDetector(smach.State):
 
             if second_position is not None:
                 if abs(first_position - second_position) < self.threshold_difference:
+                    rospy.loginfo("First position is {}\n"
+                                  "Second position is {}".format(first_position, second_position))
                     return 'true'
                 else:
+                    rospy.loginfo("First position is {}"
+                                  "Second position is {}".format(first_position, second_position))
                     return 'false'
             else:
+                rospy.loginfo("Cannot retrieve second position")
                 return 'failed'
