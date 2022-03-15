b7b571f3fca34ad22c204c1de730a60e9242bbed
Janno Lunenburg <jannolunenburg@gmail.com>
2022-03-15 21:19:32 +0100

    wip(follow)FollowBreadCrumb receives operator positions

--- robot_smach_states/src/robot_smach_states/navigation/follow_operator_machine.py
+++ robot_smach_states/src/robot_smach_states/navigation/follow_operator_machine.py
@@ -12,7 +12,7 @@ import tf2_ros
 import pykdl_ros
 # noinspection PyUnresolvedReferences
 import tf2_pykdl_ros
-from people_recognition_msgs.msg import TrackOperatorAction, TrackOperatorGoal
+from people_recognition_msgs.msg import TrackOperatorAction, TrackOperatorFeedback, TrackOperatorGoal
 from robot_skills.robot import Robot
 
 
@@ -26,17 +26,39 @@ class SelectOperator(smach.State):
         smach.State.__init__(self, outcomes=["succeeded", "failed"])
         self._robot = robot
         # ToDo: move to robot_skills
-        self._ac = actionlib.ActionClient(f"/{robot.robot_name}/track_operator", TrackOperatorAction)
+        self._ac = actionlib.SimpleActionClient(f"/{robot.robot_name}/track_operator", TrackOperatorAction)
+        self._ac_cb = None
 
     def execute(self, _):
-        self._robot.speech.speak("I am selecting the operator, still assuming he or she is right in front of me")
+        self._robot.speech.speak(
+            "I am selecting the operator, still assuming he or she is right in front of me", block=False
+        )
         robot_pose_stamped = self._robot.base.get_location()
         operator_pose = robot_pose_stamped.frame * kdl.Frame(kdl.Vector(1.0, 0.0, 0.0))
         operator_pose_stamped = pykdl_ros.FrameStamped(operator_pose, rospy.Time.now(), "map")
         goal = TrackOperatorGoal()
         goal.start_pose = tf2_ros.convert(operator_pose_stamped, geometry_msgs.msg.PoseStamped)
-        self._ac.send_goal(goal=goal)  # ToDo: how do we register the feedback CB?
+        self._ac.send_goal(goal=goal, feedback_cb=self._track_operator_cb)
         return "succeeded"
+
+    def register_callback(self, callback: callable):
+        """
+        Registers a callback method that will be passed to the action client
+
+        :param callback: callable
+        """
+        self._ac_cb = callback
+
+    def _track_operator_cb(self, feedback_msg: TrackOperatorFeedback):
+        """
+        Default callback method. Simply echos the position. Will be overridden if another callback is registered
+
+        :param feedback_msg: feedback_msg
+        """
+        if self._ac_cb is not None:
+            self._ac_cb(feedback_msg.current_pose)
+        else:
+            rospy.loginfo(f"Received operator position feedback: {feedback_msg.current_pose.pose.position}")
 
 
 class FollowBreadcrumb(smach.State):
@@ -50,8 +72,19 @@ class FollowBreadcrumb(smach.State):
         self._robot = robot
 
     def execute(self, _):
-        self._robot.speech.speak("I am trying to follow the breadcrumb")
+        self._robot.speech.speak("I am trying to follow the breadcrumb", block=False)
+        r = rospy.Rate(1.0)
+        for _ in range(5):
+            r.sleep()
         return "within_reach"
+
+    def add_breadcrumb(self, operator_pose: geometry_msgs.msg.PoseStamped):
+        """
+        Adds a breadcrumb. Typically used as a callback for the tracker
+
+        :param operator_pose: pose of the operator to follow
+        """
+        rospy.loginfo(f"FollowBreadCrumb: Received operator position: {operator_pose.pose.position}")
 
 
 class WithinReach(smach.State):
@@ -111,16 +144,20 @@ class FollowOperator(smach.StateMachine):
         """
         smach.StateMachine.__init__(self, outcomes=["done", "lost"])
 
+        select_operator = SelectOperator(robot)
+        follow_breadcrumb = FollowBreadcrumb(robot)
+        select_operator.register_callback(follow_breadcrumb.add_breadcrumb)
+
         with self:
             smach.StateMachine.add(
-                "SELECT_OPERATOR", SelectOperator(robot), transitions={
+                "SELECT_OPERATOR", select_operator, transitions={
                     "succeeded": "FOLLOW",
                     "failed": "lost",
                 }
             )
 
             smach.StateMachine.add(
-                "FOLLOW", FollowBreadcrumb(robot), transitions={
+                "FOLLOW", follow_breadcrumb, transitions={
                     "within_reach": "WITHIN_REACH",
                     "lost": "RECOVER",
                 }
