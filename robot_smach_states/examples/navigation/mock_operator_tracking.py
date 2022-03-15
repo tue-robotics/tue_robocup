a78836f82cfcc6d8d71fdf7943c1219a9feb1a5d
Janno Lunenburg <jannolunenburg@gmail.com>
2022-03-15 19:56:48 +0100

    wip(follow)Mock operator tracking starts

--- robot_smach_states/examples/navigation/mock_operator_tracking.py
+++ robot_smach_states/examples/navigation/mock_operator_tracking.py
@@ -1,3 +1,6 @@
+# System
+import argparse
+
 # ROS
 import actionlib
 import rospy
@@ -5,7 +8,7 @@ import tf2_ros
 import PyKDL as kdl
 
 # TU/e Robotics
-from people_recognition_msgs.msg import TrackOperatorAction, TrackOperatorGoal
+from people_recognition_msgs.msg import TrackOperatorAction, TrackOperatorGoal, TrackOperatorFeedback
 
 
 class TrackingMocker:
@@ -25,16 +28,44 @@ class TrackingMocker:
         self._tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
     def _execute(self, goal: TrackOperatorGoal):
-        robot_pose = self._get_robot_pose()
-        
+        rospy.loginfo("Received tracking goal, start tracking")
+        # robot_pose = self._get_robot_pose()
+        # ToDo: get rid of hardcoding
+        start_pos = kdl.Vector(1.0, 0.5, 0.0)
+        current_pos = kdl.Vector(start_pos)
+        distance = (current_pos - start_pos).Norm()
+        r = rospy.Rate(2.0)  # Assumed detection rate
+        velocity = 1.0  # Assumed operator velocity
+        while distance < 5.0:
+            current_pos.x(current_pos.x() + velocity/r)
+            distance = (current_pos - start_pos).Norm()
+            self._publish_feedback(current_pos)
+            r.sleep()
 
-    def _get_robot_pose(self) -> kdl.Pose:
-        # ToDo: implement using tf buffer
-        return kdl.Frame()
+        rospy.loginfo(f"Stopped walking after {distance} meters")
+        self._as.set_succeeded()
+
+    # def _get_robot_pose(self) -> kdl.Pose:
+    #     # ToDo: implement using tf buffer
+    #     return kdl.Frame()
+
+    def _publish_feedback(self, position):
+        feedback_msg = TrackOperatorFeedback()
+        feedback_msg.current_pose.header.frame_id = "map"
+        feedback_msg.current_pose.header.stamp = rospy.Time.now()
+        feedback_msg.current_pose.pose.position.x = position.x()
+        feedback_msg.current_pose.pose.position.y = position.y()
+        feedback_msg.current_pose.pose.position.z = position.z()
+        feedback_msg.current_pose.pose.orientation.w = 1.0
+        self._as.publish_feedback(feedback_msg)
 
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description="Test the FollowOperator state")
+    parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
+    args = parser.parse_args()
+
     rospy.init_node("mock_operator_tracking")
-    tracker = TrackingMocker()
+    tracker = TrackingMocker(args.robot)
     rospy.spin()
