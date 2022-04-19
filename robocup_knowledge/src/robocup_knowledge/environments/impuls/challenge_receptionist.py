9a59c2ccb84d53a7630b7e8afd5258c51e161f6c
lotte <l.messing@student.tue.nl>
2022-04-19 21:46:00 +0200

    Fix(robocup knowledge): add missing knowledge, blame Lars

--- robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_receptionist.py
+++ robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_receptionist.py
@@ -8,3 +8,7 @@ starting_point = "initial_pose"
 waypoint_door = {'id': 'entry_door', 'radius': 0.5}
 
 waypoint_livingroom = {'id': 'livingroom', 'radius': 0.5}
+
+sitting_room = 'living_room'  # Where people will be guided to sit and the robot will find a seat
+
+seats = ['couch_long', 'couch_short']
