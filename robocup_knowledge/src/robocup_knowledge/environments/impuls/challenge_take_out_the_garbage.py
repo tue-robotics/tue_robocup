f1cb2e9d7f4e18f603d5d2d68ac97e9909c2a4e3
P-ict0 <r.martin.nunez@student.tue.nl>
2022-05-07 10:16:38 +0200

    Fixes

---
+++ robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_take_out_the_garbage.py
@@ -0,0 +1,13 @@
+# TAKE OUT THE GARBAGE KNOWLEDGE FILE IMPULS
+# TU/e Robotics
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+starting_point = "initial_pose"
+trashbin_id = "trashbin"
+trashbin_id2 = "trashbin1"
+drop_zone_id = "take_out_garbage_drop_zone"
+helper_waypoint = "helper_take_out_garbage"
+end_waypoint = "exit_2_rips"
