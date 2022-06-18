958451ac0a031427151889961e8f22ad563e6edb
Rein Appeldoorn <reinzor@gmail.com>
2022-06-18 15:30:44 +0200

    fix(find_people_in_room): Cast filter to list

--- robot_smach_states/src/robot_smach_states/human_interaction/find_people_in_room.py
+++ robot_smach_states/src/robot_smach_states/human_interaction/find_people_in_room.py
@@ -244,7 +244,7 @@ class FindPeople(smach.State):
                     return person_attr_val == prop_value
             else:
                 if (isinstance(person_attr_val, list)
-                        and filter(lambda x: x in prop_value, person_attr_val)
+                        and list(filter(lambda x: x in prop_value, person_attr_val))
                     ) or person_attr_val in prop_value:
                     rospy.loginfo("Executing strict=False")
                     return True
