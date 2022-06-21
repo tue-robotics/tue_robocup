8892fbe7b370e6f323de196e71f179d131121398
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-06-21 17:52:36 +0200

    (smach_states) add comments to WaitForDoorOpen

--- robot_smach_states/src/robot_smach_states/startup.py
+++ robot_smach_states/src/robot_smach_states/startup.py
@@ -162,13 +162,14 @@ class WaitForDoorOpen(smach.State):
         if front_index < 2 or front_index > number_beams - 2:
             raise IndexError("Base laser can't see in front of the robot")
 
+        # Start with 5, center -/+ 2, beams. If these are all NaN, increase number of beams up to max 21
         for range_width in range(2, 10):
             if front_index < range_width or front_index > number_beams - range_width:
                 break
 
             ranges_at_center = scan_msg.ranges[front_index - range_width:front_index + range_width]  # Get some points around the middle
             rospy.logdebug(f"{ranges_at_center=}")
-            if not all(map(math.isnan, ranges_at_center)):
+            if not all(map(math.isnan, ranges_at_center)):  # Minimal one non-NaN number
                 break
 
         distance_to_door = self.avg(ranges_at_center)  # and the average of the middle range
