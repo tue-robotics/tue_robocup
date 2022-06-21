ee95a1c65a796d60a32b98f8cd166ae9b082864f
P-ict0 <r.martin.nunez@student.tue.nl>
2022-06-21 21:50:00 +0200

    fixed robot construction

--- robot_skills/examples/example_move_down_until_force_sensor_edge_up.py
+++ robot_skills/examples/example_move_down_until_force_sensor_edge_up.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import rospy
 
-from robot_skills.util.robot_constructor import robot_constructor
+from robot_skills.get_robot import get_robot
 
 rospy.init_node('test_mode_down_until_force_sensor_edge_up', anonymous=True)
-robot = robot_constructor("hero")
+robot = get_robot("hero")
 arm = robot.get_arm(force_sensor_required=True)
 
 arm.move_down_until_force_sensor_edge_up()
