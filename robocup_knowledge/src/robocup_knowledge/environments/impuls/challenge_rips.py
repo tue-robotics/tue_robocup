b76607865dc2275cf9ed9e952c2fcd02ae3695e0
Peter van Dooren <p.v.dooren@tue.nl>
2022-06-21 20:19:34 +0200

    add rips points

--- robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_rips.py
+++ robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_rips.py
@@ -1,8 +1,8 @@
 starting_point = "initial_pose"
 
-intermediate_1 = "registration_table1"
-intermediate_2 = "registration_table2"
-intermediate_3 = "registration_table3"
+intermediate_1 = "rips_point_1"
+intermediate_2 = "rips_point_2"
+intermediate_3 = "rips_point_3"
 
 exit_1 = "exit_1_rips"
 exit_2 = "exit_2_rips"
