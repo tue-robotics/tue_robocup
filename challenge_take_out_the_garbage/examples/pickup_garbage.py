289dccd33c3bc6d18f220f96749abaf28cb4e9ee
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-07-01 15:25:29 +0200

    (garbage) cleanup example

--- challenge_take_out_the_garbage/examples/pickup_garbage.py
+++ challenge_take_out_the_garbage/examples/pickup_garbage.py
@@ -4,7 +4,6 @@
 import argparse
 
 # ROS
-import PyKDL as kdl
 import rospy
 
 # TU/e Robotics
@@ -14,13 +13,12 @@ from robot_skills.get_robot import get_robot
 
 # Robot Smach States
 import robot_smach_states.util.designators as ds
-from robot_smach_states.manipulation import Grab
 
 # Take out the garbage
 from challenge_take_out_the_garbage.pick_up import PickUpTrash
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pick up trash from the trashbin")
     parser.add_argument("trashbin_id", default="trashbin", help="Id of the trashbin to be grasped from.")
     parser.add_argument("--robot", default="hero", help="Robot name (amigo, hero, sergio)")
@@ -35,9 +33,13 @@ if __name__ == "__main__":
 
     arm = ds.UnoccupiedArmDesignator(
         robot,
-        {"required_goals": ["handover", "reset", "handover_to_human"], "force_sensor_required": True,
-         "required_gripper_types": [arms.GripperTypes.GRASPING]},
-        name="empty_arm_designator").lockable()
+        {
+            "required_goals": ["handover", "reset", "handover_to_human"],
+            "force_sensor_required": True,
+            "required_gripper_types": [arms.GripperTypes.GRASPING],
+        },
+        name="empty_arm_designator",
+    ).lockable()
     arm.lock()
 
     grab_state = PickUpTrash(robot, trashbin_designator, arm)
