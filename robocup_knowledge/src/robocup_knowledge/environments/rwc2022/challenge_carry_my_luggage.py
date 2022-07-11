da39ce4392c08e8b1baeca69647b9b4e0fb41ea3
Lars <l.g.l.janssen@student.tue.nl>
2022-07-11 23:41:27 +0700

    MVP(carry_my_luggage): untested, without fallbacks

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_carry_my_luggage.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_carry_my_luggage.py
@@ -1,4 +1,5 @@
 from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
-starting_point = "carry_my_luggage_starting_point"
+starting_point = "initial_pose"
+# carry_my_luggage_starting_point
