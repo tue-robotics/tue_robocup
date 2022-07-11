58df3a1a22fd18a42bd013a81c38dcef8aa2197a
Rein Appeldoorn <reinzor@gmail.com>
2022-07-11 11:04:04 +0700

    wip for serve_breakfast

---
+++ challenge_serve_breakfast/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2022, TU/e Robotics, Netherlands
+# All rights reserved.
+#
+# \author Rein Appeldoorn
+
+from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
+
+d = generate_distutils_setup(
+    packages=['challenge_serve_breakfast'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
