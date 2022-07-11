9e4b312635038987373f4634d3767dccb00273e7
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-07-11 21:06:53 +0700

    [hand_me_that] get it starting

--- challenge_hand_me_that/src/challenge_hand_me_that/hand_me_that.py
+++ challenge_hand_me_that/src/challenge_hand_me_that/hand_me_that.py
@@ -4,7 +4,7 @@
 #
 # \author Rein Appeldoorn
 
-from smach import StateMachine
+import smach
 
 from ed.entity import Entity
 
@@ -24,47 +24,47 @@ STARTING_POINT = challenge_knowledge.starting_point  # Location where the challe
 HOME_LOCATION = challenge_knowledge.home_location  # Location where the robot will go and look at the operator
 
 
-class HandMeThat(StateMachine):
-    """ Main statemachine for the restaurant challenge """
+class HandMeThat(smach.StateMachine):
+    """ Main StateMachine for the challenge """
 
     def __init__(self, robot):
-        state_machine = StateMachine(outcomes=['done'])
+        smach.StateMachine.__init__(self, outcomes=['done'])
 
         furniture_designator = ds.VariableDesignator(resolve_type=Entity)
         entity_designator = ds.VariableDesignator(resolve_type=Entity)
         arm_designator = ds.UnoccupiedArmDesignator(robot).lockable()
 
-        with state_machine:
+        with self:
             # Intro
-            StateMachine.add('START_CHALLENGE_ROBUST', StartChallengeRobust(robot, STARTING_POINT),
+            smach.StateMachine.add('START_CHALLENGE_ROBUST', StartChallengeRobust(robot, STARTING_POINT),
                              transitions={'Done': 'SAY_START',
                                           'Aborted': 'done',
                                           'Failed': 'SAY_START'})
 
             # Say we're gonna start
-            StateMachine.add('SAY_START', Say(robot, "Hand me that it is!", block=False),
+            smach.StateMachine.add('SAY_START', Say(robot, "Hand me that it is!", block=False),
                              transitions={'spoken': 'NAVIGATE_TO_START'})
 
             # Drive to the start location
-            StateMachine.add('NAVIGATE_TO_START',
+            smach.StateMachine.add('NAVIGATE_TO_START',
                              NavigateToWaypoint(robot, ds.EdEntityDesignator(robot, uuid=HOME_LOCATION)),
                              transitions={'arrived': 'GET_FURNITURE_FROM_OPERATOR_POSE',
                                           'unreachable': 'NAVIGATE_TO_START',  # ToDo: other fallback
                                           'goal_not_defined': 'done'})  # I'm not even going to fill this in
 
             # The pre-work
-            StateMachine.add('GET_FURNITURE_FROM_OPERATOR_POSE',
+            smach.StateMachine.add('GET_FURNITURE_FROM_OPERATOR_POSE',
                              GetFurnitureFromOperatorPose(robot, furniture_designator.writeable),
                              transitions={'done': 'INSPECT_FURNITURE'})
 
             # Go to the furniture object that was pointing to see what's there
-            StateMachine.add('INSPECT_FURNITURE',
+            smach.StateMachine.add('INSPECT_FURNITURE',
                              InspectFurniture(robot, furniture_designator, entity_designator.writeable),
                              transitions={"succeeded": "IDENTIFY_OBJECT",
                                           "failed": "NAVIGATE_TO_START"})  # If no entities, try again
 
             # Point at the object
-            StateMachine.add('IDENTIFY_OBJECT',
+            smach.StateMachine.add('IDENTIFY_OBJECT',
                              IdentifyObject(robot, entity_designator, arm_designator),
                              transitions={'done': 'NAVIGATE_TO_START',  # Just keep on going
                                           'failed': 'NAVIGATE_TO_START'})  # Just keep on going
