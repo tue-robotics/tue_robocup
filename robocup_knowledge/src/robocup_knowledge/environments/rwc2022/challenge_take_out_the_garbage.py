ffcf7c7fa072da2f641633aaff18a7d8b594ac34
P-ict0 <r.martin.nunez@student.tue.nl>
2022-07-11 23:33:11 +0700

    Added robocup knowledge garbage

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_take_out_the_garbage.py
@@ -0,0 +1,13 @@
+# TAKE OUT THE GARBAGE KNOWLEDGE FILE IMPULS
+# TU/e Robotics
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+starting_point = "initial_pose"
+trashbin_id = "trashbin1"
+trashbin_id2 = "trashbin"
+drop_zone_id = "take_out_garbage_drop_zone"
+helper_waypoint = "helper_take_out_garbage"
+end_waypoint = "initial_pose"
