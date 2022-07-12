dac019414b716b79799bdb589aa19ae81bd19053
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-07-12 22:07:17 +0700

    [knowledge] fix hand_me_that rwc2022

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_hand_me_that.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_hand_me_that.py
@@ -4,13 +4,13 @@ home_location = "hand_me_that_home_location"
 all_possible_furniture = [
     "couch_table",
     "side_table",
-    "pantry"
+    "pantry",
     "dinner_table",
     "sink",
     "small_shelf",
     "washing_machine",
     "sink",
-    "small_shelf"
+    "small_shelf",
     "desk",
     "show_rack",
 ]
