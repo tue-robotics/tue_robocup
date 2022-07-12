988a13c91a054cc9b82ba7d313a0648670a1bcde
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-07-12 15:31:26 +0700

    [knowledge] update hand_me_that furniture

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_hand_me_that.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_hand_me_that.py
@@ -1,14 +1,16 @@
-starting_point = "hand_me_that_home_location"  # "initial_pose"
+starting_point = "initial_pose"
 home_location = "hand_me_that_home_location"
 
-all_possible_furniture = ['kitchen_cabinet',
-                          'kitchen_table',
-                          'island',
-                          'sink',
-                          'dishwasher',
-                          'desk',
-                          'salon_table',
-                          # 'couch',
-                          # 'armchair',
-                          'display_cabinet',
-                          'sideboard']
+all_possible_furniture = [
+    "couch_table",
+    "side_table",
+    "pantry"
+    "dinner_table",
+    "sink",
+    "small_shelf",
+    "washing_machine",
+    "sink",
+    "small_shelf"
+    "desk",
+    "show_rack",
+]
