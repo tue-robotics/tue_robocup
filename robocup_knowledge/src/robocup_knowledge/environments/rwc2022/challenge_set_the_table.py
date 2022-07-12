5c2e18c66d8c0a9852bf95f6e1853b7316a809b5
P-ict0 <r.martin.nunez@student.tue.nl>
2022-07-12 16:46:59 +0700

    Initial adaptation to RWC2022

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_set_the_table.py
@@ -0,0 +1,11 @@
+# SET THE TABLE KNOWLEDGE FILE RWC2022
+# TU/e Robotics
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+starting_point = "initial_pose"
+cupboard_id = "pantry"
+dinner_table_id = "dinner_table"
+end_waypoint = "exit_2_rips"
