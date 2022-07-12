8e6f7c2b9b372383c61d7766eac306808fbd9fa6
Arpit Aggarwal <ar13pit@gmail.com>
2022-07-12 14:23:44 +0700

    feat(challenge_carry_my_luggage): Update starting point

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_carry_my_luggage.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_carry_my_luggage.py
@@ -1,5 +1,4 @@
 from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
-starting_point = "initial_pose"
-# carry_my_luggage_starting_point
+starting_point = "carry_my_luggage_starting_point"
