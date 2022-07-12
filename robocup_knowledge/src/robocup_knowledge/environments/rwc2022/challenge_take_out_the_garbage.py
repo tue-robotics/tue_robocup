3cf83d8f735b7e1b0271c4977511f4518a4668e3
P-ict0 <r.martin.nunez@student.tue.nl>
2022-07-12 17:26:35 +0700

    Robot starts inside arena

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_take_out_the_garbage.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_take_out_the_garbage.py
@@ -1,11 +1,11 @@
-# TAKE OUT THE GARBAGE KNOWLEDGE FILE IMPULS
+# TAKE OUT THE GARBAGE KNOWLEDGE FILE RWC2022
 # TU/e Robotics
 from robocup_knowledge import knowledge_loader
 
 # Common knowledge
 common = knowledge_loader.load_knowledge("common")
 
-starting_point = "initial_pose"
+starting_point = "initial_pose_take_out_the_garbage"
 trashbin_id = "bin"
 trashbin_id2 = "kitchen_bin"
 drop_zone_id = "take_out_garbage_drop_zone"
