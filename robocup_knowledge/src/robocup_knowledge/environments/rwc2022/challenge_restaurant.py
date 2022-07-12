5e6c4e5b1d69d3409f78070e1d45635c4c56f7de
Rein Appeldoorn <reinzor@gmail.com>
2022-07-12 12:54:33 +0700

    reduce

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_restaurant.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_restaurant.py
@@ -5,18 +5,18 @@ from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
 order_grammar = """
-O[P] -> ORDER[P] | can i have a ORDER[P] | i would like ORDER[P] | can i get ORDER[P] | could i have ORDER[P] | may i get ORDER[P] | bring me ORDER[P]
+O[P] -> ORDER[P]
 ORDER[OO] -> ITEM1[OO] | ITEM2[OO] | ITEM3[OO]
 ITEM1[{"item1": F1}] -> ITEM[F1]
-ITEM2[{"item1": F1, "item2": F2}] -> ITEM[F1] ITEM[F2] | ITEM[F1] and ITEM[F2]
-ITEM3[{"item1": F1, "item2": F2, "item3": F3}] -> ITEM[F1] ITEM[F2] and ITEM[F3] | ITEM[F1] ITEM[F2] ITEM[F3] | ITEM[F1] and ITEM[F2] and ITEM[F3]
+ITEM2[{"item1": F1, "item2": F2}] -> ITEM[F1] and ITEM[F2]
+ITEM3[{"item1": F1, "item2": F2, "item3": F3}] -> ITEM[F1] ITEM[F2] and ITEM[F3]
 """
 
 # Create grammar
 for d in common.objects:
     if d["category"] in ["drink", "fruit", "snack"] or d["name"] == "corn_flakes":
         name = d["name"]
-        order_grammar += f"\nITEM['{name}'] -> {name} | a {name} | an {name}"
+        order_grammar += f"\nITEM['{name}'] -> {name}"
 
 if __name__ == "__main__":
     import rospy
