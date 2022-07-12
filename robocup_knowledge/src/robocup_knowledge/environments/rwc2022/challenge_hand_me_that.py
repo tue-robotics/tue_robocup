9539082e96f1589f57a30ade12cb7e435936516c
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-07-12 10:05:52 +0700

    (hand_me_that) furniture configurable

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_hand_me_that.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_hand_me_that.py
@@ -1,2 +1,14 @@
 starting_point = "initial_pose"
 home_location = "hand_me_that_home_location"
+
+all_possible_furniture = ['kitchen_cabinet',
+                          'kitchen_table',
+                          'island',
+                          'sink',
+                          'dishwasher',
+                          'desk',
+                          'coffee_table',
+                          # 'couch',
+                          # 'armchair',
+                          'display_cabinet',
+                          'sideboard']
