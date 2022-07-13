8fffd4a27e6ed09dbcf3e4f10ff30608895fdc57
Rein Appeldoorn <reinzor@gmail.com>
2022-07-13 23:32:25 +0700

    images

--- challenge_clean_the_table/src/challenge_clean_the_table/knowledge.py
+++ challenge_clean_the_table/src/challenge_clean_the_table/knowledge.py
@@ -3,11 +3,12 @@ from std_msgs.msg import ColorRGBA
 
 ITEMS_PLATE = ["plate"]
 ITEMS_MUG_BOWL = ["mug", "bowl"]
-ITEMS_CUTLERY = ["knife", "spoon"]
+ITEMS_CUTLERY = ["knife", "spoon", "fork"]
 ITEMS = ITEMS_CUTLERY + ITEMS_MUG_BOWL + ITEMS_PLATE
 
 ITEM_VECTOR_DICT = {
     "knife": PyKDL.Vector(-0.63, -0.325, 0),
+    "fork": PyKDL.Vector(-0.63, -0.325, 0),
     "spoon": PyKDL.Vector(-0.63, -0.325, 0),
     "mug": PyKDL.Vector(-0.46, -0.325, 0),
     "plate": PyKDL.Vector(-0.46, -0.155, 0),
@@ -16,6 +17,7 @@ ITEM_VECTOR_DICT = {
 
 ITEM_COLOR_DICT = {
     "knife": ColorRGBA(1, 0, 1, 1),
+    "fork": ColorRGBA(1, 0, 1, 1),
     "spoon": ColorRGBA(0, 1, 1, 1),
     "mug": ColorRGBA(0, 0, 1, 1),
     "plate": ColorRGBA(1, 1, 0, 1),
@@ -24,6 +26,7 @@ ITEM_COLOR_DICT = {
 
 ITEM_IMG_DICT = {
     "knife": "images/knife.jpg",
+    "fork": "images/fork.jpg",
     "spoon": "images/spoon.jpg",
     "mug": "images/mug.jpg",
     "plate": "images/plate.jpg",
@@ -31,7 +34,8 @@ ITEM_IMG_DICT = {
 }
 
 OPEN_DISHWASHER_VECTOR = PyKDL.Vector(-1.00, -0.16, 0)
-OPEN_DISHWASHER_VECTOR_OPEN = PyKDL.Vector(OPEN_DISHWASHER_VECTOR.x() - 0.45, OPEN_DISHWASHER_VECTOR.y(), OPEN_DISHWASHER_VECTOR.z())
+OPEN_DISHWASHER_VECTOR_OPEN = PyKDL.Vector(OPEN_DISHWASHER_VECTOR.x() - 0.45, OPEN_DISHWASHER_VECTOR.y(),
+                                           OPEN_DISHWASHER_VECTOR.z())
 
 PICK_ID = "table_salon"
 PICK_AREA_ID = "in_front_of"
