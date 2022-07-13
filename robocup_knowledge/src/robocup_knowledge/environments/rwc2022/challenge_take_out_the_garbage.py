0f983405225ffd80c3b93a623b9d76e52119e374
P-ict0 <r.martin.nunez@student.tue.nl>
2022-07-13 13:32:12 +0700

    Added comments and added initial helper waypoint

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_take_out_the_garbage.py
@@ -0,0 +1,13 @@
+# TAKE OUT THE GARBAGE KNOWLEDGE FILE RWC2022
+# TU/e Robotics
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+starting_point = "initial_pose_take_out_the_garbage"
+trashbin_id = "bin"
+trashbin_id2 = "kitchen_bin"
+drop_zone_id = "take_out_garbage_drop_zone"
+end_waypoint = "initial_pose"
+helper_waypoint = "helper_waypoint_take_out_the_garbage"
