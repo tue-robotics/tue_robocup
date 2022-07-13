5ddd9244e467722874bbfa2f426724636add98ea
P-ict0 <r.martin.nunez@student.tue.nl>
2022-07-13 23:00:22 +0700

    Started implementing Smach state for picking up cup

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_set_the_table.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_set_the_table.py
@@ -6,6 +6,6 @@ from robocup_knowledge import knowledge_loader
 common = knowledge_loader.load_knowledge("common")
 
 starting_point = "initial_pose"
-cupboard_id = "pantry"
+cupboard_id = "cupboard"
 dinner_table_id = "dinner_table"
 end_waypoint = "exit_2_rips"
