184d3b870ddb1d220659e6cefd4bbe1b4c48ef6f
Arpit Aggarwal <ar13pit@gmail.com>
2022-07-13 17:12:34 +0700

    format and update arrived states to SAY_ARRIVED

--- challenge_where_is_this/src/challenge_where_is_this/where_is_this.py
+++ challenge_where_is_this/src/challenge_where_is_this/where_is_this.py
@@ -2,7 +2,7 @@
 import math
 
 # ROS
-from pykdl_ros import FrameStamped
+import rospy
 import smach
 
 # TU/e Robotics
@@ -35,94 +35,100 @@ class WhereIsThis(smach.StateMachine):
         information_point_id_designator = ds.AttrDesignator(hmi_result_des, "semantics", resolve_type=str)
         information_point_designator = ds.EdEntityDesignator(robot, uuid_designator=information_point_id_designator)
 
+        rospy.loginfo("Starting WhereIsThis StateMachine")
+
         with self:
-            single_item = InformMachine(robot)
 
             if START_ROBUST:
-                smach.StateMachine.add("START_CHALLENGE",
-                                       StartChallengeRobust(robot, INITIAL_POSE_ID),
-                                       transitions={"Done": "NAV_TO_START",
-                                                    "Aborted": "Aborted",
-                                                    "Failed": "Aborted"})
+                smach.StateMachine.add(
+                    "START_CHALLENGE",
+                    StartChallengeRobust(robot, INITIAL_POSE_ID),
+                    transitions={"Done": "NAV_TO_START", "Aborted": "Aborted", "Failed": "Aborted"},
+                )
 
-                smach.StateMachine.add('NAV_TO_START',
-                                       NavigateToWaypoint(robot, ds.EntityByIdDesignator(
-                                           robot, uuid=INFORMATION_POINT_ID
-                                       ),
-                                                          radius=0.375),
-                                       transitions={"arrived": "TURN_AROUND",
-                                                    "unreachable": "WAIT_NAV_BACKUP",
-                                                    "goal_not_defined": "Aborted"})
+                smach.StateMachine.add(
+                    "NAV_TO_START",
+                    NavigateToWaypoint(robot, ds.EntityByIdDesignator(robot, uuid=INFORMATION_POINT_ID), radius=0.375),
+                    transitions={
+                        "arrived": "TURN_AROUND",
+                        "unreachable": "WAIT_NAV_BACKUP",
+                        "goal_not_defined": "Aborted",
+                    },
+                )
 
-                smach.StateMachine.add("WAIT_NAV_BACKUP",
-                                       WaitTime(robot, 3.0),
-                                       transitions={"waited": "NAV_TO_START_BACKUP",
-                                                    "preempted": "Aborted"})
+                smach.StateMachine.add(
+                    "WAIT_NAV_BACKUP",
+                    WaitTime(robot, 3.0),
+                    transitions={"waited": "NAV_TO_START_BACKUP", "preempted": "Aborted"},
+                )
 
-                smach.StateMachine.add("NAV_TO_START_BACKUP",
-                                       NavigateToSymbolic(
-                                           robot=robot,
-                                           entity_designator_area_name_map={information_point_designator: "near"},
-                                           entity_lookat_designator=information_point_designator
-                                       ),
-                                       transitions={"arrived": "TURN_AROUND",
-                                                    "unreachable": "SAY_CANNOT_REACH_WAYPOINT",  # Current pose backup
-                                                    "goal_not_defined": "Aborted"})  # If this happens: never mind
+                smach.StateMachine.add(
+                    "NAV_TO_START_BACKUP",
+                    NavigateToSymbolic(
+                        robot=robot,
+                        entity_designator_area_name_map={information_point_designator: "near"},
+                        entity_lookat_designator=information_point_designator,
+                    ),
+                    transitions={
+                        "arrived": "TURN_AROUND",
+                        "unreachable": "SAY_CANNOT_REACH_WAYPOINT",  # Current pose backup
+                        "goal_not_defined": "Aborted",
+                    },
+                )  # If this happens: never mind
 
                 @smach.cb_interface(outcomes=["done"])
                 def _turn_around(userdata=None):
-                    """ Turns the robot approximately 180 degrees around """
+                    """Turns the robot approximately 180 degrees around"""
                     v_th = 0.5
                     robot.base.force_drive(vx=0.0, vy=0.0, vth=v_th, timeout=math.pi / v_th)
                     return "done"
 
-                smach.StateMachine.add("TURN_AROUND",
-                                       smach.CBState(_turn_around),
-                                       transitions={"done": "STORE_STARTING_POSE"})
+                smach.StateMachine.add(
+                    "TURN_AROUND", smach.CBState(_turn_around), transitions={"done": "STORE_STARTING_POSE"}
+                )
 
-                smach.StateMachine.add("SAY_CANNOT_REACH_WAYPOINT",
-                                       Say(robot, "I am not able to reach the starting point."
-                                                         "I'll use this as starting point"),
-                                       transitions={"spoken": "STORE_STARTING_POSE"})
+                smach.StateMachine.add(
+                    "SAY_CANNOT_REACH_WAYPOINT",
+                    Say(robot, "I am not able to reach the starting point." "I'll use this as starting point"),
+                    transitions={"spoken": "STORE_STARTING_POSE"},
+                )
             else:
-                smach.StateMachine.add("INITIALIZE",
-                                       Initialize(robot),
-                                       transitions={"initialized": "STORE_STARTING_POSE",
-                                                    "abort": "Aborted"})
+                smach.StateMachine.add(
+                    "INITIALIZE",
+                    Initialize(robot),
+                    transitions={"initialized": "STORE_STARTING_POSE", "abort": "Aborted"},
+                )
 
             # This is purely for a back-up scenario until the range iterator
             @smach.cb_interface(outcomes=["succeeded"])
             def store_pose(userdata=None):
                 base_loc = robot.base.get_location()
                 location_id = INFORMATION_POINT_ID
-                robot.ed.update_entity(uuid=location_id,
-                                       frame_stamped=base_loc,
-                                       etype="waypoint")
+                robot.ed.update_entity(uuid=location_id, frame_stamped=base_loc, etype="waypoint")
 
                 return "succeeded"
 
-            smach.StateMachine.add("STORE_STARTING_POSE",
-                                   smach.CBState(store_pose),
-                                   transitions={"succeeded": "RANGE_ITERATOR"})
+            smach.StateMachine.add(
+                "STORE_STARTING_POSE", smach.CBState(store_pose), transitions={"succeeded": "RANGE_ITERATOR"}
+            )
 
             # Begin setup iterator
             # The exhausted argument should be set to the preferred state machine outcome
-            range_iterator = smach.Iterator(outcomes=["succeeded", "failed"],  # Outcomes of the iterator state
-                                            input_keys=[], output_keys=[],
-                                            it=lambda: range(1000),
-                                            it_label="index",
-                                            exhausted_outcome="succeeded")
+            range_iterator = smach.Iterator(
+                outcomes=["succeeded", "failed"],  # Outcomes of the iterator state
+                input_keys=[],
+                output_keys=[],
+                it=lambda: range(1000),
+                it_label="index",
+                exhausted_outcome="succeeded",
+            )
+
+            single_item = InformMachine(robot)
 
             with range_iterator:
-                smach.Iterator.set_contained_state("SINGLE_ITEM",
-                                                   single_item,
-                                                   loop_outcomes=["succeeded", "failed"])
+                smach.Iterator.set_contained_state("SINGLE_ITEM", single_item, loop_outcomes=["succeeded", "failed"])
 
-            smach.StateMachine.add("RANGE_ITERATOR", range_iterator,
-                                   {"succeeded": "AT_END",
-                                    "failed": "Aborted"})
+            smach.StateMachine.add("RANGE_ITERATOR", range_iterator, {"succeeded": "AT_END", "failed": "Aborted"})
             # End setup iterator
 
-            smach.StateMachine.add("AT_END",
-                                   Say(robot, "Goodbye"),
-                                   transitions={"spoken": "Done"})
+            smach.StateMachine.add("AT_END", Say(robot, "Goodbye"), transitions={"spoken": "Done"})
