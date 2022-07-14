a9cd290c55f3b4e2f9ace90f4b99a53e4d75216c
Lars <l.g.l.janssen@student.tue.nl>
2022-07-14 08:40:53 +0700

    feat(carry_my_luggage): Add new waypoint

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_carry_my_luggage.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2022/challenge_carry_my_luggage.py
@@ -4,3 +4,4 @@ common = knowledge_loader.load_knowledge("common")
 starting_point = "carry_my_luggage_starting_point"
 actual_starting_point = "carry_my_luggage_actual_starting_point"
 entrance_door = "carry_my_luggage_entrance_door"
+before_entrance_door = "carry_my_luggage_before_entrance_door"
