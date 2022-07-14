831ed26f67815ffcbc85e9f8d70611523e3f919c
Arpit Aggarwal <ar13pit@gmail.com>
2022-07-14 11:31:47 +0700

    fix: Wiggle Wiggle

--- robot_smach_states/src/robot_smach_states/navigation/navigate_wiggle.py
+++ robot_smach_states/src/robot_smach_states/navigation/navigate_wiggle.py
@@ -9,28 +9,15 @@ class NavigateWiggle(smach.State):
 
     def execute(self, userdata=None):
         start = None
-        last_say = None
 
         turn = -1
 
-        current_seconds = rospy.Time.now().to_sec()
-
-        radians = 0.15
+        start = rospy.Time.now()
         vth = 0.5
-        if start is None:
-            self.robot.base.force_drive(0, 0, vth, radians / vth)
-            start = current_seconds
-
-        dt = current_seconds - start
-
-        if dt > self.duration:
-            return "done"
-
-        if last_say is None or current_seconds - last_say > 10:
-            self.robot.speech.speak("Trying for another %d seconds .. wiggle wiggle" % int(self.duration - dt), block=False)
-            last_say = current_seconds
-
-        self.robot.base.force_drive(0, 0, turn * vth, (2 * radians) / vth)
-        turn = -turn
+        radians = 0.15
+        while not rospy.is_shutdown() and (rospy.Time.now() - start).to_sec() < self.duration:
+            self.robot.speech.speak("Wiggle wiggle", block=False)
+            self.robot.base.force_drive(0, 0, turn * vth, (2 * radians) / vth)
+            turn = -turn
 
         return "done"
