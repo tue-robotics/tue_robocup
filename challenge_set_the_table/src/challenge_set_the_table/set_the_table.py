b8ac960b575a9bc6d683e5cba53bfb970acebcd1
Rein Appeldoorn <reinzor@gmail.com>
2022-07-14 08:35:31 +0700

    revert to adaptation for rwc2022

--- challenge_set_the_table/src/challenge_set_the_table/set_the_table.py
+++ challenge_set_the_table/src/challenge_set_the_table/set_the_table.py
@@ -7,17 +7,16 @@ import rospy
 
 from challenge_set_the_table.navigate_to_and_close_cupboard_drawer import NavigateToAndCloseCupboard
 from challenge_set_the_table.navigate_to_and_open_cupboard_drawer import NavigateToAndOpenCupboard
-from challenge_set_the_table.navigate_to_and_pick_item import NavigateToAndPickItem
+from challenge_set_the_table.navigate_to_and_pick_item_from_cupboard_drawer import \
+    NavigateToAndPickItemFromCupboardDrawer
 from challenge_set_the_table.navigate_to_and_place_item_on_table import NavigateToAndPlaceItemOnTable
 from robot_smach_states.human_interaction import Say
-from robot_smach_states.navigation import NavigateToWaypoint
-import robot_smach_states.util.designators as ds
 from robot_smach_states.startup import StartChallengeRobust
+from robot_smach_states.utility import WaitTime
 from smach import StateMachine, cb_interface, CBState
 from robocup_knowledge import load_knowledge
-from robot_smach_states.utility import WaitTime
-
 CHALLENGE_KNOWLEDGE = load_knowledge('challenge_set_the_table')
+
 
 items_picked = []
 required_items = ["plate", "napkin", "knife", "fork", "bowl"]
@@ -43,9 +42,9 @@ def check_if_we_have_it_all(user_data, robot):
 
     missing_items = [item for item in required_items if item not in items_picked]
 
-    robot.speech.speak("So far we have: {}".format(", ".join(items_picked)), block=False)
+    robot.speech.speak("So far we have: {}".format(" ".join(items_picked)), block=False)
     if missing_items:
-        robot.speech.speak("Still missing the {}".format(", ".join(missing_items)), block=False)
+        robot.speech.speak("Still missing the {}".format(missing_items), block=False)
 
     return 'keep_going' if missing_items else 'we_have_it_all'
 
@@ -56,6 +55,9 @@ def setup_statemachine(robot):
 
     with state_machine:
         # Intro
+        # StateMachine.add('START_CHALLENGE_ROBUST',
+        #                  Initialize(robot),
+        #                  transitions={'initialized': 'SAY_START', 'abort': 'done'})
 
         StateMachine.add('START_CHALLENGE_ROBUST', StartChallengeRobust(robot, CHALLENGE_KNOWLEDGE.starting_point),
                          transitions={'Done': 'SAY_START',
@@ -63,19 +65,39 @@ def setup_statemachine(robot):
                                       'Failed': 'SAY_START'})
 
         StateMachine.add('SAY_START',
-                         Say(robot, "Let's set the table baby!", block=False),
-                         transitions={'spoken': 'NAVIGATE_AND_PICK_ITEM'})
+                         Say(robot,
+                             "Let's set the table baby! If there are any chairs near the kitchen_table, please remove them",
+                             block=False),
+                         transitions={'spoken': 'NAVIGATE_AND_OPEN_CUPBOARD'})
+
+        # The pre-work
+        
+        StateMachine.add('NAVIGATE_AND_OPEN_CUPBOARD',
+                         NavigateToAndOpenCupboard(robot, CHALLENGE_KNOWLEDGE.cupboard_id, "in_front_of"),
+                         transitions={'succeeded': 'NAVIGATE_AND_PICK_ITEM_FROM_CUPBOARD_DRAWER',
+                                      'failed': 'SAY_OPEN_FAILED'})
+
+        StateMachine.add('SAY_OPEN_FAILED',
+                         Say(robot, "I failed to open the cupboard drawer"),
+                         transitions={'spoken': 'WAIT_OPEN'})
+
+        StateMachine.add('WAIT_OPEN',
+                         WaitTime(robot, 5),
+                         transitions={'waited': 'SAY_OPEN_THANKS', 'preempted': 'done'})
+
+        StateMachine.add('SAY_OPEN_THANKS',
+                         Say(robot, "Thank you darling"),
+                         transitions={'spoken': 'NAVIGATE_AND_PICK_ITEM_FROM_CUPBOARD_DRAWER'})
 
         # The loop
-        StateMachine.add('NAVIGATE_AND_PICK_ITEM',
-                         NavigateToAndPickItem(robot, CHALLENGE_KNOWLEDGE.dinner_table_id,
-                                               required_items),
+        StateMachine.add('NAVIGATE_AND_PICK_ITEM_FROM_CUPBOARD_DRAWER',
+                         NavigateToAndPickItemFromCupboardDrawer(robot, CHALLENGE_KNOWLEDGE.cupboard_id, "in_front_of",
+                                                                 required_items),
                          transitions={'succeeded': 'PLACE_ITEM_ON_TABLE',
                                       'failed': 'CHECK_IF_WE_HAVE_IT_ALL'})
 
         StateMachine.add('PLACE_ITEM_ON_TABLE',
-                         NavigateToAndPlaceItemOnTable(robot, CHALLENGE_KNOWLEDGE.dinner_table_id, "in_front_of",
-                                                       "in_front_of"),
+                         NavigateToAndPlaceItemOnTable(robot, CHALLENGE_KNOWLEDGE.dinner_table_id, "right_of", "in_front_of"),
                          transitions={'succeeded': 'CHECK_IF_WE_HAVE_IT_ALL',
                                       'failed': 'WAIT'})
 
@@ -86,7 +108,7 @@ def setup_statemachine(robot):
         StateMachine.add('CHECK_IF_WE_HAVE_IT_ALL',
                          CBState(check_if_we_have_it_all, cb_args=[robot]),
                          transitions={'we_have_it_all': 'SAY_END_CHALLENGE',
-                                      'keep_going': 'NAVIGATE_AND_PICK_ITEM'})
+                                      'keep_going': 'NAVIGATE_AND_PICK_ITEM_FROM_CUPBOARD_DRAWER'})
 
         # Outro
         StateMachine.add('SAY_END_CHALLENGE',
