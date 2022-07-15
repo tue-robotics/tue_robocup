1264471e47ba1d1d770b1271aea8a104ea1c7b9a
Arpit Aggarwal <ar13pit@gmail.com>
2022-07-16 00:22:40 +0700

    feat: Add working version of Telegram send and receive

--- challenge_final/src/challenge_final/call_neighbor.py
+++ challenge_final/src/challenge_final/call_neighbor.py
@@ -2,24 +2,25 @@ import os
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from smach.state import CBState
+from std_msgs.msg import Header, String
+from sensor_msgs.msg import Image
 from smach.state_machine import StateMachine
+from smach.state import State, CBState
 from smach.util import cb_interface
-from std_msgs.msg import Header
 
 from robot_skills import get_robot
 
 
 class GetCupboardRay(StateMachine):
-    def __init__(self, robot, timeout=30.):
-        StateMachine.__init__(self, outcomes=['done', 'failed'])
+    def __init__(self, robot, timeout=30.0):
+        StateMachine.__init__(self, outcomes=["done", "failed"])
 
         def _show_view():
             rgb, depth, depth_info = robot.perception.get_rgb_depth_caminfo()
-            robot.hmi.show_image_from_msg(rgb, 5.)
+            robot.hmi.show_image_from_msg(rgb, 5.0)
             return rgb, depth, depth_info
 
-        @cb_interface(outcomes=['done', 'failed'])
+        @cb_interface(outcomes=["done", "failed"])
         def _get_cupboard_ray(_):
             def _is_operator(person):
                 if "is_pointing" not in person.tags:
@@ -35,13 +36,16 @@ class GetCupboardRay(StateMachine):
                     persons = sorted(persons, key=lambda x: x.position.z)
                     person = persons[0]
                     if _is_operator(person):
-                        map_pose = robot.tf_buffer.transform(PoseStamped(
-                            header=Header(
-                                frame_id=person.header.frame_id,
-                                stamp=rospy.Time.now() - rospy.Duration.from_sec(0.5)
+                        map_pose = robot.tf_buffer.transform(
+                            PoseStamped(
+                                header=Header(
+                                    frame_id=person.header.frame_id,
+                                    stamp=rospy.Time.now() - rospy.Duration.from_sec(0.5),
+                                ),
+                                pose=person.pointing_pose,
                             ),
-                            pose=person.pointing_pose
-                        ), "map")
+                            "map",
+                        )
                         rospy.logwarn(f"{map_pose=}")
                         try:
                             result = robot.ed.ray_trace(map_pose)
@@ -53,10 +57,64 @@ class GetCupboardRay(StateMachine):
                             pass
                 rospy.sleep(0.2)
 
-            return 'failed'
+            return "failed"
 
         with self:
-            self.add('GET_CUPBOARD_RAY', CBState(_get_cupboard_ray), transitions={'done': 'done', 'failed': 'failed'})
+            self.add("GET_CUPBOARD_RAY", CBState(_get_cupboard_ray), transitions={"done": "done", "failed": "failed"})
+
+
+class SendTelegram(State):
+    def __init__(self, robot, neighbor="Lotte", victim="Arpit", victim_pronoun="He") -> None:
+        super().__init__(outcomes=["done"])
+
+        self.robot = robot
+        self.neighbor = neighbor
+        self.victim = victim
+        self.victim_pronoun = victim_pronoun
+
+        self._telegram_message_pub = rospy.Publisher(
+            f"/{self.robot.robot_name}/message_from_ros", String, queue_size=10
+        )
+        self._telegram_image_pub = rospy.Publisher(f"/{self.robot.robot_name}/image_from_ros", Image, queue_size=10)
+
+        rospy.sleep(2)
+
+    def execute(self, userdata=None) -> str:
+        img = self.robot.perception.get_image()
+        img.header.frame_id = ""
+
+        self._telegram_message_pub.publish(f"Hi {self.neighbor}")
+        rospy.sleep(0.2)
+        self._telegram_message_pub.publish(f"{self.victim} needs your help. {self.victim_pronoun} is injured")
+        rospy.sleep(0.2)
+
+        self._telegram_image_pub.publish(img)
+        rospy.sleep(2)
+
+        self._telegram_message_pub.publish(f"Could you please get here ASAP?")
+        rospy.sleep(0.2)
+
+        return "done"
+
+
+class ReceiveTelegram(State):
+    def __init__(self, robot, neighbor="Lotte", timeout=20):
+        super().__init__(outcomes=["done", "timeout"])
+
+        self.robot = robot
+        self.neighbor = neighbor
+        self.timeout = timeout
+
+    def execute(self, ud=None):
+        try:
+            telegram_response = rospy.wait_for_message(
+                f"/{self.robot.robot_name}/message_to_ros", String, timeout=self.timeout
+            )
+        except:
+            return "timeout"
+
+        self.robot.speech.speak(f"{self.neighbor} said, {telegram_response.data}", block=True)
+        return "done"
 
 
 class CallNeighbor(StateMachine):
@@ -64,14 +122,15 @@ class CallNeighbor(StateMachine):
         StateMachine.__init__(self, outcomes=["done", "preempted"])
 
         with self:
-            @cb_interface(outcomes=['done'])
+
+            @cb_interface(outcomes=["done"])
             def _look_down_and_say(_):
                 robot.head.look_down()
                 robot.head.wait_for_motion_done()
-                robot.speech.speak('Tell me where the emergency kit is')
+                robot.speech.speak("Tell me where the emergency kit is")
                 return "done"
 
-            self.add('LOOK_DOWN_AND_SAY', CBState(_look_down_and_say), transitions={'done': 'GET_CUPBOARD_RAY'})
+            self.add("LOOK_DOWN_AND_SAY", CBState(_look_down_and_say), transitions={"done": "GET_CUPBOARD_RAY"})
             self.add("GET_CUPBOARD_RAY", GetCupboardRay(robot), transitions={"done": "done", "failed": "preempted"})
 
 
