3204ae06675f884334b74abdac92d81d224e0a79
Rein Appeldoorn <reinzor@gmail.com>
2022-07-15 20:24:37 +0700

    look for waving

--- challenge_final/src/challenge_final/navigate_arbitrarily.py
+++ challenge_final/src/challenge_final/navigate_arbitrarily.py
@@ -1,18 +1,37 @@
 import os
 
+import numpy as np
 import rospy
 from smach.state_machine import StateMachine
 
+from ed.entity import Entity
 from robot_skills import get_robot
-from robot_smach_states.human_interaction import Say
+from robot_smach_states.human_interaction import FindFirstPerson
+from robot_smach_states.util.designators import VariableDesignator
 
 
 class NavigateArbitrarily(StateMachine):
     def __init__(self, robot):
         StateMachine.__init__(self, outcomes=["done", "preempted"])
 
+        customer_designator = VariableDesignator(resolve_type=Entity, name="detected_victim")
+
         with self:
-            StateMachine.add("SAY", Say(robot, "NavigateArbitrarily"), transitions={"spoken": "done"})
+            StateMachine.add(
+                "WAIT_FOR_WAVING_PERSON",
+                FindFirstPerson(
+                    robot,
+                    customer_designator.writeable,
+                    properties={"tags": ["LWave", "RWave"]},
+                    strict=False,
+                    nearest=True,
+                    speak=True,
+                    look_range=(-np.pi / 4, np.pi / 4),
+                    look_steps=4,
+                    search_timeout=600,
+                ),  # 10 minutes
+                transitions={"found": "done", "failed": "preempted"},
+            )
 
 
 if __name__ == "__main__":
