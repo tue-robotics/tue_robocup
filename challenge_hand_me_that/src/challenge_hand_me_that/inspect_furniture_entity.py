1a4b53294adb8dcd9af00b2fc543e25fb8955787
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-07-15 11:46:00 +0700

    (hand_me_that) inspect/nav area logic

--- challenge_hand_me_that/src/challenge_hand_me_that/inspect_furniture_entity.py
+++ challenge_hand_me_that/src/challenge_hand_me_that/inspect_furniture_entity.py
@@ -10,10 +10,17 @@ import math
 # ROS
 import geometry_msgs.msg
 import rospy
+import tf2_ros
+from tf2_pykdl_ros import VectorStamped
+# noinspection PyUnresolvedReferences
+import tf2_pykdl_ros
+# noinspection PyUnresolvedReferences
+import tf2_geometry_msgs
+
 import smach
 
 # TU/e Robotics
-from ed.entity import Entity
+from ed.entity import Entity, Volume
 
 import robot_smach_states.util.designators as ds
 from robot_smach_states.human_interaction import Say
@@ -23,6 +30,8 @@ from robot_smach_states.utility import WriteDesignator
 from robot_skills import get_robot_from_argv
 from robot_skills.robot import Robot
 from robot_skills.classification_result import ClassificationResult
+
+from robocup_knowledge import load_knowledge
 
 # Items with x- or y-dimension larger than this value will be filtered out
 SIZE_LIMIT = 0.2
@@ -48,6 +57,61 @@ class InspectFurniture(smach.StateMachine):
 
         assert ds.is_writeable(entity_designator), "Entity designator must be writeable for this purpose"
         object_ids_des = ds.VariableDesignator([], resolve_type=[ClassificationResult])
+        inspect_area_des = ds.VariableDesignator(resolve_type=str)
+        nav_area_des = ds.VariableDesignator(resolve_type=str)
+
+        common_knowledge = load_knowledge('common')
+
+        @smach.cb_interface(outcomes=["done", "failed"], input_keys=["laser_dot"])
+        def _select_inspect_area(userdata, robot, furniture_des, inspect_area_des):
+            def _ray_volume_check(ray_vs: VectorStamped, vol: Volume):
+                if vol.min_corner.z() <= ray_vs.vector.z() <= vol.max_corner.z():
+                    return 0
+                else:
+                    return min(abs(ray_vs.vector.z()-vol.min_corner.z()), abs(ray_vs.vector.z()-vol.max_corner.z()))
+
+            entity_to_inspect = furniture_des.resolve()
+            if entity_to_inspect is None:
+                area_des.write("on_top_of")
+                return "failed"
+
+            laser_dot = tf2_ros.convert(user_data["laser_dot"], VectorStamped)
+            laser_dot.header.stamp = rospy.Time()
+            laser_dot_entity_frame = robot.tf_buffer.transform(laser_dot, entity_to_inspect.uuid)
+
+            best_volumes = []
+            for vol_name, vol in entity_to_inspect.volumes.items():
+                if vol_name not in common_knowledge.get_inspect_areas(entity_to_inspect.uuid):
+                    continue
+
+                best_volumes.append((vol_name, _ray_volume_check(laser_dot_entity_frame, vol)))
+
+            if not best_volumes:
+                rospy.loginfo("No volumes found from knowledge, using 'on_top_of'")
+                inspect_area_des.write("on_top_of")
+                return "failed"
+
+            best_volumes.sort(key=lambda tup: tup[1], reverse=False)
+            rospy.loginfo(f"Volumes: {best_volumes}")
+            best_vol = best_volumes[0]
+            rospy.loginfo(f"Using volume: {best_vol[0]}")
+
+            inspect_area_des.write(best_vol[0])
+            return "done"
+
+        @smach.cb_interface(outcomes=["done", "failed"])
+        def _select_nav_area(userdata, furniture_des, inspect_area_des, nav_area_des):
+            entity_to_inspect = furniture_des.resolve()
+            inspect_area = inspect_area_des.resolve()
+            if entity_to_inspect is None or not inspect_area:
+                rospy.loginfo("Using backup nav_area: 'in_front_of'")
+                nav_area_des.write("in_front_of")
+                return "failed"
+
+            nav_area = common_knowledge.get_inspect_position(entity_to_inspect.uuid, inspect_area)
+            rospy.loginfo(f"For {entity_to_inspect.uuid}:{inspect_area} selected '{nav_area}' to navigate")
+            nav_area_des.write(nav_area)
+            return "done"
 
         with self:
 
@@ -59,11 +123,25 @@ class InspectFurniture(smach.StateMachine):
 
             smach.StateMachine.add('CLEAR_FOUND_ENTITY_DESIGNATOR',
                                    WriteDesignator(object_ids_des.writeable, []),
-                                   transitions={'written': 'INSPECT_FURNITURE'})
+                                   transitions={'written': 'SELECT_INSPECT_AREA'})
+
+            smach.StateMachine.add("SELECT_INSPECT_AREA", smach.CBState(_select_inspect_area,
+                                                                        cb_args=[robot,
+                                                                                 furniture_designator,
+                                                                                 inspect_area_des.writeable]),
+                                   transitions={"done": "SELECT_NAV_AREA",
+                                                "failed": "SELECT_NAV_AREA"})
+
+            smach.StateMachine.add("SELECT_NAV_AREA", smach.CBState(_select_nav_area,
+                                                                        cb_args=[furniture_designator,
+                                                                                 inspect_area_des.writeable,
+                                                                                 nav_area_des.writeable]),
+                                   transitions={"done": "INSPECT_FURNITURE",
+                                                "failed": "INSPECT_FURNITURE"})
 
             smach.StateMachine.add("INSPECT_FURNITURE",
-                                   Inspect(robot=robot, entityDes=furniture_designator, objectIDsDes=object_ids_des,
-                                           navigation_area="in_front_of"),
+                                   Inspect(robot=robot, entityDes=furniture_designator, searchArea=inspect_area_des,
+                                           objectIDsDes=object_ids_des, navigation_area=nav_area_des),
                                    transitions={"done": "SELECT_ENTITY",
                                                 "failed": "SAY_INSPECTION_FAILED"})  # ToDo: fallback?
 
