1c1ec428a10eaa2f858d827949907b2adfa36792
lotte <l.messing@student.tue.nl>
2022-07-15 20:57:42 +0700

    Challenge_final: added navigate to door and welcome neighbor

--- challenge_final/src/challenge_final/navigate_to_the_door_and_guide_neighbor_to_victim.py
+++ challenge_final/src/challenge_final/navigate_to_the_door_and_guide_neighbor_to_victim.py
@@ -1,22 +1,56 @@
 import os
+import math
 
 import rospy
 from smach.state_machine import StateMachine
+import robot_smach_states.util.designators as ds
+
 
 from robot_skills import get_robot
 from robot_smach_states.human_interaction import Say
+from robot_smach_states.navigation.navigate_to_waypoint import NavigateToWaypoint
+from robot_smach_states.human_interaction.human_interaction import WaitForPersonInFront
+from robot_smach_states.navigation import ForceDrive
 
 
 class NavigateToTheDoorAndGuideNeighborToVictim(StateMachine):
     def __init__(self, robot):
         StateMachine.__init__(self, outcomes=["done", "preempted"])
 
+        waypoint_door = {'id': 'entry_door', 'radius': 0.5}
+        door_waypoint = ds.EntityByIdDesignator(robot, uuid=waypoint_door['id'])
+
         with self:
-            StateMachine.add(
-                "SAY",
-                Say(robot, "NavigateToTheDoorAndGuideNeighborToVictim"),
-                transitions={"spoken": "done"},
-            )
+            StateMachine.add('NAVIGATE_DOOR', NavigateToWaypoint(robot, door_waypoint, waypoint_door['radius']),
+                             transitions={'arrived': 'SAY_PLEASE_COME_IN',
+                                          'unreachable': 'SAY_NAVIGATE_TO_DOOR_FALLBACK',
+                                          'goal_not_defined': 'SAY_PLEASE_COME_IN'})
+
+            StateMachine.add('SAY_NAVIGATE_TO_DOOR_FALLBACK', Say(robot, "Help, lets try it another way"),
+                             transitions={'spoken': 'TURN_AROUND'})
+
+            StateMachine.add('TURN_AROUND', ForceDrive(robot, 0, 0, 0.5, (2 * math.pi) / 0.5),
+                             transitions={'done': 'NAVIGATE_TO_DOOR_FALLBACK'})
+
+            StateMachine.add('NAVIGATE_TO_DOOR_FALLBACK', NavigateToWaypoint(robot, door_waypoint, 0.55),
+                             transitions={'arrived': 'SAY_PLEASE_COME_IN',
+                                          'unreachable': 'SAY_NAVIGATE_TO_DOOR_FALLBACK',
+                                          'goal_not_defined': 'SAY_PLEASE_COME_IN'})
+
+            StateMachine.add('SAY_PLEASE_COME_IN',
+                             Say(robot, ["Please come in, I'm waiting for you to step in front of me"],
+                                 block=True, look_at_standing_person=True),
+                             transitions={'spoken': 'WAIT_FOR_GUEST'})
+
+            StateMachine.add("WAIT_FOR_GUEST", WaitForPersonInFront(robot, attempts=30, sleep_interval=1),
+                             transitions={'success': 'SAY_FOLLOW_ME',
+                                          'failed': 'SAY_PLEASE_COME_IN'})
+
+            StateMachine.add('SAY_FOLLOW_ME',
+                             Say(robot, ["Please follow me fast to Arpit"],
+                                 block=False, look_at_standing_person=True),
+                             transitions={'spoken': 'done'})
+
 
 
 if __name__ == "__main__":
