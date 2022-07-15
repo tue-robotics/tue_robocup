38ea233dcd643b10be3c979e1bed59db15e7cd3a
Rein Appeldoorn <reinzor@gmail.com>
2022-07-15 19:43:40 +0700

    scaffold challenge

---
+++ challenge_final/src/challenge_final/navigate_arbitrarily.py
@@ -0,0 +1,21 @@
+import os
+
+import rospy
+from smach.state_machine import StateMachine
+
+from robot_skills import get_robot
+from robot_smach_states.human_interaction import Say
+
+
+class NavigateArbitrarily(StateMachine):
+    def __init__(self, robot):
+        StateMachine.__init__(self, outcomes=["done", "preempted"])
+
+        with self:
+            StateMachine.add("SAY", Say(robot, "NavigateArbitrarily"), transitions={"spoken": "done"})
+
+
+if __name__ == "__main__":
+    rospy.init_node(os.path.splitext("test_" + os.path.basename(__file__))[0])
+    robot_instance = get_robot("hero")
+    NavigateArbitrarily(robot_instance).execute()
