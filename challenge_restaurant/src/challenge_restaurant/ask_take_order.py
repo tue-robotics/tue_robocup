28ff4d90e383298faa4de9778684ff0c893eb39d
Rein Appeldoorn <reinzor@gmail.com>
2022-07-15 08:49:06 +0700

    pico for order taking

--- challenge_restaurant/src/challenge_restaurant/ask_take_order.py
+++ challenge_restaurant/src/challenge_restaurant/ask_take_order.py
@@ -1,5 +1,31 @@
+import actionlib
+import rospy
+from picovoice_msgs.msg import GetIntentAction, GetIntentGoal
+
 import smach
-from hmi import TimeoutException
+from hmi import TimeoutException, HMIResult
+
+
+class GetIntent:
+    def __init__(self):
+        self._client = actionlib.SimpleActionClient("/get_intent", GetIntentAction)
+
+    def query(self):
+        if self._client.send_goal_and_wait(GetIntentGoal(
+            context_url='restaurant_take_the_order',
+            require_endpoint=True
+        ), preempt_timeout=rospy.Duration(10), execute_timeout=rospy.Duration(10)):
+            result = self._client.get_result()
+            if result is None:
+                rospy.logerr("Picovoice result None")
+                raise TimeoutException("Picovoice result None")
+            if not result.is_understood:
+                rospy.logwarn("Not understood")
+                raise TimeoutException("Not understood")
+            return HMIResult(sentence="", semantics={slot.key: slot.value for slot in result.slots})
+        else:
+            rospy.logerr("Picovoice failed")
+            raise TimeoutException("Picovoice failed")
 
 
 class AskTakeTheOrder(smach.State):
@@ -7,19 +33,17 @@ class AskTakeTheOrder(smach.State):
 
     def __init__(self, robot):
         smach.State.__init__(self, outcomes=['yes', 'wait', 'timeout'])
+        self._get_intent = GetIntent()
 
         self.robot = robot
 
     def execute(self, userdata=None):
-        cgrammar = """
-        C['yes'] -> {robot} take the order
-        C['wait'] -> {robot} wait
-        """.format(robot=self.robot.robot_name)
         for i in range(3):
             try:
-                speech_result = self.robot.hmi.query(description="Should I get the order?",
-                                                     grammar=cgrammar, target="C")
-                return speech_result.semantics
+                speech_result = self._get_intent.query()
+                if "take the order" in str(speech_result.semantics):
+                    return 'yes'
+                return 'wait'
             except TimeoutException:
                 pass
         return 'timeout'
