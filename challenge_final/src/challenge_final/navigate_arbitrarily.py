7386da12008c7cf0664ca0685ba8639882835b58
Rein Appeldoorn <reinzor@gmail.com>
2022-07-16 10:48:30 +0700

    remove first two

--- challenge_final/src/challenge_final/navigate_arbitrarily.py
+++ challenge_final/src/challenge_final/navigate_arbitrarily.py
@@ -57,12 +57,6 @@ class NavigateArbitrarily(StateMachine):
 
                 return "done"
 
-            self.add("NAVIGATE_LIVING_ROOM", NavigateToWaypoint(robot, self._living_room_waypoint_des, radius=0.25),
-                     transitions={"arrived": "LOOK_AROUND_LIVING_ROOM",
-                                  "unreachable": "LOOK_AROUND_LIVING_ROOM",
-                                  "goal_not_defined": "preempted"})
-
-            self.add("LOOK_AROUND_LIVING_ROOM", CBState(_look_around), transitions={"done": "NAVIGATE_OFFICE"})
             self.add("NAVIGATE_OFFICE", NavigateToWaypoint(robot, self._office_waypoint_des, radius=0.25),
                      transitions={"arrived": "LOOK_AROUND_OFFICE",
                                   "unreachable": "LOOK_AROUND_OFFICE",
