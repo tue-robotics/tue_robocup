c6499395a958020c57511cd3b0712521e20a2bb8
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-07-16 08:48:16 +0700

    [final] fix nav arbiii

--- challenge_final/src/challenge_final/navigate_arbitrarily.py
+++ challenge_final/src/challenge_final/navigate_arbitrarily.py
@@ -12,9 +12,12 @@ from std_msgs.msg import Header, ColorRGBA
 from tf_conversions import toMsg
 from visualization_msgs.msg import MarkerArray, Marker
 
-import robot_smach_states.util.designators as ds
 from robot_skills import get_robot
 from robot_smach_states.navigation import NavigateToWaypoint, ForceDrive
+
+from robot_smach_states.navigation import NavigateToSymbolic
+from robot_smach_states.util.designators import EntityByIdDesignator
+from robot_smach_states.world_model import Inspect
 
 
 class NavigateArbitrarily(StateMachine):
@@ -25,10 +28,38 @@ class NavigateArbitrarily(StateMachine):
         self._look_angles = np.linspace(look_range[0], look_range[1], look_steps)
         self._visualization_marker_pub = rospy.Publisher('/markers', MarkerArray, queue_size=1)
 
-        waypoint = ds.EntityByIdDesignator(robot, uuid="hand_me_that_kitchen")
-        victim = ds.EntityByIdDesignator(robot, uuid="victim")
+
+        self.waypoint = EntityByIdDesignator(robot, uuid="hand_me_that_kitchen")
+        self.victim = EntityByIdDesignator(robot, uuid="victim")
+        self._cupboard_des = EntityByIdDesignator(robot, uuid="cupboard")
+        self._bedroom_des = EntityByIdDesignator(robot, uuid="bedroom")
+        self._kitchen_des = EntityByIdDesignator(robot, uuid="kitchen")
+        self._dinner_table_des = EntityByIdDesignator(robot, uuid="dinner_table")
 
         with self:
+
+            @cb_interface(outcomes=["done"])
+            def _look_around(_):
+                start_time = rospy.Time.now()
+                head_goals = [VectorStamped.from_xyz(x=self._look_distance * math.cos(angle),
+                                                     y=self._look_distance * math.sin(angle),
+                                                     z=1.0,
+                                                     stamp=start_time,
+                                                     frame_id=self._robot.base_link_frame)
+                              for angle in self._look_angles]
+
+                i = 0
+                while not rospy.is_shutdown():
+                    self._robot.head.look_at_point(head_goals[i])
+                    i += 1
+
+                    if i == len(head_goals):
+                        break
+
+                    self._robot.head.wait_for_motion_done()
+
+                return "done"
+
             @cb_interface(outcomes=["done"])
             def _look_for_victim(_):
                 start_time = rospy.Time.now()
@@ -101,13 +132,34 @@ class NavigateArbitrarily(StateMachine):
                     else:
                         rospy.logwarn("Could not find people meeting the requirements")
 
-            StateMachine.add('NAVIGATE_WAYPOINT', NavigateToWaypoint(robot, waypoint, 0.3, victim),
-                             transitions={'arrived': 'LOOK_FOR_VICTIM',
-                                          'unreachable': 'NAVIGATE_WAYPOINT_FAILED',
-                                          'goal_not_defined': 'preempted'})
+            self.add("LOOK_AROUND_OFFICE", CBState(_look_around), transitions={"done": "NAVIGATE_BEDROOM"})
+            self.add("NAVIGATE_BEDROOM", NavigateToSymbolic(robot,
+                                                            {self._cupboard_des: "in_front_of"},
+                                                            entity_lookat_designator=self._bedroom_des),
+                     transitions={"arrived": "LOOK_AROUND_BEDROOM",
+                                  "unreachable": "LOOK_AROUND_BEDROOM",
+                                  "goal_not_defined": "preempted"})
+            self.add("LOOK_AROUND_BEDROOM", CBState(_look_around), transitions={"done": "NAVIGATE_WAYPOINT1"})
 
-            StateMachine.add('NAVIGATE_WAYPOINT_FAILED', ForceDrive(robot, 0, 0, 0.5, (2 * math.pi) / 0.5),
-                             transitions={'done': 'NAVIGATE_WAYPOINT'})
+            self.add('NAVIGATE_WAYPOINT1', NavigateToWaypoint(robot, self.waypoint, 0.3, self.victim),
+                     transitions={'arrived': 'INSPECT_DINNER_TABLE',
+                                  'unreachable': 'NAVIGATE_WAYPOINT_FAILED1',
+                                  'goal_not_defined': 'preempted'})
+
+            self.add('NAVIGATE_WAYPOINT_FAILED1', ForceDrive(robot, 0, 0, 0.5, (2 * math.pi) / 0.5),
+                     transitions={'done': 'NAVIGATE_WAYPOINT1'})
+
+            self.add("INSPECT_DINNER_TABLE", Inspect(robot, self._dinner_table_des, navigation_area="in_front_of"),
+                     transitions={"done": "NAVIGATE_WAYPOINT2",
+                                  "failed": "NAVIGATE_WAYPOINT2"})
+
+            self.add('NAVIGATE_WAYPOINT2', NavigateToWaypoint(robot, self.waypoint, 0.3, self.victim),
+                     transitions={'arrived': 'LOOK_FOR_VICTIM',
+                                  'unreachable': 'NAVIGATE_WAYPOINT_FAILED2',
+                                  'goal_not_defined': 'preempted'})
+
+            self.add('NAVIGATE_WAYPOINT_FAILED2', ForceDrive(robot, 0, 0, 0.5, (2 * math.pi) / 0.5),
+                     transitions={'done': 'NAVIGATE_WAYPOINT2'})
 
             self.add("LOOK_FOR_VICTIM", CBState(_look_for_victim), transitions={"done": "done"})
 
