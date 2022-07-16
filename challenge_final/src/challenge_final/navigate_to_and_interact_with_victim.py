be0171afa4ff766f9f72b4b960d33bcc186fea01
Rein Appeldoorn <reinzor@gmail.com>
2022-07-16 08:34:36 +0700

    look down

--- challenge_final/src/challenge_final/navigate_to_and_interact_with_victim.py
+++ challenge_final/src/challenge_final/navigate_to_and_interact_with_victim.py
@@ -2,7 +2,9 @@ import math
 import os
 
 import rospy
+from smach.state import CBState
 from smach.state_machine import StateMachine
+from smach.util import cb_interface
 
 from robot_skills import get_robot
 from robot_smach_states.human_interaction import Say, AskYesNo
@@ -19,16 +21,25 @@ class NavigateToAndInteractWithVictim(StateMachine):
         with self:
             StateMachine.add("NAVIGATE_VICTIM",
                              NavigateToWaypoint(robot, entity_des, radius=0.3, look_at_designator=entity_des),
-                             transitions={"arrived": "SAY_OKAY",
+                             transitions={"arrived": "LOOK_DOWN",
                                           "unreachable": "NAVIGATE_VICTIM2",
                                           "goal_not_defined": "preempted"})
             StateMachine.add("NAVIGATE_VICTIM2",
                              NavigateToWaypoint(robot, entity_des, radius=0.5, look_at_designator=entity_des),
-                             transitions={"arrived": "SAY_OKAY",
+                             transitions={"arrived": "LOOK_DOWN",
                                           "unreachable": "TURN_AROUND",
                                           "goal_not_defined": "preempted"})
             StateMachine.add('TURN_AROUND', ForceDrive(robot, 0, 0, 0.5, (2 * math.pi) / 0.5),
                              transitions={'done': 'NAVIGATE_VICTIM2'})
+
+            @cb_interface(outcomes=["done"])
+            def _look_down(_):
+                robot.head.look_down()
+                robot.head.wait_for_motion_done()
+                return "done"
+
+            self.add("LOOK_DOWN", CBState(_look_down), transitions={"done": "SAY_OK"})
+
             StateMachine.add("SAY_OKAY", Say(robot, "Are you Okay? Do you need any help?"),
                              transitions={"spoken": "ASK_YES_NO"})
             StateMachine.add("ASK_YES_NO", AskYesNo(robot),
