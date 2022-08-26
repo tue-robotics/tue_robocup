81642805c0ca82704c96df31ded4bf03534d5035
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-08-26 12:00:27 +0200

    [where_is_this] use impuls for construction test

--- challenge_where_is_this/test/test_construction.py
+++ challenge_where_is_this/test/test_construction.py
@@ -7,7 +7,7 @@ class TestChallengeConstruction(unittest.TestCase):
         """
         If no exception is raised, this test will succeed
         """
-        os.environ["ROBOT_ENV"] = "robotics_testlabs"
+        os.environ["ROBOT_ENV"] = "impuls"
         from robot_skills.mockbot import Mockbot
         from challenge_where_is_this.where_is_this import WhereIsThis
         robot = Mockbot()
