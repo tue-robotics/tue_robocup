83bc4e89563a0916162b6d9c01891538b499b3eb
PetervDooren <19806646+PetervDooren@users.noreply.github.com>
2022-08-30 19:11:57 +0200

    update warning

--- robot_skills/src/robot_skills/arm/gripper.py
+++ robot_skills/src/robot_skills/arm/gripper.py
@@ -106,7 +106,7 @@ class ParrallelGripper(Gripper):
         if goal.command.direction == GripperState.OPEN:
             if self.occupied_by is not None:
                 rospy.logerr("ParrallelGripper.send_goal open is called but there is still an entity with id '%s' \
-                occupying the gripper, removing this entity. Please update your worldmodel" % self.occupied_by.uuid)
+                occupying the gripper, removing this entity from the robot interface." % self.occupied_by.uuid)
             self.occupied_by = None
 
         goal_status = GoalStatus.SUCCEEDED
