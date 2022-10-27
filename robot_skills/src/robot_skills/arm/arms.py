c2e4f703f54d4c306db3a40bc8a41938927bf790
P-ict0 <r.martin.nunez@student.tue.nl>
2022-10-27 14:56:29 +0200

    Removed class for global variables

--- robot_skills/src/robot_skills/arm/arms.py
+++ robot_skills/src/robot_skills/arm/arms.py
@@ -20,11 +20,11 @@ from robot_skills.robot_part import RobotPart
 # Constants for arm requirements. Note that "don't care at all" is not here, as
 # it can be expressed by not imposing a requirement (set it to None).
 
-class JointConstants(object):
-    # Commonly used values when sending goals to a joint
-    MAX_JOINT_VEL = 0.7
-    JOINT_ACC = 0.7
-    TIMEOUT = 5.0
+
+# Commonly used values when sending goals to a joint
+MAX_JOINT_VEL = 0.7
+JOINT_ACC = 0.7
+JOINT_TIMEOUT = 5.0
 
 
 # Specific types of gripper.
@@ -125,8 +125,8 @@ class PublicArm(object):
         """
         return configuration in self._available_joint_goals
 
-    def send_joint_goal(self, configuration, timeout=JointConstants.TIMEOUT, max_joint_vel=JointConstants.MAX_JOINT_VEL,
-                        joint_acc=JointConstants.JOINT_ACC):
+    def send_joint_goal(self, configuration, timeout=JOINT_TIMEOUT, max_joint_vel=MAX_JOINT_VEL,
+                        joint_acc=JOINT_ACC):
         self._test_die(configuration in self._available_joint_goals, 'joint-goal ' + configuration,
                        "Specify get_arm(..., required_goals=['{}'])".format(configuration))
         return self._arm.send_joint_goal(configuration, timeout=timeout,
@@ -139,8 +139,8 @@ class PublicArm(object):
         """
         return configuration in self._available_joint_trajectories
 
-    def send_joint_trajectory(self, configuration, timeout=JointConstants.TIMEOUT,
-                              max_joint_vel=JointConstants.MAX_JOINT_VEL, joint_acc=JointConstants.JOINT_ACC):
+    def send_joint_trajectory(self, configuration, timeout=JOINT_TIMEOUT, max_joint_vel=MAX_JOINT_VEL,
+                              joint_acc=JOINT_ACC):
         self._test_die(configuration in self._available_joint_trajectories, 'joint-goal ' + configuration,
                        "Specify get_arm(..., required_trajectories=['{}'])".format(configuration))
         return self._arm.send_joint_trajectory(configuration, timeout=timeout,
@@ -496,8 +496,8 @@ class Arm(RobotPart):
                 rospy.logerr('grasp precompute goal failed: \n%s', repr(myargs))
                 return False
 
-    def send_joint_goal(self, configuration, timeout=JointConstants.TIMEOUT, max_joint_vel=JointConstants.MAX_JOINT_VEL,
-                        joint_acc=JointConstants.JOINT_ACC):
+    def send_joint_goal(self, configuration, timeout=JOINT_TIMEOUT, max_joint_vel=MAX_JOINT_VEL,
+                        joint_acc=JOINT_ACC):
         """
         Send a named joint goal (pose) defined in the parameter default_configurations to the arm
         :param configuration:(str) name of configuration, configuration should be loaded as parameter
@@ -514,8 +514,8 @@ class Arm(RobotPart):
             rospy.logwarn('Default configuration {0} does not exist'.format(configuration))
             return False
 
-    def send_joint_trajectory(self, configuration, timeout=JointConstants.TIMEOUT,
-                              max_joint_vel=JointConstants.MAX_JOINT_VEL, joint_acc=JointConstants.JOINT_ACC):
+    def send_joint_trajectory(self, configuration, timeout=JOINT_TIMEOUT,
+                              max_joint_vel=MAX_JOINT_VEL, joint_acc=JOINT_ACC):
         """
         Send a named joint trajectory (sequence of poses) defined in the default_trajectories to the arm
 
@@ -528,7 +528,8 @@ class Arm(RobotPart):
         if configuration in self.default_trajectories:
             return self._send_joint_trajectory(self.default_trajectories[configuration],
                                                timeout=rospy.Duration.from_sec(timeout),
-                                               max_joint_vel=max_joint_vel, joint_acc=joint_acc)
+                                               max_joint_vel=max_joint_vel,
+                                               joint_acc=joint_acc)
         else:
             rospy.logwarn('Default trajectories {0} does not exist'.format(configuration))
             return False
@@ -541,8 +542,8 @@ class Arm(RobotPart):
         """
         return self.send_joint_goal('reset', timeout=0.0)
 
-    def _send_joint_trajectory(self, joints_references, max_joint_vel=JointConstants.MAX_JOINT_VEL,
-                               timeout=rospy.Duration(JointConstants.TIMEOUT), joint_acc=JointConstants.JOINT_ACC):
+    def _send_joint_trajectory(self, joints_references, max_joint_vel=MAX_JOINT_VEL,
+                               timeout=rospy.Duration(JOINT_TIMEOUT), joint_acc=JOINT_ACC):
         """
         Low level method that sends a array of joint references to the arm.
 
