d0ba3596edd5caec4d773970f25d8de79747d773
Janno Lunenburg <jannolunenburg@gmail.com>
2022-11-15 20:37:06 +0100

    wip(robot_smach_states)Added method to filter faces

--- robot_smach_states/examples/human_interaction/example_people_recognition.py
+++ robot_smach_states/examples/human_interaction/example_people_recognition.py
@@ -1,10 +1,17 @@
-#ROS
+# System
+import argparse
+import functools
+import math
+import typing
+
+# ROS
 import rospy
 import smach
-import argparse
+import PyKDL as kdl
 
 # TU/e Robotics
 from image_recognition_msgs.msg import CategoricalDistribution, CategoryProbability, Recognition
+from pykdl_ros import VectorStamped
 from robot_skills.get_robot import get_robot
 from robot_skills.robot import Robot
 
@@ -23,7 +30,7 @@ def get_operator_name(recognition: Recognition) -> str:
     # ToDo: move to a better location
     # ToDo: what can it raise? --> add to docstring
     # ToDo: add tests
-    distribution = Recognition.categorical_distribution  # type: CategoricalDistribution
+    distribution = recognition.categorical_distribution  # type: CategoricalDistribution
     best_match = max(distribution.probabilities, key=lambda cp: cp.probability)  # type: CategoryProbability
     return best_match.label
 
@@ -43,17 +50,66 @@ class RecognizePerson(smach.State):
 
     def execute(self, userdata=None):
         self._robot.head.look_at_standing_person(timeout=3.0)
-        # ToDo: the following two methods won't work: they'll return whether a learned person is in the image,
-        # not if the person in front of the robot is learned (and his name)
-        recognition = self._robot.perception.detect_face()
+        # recognition = self._robot.perception.detect_face()
+        recognitions = self._robot.perception.detect_faces()  # For now, detect all faces here
         try:
-            operator_name = get_operator_name(recognition=recognition)
+            operator_recognition = self.filter_operator_recognition(recognitions)
+            operator_name = get_operator_name(recognition=operator_recognition)
+            rospy.loginfo(f"Operator name: {operator_name}")
         except:  # ToDo: narrow down exceptions
             rospy.loginfo("Did not recognize the operator")
             return "failed"
 
         # ToDo: write this in the designator
         return "succeeded"
+
+    def filter_operator_recognition(
+          self,
+          recognitions: typing.List[Recognition],
+          expected_operator_pos: typing.Optional[kdl.Vector] = None,
+          threshold: float = 0.5,
+    ) -> Recognition:
+        """
+        For all provided recognitions, project the ROI and determine the one closest to the expected operator position,
+        computed on the floor plane.
+
+        N.B.: it might make sense to move this method to 'Perception.py' as 'detect_operator_face'
+
+        :param recognitions: recognitions
+        :param expected_operator_pos: expected position of the operator w.r.t. the robot
+        :param threshold: if the distance between the recognition and the expected pos exceed the threshold, an error
+        is raised
+        :return: Recognition closed to the expected position
+        :raises: RuntimeError
+        """
+        # ToDo: move this method to Perception.py?
+        frame_id = self._robot.base_link_frame
+        projected_recognitions = [(rcg, self._robot.perception.project_roi(r.roi, frame_id)) for rcg in recognitions]
+        expected_operator_pos = kdl.Vector(0.8, 0.0, 0.0) if expected_operator_pos is None else expected_operator_pos
+
+        def _distance_from_expected(
+              expected_pos: kdl.Vector,
+              projected_tup: typing.Tuple[Recognition, VectorStamped]
+        ) -> float:
+            """
+            Helper method to compute the distance between the projected tuple and the expected position
+
+            :param expected_pos:
+            :param projected_tup:
+            :return:
+            """
+            dx = projected_tup[1].vector.x() - expected_pos.x()
+            dy = projected_tup[1].vector.y() - expected_pos.y()
+            return math.hypot(dx, dy)
+
+        projected_recognitions.sort(functools.partial(_distance_from_expected, expected_operator_pos))
+        best_distance = _distance_from_expected(expected_operator_pos, projected_recognitions[0])
+        if best_distance > threshold:
+            err_msg = f"Distance between face detection and expected pos {best_distance} exceeds threshold {threshold}"
+            rospy.logwarn(err_msg)
+            raise RuntimeError(err_msg)
+        else:
+            return projected_recognitions[0]
 
 
 class ExamplePeopleRecognition(smach.StateMachine):
