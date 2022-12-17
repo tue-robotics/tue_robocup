899e0773b611be4f4f5deed79d589b5d67986df2
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-12-17 16:45:47 +0100

    (smach_states) create get_show_map example

--- robot_smach_states/src/robot_smach_states/world_model/get_show_map.py
+++ robot_smach_states/src/robot_smach_states/world_model/get_show_map.py
@@ -95,16 +95,3 @@ class GetMapAndShow(smach.StateMachine):
                 ShowImage(robot, filename_des, duration),
                 transitions={"succeeded": "done", "failed": "failed"},
             )
-
-
-if __name__ == "__main__":
-    from robot_skills import get_robot
-
-    rospy.init_node(os.path.splitext("test_" + os.path.basename(__file__))[0])
-    r = get_robot("hero")
-    # Works for impuls environment
-    entities_des = VariableDesignator(["dining_room"], resolve_type=[str])
-    mark_ids_des = VariableDesignator(["initial_pose", "salon_table"], resolve_type=[str])
-    plan_points_des = VariableDesignator(resolve_type=Vector)
-    sm = GetMapAndShow(r, entities_des, mark_ids_des, plan_points_des)
-    sm.execute()
