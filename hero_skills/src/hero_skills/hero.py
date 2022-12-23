cca7aeb249633395d1d5acde61522c6879f887b3
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-12-23 15:58:56 +0100

    Make base_link_frame constructor argument

--- hero_skills/src/hero_skills/hero.py
+++ hero_skills/src/hero_skills/hero.py
@@ -21,10 +21,7 @@ class Hero(robot.Robot):
         :param connection_timeout: timeout to wait for ROS connections
         :type connection_timeout: Optional[float]
         """
-        super(Hero, self).__init__(robot_name="hero", connection_timeout=connection_timeout)
-
-        # override base_link attribute (no namespace)
-        self.base_link_frame = "base_link"
+        super().__init__(robot_name="hero", base_link_frame="base_link", connection_timeout=connection_timeout)
 
         self._ignored_parts = ["leftArm", "torso", "spindle", "head"]
 
