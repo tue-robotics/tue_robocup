d6504cba65a69ef6e53b002de96eea93f6162529
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-04-19 13:47:21 +0200

    (knowledge)(impuls) update hand_me_that

--- robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_hand_me_that.py
+++ robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_hand_me_that.py
@@ -1,4 +1,4 @@
 starting_point = "initial_pose"
 home_location = "rips_point_1"
-
-all_possible_furniture = ["dinner_table", "closet", "work_bench1", "work_bench2"]
+room = "dining_room"
+all_possible_furniture = ["dinner_table", "cabinet"]
