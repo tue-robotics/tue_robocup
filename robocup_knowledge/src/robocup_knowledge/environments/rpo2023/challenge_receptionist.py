83dc485aa54d0daca4fb19bd7a0c6c0892cf33fe
Rodrigo <r.martin.nunez@student.tue.nl>
2023-04-27 16:26:59 +0100

    receptionist knowledge

--- robocup_knowledge/src/robocup_knowledge/environments/rpo2023/challenge_receptionist.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rpo2023/challenge_receptionist.py
@@ -1,4 +1,6 @@
 from robocup_knowledge import knowledge_loader
+
+
 common = knowledge_loader.load_knowledge("common")
 
 operator_name = "john"
@@ -13,4 +15,4 @@ waypoint_livingroom = {'id': 'living_room', 'radius': 0.5}
 
 sitting_room = 'living_room'  # Where people will be guided to sit and the robot will find a seat
 
-seats = ['couch_long', 'couch_short']
+seats = ['couch', 'arm_chair', 'easy_chair']
