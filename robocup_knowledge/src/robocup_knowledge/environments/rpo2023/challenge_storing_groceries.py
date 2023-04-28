bd5e2a43f01cfbfd32194ac51b4ae9d569e927ae
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-04-28 14:52:42 +0100

    (knowledge)(rpo2023)(groceries) room dynamic

--- robocup_knowledge/src/robocup_knowledge/environments/rpo2023/challenge_storing_groceries.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rpo2023/challenge_storing_groceries.py
@@ -9,9 +9,9 @@ starting_point = 'initial_pose'
 
 # Detection
 shelf = "cabinet"
-room = "kitchen"
 object_shelves = ["shelf3", "shelf4", "shelf5"]
 object_types = [obj["name"] for obj in common.objects]
 
 # Grasping
 table = "side_board"
+room = common.get_room(table)
