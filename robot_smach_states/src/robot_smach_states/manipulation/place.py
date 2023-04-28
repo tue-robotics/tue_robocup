3ab13a40437eb26b667f7f31bbbfeeda5a541fae
Rodrigo <r.martin.nunez@student.tue.nl>
2023-04-28 22:30:42 +0100

    Fix PLace

--- robot_smach_states/src/robot_smach_states/manipulation/place.py
+++ robot_smach_states/src/robot_smach_states/manipulation/place.py
@@ -158,6 +158,9 @@ class Put(smach.State):
                                                 frame_id=self._robot.base_link_frame),
                       timeout=0.0)
 
+        # Wait for arm to finish their motions
+        arm.wait_for_motion_done()
+        
         # Move back
         self._robot.base.force_drive(-0.125, 0, 0, 1.5, ax=0.5)
 
