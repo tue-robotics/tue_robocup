37ed1330c605770f5ac9fa56ee4c9c8193911d6b
Peter van Dooren <pepevandooren@hotmail.com>
2023-05-13 11:38:17 +0200

    change inspect area

--- robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_storing_groceries.py
+++ robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_storing_groceries.py
@@ -10,7 +10,7 @@ starting_point = 'initial_pose'
 # Detection
 shelf = "closet"
 default_area = "shelf4"
-inspect_area = "inspect_area"
+inspect_area = "in_front_of"
 object_shelves = ["shelf3", "shelf4", "shelf5"] #TODO unused variable?
 object_types = [obj["name"] for obj in common.objects] #TODO unused variable?
 
