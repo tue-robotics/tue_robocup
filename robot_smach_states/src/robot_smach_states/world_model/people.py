9c41a46728c50ae70e1a2efe84d2307a03401700
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-05-13 20:05:19 +0200

    (states)(wm)(people) cleanup

--- robot_smach_states/src/robot_smach_states/world_model/people.py
+++ robot_smach_states/src/robot_smach_states/world_model/people.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List
 
 import rospy
 import smach
@@ -11,7 +11,7 @@ from robot_smach_states.human_interaction import FindPeople
 
 
 class PeopleInSeatDesignator(ds.Designator):
-    def __init__(self, robot, seat: Entity, room: Entity=None, name=None):
+    def __init__(self, robot, seat: Entity, room: Entity = None, name=None):
         super(PeopleInSeatDesignator, self).__init__(resolve_type=[Entity], name=name)
 
         self.robot = robot
@@ -24,7 +24,6 @@ class PeopleInSeatDesignator(ds.Designator):
         self.seat = seat
 
     def _resolve(self) -> Entity:
-
         if self.room:
             room = self.room.resolve() if hasattr(self.room, 'resolve') else self.room  # type: Entity
             if not room:
@@ -37,13 +36,15 @@ class PeopleInSeatDesignator(ds.Designator):
         if not seat.pose:
             rospy.logwarn("Seat does not have a pose")
             return None
-        
-        seat_point = VectorStamped(seat.pose.frame.p, seat.pose.header.stamp, seat.pose.header.frame_id)
-        
-        # query the people within 1m of the furniture: TODO adapt to the size of the furniture. i.e. couch/sofa
-        people: List(Entity) = self.robot.ed.get_entities(etype="person", center_point=seat_point, radius=1.0, ignore_z=True)
 
-        #TODO ignore people not within the specified room. i.e spectators
+        seat_point = VectorStamped(seat.pose.frame.p, seat.pose.header.stamp, seat.pose.header.frame_id)
+
+        # query the people within 1m of the furniture: TODO adapt to the size of the furniture. i.e. couch/sofa
+        people: List[Entity] = self.robot.ed.get_entities(
+            etype="person", center_point=seat_point, radius=1.0, ignore_z=True
+        )
+
+        # TODO ignore people not within the specified room. i.e spectators
         people = [person for person in people if person.pose.frame.p]
         return people
 
@@ -52,12 +53,7 @@ class PeopleInSeatDesignator(ds.Designator):
 
 
 class CheckSeatEmpty(smach.StateMachine):
-    def __init__(
-        self,
-        robot,
-        entity_des,
-        number_of_people_des = None
-    ):
+    def __init__(self, robot, entity_des, number_of_people_des=None):
         """
         Constructor
 
@@ -69,7 +65,7 @@ class CheckSeatEmpty(smach.StateMachine):
         smach.StateMachine.__init__(self, outcomes=["empty", "occupied", "failed"])
 
         if number_of_people_des:
-            #TODO check if seat_is_for is writeable. i.e. of type VariableWriter
+            # TODO check if seat_is_for is writeable. i.e. of type VariableWriter
             ds.check_type(number_of_people_des, int)
         else:
             number_of_people_des = ds.VariableDesignator(None, resolve_type=int).writeable()
@@ -77,21 +73,23 @@ class CheckSeatEmpty(smach.StateMachine):
         people_in_seat_des = PeopleInSeatDesignator(robot, entity_des)
 
         with self:
-            #TODO add state to turn head towards the entity.
+            # TODO add state to turn head towards the entity.
             smach.StateMachine.add(
                 "INSPECT_SEAT",
-                FindPeople(robot=robot,
-                              #properties=None,
-                              #query_entity_designator=None,
-                              #found_people_designator=None,
-                              #look_distance=10.0,
-                              #speak=False,
-                              #strict=True,
-                              #nearest=False,
-                              attempts=1,
-                              #search_timeout=60,
-                              look_range=(0.0, 0.0),
-                              look_steps=1),
+                FindPeople(
+                    robot=robot,
+                    # properties=None,
+                    # query_entity_designator=None,
+                    # found_people_designator=None,
+                    # look_distance=10.0,
+                    # speak=False,
+                    # strict=True,
+                    # nearest=False,
+                    attempts=1,
+                    # search_timeout=60,
+                    look_range=(0.0, 0.0),
+                    look_steps=1,
+                ),
                 transitions={"done": "CHECK_EMPTY", "failed": "failed"},
             )
 
@@ -106,8 +104,8 @@ class CheckSeatEmpty(smach.StateMachine):
                 number_of_people_des.write(len(people))
                 return "occupied"
 
-            smach.StateMachine.add("CHECK_EMPTY",
-                                   smach.CBState(check_people),
-                                   transitions={'empty': 'empty',
-                                                'occupied': 'occupied',
-                                                'failed': 'failed'})
+            smach.StateMachine.add(
+                "CHECK_EMPTY",
+                smach.CBState(check_people),
+                transitions={"empty": "empty", "occupied": "occupied", "failed": "failed"},
+            )
