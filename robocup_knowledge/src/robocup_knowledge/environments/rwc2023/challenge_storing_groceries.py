40b31c078886508239a1c69c3637d84b74305d66
Rodrigo <r.martin.nunez@student.tue.nl>
2023-07-05 10:56:34 +0200

    (Storing groceries) Knowledge

---
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2023/challenge_storing_groceries.py
@@ -0,0 +1,17 @@
+# TU/e Robotics
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+# General
+starting_point = 'initial_pose'
+
+# Detection
+shelf = "pantry"
+default_area = "shelf3"
+inspect_area = "in_front_of"
+
+# Grasping
+table = "side_table"
+room = common.get_room(table)
