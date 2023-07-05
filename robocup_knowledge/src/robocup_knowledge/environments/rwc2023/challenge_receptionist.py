b830e91e205ace446ffc1250470c4c479b647dd5
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-07-05 14:02:34 +0200

    (knowledge)(rwc2023) update receptionist

--- robocup_knowledge/src/robocup_knowledge/environments/rwc2023/challenge_receptionist.py
+++ robocup_knowledge/src/robocup_knowledge/environments/rwc2023/challenge_receptionist.py
@@ -1,16 +1,17 @@
 from robocup_knowledge import knowledge_loader
+
 common = knowledge_loader.load_knowledge("common")
 
 operator_name = "charlie"
 
-operator_drink = "ice tea"
+operator_drink = "iced_tea"
 
 starting_point = "initial_pose_receptionist"
 
-waypoint_door = {'id': 'entry_door', 'radius': 0.5}
+waypoint_door = {"id": "entry_door", "radius": 0.5}
 
-waypoint_livingroom = {'id': 'living_room', 'radius': 0.5}
+waypoint_livingroom = {"id": "living_room", "radius": 0.5}
 
-sitting_room = 'living_room'  # Where people will be guided to sit and the robot will find a seat
+sitting_room = "living_room"  # Where people will be guided to sit and the robot will find a seat
 
-seats = ['sofa']
+seats = {"sofa": ["on_top_of_sofa_left", "on_top_of_sofa_right"]}
