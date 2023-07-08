96b115741fda184b34d8cf1dade143c77480c18a
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-07-08 09:18:41 +0200

    (stickler) black patrol.py

--- challenge_stickler_for_rules/src/challenge_stickler_for_rules/patrol.py
+++ challenge_stickler_for_rules/src/challenge_stickler_for_rules/patrol.py
@@ -18,7 +18,7 @@ from smach import cb_interface, CBState
 from robocup_knowledge import load_knowledge
 from robot_smach_states.human_interaction.find_people_in_room import FindPeople
 
-challenge_knowledge = load_knowledge('challenge_stickler_for_the_rules')
+challenge_knowledge = load_knowledge("challenge_stickler_for_the_rules")
 
 
 class CheckPeopleInForbiddenRoom(smach.StateMachine):
@@ -27,49 +27,54 @@ class CheckPeopleInForbiddenRoom(smach.StateMachine):
 
         robot = robot_name
         room = room_des
-        found_people = ds.VariableDesignator(resolve_type=[Entity], name='found_people')
-        violating_person = ds.VariableDesignator(resolve_type=Entity, name='violating_person')
-        forbidden_room_waypoint = ds.EntityByIdDesignator(robot,
-                                                          uuid=challenge_knowledge.forbidden_room_waypoint)
+        found_people = ds.VariableDesignator(resolve_type=[Entity], name="found_people")
+        violating_person = ds.VariableDesignator(resolve_type=Entity, name="violating_person")
+        forbidden_room_waypoint = ds.EntityByIdDesignator(robot, uuid=challenge_knowledge.forbidden_room_waypoint)
 
         with self:
+
             @cb_interface(outcomes=["yes", "no"])
             def check_forbidden_room(userdata=None):
                 return "yes" if room_des.uuid == challenge_knowledge.forbidden_room else "no"
 
-            smach.StateMachine.add("CHECK_IN_FORBIDDEN_ROOM", CBState(check_forbidden_room),
-                                   transitions={"yes": "NAVIGATE_TO_CHECK",
-                                                "no": "done"})
+            smach.StateMachine.add(
+                "CHECK_IN_FORBIDDEN_ROOM",
+                CBState(check_forbidden_room),
+                transitions={"yes": "NAVIGATE_TO_CHECK", "no": "done"},
+            )
             smach.StateMachine.add(
                 "NAVIGATE_TO_CHECK",
                 NavigateToWaypoint(robot, forbidden_room_waypoint),
-                transitions={"arrived": "FIND_PEOPLE", "unreachable": "done",
-                             "goal_not_defined": "done"},
+                transitions={"arrived": "FIND_PEOPLE", "unreachable": "done", "goal_not_defined": "done"},
             )
             smach.StateMachine.add(
                 "FIND_PEOPLE",
-                FindPeople(robot=robot,
-                           query_entity_designator=room,
-                           found_people_designator=found_people.writeable,
-                           speak=True),
-                transitions={"found": "ITERATE_PEOPLE", "failed": "done"})
+                FindPeople(
+                    robot=robot,
+                    query_entity_designator=room,
+                    found_people_designator=found_people.writeable,
+                    speak=True,
+                ),
+                transitions={"found": "ITERATE_PEOPLE", "failed": "done"},
+            )
 
-            smach.StateMachine.add('ITERATE_PEOPLE',
-                                   IterateDesignator(found_people,
-                                                     violating_person.writeable),
-                                   transitions={'next': 'LOOKAT_PERSON',
-                                                'stop_iteration': 'done'})
+            smach.StateMachine.add(
+                "ITERATE_PEOPLE",
+                IterateDesignator(found_people, violating_person.writeable),
+                transitions={"next": "LOOKAT_PERSON", "stop_iteration": "done"},
+            )
 
-            smach.StateMachine.add('LOOKAT_PERSON',
-                                   LookAtEntity(robot, violating_person),
-                                   transitions={'arrived': 'SAY_BEHAVE',
-                                                'unreachable': 'SAY_BEHAVE',
-                                                'goal_not_defined': 'SAY_BEHAVE'})
-            smach.StateMachine.add("SAY_BEHAVE",
-                                   Say(robot,
-                                       "Unfortunately, the party is not in this room,"
-                                       "please leave the room.", block=True),
-                                   transitions={"spoken": "NAVIGATE_TO_CHECK"})
+            smach.StateMachine.add(
+                "LOOKAT_PERSON",
+                LookAtEntity(robot, violating_person),
+                transitions={"succeeded": "SAY_BEHAVE", "failed": "ITERATE_PEOPLE"},
+            )
+
+            smach.StateMachine.add(
+                "SAY_BEHAVE",
+                Say(robot, "Unfortunately, the party is not in this room," "please leave the room.", block=True),
+                transitions={"spoken": "NAVIGATE_TO_CHECK"},
+            )
 
 
 class Patrol(smach.StateMachine):
@@ -89,11 +94,15 @@ class Patrol(smach.StateMachine):
             smach.StateMachine.add(
                 "NAVIGATE_TO_ROOM",
                 NavigateToSymbolic(robot, {room_des: "in"}, room_des),
-                transitions={"arrived": "CHECK_FORBIDDEN_ROOM", "unreachable": "CHECK_FORBIDDEN_ROOM",
-                             "goal_not_defined": "CHECK_FORBIDDEN_ROOM"},
+                transitions={
+                    "arrived": "CHECK_FORBIDDEN_ROOM",
+                    "unreachable": "CHECK_FORBIDDEN_ROOM",
+                    "goal_not_defined": "CHECK_FORBIDDEN_ROOM",
+                },
             )
 
-            smach.StateMachine.add("CHECK_FORBIDDEN_ROOM",
-                                   CheckPeopleInForbiddenRoom(robot_name=self.robot, room_des=self.room_des),
-                                   transitions={"done": "done"})
-
+            smach.StateMachine.add(
+                "CHECK_FORBIDDEN_ROOM",
+                CheckPeopleInForbiddenRoom(robot_name=self.robot, room_des=self.room_des),
+                transitions={"done": "done"},
+            )
