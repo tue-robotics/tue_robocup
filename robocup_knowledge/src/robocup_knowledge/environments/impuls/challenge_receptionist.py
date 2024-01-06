e9b1698530ff666744e68d9621648cc9366e7622
shanki98 <shanki98@gmail.com>
2024-01-06 15:04:41 +0100

    Added knowledge changes to impuls

--- robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_receptionist.py
+++ robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_receptionist.py
@@ -5,6 +5,13 @@ operator_name = "john"
 
 operator_drink = "beer"
 
+default_guest_1_name = "adel"
+default_guest_1_drink = "cola"
+
+default_guest_2_name = "morgan"
+default_guest_2_drink = "orange_juice"
+
+
 starting_point = "initial_pose"
 
 waypoint_door = {'id': 'entry_door', 'radius': 0.5}
@@ -13,4 +20,6 @@ waypoint_livingroom = {'id': 'living_room', 'radius': 0.5}
 
 sitting_room = 'living_room'  # Where people will be guided to sit and the robot will find a seat
 
-seats = {'couch_long': ['on_top_of'], 'couch_short': ['on_top_of']}
+# seats = {'couch_long': ['on_top_of'], 'couch_short': ['on_top_of']}
+seats = {'couch_long': ['on_top_of_couch_long_left','on_top_of_couch_long_right'], 'couch_short': ['on_top_of_couch_short_left','on_top_of_couch_short_right']}
+
