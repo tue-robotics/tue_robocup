a525d7e902dd86bb819686eec4f1c19faf29fad3
Lucette Alewijns <l.a.j.alewijns@student.tue.nl>
2024-04-21 22:43:44 +0200

    error fix

--- robot_smach_states/src/robot_smach_states/manipulation/gripper_tf2_listener.py
+++ robot_smach_states/src/robot_smach_states/manipulation/gripper_tf2_listener.py
@@ -6,36 +6,47 @@ from pykdl_ros import FrameStamped
 from robot_skills.robot import Robot
 from robot_smach_states.util.designators.arm import ArmDesignator
 
-if __name__ == '__main__':
-    rospy.init_node('gripper_tf2_listener')
+class GripperTfListener:
+    def __init__(self, robot: Robot, arm: ArmDesignator) -> None:
+        self.robot = robot
+        self.arm_designator = arm
 
-    robot = Robot()
-    arm = ArmDesignator(robot)
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
 
-    tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
-
-    gripper_coordinates = rospy.Publisher('/gripper_coordinates', geometry_msgs.msg.PoseStamped, queue_size=1)
+        self.gripper_coordinates = rospy.Publisher('/gripper_coordinates', geometry_msgs.msg.PoseStamped, queue_size=1)
     
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        try:
-            gripper_bl = tfBuffer.lookup_transform('hand_palm_link', 'base_link', rospy.Time(0))
-            gripper_bl.frame = arm._arm.offset.Inverse() * gripper_bl.frame  # compensate for the offset in hand palm link
+        self.rate = rospy.Rate(10.0)
+    
+    def listening(self):
+        arm = self.arm_designator.resolve()
+        while not rospy.is_shutdown():
 
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rate.sleep()
-            continue
+            try:
+                gripper_bl = self.tfBuffer.lookup_transform('hand_palm_link', 'base_link', rospy.Time(0))
+                
+                gripper_bl.frame = arm._arm.offset.Inverse() * gripper_bl.frame  # compensate for the offset in hand palm link
 
-        # PoseStamped coordinates
-        msg = geometry_msgs.msg.PoseStamped()
-        msg.header = gripper_bl.header
-        msg.pose.position.x = gripper_bl.transform.translation.x
-        msg.pose.position.y = gripper_bl.transform.translation.y
-        msg.pose.position.z = gripper_bl.transform.translation.z
-        msg.pose.orientation = gripper_bl.transform.rotation
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                self.rate.sleep()
+                continue
+
+            # PoseStamped coordinates
+            msg = geometry_msgs.msg.PoseStamped()
+            msg.header = gripper_bl.header
+            msg.pose.position.x = gripper_bl.transform.translation.x
+            msg.pose.position.y = gripper_bl.transform.translation.y
+            msg.pose.position.z = gripper_bl.transform.translation.z
+            msg.pose.orientation = gripper_bl.transform.rotation
 
 
-        gripper_coordinates.publish(msg)
+            self.gripper_coordinates.publish(msg)
 
-        rate.sleep()
+            self.rate.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('tf2_gripper_listener')   
+    
+    gripper_tf_listener = GripperTfListener()
+    gripper_tf_listener.listening()     
