13727c25f4eaac7ee1825109286fcab95cc9230b
Lucette Alewijns <l.a.j.alewijns@student.tue.nl>
2024-04-30 23:44:54 +0200

    gripper coordinate listener gives output for each time step

--- robot_smach_states/src/robot_smach_states/manipulation/gripper_tf2_listener.py
+++ robot_smach_states/src/robot_smach_states/manipulation/gripper_tf2_listener.py
@@ -18,23 +18,23 @@ if __name__ == '__main__':
             gripper_id = FrameStamped.from_xyz_rpy(0, 0, 0, 0, 0, 0, rospy.Time(), frame_id="hand_palm_link") #generates the frame of the gripper 
             gripper_bl = tfBuffer.lookup_transform("hand_palm_link", "base_link", rospy.Time()) #calculates the transform between the origin of the base frame and that of the gripper frame
             
+            # PoseStamped coordinates
+            # obtains coordinates and orientation of the gripper transformed into the coordinate frame of the robot's base
+            msg = geometry_msgs.msg.PoseStamped()
+            msg.header = gripper_bl.header
+            msg.pose.position.x = gripper_bl.transform.translation.x
+            msg.pose.position.y = gripper_bl.transform.translation.y
+            msg.pose.position.z = gripper_bl.transform.translation.z
+            msg.pose.orientation = gripper_bl.transform.rotation
+
+            # publishing coordinates
+            gripper_coordinates.publish(msg)
+
+    
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             
             rate.sleep()
             continue
-
-    # PoseStamped coordinates
-    # obtains coordinates and orientation of the gripper transformed into the coordinate frame of the robot's base
-    msg = geometry_msgs.msg.PoseStamped()
-    msg.header = gripper_bl.header
-    msg.pose.position.x = gripper_bl.transform.translation.x
-    msg.pose.position.y = gripper_bl.transform.translation.y
-    msg.pose.position.z = gripper_bl.transform.translation.z
-    msg.pose.orientation = gripper_bl.transform.rotation
-
-    # publishing coordinates
-    gripper_coordinates.publish(msg)
-    rospy.loginfo("Gripper coordinates published: %s", msg) #output message to check if the gripper coordinates were published
 
     rate.sleep()
 
