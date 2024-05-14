d8a11ece455f649d68a59985c9498851787f31e0
shanki98 <shanki98@gmail.com>
2024-05-14 21:17:33 +0200

    Add Stickler_for_rules knowledge to impuls

---
+++ robocup_knowledge/src/robocup_knowledge/environments/impuls/challenge_stickler_for_the_rules.py
@@ -0,0 +1,15 @@
+# TU/e Robotics
+from robocup_knowledge import knowledge_loader
+
+# Common knowledge
+common = knowledge_loader.load_knowledge("common")
+
+# General
+starting_point = 'initial_pose'
+waypoint_ids = ["living_room", "kitchen", "dining_room", "office"]
+
+# Detection
+forbidden_room = "office"
+forbidden_room_waypoint = "forbidden_room_waypoint"
+
+drinks_entity_id = "fridge"
