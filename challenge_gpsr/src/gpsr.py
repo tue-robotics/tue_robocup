e0b26dd0ac45c7ab650fd5fbcda5ba478f9d84a6
Arpit Aggarwal <ar13pit@gmail.com>
2024-07-18 14:52:46 +0200

    feat(gpsr): Add picovoice yesOrNo

--- challenge_gpsr/src/gpsr.py
+++ challenge_gpsr/src/gpsr.py
@@ -14,6 +14,7 @@ import hmi
 from conversation_engine import ConversationEngine
 from robocup_knowledge import load_knowledge
 from robot_skills import get_robot
+from robot_skills.simulation.sim_mode import is_sim_mode
 from robot_smach_states.navigation import NavigateToWaypoint
 from robot_smach_states.startup import StartChallengeRobust
 from robot_smach_states.util.designators import EntityByIdDesignator
@@ -193,15 +194,21 @@ class ConversationEngineWithHmi(ConversationEngine):
     def heard_correct(self, sentence):
         self.robot.speech.speak('I heard %s, is this correct? Please speak loudly into the microphone!' % sentence)
         try:
-            if 'no' == self.robot.hmi.query('', 'T -> yes | no', 'T').sentence:
-                self.robot.speech.speak('Sorry, please try again')
-                return False
+            if is_sim_mode():
+                answer = self.robot.hmi.query("", "T -> yes | no", "T")
             else:
+                answer = self.robot.picovoice.get_intent("yesOrNo")
+
+        except hmi.TimeoutException as e:
+            rospy.logwarn("HMI failed when getting confirmation: {}".format(e))
+            return True
+        else:
+            if (is_sim_mode() and answer.sentence == "yes") or (not is_sim_mode() and "yes" in answer.semantics):
                 rospy.loginfo("'{}' was correct".format(sentence))
                 return True
-        except hmi.TimeoutException as e:
-            rospy.logwarn("HMI failed when getting confirmation: {}" .format(e))
-            return True
+            else:
+                self.robot.speech.speak('Sorry, please try again')
+                return False
 
 
 def task_result_to_report(task_result):
