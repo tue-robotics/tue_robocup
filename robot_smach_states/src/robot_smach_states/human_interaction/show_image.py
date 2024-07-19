2c4a5889e1cc91a52581431a0095b2160b03d8e8
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2024-07-19 23:52:56 +0200

    (states) ShowImageArray state

--- robot_smach_states/src/robot_smach_states/human_interaction/show_image.py
+++ robot_smach_states/src/robot_smach_states/human_interaction/show_image.py
@@ -1,9 +1,10 @@
 import os.path
 
+import numpy as np
 import rospy
 import smach
 
-from robot_smach_states.util.designators import check_type
+from robot_smach_states.util.designators import check_type, value_or_resolve
 
 
 class ShowImage(smach.State):
@@ -35,4 +36,36 @@ class ShowImage(smach.State):
             return "failed"
 
         self._robot.hmi.show_image(filename, self._duration)
+        return "succeeded"
+
+
+class ShowImageArray(smach.State):
+    """
+    This state allows images to be shown on the robot display
+    """
+    def __init__(self, robot, image, duration=5.0):
+        """
+        Constructor
+
+        :param robot: the robot object
+        :param image: image or designator describing the image
+        :param duration: Duration for which the image should be shown in seconds
+        """
+        super().__init__(outcomes=["succeeded", "failed"])
+
+        self._robot = robot
+        check_type(image, np.ndarray)
+        self._image = image
+        self._duration = duration
+
+    def execute(self, ud=None):
+        image: np.ndarray = value_or_resolve(self._image)
+        if image is None:
+            rospy.logerr(f"Failed to display image: Could not resolve a image from {self._image}")
+            return "failed"
+        if not image.size:
+            rospy.logerr("Failed to display image: empty image")
+            return "failed"
+
+        self._robot.hmi.show_image_from_array(image, self._duration)
         return "succeeded"
